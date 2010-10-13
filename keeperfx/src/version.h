684183a233e5940086b8ca23cce2f86e85c5d193
mefistotelis <mefistotelis@40a56aae-0377-11df-bfb3-7781b9cb07ea>
2010-10-13 21:34:48 +0000

    updated way of storing version, added doxygen docs generation

--- keeperfx/src/version.h
+++ keeperfx/src/version.h
@@ -25,11 +25,12 @@
 #endif
 #define EXPERIMENTAL_NET //uncomment if no experimental network support
 /* Version definitions */
-#define VER_MAJOR         0
-#define VER_MINOR         3
-#define VER_RELEASE       8
-#define VER_BUILD         57
-#define VER_STRING        "0.3.8.57"
+#include "../obj/ver_defs.h"
+//#define VER_MAJOR         1
+//#define VER_MINOR         2
+//#define VER_RELEASE       3
+//#define VER_BUILD         4
+//#define VER_STRING        "1.2.3.4"
 /* Program name, copyrights and file names */
 #define PROGRAM_NAME      "Dungeon Keeper FX"
 #define PROGRAM_FULL_NAME "Dungeon Keeper Fan eXpansion"
