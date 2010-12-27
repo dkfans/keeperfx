b0c4cfb42681978ecbcb32965f0ddd4eeef2b6d6
eatpajdittsvin@gmail.com <eatpajdittsvin@gmail.com@40a56aae-0377-11df-bfb3-7781b9cb07ea>
2010-12-27 06:38:52 +0000

    Deleting dead branch for net testing from earlier this Autumn

--- branches/net/keeperfx/peresec.bat
+++
@@ -1,7 +0,0 @@
-REM Bat file which deals with Mingw make bug which interprets peresec/bin/peresec as peresec /bin/peresec
-
-REM We must shift out /bin/peresec part from command line.
-SHIFT
-
-REM Now do the actual execution of real peresec.
-peresec\bin\peresec %1 %2 %3 %4 %5 %6 %7 %8 %9
