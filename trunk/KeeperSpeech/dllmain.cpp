6a09524293e8e624ddb4a582d3d2ff3c264fe640
eatpajdittsvin@gmail.com <eatpajdittsvin@gmail.com@40a56aae-0377-11df-bfb3-7781b9cb07ea>
2010-12-28 16:50:52 +0000

    Added resource file, for now loading grammar of Speech SDK 5.1

--- trunk/KeeperSpeech/dllmain.cpp
+++ trunk/KeeperSpeech/dllmain.cpp
@@ -1,6 +1,8 @@
 // dllmain.cpp : Defines the entry point for the DLL application.
 #include "stdafx.h"
 
+HMODULE hModule;
+
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
@@ -9,6 +11,8 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
+		::hModule = hModule;
+        break;
 	case DLL_THREAD_ATTACH:
 	case DLL_THREAD_DETACH:
 	case DLL_PROCESS_DETACH:
