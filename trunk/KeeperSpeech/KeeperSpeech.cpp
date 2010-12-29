3e85fa6fe51f85b9da661b2090638a8ffb224c98
eatpajdittsvin@gmail.com <eatpajdittsvin@gmail.com@40a56aae-0377-11df-bfb3-7781b9cb07ea>
2010-12-29 00:12:09 +0000

    Speech recognition test inside Dungeon Keeper frontend loop working.

--- trunk/KeeperSpeech/KeeperSpeech.cpp
+++ trunk/KeeperSpeech/KeeperSpeech.cpp
@@ -6,6 +6,8 @@
 #include "KeeperSpeech.h"
 #include "resource.h"
 
+#define MAX_EVENTS	16
+
 extern HMODULE hModule;
 
 struct State
@@ -13,6 +15,9 @@ struct State
 	CComPtr<ISpRecognizer> engine;
 	CComPtr<ISpRecoContext> recog;
 	CComPtr<ISpRecoGrammar> grammar;
+
+	int next_event;
+	KEEPERSPEECH_EVENT events[MAX_EVENTS];
 } static state;
 
 enum Reason
@@ -25,7 +30,18 @@ enum Reason
 	REASON_SET_INTEREST,
 	REASON_CREATE_GRAMMAR,
 	REASON_LOAD_GRAMMAR,
+	REASON_NOMOREEVENTS,
+	REASON_ACTIVATE_GRAMMAR,
 };
+
+static void pushEvent(KEEPERSPEECH_EVENT_TYPE type)
+{
+	KEEPERSPEECH_EVENT * ev = &state.events[state.next_event];
+	state.next_event = (state.next_event + 1) % MAX_EVENTS;
+
+	memset(ev, 0, sizeof(*ev));
+	ev->type = type;
+}
 
 static void handleRecognition(ISpPhrase * p)
 {
@@ -39,11 +55,22 @@ static void handleRecognition(ISpPhrase * p)
 
 	//empty code simply displaying how I intend to parse rules
 	switch (phrase->Rule.ulId) {
-	case VID_Navigation:
+	case VID_Navigation: //example
 		switch (phrase->pProperties->vValue.ulVal) {
 		case VID_Counter:
 			break;
 		}
+		break;
+		
+	case VID_Slap:
+		pushEvent(KS_SLAP);
+		break;
+	case VID_PickUp:
+		pushEvent(KS_PICKUP);
+		break;
+	case VID_Drop:
+		pushEvent(KS_DROP);
+		break;
 	}
 
 	CoTaskMemFree(phrase);
@@ -105,6 +132,12 @@ KEEPERSPEECH_API int __cdecl KeeperSpeechInit(void)
 			break;
 		}
 
+		res = state.grammar->SetRuleState(NULL, NULL, SPRS_ACTIVE);
+		if (FAILED(res)) {
+			reason = REASON_ACTIVATE_GRAMMAR;
+			break;
+		}
+
 		return REASON_OK;
 	}
 
@@ -122,6 +155,8 @@ KEEPERSPEECH_API const char * __cdecl KeeperSpeechErrorMessage(int reason)
 	case REASON_SET_INTEREST:			return "Error setting what recognition events interest us";
 	case REASON_CREATE_GRAMMAR:			return "Error creating grammar";
 	case REASON_LOAD_GRAMMAR:			return "Error loading grammar";
+	case REASON_NOMOREEVENTS:			return "No more events in queue";
+	case REASON_ACTIVATE_GRAMMAR:		return "Error activating grammar rules";
 	default:							return "Unknown error";
 	}
 }
@@ -139,5 +174,30 @@ KEEPERSPEECH_API void __cdecl KeeperSpeechExit(void)
 
 	if (state.engine) {
 		state.engine.Release();
+	}
+}
+
+KEEPERSPEECH_API int __cdecl KeeperSpeechPopEvent(KEEPERSPEECH_EVENT * ev)
+{
+	int i = state.next_event;
+	do {
+		if (state.events[i].type != KS_UNUSED) {
+			memcpy(ev, &state.events[i], sizeof(*ev));
+			state.events[i].type = KS_UNUSED;
+			return REASON_OK;
+		}
+
+		i = (i + 1) % MAX_EVENTS;
+	} while (i != state.next_event);
+
+	return REASON_NOMOREEVENTS;
+}
+
+KEEPERSPEECH_API void __cdecl KeeperSpeechClearEvents(void)
+{
+	int i;
+
+	for (i = 0; i < MAX_EVENTS; ++i) {
+		state.events[i].type = KS_UNUSED;
 	}
 }
