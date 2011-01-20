967365e87acd5b673d6f76e32bc7ca99960ca55e
eatpajdittsvin@gmail.com <eatpajdittsvin@gmail.com@40a56aae-0377-11df-bfb3-7781b9cb07ea>
2011-01-20 16:44:01 +0000

    Intermediate commit of experimental AI even though it does nothing, so I don't rip out hair in cash of hard drive crash.
    Compiled by conditional switch so it *shouldn't* influence normal builds. If it does, please surround offending code with a #ifdef PETTER_AI block.

--- trunk/keeperfx/src/globals.h
+++ trunk/keeperfx/src/globals.h
@@ -130,6 +130,9 @@ extern "C" {
   #define SCRIPTDBG(dblv,format,args...) {\
     if (BFDEBUG_LEVEL > dblv)\
       LbScriptLog(text_line_number,"%s: " format "\n", __func__ , ## args); }
+  #define AIDBG(dblv,format,args...) {\
+    if (BFDEBUG_LEVEL > dblv)\
+      LbAiLog("%s: " format "\n", __func__ , ## args); }
 #else
   #define SYNCDBG(dblv,format,args...)
   #define WARNDBG(dblv,format,args...)
@@ -137,6 +140,7 @@ extern "C" {
   #define NAVIDBG(dblv,format,args...)
   #define NETDBG(dblv,format,args...)
   #define SCRIPTDBG(dblv,format,args...)
+  #define AIDBG(dblv,format,args...)
 #endif
 
 #ifdef __cplusplus
