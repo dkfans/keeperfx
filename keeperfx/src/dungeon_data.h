a15ac3da5fc9037f499958ad38ea2d01990d8771
mefistotelis <mefistotelis@40a56aae-0377-11df-bfb3-7781b9cb07ea>
2011-02-01 21:47:05 +0000

    some simple updates to entrance generation code

--- keeperfx/src/dungeon_data.h
+++ keeperfx/src/dungeon_data.h
@@ -310,7 +310,8 @@ long field_1193;
     unsigned char field_13A7[EVENT_BUTTONS_COUNT+1];
     long field_13B4[EVENT_KIND_COUNT]; // warning: missing 4 bytes!
 unsigned short tortured_creatures[32];
-unsigned char field_1460[41];
+unsigned char field_1460[37];
+    long field_1485;
 unsigned char field_1489[32];
 int field_14A9;
 unsigned char research_override; // could be easily changed into flags..
