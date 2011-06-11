ce8c4639fc06ae861e787d35424b44d3b42aa956
mefistotelis <mefistotelis@40a56aae-0377-11df-bfb3-7781b9cb07ea>
2011-06-11 10:59:28 +0000

    working on files integrity check.

---
+++ trunk/launcherwx/src/FilelistChecker.cpp
@@ -0,0 +1,168 @@
+/******************************************************************************/
+// Utility routines.
+/******************************************************************************/
+/** @file FilelistChecker.cpp
+ *     Checks integrity of list of files.
+ * @par Purpose:
+ *     Allows checking if files exist and are in correct version.
+ * @par Comment:
+ *     None.
+ * @author   KeeperFX Team
+ * @date     08 Jun 2011 - 10 Jun 2011
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+#include "FilelistChecker.hpp"
+
+#include <cstddef>
+#include <cstring>
+#include <sys/stat.h>
+#include <stdexcept>
+#include <wx/log.h>
+
+/** Files for basic check which are supplied with KeeperFX. */
+const struct CheckItem supplied_basic_check[] = {
+    {"data/tmapa007.dat",CIF_Default,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00} },
+    {"ldata/dkmap01.raw",CIF_Default,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00} },
+    {"sound/sound.dat",CIF_Default,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00} },
+    {"keeperfx.exe",CIF_Default,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00} },
+    {NULL,CIF_Default,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00} },
+};
+
+/** Files for basic check which have to be copied from original DK. */
+const struct CheckItem additional_basic_check[] = {
+    {"data/creature.jty",CIF_Default,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00} },
+    {"ldata/front.raw",CIF_Default,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00} },
+    {"sound/atmos2.sbk",CIF_Default,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00} },
+    {"keeperfx.exe",CIF_Default,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00} },
+    {NULL,CIF_Default,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00} },
+};
+
+/** Files for complete check which have to be copied from original DK. */
+const struct CheckItem additional_complete_check[] = {
+    {"data/creature.jty",CIF_Default,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00} },
+    {"ldata/front.raw",CIF_Default,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00} },
+    {"sound/atmos2.sbk",CIF_Default,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00} },
+    {"keeperfx.exe",CIF_Default,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00} },
+    {NULL,CIF_Default,{0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00} },
+};
+
+FilelistChecker::FilelistChecker()
+{
+    ntotal = 0;
+    nfailed = 0;
+}
+
+FilelistChecker::~FilelistChecker()
+{
+}
+
+bool FilelistChecker::verifyList(const struct CheckItem *ciList)
+{
+    const struct CheckItem *cItem;
+    cItem = &ciList[0];
+    while (cItem->filename != NULL)
+    {
+        int result;
+        result = verifyItem(*cItem);
+        if (result != CIR_Correct)
+        {
+            nfailed++;
+            failedFiles.push_back(cItem->filename);
+        }
+        ntotal++;
+        cItem++;
+    }
+    return (nfailed == 0);
+}
+
+int FilelistChecker::verifyItem(const struct CheckItem &cItem)
+{
+    try {
+        if (!fileExists(cItem.filename,false,false))
+            return CIR_NotFound;
+    }
+    catch (std::exception &e) {
+        wxLogMessage(wxT("Error while checking \"%s\": %s"),cItem.filename,e.what());
+        return CIR_AccessFail;
+    }
+    return CIR_Correct;
+}
+
+/**
+ * Checks if file of given name exists.
+ * @param filename File name and path to be checked.
+ * @param acceptFolders Selects whether the function will succeed when given filename exists, but represents a folder.
+ * @param acceptSpecial Selects whether the function will succeed when given filename exists, but represents a special file.
+ *     Special files are Sockets, FIFOs, block devices and other things that don't really exist on disk.
+ * @return
+ */
+bool FilelistChecker::fileExists(const char * filename, bool acceptFolders, bool acceptSpecial)
+{
+  struct stat fileInfo;
+  int statRet;
+
+  // Attempt to get the file attributes
+  statRet = stat(filename,&fileInfo);
+  if(statRet == 0) {
+      // We were able to get the attributes so entry obviously exists.
+      // Now, it it a file or a directory?
+      if (!acceptFolders)
+      {
+          if ((fileInfo.st_mode & S_IFMT) == S_IFDIR)
+              return false;
+      }
+      if (!acceptSpecial)
+      {
+          if ( ((fileInfo.st_mode & S_IFMT) != S_IFREG) && ((fileInfo.st_mode & S_IFMT) != S_IFDIR))
+              return false;
+      }
+      return true;
+  } else {
+      // We were not able to get the file attributes.
+      // This may mean that we don't have permission to
+      // access the folder which contains this file.
+      switch (errno)
+      {
+      case ENOENT:
+      case ENOTDIR:
+          // File really doesn't exist.
+          break;
+      case EACCES:
+          // File may exist, we just don't have permission to it, or Search permission to its path.
+          break;
+      case EIO:
+          // An error occurred while reading from the file system.
+          break;
+      default:
+          throw new std::runtime_error(strerror(errno));
+      }
+      return false;
+  }
+}
+
+void FilelistChecker::clearResults(void)
+{
+    ntotal = 0;
+    nfailed = 0;
+    failedFiles.clear();
+}
+
+int FilelistChecker::getNumTotal(void)
+{
+    return ntotal;
+}
+
+int FilelistChecker::getNumFailed(void)
+{
+    return nfailed;
+}
+
+const char * FilelistChecker::getFailedFilename(int idx)
+{
+    return failedFiles[idx].c_str();
+}
