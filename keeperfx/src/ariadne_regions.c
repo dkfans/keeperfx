091edac50093d45493cf1dbab87d59c0fcb1b0c9
mefistotelis <mefistotelis@40a56aae-0377-11df-bfb3-7781b9cb07ea>
2012-08-19 17:53:18 +0000

    Rewritten region ID allocation routine in pathfinding system.

--- keeperfx/src/ariadne_regions.c
+++ keeperfx/src/ariadne_regions.c
@@ -2,7 +2,7 @@
 // Free implementation of Bullfrog's Dungeon Keeper strategy game.
 /******************************************************************************/
 /** @file ariadne_regions.c
- *     Regions array for Ariande system support functions.
+ *     Regions array for Ariadne system support functions.
  * @par Purpose:
  *     Functions to manage list or Regions.
  * @par Comment:
@@ -29,13 +29,80 @@ extern "C" {
 DLLIMPORT unsigned long _DK_regions_connected(long tree_reg1, long tree_reg2);
 DLLIMPORT void _DK_region_alloc(unsigned long tree_reg);
 /******************************************************************************/
+/** Array of regions.
+ * Note that region[0] is used for storing unused triangles and shouldn't be
+ * used for actual calculations.
+ */
 DLLIMPORT struct RegionT _DK_Regions[REGIONS_COUNT];
 #define Regions _DK_Regions
 /******************************************************************************/
 struct RegionT bad_region;
 /******************************************************************************/
+/**
+ * Allocates region ID.
+ * @return The region ID, or 0 on failure.
+ */
+unsigned long region_alloc_id(void)
+{
+    struct RegionT * rgn;
+    struct Triangle * tri;
+    long reg_id,sreg_id;
+    long i;
+    int min_f0;
+
+    NAVIDBG(19,"Starting");
+    reg_id = -1;
+    min_f0 = 2147483647;
+    for (i=1; i < REGIONS_COUNT; i++)
+    {
+        rgn = &Regions[i];
+        if (min_f0 > rgn->num_triangles)
+        {
+            if (rgn->num_triangles == 0) {
+                return i;
+            }
+            if (rgn->field_2 == 0) {
+                reg_id = i;
+                min_f0 = rgn->num_triangles;
+            }
+        }
+    }
+    if (reg_id == -1) {
+        ERRORLOG("region_alloc:overflow");
+        return 0;
+    }
+    NAVIDBG(19,"removing triangles from region %ld",reg_id);
+    for (i=0; i < ix_Triangles; i++)
+    {
+        tri = &Triangles[i];
+        if (tri->field_D != -1)
+        {
+          sreg_id = (tri->field_E >> 6);
+          if (sreg_id >= REGIONS_COUNT) {
+              ERRORLOG("triangle %ld in outranged region",(long)i);
+              continue;
+          }
+          if (sreg_id == reg_id)
+          {
+              if (sreg_id > 0) {
+                  Regions[sreg_id].num_triangles--;
+                  tri->field_E &= 0x3F;
+                  Regions[sreg_id].field_2 = 0;
+                  Regions[0].num_triangles++;
+              }
+              if (Regions[reg_id].num_triangles == 0)
+                  break;
+          }
+        }
+    }
+    return reg_id;
+}
+
 void region_alloc(unsigned long tree_reg)
 {
+    /* Note that the beginning of this function is:
+       reg_id = region_alloc_id();
+    */
     _DK_region_alloc(tree_reg); return;
 }
 
@@ -83,12 +150,12 @@ void region_set_f(long ntri, unsigned long nreg, const char *func_name)
         // Remove from old region
         if (oreg < REGIONS_COUNT)
         {
-            Regions[oreg].field_0--;
+            Regions[oreg].num_triangles--;
             Regions[oreg].field_2 = 0;
         }
         // And add to new one
         set_triangle_region_id(ntri, nreg);
-        Regions[nreg].field_0++;
+        Regions[nreg].num_triangles++;
     }
 }
 
