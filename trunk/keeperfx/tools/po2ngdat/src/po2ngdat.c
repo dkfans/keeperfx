c2c36780dde7e3344c59b21cee2bf0f456547000
mefistotelis <mefistotelis@40a56aae-0377-11df-bfb3-7781b9cb07ea>
2012-09-29 21:35:08 +0000

    Started work on a tool to convert .PO files into KeeperFX .DAT files.

---
+++ trunk/keeperfx/tools/po2ngdat/src/po2ngdat.c
@@ -0,0 +1,28 @@
+/******************************************************************************/
+// PO translation to engine DAT files converter for KeeperFX
+/******************************************************************************/
+/** @file po2ngdat.c
+ *     Program code file.
+ * @par Purpose:
+ *     Contains code to read UTF .PO translation files, convert texts
+ *     into engine-readable encoding and write as .DAT files.
+ * @par Comment:
+ *     None.
+ * @author   Tomasz Lis <listom@gmail.com>
+ * @date     5 Aug 2012 - 22 Sep 2012
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char * argv[])
+{
+    //TODO
+    return 0;
+}
