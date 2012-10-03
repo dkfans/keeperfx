edf0c48c96a54c14c1946361500ff3e6147da22a
mefistotelis <mefistotelis@40a56aae-0377-11df-bfb3-7781b9cb07ea>
2012-10-03 22:45:27 +0000

    Po2ngdat - added support of multiple encoding files. Started work on Russian and Japanese encoding.

--- trunk/keeperfx/tools/po2ngdat/src/kfxenc.cpp
+++ trunk/keeperfx/tools/po2ngdat/src/kfxenc.cpp
@@ -1,3 +1,22 @@
+/******************************************************************************/
+// PO translation to engine DAT files converter for KeeperFX
+/******************************************************************************/
+/** @file kfxenc.cpp
+ *     Code pages conversion routines.
+ * @par Purpose:
+ *     Contains code to read code page tables and use them to encode
+ *     UTF strings into different code pages.
+ * @par Comment:
+ *     None.
+ * @author   Tomasz Lis <listom@gmail.com>
+ * @date     5 Aug 2012 - 22 Sep 2012
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
 
 #include <string>
 #include <map>
