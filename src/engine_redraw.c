9bdbe098f9d58f200738d191da7e2336a7cfe384
mefistotelis <mefistotelis@gmail.com>
2013-01-22 00:59:33 +0000

    New way of checking where spell is castable.
    Introduced a new way of determining where a spell
    can be casted. The new code most likely has some bugs.

--- src/engine_redraw.c
+++ src/engine_redraw.c
@@ -458,7 +458,7 @@ void process_pointer_graphic(void)
               spl_id = player_state_to_spell[player->work_state];
           thing = thing_get(battle_creature_over);
           TRACE_THING(thing);
-          if (can_cast_spell_on_creature(player->id_number, thing, spl_id))
+          if (can_cast_spell_on_thing(player->id_number, thing, spl_id))
           {
               draw_spell_cursor(player->work_state, battle_creature_over,
                   thing->mappos.x.stl.num, thing->mappos.y.stl.num);
