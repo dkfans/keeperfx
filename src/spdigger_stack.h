1c601cdebde43aff2abba891d0c36869f187bca7
mefistotelis <mefistotelis@gmail.com>
2013-02-05 20:17:55 +0000

    Avoid adding duplicated digger tasks.
    Improved adding re-arm tasks to digger queue - the function
    now makes sure that the same task isn't added twice.

--- src/spdigger_stack.h
+++ src/spdigger_stack.h
@@ -67,6 +67,7 @@ struct ExtraSquares {
 #pragma pack()
 /******************************************************************************/
 TbBool add_to_imp_stack_using_pos(long stl_num, long task_type, struct Dungeon *dungeon);
+long find_in_imp_stack_using_pos(long stl_num, long task_type, const struct Dungeon *dungeon);
 long add_undug_to_imp_stack(struct Dungeon *dungeon, long num);
 void add_pretty_and_convert_to_imp_stack(struct Dungeon *dungeon);
 long add_unclaimed_gold_to_imp_stack(struct Dungeon *dungeon);
@@ -74,7 +75,7 @@ void setup_imp_stack(struct Dungeon *dungeon);
 long add_unclaimed_unconscious_bodies_to_imp_stack(struct Dungeon *dungeon, long a2);
 TbBool add_unclaimed_dead_bodies_to_imp_stack(struct Dungeon *dungeon, long max_tasks);
 long add_unclaimed_spells_to_imp_stack(struct Dungeon *dungeon, long a2);
-long add_object_for_trap_to_imp_stack(struct Dungeon *dungeon, struct Thing *thing);
+TbBool add_object_for_trap_to_imp_stack(struct Dungeon *dungeon, struct Thing *thing);
 TbBool add_empty_traps_to_imp_stack(struct Dungeon *dungeon, long num);
 TbBool add_unclaimed_traps_to_imp_stack(struct Dungeon *dungeon);
 TbBool imp_will_soon_be_arming_trap(struct Thing *traptng);
