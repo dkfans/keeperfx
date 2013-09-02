6f3ac79c4326edc6d998558c783161651a85ec3e
mefistotelis <mefistotelis@gmail.com>
2013-09-02 22:54:04 +0000

    Remade drawing Transfer Creature list.
    Rewritten drawing the creatures list in Transfer Creature special.

--- src/frontmenu_specials.c
+++ src/frontmenu_specials.c
@@ -20,10 +20,18 @@
 #include "globals.h"
 #include "bflib_basics.h"
 
+#include "bflib_sprfnt.h"
+#include "bflib_vidraw.h"
 #include "bflib_guibtns.h"
+#include "dungeon_data.h"
+#include "creature_control.h"
+#include "config_creature.h"
+#include "config_strings.h"
+#include "power_specials.h"
 #include "gui_draw.h"
 #include "gui_frontbtns.h"
 #include "frontend.h"
+#include "game_legacy.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -132,7 +140,66 @@ void select_transfer_creature(struct GuiButton *gbtn)
 
 void draw_transfer_creature(struct GuiButton *gbtn)
 {
-  _DK_draw_transfer_creature(gbtn);
+    long listitm_idx;
+    unsigned long flgmem;
+    if (gbtn == NULL)
+      return;
+    SYNCDBG(7,"Starting");
+    //_DK_draw_transfer_creature(gbtn); return;
+    flgmem = lbDisplay.DrawFlags;
+    lbDisplay.DrawFlags = Lb_SPRITE_TRANSPAR4;
+    LbTextSetFont(winfont);
+    LbDrawBox(gbtn->scr_pos_x/pixel_size, gbtn->scr_pos_y/pixel_size,
+        gbtn->width/pixel_size, gbtn->height/pixel_size, 0); // The 0 means black color
+    LbTextSetWindow(gbtn->scr_pos_x/pixel_size, gbtn->scr_pos_y/pixel_size,
+        gbtn->width/pixel_size, gbtn->height/pixel_size);
+    listitm_idx = transfer_creature_scroll_offset + gbtn->field_1B;
+    struct Dungeon *dungeon;
+    dungeon = get_my_dungeon();
+    if (dungeon->num_active_creatrs > listitm_idx)
+    {
+        unsigned long k;
+        int i;
+        k = 0;
+        i = dungeon->creatr_list_start;
+        struct CreatureControl *cctrl;
+        struct Thing *thing;
+        thing = INVALID_THING;
+        while (i != 0)
+        {
+            // Do the drawing loop
+            thing = thing_get(i);
+            TRACE_THING(thing);
+            cctrl = creature_control_get_from_thing(thing);
+            if (thing_is_invalid(thing) || creature_control_invalid(cctrl))
+            {
+                ERRORLOG("Jump to invalid creature detected");
+                break;
+            }
+            i = cctrl->players_next_creature_idx;
+            // Thing list loop body
+            if (listitm_idx <= 0)
+                break;
+            listitm_idx--;
+            // Thing list loop body ends
+            k++;
+            if (k > CREATURES_COUNT)
+            {
+                ERRORLOG("Infinite loop detected when sweeping creatures list");
+                break;
+            }
+        }
+        if (!thing_is_invalid(thing))
+        {
+            lbDisplay.DrawFlags = Lb_TEXT_HALIGN_LEFT;
+            struct CreatureData *crdata;
+            crdata = creature_data_get_from_thing(thing);
+            LbTextDrawFmt(0/pixel_size, 0/pixel_size, " %s", cmpgn_string(crdata->namestr_idx));
+            lbDisplay.DrawFlags = Lb_TEXT_HALIGN_RIGHT;
+            LbTextDrawFmt(0/pixel_size, 0/pixel_size, "%s %d ", gui_string(406), (int)(cctrl->explevel+1));
+        }
+    }
+    lbDisplay.DrawFlags = flgmem;
 }
 
 void select_transfer_creature_up(struct GuiButton *gbtn)
