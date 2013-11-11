d0a0b1de09086810671a4445a13983867de39118
mefistotelis@gmail.com <mefistotelis@gmail.com@40a56aae-0377-11df-bfb3-7781b9cb07ea>
2013-11-11 12:09:45 +0000

    Added stats increase with experience as config parameters. Added different default values for Ancient Keeper.

--- trunk/keeperfx/src/thing_stats.h
+++ trunk/keeperfx/src/thing_stats.h
@@ -51,7 +51,6 @@ long compute_creature_attack_damage(long base_param,long luck,unsigned short crl
 long project_creature_attack_damage(long base_param,long luck,unsigned short crlevel);
 long compute_creature_attack_range(long base_param,long luck,unsigned short crlevel);
 long compute_creature_work_value(long base_param,long efficiency,unsigned short crlevel);
-long compute_creature_max_sparameter(long base_param,unsigned short crlevel);
 long compute_creature_max_dexterity(long base_param,unsigned short crlevel);
 long compute_creature_max_defense(long base_param,unsigned short crlevel);
 long compute_creature_max_strength(long base_param,unsigned short crlevel);
