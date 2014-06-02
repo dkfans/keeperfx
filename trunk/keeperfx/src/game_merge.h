1fc26c90b7477a27320d87988b4ac4828bad8280
mefistotelis@gmail.com <mefistotelis@gmail.com@40a56aae-0377-11df-bfb3-7781b9cb07ea>
2014-06-02 19:58:56 +0000

    Moved creature unconscious time to config file.

--- trunk/keeperfx/src/game_merge.h
+++ trunk/keeperfx/src/game_merge.h
@@ -91,6 +91,7 @@ struct GameAdd {
     unsigned long turn_last_checked_for_gold;
     unsigned long flee_zone_radius;
     unsigned long time_between_prison_break;
+    unsigned short game_turns_unconscious;
     long critical_health_permil;
     long friendly_fight_area_damage_permil;
     long friendly_fight_area_range_permil;
