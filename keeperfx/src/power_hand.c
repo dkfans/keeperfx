dkfans/keeperfx

commit 1d31d10dc4736cf90a723a6aa47b38ffe41e7da7

Modified creatures to not assign jobs which are not in their primary/secondary jobs list. Also fixed assigning captivity job by dropping.
git-svn-id: http://keeperfx.googlecode.com/svn/trunk@1157 40a56aae-0377-11df-bfb3-7781b9cb07ea

keeperfx/src/power_hand.c
@@ -23,6 +23,7 @@
 #include "bflib_math.h"
 #include "bflib_planar.h"
 #include "bflib_vidraw.h"
+#include "bflib_sound.h"
 
 #include "magic.h"
 #include "power_specials.h"
@@ -750,10 +751,113 @@ long gold_being_dropped_on_creature(long plyr_idx, struct Thing *goldtng, struct
     return 1;
 }
 
-short dump_held_things_on_map(PlayerNumber plyr_idx, long a2, long a3, short a4)
+void drop_held_thing_on_ground(struct Dungeon *dungeon, struct Thing *droptng, MapSubtlCoord stl_x, MapSubtlCoord stl_y)
 {
-    //TODO rewrite - this is quite high level function
-    return _DK_dump_held_things_on_map(plyr_idx, a2, a3, a4);
+    droptng->mappos.x.val = subtile_coord_center(stl_x);
+    droptng->mappos.y.val = subtile_coord_center(stl_y);
+    droptng->mappos.z.val = subtile_coord(8,0);
+    long fall_dist;
+    fall_dist = get_ceiling_height_at(&droptng->mappos) - get_floor_height_at(&droptng->mappos);
+    if (fall_dist < 0) {
+        fall_dist = 0;
+    } else
+    if (fall_dist > subtile_coord(3,0)) {
+        fall_dist = subtile_coord(3,0);
+    }
+    droptng->mappos.z.val = fall_dist + get_floor_height_at(&droptng->mappos);
+    droptng->alloc_flags &= ~0x10;
+    droptng->field_4F &= ~0x01;
+    place_thing_in_mapwho(droptng);
+    if (thing_is_creature(droptng))
+    {
+        initialise_thing_state(droptng, CrSt_CreatureBeingDropped);
+        stop_creature_sound(droptng, 5);
+        play_creature_sound(droptng, 6, 3, 0);
+        dungeon->field_14AE = game.play_gameturn;
+    } else
+    if (thing_is_object(droptng))
+    {
+        if (object_is_mature_food(droptng)) {
+            set_thing_draw(droptng, convert_td_iso(819), 256, -1, -1, 0, 2);
+        }
+        droptng->continue_state = droptng->active_state;
+        droptng->active_state = ObSt_BeingDropped;
+    }
+}
+
+short dump_held_thing_on_map(PlayerNumber plyr_idx, MapSubtlCoord stl_x, MapSubtlCoord stl_y, TbBool update_hand)
+{
+    //return _DK_dump_held_things_on_map(plyr_idx, stl_x, stl_y, a4);
+    struct PlayerInfo *player;
+    player = get_player(plyr_idx);
+    struct Dungeon *dungeon;
+    dungeon = get_players_dungeon(player);
+    // If nothing in hand - nothing to do
+    if (dungeon->num_things_in_hand < 1) {
+        return 0;
+    }
+    // Check if drop position is allowed
+    struct Thing *droptng;
+    droptng = thing_get(dungeon->things_in_hand[0]);
+    if (!can_drop_thing_here(stl_x, stl_y, plyr_idx, thing_is_creature_special_digger(droptng))) {
+        return 0;
+    }
+    // Check if object will fit into that position
+    struct Coord3d pos;
+    pos.x.val = subtile_coord_center(stl_x);
+    pos.y.val = subtile_coord_center(stl_y);
+    pos.z.val = get_thing_height_at(droptng, &pos);
+    if (thing_in_wall_at(droptng, &pos)) {
+        return 0;
+    }
+    struct Thing *overtng;
+    overtng = thing_get(player->thing_under_hand);
+    if (thing_is_object(droptng) && object_is_gold_pile(droptng))
+    {
+        if (thing_is_creature(overtng) && !thing_is_creature_special_digger(overtng))
+        {
+            gold_being_dropped_on_creature(plyr_idx, droptng, overtng);
+        } else
+        {
+            drop_gold_coins(&pos, droptng->valuable.gold_stored, plyr_idx);
+            if (is_my_player_number(plyr_idx)) {
+                play_non_3d_sample(88);
+            }
+        }
+        delete_thing_structure(droptng, 0);
+    } else
+    if (thing_is_object(droptng) && object_is_mature_food(droptng))
+    {
+        if (thing_is_creature(overtng) && !thing_is_creature_special_digger(overtng))
+        {
+            food_eaten_by_creature(droptng, thing_get(player->thing_under_hand));
+        } else
+        {
+            drop_held_thing_on_ground(dungeon, droptng, stl_x, stl_y);
+        }
+    } else
+    {
+        drop_held_thing_on_ground(dungeon, droptng, stl_x, stl_y);
+    }
+    if (dungeon->num_things_in_hand == 1) {
+        set_player_instance(player, PI_Drop, 0);
+    }
+    remove_thing_from_power_hand_list(thing_get(dungeon->things_in_hand[0]), plyr_idx);
+    if ( update_hand ) {
+      set_power_hand_offset(player, thing_get(dungeon->things_in_hand[0]));
+    }
+    return 1;
+}
+
+int dump_all_held_things_on_map(PlayerNumber plyr_idx, MapSubtlCoord stl_x, MapSubtlCoord stl_y)
+{
+    int k;
+    // Dump all things
+    k = 0;
+    while (dump_held_thing_on_map(plyr_idx, stl_x, stl_y, 0) == 1) {
+        k++;
+    }
+    return k;
 }
 
 void clear_things_in_hand(struct PlayerInfo *player)

keeperfx/config/fxdata/creature.cfg
@@ -703,7 +703,7 @@ Functions = work_in_room
 Name = FREEZE_PRISONERS
 RelatedRoom = PRISON
 InitialState = CreatureFreezePrisonrs
-Assign = OWNED_CREATURES HUMAN_DROP_IN_ROOM COMPUTER_DROP_IN_ROOM WHOLE_ROOM
+Assign = OWNED_CREATURES WHOLE_ROOM
 Functions = in_state_on_room_content
 
 [job15]

keeperfx/src/config_creature.c
@@ -1610,7 +1610,7 @@ struct CreatureJobConfig *get_config_for_job(CreatureJob job_flags)
  * @param stl_y
  * @return
  */
-CreatureJob get_job_for_subtile(const struct Thing *creatng, MapSubtlCoord stl_x, MapSubtlCoord stl_y)
+CreatureJob get_job_for_subtile(const struct Thing *creatng, MapSubtlCoord stl_x, MapSubtlCoord stl_y, unsigned long drop_kind_flags)
 {
     struct Room *room;
     room = get_room_thing_is_on(creatng);
@@ -1618,7 +1618,7 @@ CreatureJob get_job_for_subtile(const struct Thing *creatng, MapSubtlCoord stl_x
     {
         unsigned long required_kind_flags;
         // Detect the job which we will do in the room
-        required_kind_flags = JoKF_None;
+        required_kind_flags = drop_kind_flags;
         if (creatng->owner == room->owner)
         {
             if (creatng->model == get_players_special_digger_model(creatng->owner)) {

keeperfx/src/config_creature.h
@@ -244,7 +244,7 @@ CrtrStateId get_arrive_at_state_for_job(CreatureJob jobpref);
 CrtrStateId get_arrive_at_state_for_room(RoomKind rkind);
 unsigned long get_flags_for_job(CreatureJob jobpref);
 CreatureJob get_creature_job_causing_stress(CreatureJob job_flags, RoomKind rkind);
-CreatureJob get_job_for_subtile(const struct Thing *creatng, MapSubtlCoord stl_x, MapSubtlCoord stl_y);
+CreatureJob get_job_for_subtile(const struct Thing *creatng, MapSubtlCoord stl_x, MapSubtlCoord stl_y, unsigned long drop_kind_flags);
 CreatureJob get_job_for_room(RoomKind rkind, unsigned long required_kind_flags);
 const char *creature_job_code_name(CreatureJob job_flag);
 /******************************************************************************/

keeperfx/src/creature_control.h
@@ -668,6 +668,7 @@ TbBool disband_creatures_group(struct Thing *thing);
 struct Thing *get_group_last_member(struct Thing *thing);
 
 void play_creature_sound(struct Thing *thing, long snd_idx, long a3, long a4);
+void stop_creature_sound(struct Thing *thing, long snd_idx);
 void play_creature_sound_and_create_sound_thing(struct Thing *thing, long snd_idx, long a2);
 struct CreatureSound *get_creature_sound(struct Thing *thing, long snd_idx);
 void reset_creature_eye_lens(struct Thing *thing);

keeperfx/src/creature_jobs.c
@@ -667,15 +667,18 @@ TbBool attempt_job_secondary_preference(struct Thing *creatng, long jobpref)
     // Probably needs unification
     for (i=0; i < sizeof(jobs_list)/sizeof(jobs_list[0]); i++)
     {
-        CreatureJob job_check = jobs_list[i];
+        CreatureJob new_job = jobs_list[i];
+        if ((jobpref & new_job) == 0) {
+            continue;
+        }
         if (select_val <= select_curr)
         {
             select_curr += select_delta;
         } else
-        if (creature_can_do_job_for_player(creatng, creatng->owner, job_check))
+        if (creature_can_do_job_for_player(creatng, creatng->owner, new_job))
         {
             //TODO CREATURE_JOBS this is a bit different than attempt_job_work_in_room(), should be unified.
-            switch (job_check)
+            switch (new_job)
             {
             case Job_TRAIN:
             case Job_RESEARCH:
@@ -686,10 +689,10 @@ TbBool attempt_job_secondary_preference(struct Thing *creatng, long jobpref)
             case Job_TEMPLE_PRAY:
             case Job_BARRACK: {
                 struct Room *room;
-                room = find_nearest_room_for_thing_with_spare_capacity(creatng, creatng->owner, get_room_for_job(job_check), 0, 1);
+                room = find_nearest_room_for_thing_with_spare_capacity(creatng, creatng->owner, get_room_for_job(new_job), 0, 1);
                 if (!room_is_invalid(room))
                 {
-                    if (send_creature_to_room(creatng, room, job_check)) {
+                    if (send_creature_to_room(creatng, room, new_job)) {
                       return true;
                     }
                 }

keeperfx/src/creature_states.c
@@ -1591,7 +1591,7 @@ short creature_being_dropped(struct Thing *creatng)
         }
     }
     CreatureJob new_job;
-    new_job = get_job_for_subtile(creatng, stl_x, stl_y);
+    new_job = get_job_for_subtile(creatng, stl_x, stl_y, JoKF_AssignHumanDropInRoom);
     if (new_job == Job_NULL)
     {
         SYNCDBG(3,"No job found at (%d,%d) for %s index %d owner %d",(int)stl_x,(int)stl_y,thing_model_name(creatng),(int)creatng->index,(int)creatng->owner);

keeperfx/src/frontmenu_ingame_map.c
@@ -580,7 +580,7 @@ short do_right_map_click(long start_x, long start_y, long curr_mx, long curr_my,
         right_button_released = 0;
         if (subtile_has_slab(x, y))
         {
-          set_players_packet_action(player, PckA_DumpHeldThings, x, y, 0, 0);
+          set_players_packet_action(player, PckA_DumpHeldThing, x, y, 0, 0);
           return 1;
         }
     }

commit 34219d4812fe8d48a85e8958d0ebd12ec55fe997

Fixed problem with dropping things from power hand introduced in r1157.

keeperfx/src/power_hand.c
@@ -342,6 +342,30 @@ struct Thing *get_first_thing_in_power_hand(struct PlayerInfo *player)
  * @param plyr_idx
  * @return
  */
+TbBool remove_first_thing_from_power_hand_list(PlayerNumber plyr_idx)
+{
+  struct Dungeon *dungeon;
+  long i;
+  dungeon = get_dungeon(plyr_idx);
+  if (dungeon->num_things_in_hand > 0)
+  {
+      for (i = 0; i < dungeon->num_things_in_hand-1; i++)
+      {
+        dungeon->things_in_hand[i] = dungeon->things_in_hand[i+1];
+      }
+      dungeon->num_things_in_hand--;
+      dungeon->things_in_hand[dungeon->num_things_in_hand] = 0;
+      return true;
+  }
+  return false;
+}
+
+/** Removes a thing from player's power hand list without any further processing.
+ *
+ * @param thing
+ * @param plyr_idx
+ * @return
+ */
 TbBool remove_thing_from_power_hand_list(struct Thing *thing, PlayerNumber plyr_idx)
 {
   struct Dungeon *dungeon;
@@ -787,7 +811,7 @@ void drop_held_thing_on_ground(struct Dungeon *dungeon, struct Thing *droptng, M
 
 short dump_held_thing_on_map(PlayerNumber plyr_idx, MapSubtlCoord stl_x, MapSubtlCoord stl_y, TbBool update_hand)
 {
-    //return _DK_dump_held_things_on_map(plyr_idx, stl_x, stl_y, a4);
+    //return _DK_dump_held_things_on_map(plyr_idx, stl_x, stl_y, update_hand);
     struct PlayerInfo *player;
     player = get_player(plyr_idx);
     struct Dungeon *dungeon;
@@ -842,7 +866,7 @@ short dump_held_thing_on_map(PlayerNumber plyr_idx, MapSubtlCoord stl_x, MapSubt
     if (dungeon->num_things_in_hand == 1) {
         set_player_instance(player, PI_Drop, 0);
     }
-    remove_thing_from_power_hand_list(thing_get(dungeon->things_in_hand[0]), plyr_idx);
+    remove_first_thing_from_power_hand_list(plyr_idx);
     if ( update_hand ) {
       set_power_hand_offset(player, thing_get(dungeon->things_in_hand[0]));
     }

keeperfx/src/power_hand.h
@@ -52,6 +52,7 @@ struct Thing *get_nearest_thing_for_hand_or_slap(PlayerNumber plyr_idx, MapCoord
 
 TbBool insert_thing_into_power_hand_list(struct Thing *thing, PlayerNumber plyr_idx);
 TbBool remove_thing_from_power_hand_list(struct Thing *thing, PlayerNumber plyr_idx);
+TbBool remove_first_thing_from_power_hand_list(PlayerNumber plyr_idx);
 TbBool thing_is_in_power_hand_list(const struct Thing *thing, PlayerNumber plyr_idx);
 
 long can_thing_be_picked_up_by_player(const struct Thing *thing, PlayerNumber plyr_idx);