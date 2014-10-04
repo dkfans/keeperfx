6fd31b5a959b3ea871f28c0a818b7eb59cd867d8
mefistotelis <mefistotelis@gmail.com>
2014-10-04 13:22:05 +0000

    Modified minimap scaling to better fit rescaled panel.

--- src/frontmenu_ingame_tabs.c
+++ src/frontmenu_ingame_tabs.c
@@ -2209,7 +2209,7 @@ void draw_whole_status_panel(void)
         int mnu_num;
         mnu_num = menu_id_to_number(GMnu_MAIN);
         gmnu = get_active_menu(mnu_num);
-        mm_units_per_px = (gmnu->width * 16 + 136/2) / 136;
+        mm_units_per_px = (gmnu->width * 16 + 140/2) / 140;
         if (mm_units_per_px < 1)
             mm_units_per_px = 1;
         fs_units_per_px = (gmnu->height * 16 + 8) / LbTiledSpriteHeight(&status_panel, gui_panel_sprites);
