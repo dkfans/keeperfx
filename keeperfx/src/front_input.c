6f4cad482b6a80785414e7049dd739c70a991088
HeMeng86 <HeMeng86@gmail.com>
2015-04-25 10:42:15 +0000

    ESC key to exit creature control mode

--- keeperfx/src/front_input.c
+++ keeperfx/src/front_input.c
@@ -947,7 +947,7 @@ short get_creature_control_action_inputs(void)
 
     if (player->controlled_thing_idx != 0)
     {
-        short make_packet = right_button_released;
+        short make_packet = right_button_released || is_key_pressed(KC_ESCAPE, KMod_DONTCARE);
         if (!make_packet)
         {
           struct Thing *thing;
@@ -960,6 +960,7 @@ short get_creature_control_action_inputs(void)
         if (make_packet)
         {
             right_button_released = 0;
+            clear_key_pressed(KC_ESCAPE);
             set_players_packet_action(player, PckA_Unknown033, player->controlled_thing_idx,0);
         }
     }
