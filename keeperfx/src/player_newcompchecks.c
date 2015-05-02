425bee4b3bda36a7df60e30ec0b2d58f38771c13
eatpajdittsvin <eatpajdittsvin@gmail.com>
2015-05-02 13:28:20 +0000

    Copyright notices

--- keeperfx/src/player_newcompchecks.c
+++ keeperfx/src/player_newcompchecks.c
@@ -1,3 +1,23 @@
+/******************************************************************************/
+// Free implementation of Bullfrog's Dungeon Keeper strategy game.
+/******************************************************************************/
+/** @file player_newcompchecks.c
+ *     New computer player checks.
+ * @par Purpose:
+ *     Cleanly separates computer player checks that weren't in the original
+ *     game.
+ * @par Comment:
+ *     None.
+ * @author   KeeperFX Team
+ * @date     10 Mar 2009 - 02 May 2015
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+
 #include "player_newcomp.h"
 
 #include "bflib_math.h"
--- keeperfx/src/player_newcompeval.c
+++ keeperfx/src/player_newcompeval.c
@@ -1,3 +1,23 @@
+/******************************************************************************/
+// Free implementation of Bullfrog's Dungeon Keeper strategy game.
+/******************************************************************************/
+/** @file player_newcompeval.c
+ *     New computer player evaluation functions.
+ * @par Purpose:
+ *     Defines functions that look at the game state to determine something, but
+ *     that do not take any action themselves.
+ * @par Comment:
+ *     None.
+ * @author   KeeperFX Team
+ * @date     10 Mar 2009 - 02 May 2015
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+
 #include "creature_states.h"
 #include "game_legacy.h"
 #include "magic.h"
@@ -5,11 +25,6 @@
 #include "power_hand.h"
 #include "spdigger_stack.h"
 #include "thing_data.h"
-
-/*
-	Various kinds of evaluation functions: functions that look at the game state to determine something, but
-	don't take any action themselves.
-	*/
 
 /************************************************************************/
 /* Any imps thinking to dig gems right now?                             */
