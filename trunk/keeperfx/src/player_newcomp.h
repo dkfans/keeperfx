31b7b5294ca08e51314b29ec23aed62af9f23513
eatpajdittsvin@gmail.com <eatpajdittsvin@gmail.com@40a56aae-0377-11df-bfb3-7781b9cb07ea>
2015-05-02 13:28:20 +0000

    Copyright notices

--- trunk/keeperfx/src/player_newcomp.h
+++ trunk/keeperfx/src/player_newcomp.h
@@ -1,3 +1,22 @@
+/******************************************************************************/
+// Free implementation of Bullfrog's Dungeon Keeper strategy game.
+/******************************************************************************/
+/** @file player_newcomp.h
+ *     New computer player header.
+ * @par Purpose:
+ *     Cleanly separates computer player code that wasn't in the original game.
+ * @par Comment:
+ *     None.
+ * @author   KeeperFX Team
+ * @date     10 Mar 2009 - 02 May 2015
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+
 #ifndef PLAYER_NEWCOMP_H
 #define PLAYER_NEWCOMP_H
 
