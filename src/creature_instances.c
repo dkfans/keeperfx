dkfans/keeperfx

commit d43abd619626582842ffb6d2739931e2d2933fd1

Fix spell cooldowns not always working
Fixes cases such as retreating Archers firing 3-4 arrows in half a second (especially when backed against walls)

src/creature_instances.c
@@ -465,7 +465,8 @@ void process_creature_instance(struct Thing *thing)
                 cctrl->inst_repeat = 0;
                 return;
             }
-            cctrl->instance_use_turn[cctrl->instance_id] = game.play_gameturn;
+			// Instances sometimes failed to reach this. More reliable to set instance_use_turn in set_creature_instance instead
+            // cctrl->instance_use_turn[cctrl->instance_id] = game.play_gameturn;
             cctrl->instance_id = CrInst_NULL;
         }
         cctrl->inst_repeat = 0;

src/frontmenu_ingame_tabs.c
@@ -1699,10 +1699,11 @@ void gui_area_instance_button(struct GuiButton *gbtn)
         long turns_progress, turns_required;
         if ((ctrltng->alloc_flags & TAlF_IsControlled) != 0) {
             turns_required = inst_inf->fp_reset_time;
+			turns_progress = (long)game.play_gameturn - inst_inf->fp_time - (long)cctrl->instance_use_turn[curbtn_inst_id];
         } else {
             turns_required = inst_inf->reset_time;
+			turns_progress = (long)game.play_gameturn - inst_inf->time - (long)cctrl->instance_use_turn[curbtn_inst_id];
         }
-        turns_progress = (long)game.play_gameturn - (long)cctrl->instance_use_turn[curbtn_inst_id];
         gui_area_progress_bar_short(gbtn, units_per_px, turns_progress, turns_required);
     } else
     {

src/thing_creature.c
@@ -2982,10 +2982,10 @@ long creature_instance_has_reset(const struct Thing *thing, long inst_idx)
     delta = (long)game.play_gameturn - (long)cctrl->instance_use_turn[inst_idx];
     if ((thing->alloc_flags & TAlF_IsControlled) != 0)
     {
-        ritime = inst_inf->fp_reset_time;
+        ritime = inst_inf->fp_reset_time + inst_inf->fp_time;
     } else
     {
-        ritime = inst_inf->reset_time;
+        ritime = inst_inf->reset_time + inst_inf->time;
     }
     return (delta >= ritime);
 }
@@ -3079,6 +3079,7 @@ void set_creature_instance(struct Thing *thing, CrInstance inst_idx, long a2, lo
     cctrl->inst_turn = 0;
     cctrl->inst_total_turns = itime;
     cctrl->inst_action_turns = aitime;
+    cctrl->instance_use_turn[cctrl->instance_id] = game.play_gameturn;
     i = get_creature_model_graphics(thing->model,inst_inf->graphics_idx);
     cctrl->instance_anim_step_turns = get_lifespan_of_animation(i, 1) / itime;
     if (pos != NULL)

commit fa386f2bd924e93dfc6f285fc358fe12ab690c90

Start cooldowns when instance effect activates (#83)
Fix to bug introduced in d43abd619626582842ffb6d2739931e2d2933fd1

src/creature_instances.c
@@ -455,6 +455,8 @@ void process_creature_instance(struct Thing *thing)
             {
                 SYNCDBG(18,"Executing %s for %s index %d.",creature_instance_code_name(cctrl->instance_id),thing_model_name(thing),(int)thing->index);
                 inst_inf->func_cb(thing, inst_inf->func_params);
+				// Start cooldown when spell effect activates
+				cctrl->instance_use_turn[cctrl->instance_id] = game.play_gameturn;
             }
         }
         if (cctrl->inst_turn >= cctrl->inst_total_turns)
@@ -465,7 +467,7 @@ void process_creature_instance(struct Thing *thing)
                 cctrl->inst_repeat = 0;
                 return;
             }
-			// Instances sometimes failed to reach this. More reliable to set instance_use_turn in set_creature_instance instead
+			// Instances sometimes failed to reach this. More reliable to set instance_use_turn sooner
             // cctrl->instance_use_turn[cctrl->instance_id] = game.play_gameturn;
             cctrl->instance_id = CrInst_NULL;
         }

src/frontmenu_ingame_tabs.c
@@ -1698,10 +1698,10 @@ void gui_area_instance_button(struct GuiButton *gbtn)
     {
         long turns_progress, turns_required;
         if ((ctrltng->alloc_flags & TAlF_IsControlled) != 0) {
-            turns_required = inst_inf->fp_reset_time;
+            turns_required = inst_inf->fp_reset_time - inst_inf->fp_action_time;
 			turns_progress = (long)game.play_gameturn - inst_inf->fp_time - (long)cctrl->instance_use_turn[curbtn_inst_id];
         } else {
-            turns_required = inst_inf->reset_time;
+            turns_required = inst_inf->reset_time - inst_inf->action_time;
 			turns_progress = (long)game.play_gameturn - inst_inf->time - (long)cctrl->instance_use_turn[curbtn_inst_id];
         }
         gui_area_progress_bar_short(gbtn, units_per_px, turns_progress, turns_required);

src/thing_creature.c
@@ -2982,10 +2982,10 @@ long creature_instance_has_reset(const struct Thing *thing, long inst_idx)
     delta = (long)game.play_gameturn - (long)cctrl->instance_use_turn[inst_idx];
     if ((thing->alloc_flags & TAlF_IsControlled) != 0)
     {
-        ritime = inst_inf->fp_reset_time + inst_inf->fp_time;
+        ritime = inst_inf->fp_reset_time + inst_inf->fp_time - inst_inf->fp_action_time;
     } else
     {
-        ritime = inst_inf->reset_time + inst_inf->time;
+        ritime = inst_inf->reset_time + inst_inf->time - inst_inf->action_time;
     }
     return (delta >= ritime);
 }
@@ -3079,7 +3079,6 @@ void set_creature_instance(struct Thing *thing, CrInstance inst_idx, long a2, lo
     cctrl->inst_turn = 0;
     cctrl->inst_total_turns = itime;
     cctrl->inst_action_turns = aitime;
-    cctrl->instance_use_turn[cctrl->instance_id] = game.play_gameturn;
     i = get_creature_model_graphics(thing->model,inst_inf->graphics_idx);
     cctrl->instance_anim_step_turns = get_lifespan_of_animation(i, 1) / itime;
     if (pos != NULL)