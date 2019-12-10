dkfans/keeperfx

commit d43abd619626582842ffb6d2739931e2d2933fd1

Fix spell cooldowns not always working
Fixes cases such as retreating Archers firing 3-4 arrows in half a second (especially when backed against walls)

src/creature_instances.c
@@ -465,7 +465,8 @@ void process_creature_instance(struct Thing *thing)
                 cctrl->inst_repeat = 0;
                 return;
             }
-            cctrl->instance_use_turn[cctrl->instance_id] = game.play_gameturn;
+			// Instances sometimes failed to reach this. More reliable to set instance_use_turn in set_creature_instance instead
+            // cctrl->instance_use_turn[cctrl->instance_id] = game.play_gameturn;
             cctrl->instance_id = CrInst_NULL;
         }
         cctrl->inst_repeat = 0;

src/frontmenu_ingame_tabs.c
@@ -1699,10 +1699,11 @@ void gui_area_instance_button(struct GuiButton *gbtn)
         long turns_progress, turns_required;
         if ((ctrltng->alloc_flags & TAlF_IsControlled) != 0) {
             turns_required = inst_inf->fp_reset_time;
+			turns_progress = (long)game.play_gameturn - inst_inf->fp_time - (long)cctrl->instance_use_turn[curbtn_inst_id];
         } else {
             turns_required = inst_inf->reset_time;
+			turns_progress = (long)game.play_gameturn - inst_inf->time - (long)cctrl->instance_use_turn[curbtn_inst_id];
         }
-        turns_progress = (long)game.play_gameturn - (long)cctrl->instance_use_turn[curbtn_inst_id];
         gui_area_progress_bar_short(gbtn, units_per_px, turns_progress, turns_required);
     } else
     {

src/thing_creature.c
@@ -2982,10 +2982,10 @@ long creature_instance_has_reset(const struct Thing *thing, long inst_idx)
     delta = (long)game.play_gameturn - (long)cctrl->instance_use_turn[inst_idx];
     if ((thing->alloc_flags & TAlF_IsControlled) != 0)
     {
-        ritime = inst_inf->fp_reset_time;
+        ritime = inst_inf->fp_reset_time + inst_inf->fp_time;
     } else
     {
-        ritime = inst_inf->reset_time;
+        ritime = inst_inf->reset_time + inst_inf->time;
     }
     return (delta >= ritime);
 }
@@ -3079,6 +3079,7 @@ void set_creature_instance(struct Thing *thing, CrInstance inst_idx, long a2, lo
     cctrl->inst_turn = 0;
     cctrl->inst_total_turns = itime;
     cctrl->inst_action_turns = aitime;
+    cctrl->instance_use_turn[cctrl->instance_id] = game.play_gameturn;
     i = get_creature_model_graphics(thing->model,inst_inf->graphics_idx);
     cctrl->instance_anim_step_turns = get_lifespan_of_animation(i, 1) / itime;
     if (pos != NULL)

commit a06b9427a95340d582b8bf551e919e3b5cd80eb5

Start creature spell cooldown only after successful use (#91)
Second attempt to fix bug introduced in https://github.com/Loobinex/keeperfx-unofficial/commit/d43abd619626582842ffb6d2739931e2d2933fd1

src/creature_instances.c
@@ -455,8 +455,6 @@ void process_creature_instance(struct Thing *thing)
             {
                 SYNCDBG(18,"Executing %s for %s index %d.",creature_instance_code_name(cctrl->instance_id),thing_model_name(thing),(int)thing->index);
                 inst_inf->func_cb(thing, inst_inf->func_params);
-				// Start cooldown when spell effect activates
-				cctrl->instance_use_turn[cctrl->instance_id] = game.play_gameturn;
             }
         }
         if (cctrl->inst_turn >= cctrl->inst_total_turns)
@@ -467,8 +465,8 @@ void process_creature_instance(struct Thing *thing)
                 cctrl->inst_repeat = 0;
                 return;
             }
-			// Instances sometimes failed to reach this. More reliable to set instance_use_turn sooner
-            // cctrl->instance_use_turn[cctrl->instance_id] = game.play_gameturn;
+            // Instances sometimes failed to reach this. More reliable to set instance_use_turn sooner
+            cctrl->instance_use_turn[cctrl->instance_id] = game.play_gameturn;
             cctrl->instance_id = CrInst_NULL;
         }
         cctrl->inst_repeat = 0;
@@ -520,6 +518,8 @@ long instf_creature_fire_shot(struct Thing *creatng, long *param)
         SYNCDBG(8,"The %s index %d fires %s",thing_model_name(creatng),(int)creatng->index,shot_code_name(*param));
     }
     creature_fire_shot(creatng, target, *param, 1, i);
+    // Start cooldown after shot is fired
+    cctrl->instance_use_turn[cctrl->instance_id] = game.play_gameturn;
     return 0;
 }
 
@@ -540,10 +540,14 @@ long instf_creature_cast_spell(struct Thing *creatng, long *param)
         if (!thing_is_invalid(trthing))
         {
             creature_cast_spell_at_thing(creatng, trthing, spl_idx, cctrl->explevel);
+            // Start cooldown after spell is cast
+            cctrl->instance_use_turn[cctrl->instance_id] = game.play_gameturn;
             return 0;
         }
     }
     creature_cast_spell(creatng, spl_idx, cctrl->explevel, cctrl->targtstl_x, cctrl->targtstl_y);
+    // Start cooldown after spell effect activates
+    cctrl->instance_use_turn[cctrl->instance_id] = game.play_gameturn;
     return 0;
 }
 
@@ -766,6 +770,10 @@ long instf_fart(struct Thing *creatng, long *param)
     if (!thing_is_invalid(efftng))
         efftng->byte_16 = 4;
     thing_play_sample(creatng,94+UNSYNC_RANDOM(6), NORMAL_PITCH, 0, 3, 0, 4, FULL_LOUDNESS);
+    // Start cooldown after fart created
+    struct CreatureControl *cctrl;
+    cctrl = creature_control_get_from_thing(creatng);
+    cctrl->instance_use_turn[cctrl->instance_id] = game.play_gameturn;
     return 1;
 }
 

version.mk
@@ -1,5 +1,5 @@
 VER_MAJOR=0
 VER_MINOR=4
 VER_RELEASE=7
-VER_BUILD=1968
+VER_BUILD=1973
 PACKAGE_SUFFIX=unofficial