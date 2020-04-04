eec89889cd2d67e0a639bd7863cc7bf2ddb7c3ec
Loobinex <Loobinex@users.noreply.github.com>
2020-04-04 17:43:35 +0200

    Fixed AlwaysTunnelToRed classic bug

--- src/creature_states_hero.c
+++ src/creature_states_hero.c
@@ -868,7 +868,7 @@ long get_best_dungeon_to_tunnel_to(struct Thing *creatng)
         if (player_exists(player) && !dungeon_invalid(dungeon) && (creatng->owner != plyr_idx))
         {
             long score = dungeon->total_score - 20 * dungeon->total_score * dungeon->field_F7D / 100;
-            if ((score <= 0) || (gameadd.classic_bugs_flags & ClscBug_BreakNeutralWalls))
+            if ((score <= 0) || (gameadd.classic_bugs_flags & ClscBug_AlwaysTunnelToRed))
             {
                 score = 0;
             }
