154f1c764f765dc113b9066c3d27a3b912affee6
SimLV <github@iamsim.ru>
2020-08-10 00:28:51 +0300

    * -ai_player flag is now works as expected

--- src/main.cpp
+++ src/main.cpp
@@ -3754,6 +3754,12 @@ void keeper_gameplay_loop(void)
     PaletteSetPlayerPalette(player, engine_palette);
     if ((game.operation_flags & GOF_SingleLevel) != 0)
         initialise_eye_lenses();
+
+    if ((start_params.autotest_flags & ATF_AI_Player) != 0)
+    {
+        toggle_computer_player(player->id_number);
+    }
+
     SYNCDBG(0,"Entering the gameplay loop for level %d",(int)get_loaded_level_number());
 
     KeeperSpeechClearEvents();
