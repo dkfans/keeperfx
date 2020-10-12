34210114a30146b427b6191d34653690ea6d08a2
eddebaby <ed_j_kearney@yahoo.co.uk>
2020-10-12 17:36:25 +0100

    SnapCamera now uses keybinding in-game
    ... rather than a hard-coded use of middle mouse button, it will now use whatever is set in the key binding menu.

--- src/front_input.c
+++ src/front_input.c
@@ -792,7 +792,7 @@ long get_dungeon_control_action_inputs(void)
           toggle_gui();
       }
       // Middle mouse camera actions for IsometricView
-      if (lbDisplay.MiddleButton >= 1)
+      if (is_game_key_pressed(Gkey_SnapCamera, &val, true))
       {
           struct Camera* cam = &player->cameras[CamIV_Isometric];
           struct Packet* pckt = get_packet(my_player_number);
@@ -867,8 +867,8 @@ long get_dungeon_control_action_inputs(void)
         {
             (angle = 0);
         }
-      set_packet_action(pckt,PckA_SetMapRotation,angle,0,0,0);
-      lbDisplay.MiddleButton = 0;
+        set_packet_action(pckt,PckA_SetMapRotation,angle,0,0,0);
+        clear_key_pressed(val);
       }
     }
     if (player->view_mode == PVM_FrontView)
@@ -883,7 +883,7 @@ long get_dungeon_control_action_inputs(void)
           toggle_gui();
       }
       // Middle mouse camera actions for FrontView
-      if (lbDisplay.MiddleButton >= 1)
+      if (is_game_key_pressed(Gkey_SnapCamera, &val, true))
       {
           struct Camera* cam = &player->cameras[CamIV_FrontView];
           struct Packet* pckt = get_packet(my_player_number);
@@ -916,7 +916,7 @@ long get_dungeon_control_action_inputs(void)
             }
         set_packet_action(pckt,PckA_SetMapRotation,angle,0,0,0);
         }
-      lbDisplay.MiddleButton = 0;
+        clear_key_pressed(val);
       }
     }
 
