bba5184f1a17717ebb46065c4ce1f1aeeca723ae
AdamPlenty <58278560+AdamPlenty@users.noreply.github.com>
2020-10-25 18:17:56 +0000

    Update keeperfx.hpp

--- src/keeperfx.hpp
+++ src/keeperfx.hpp
@@ -357,6 +357,7 @@ void startup_network_game(TbBool local);
 void startup_saved_packet_game(void);
 void faststartup_saved_packet_game(void);
 void reinit_level_after_load(void);
+void update_time(void);
 extern TbClockMSec starttime;
 extern TbClockMSec totaltime;
 extern unsigned long seconds;
