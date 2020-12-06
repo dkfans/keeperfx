9bf60c6a7ee7e4050cbdb80fbd60a2f78a9b8f6c
Sim <68837817+SimLV@users.noreply.github.com>
2020-12-06 23:21:48 +0300

    Levels can now support multiple tile sets on the same map (#869)
    When a map#####.slx file exists this is used to get multiple tile sets. The file should be 85x85 bytes. Low 4 bits contains tileset for corresponding slab (0 means default for map) upper 4 bits are reserved for future use.
    Demo map: https://github.com/dkfans/keeperfx/files/5534937/demo.zip
    Adikted update: https://github.com/dkfans/ADiKtEd/pull/12

--- src/engine_textures.h
+++ src/engine_textures.h
@@ -22,18 +22,27 @@
 
 #include "bflib_basics.h"
 #include "globals.h"
+#include "game_legacy.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 /******************************************************************************/
+// Num of known texture files
+#define TEXTURE_FILES_COUNT           16
+
+#define  TEXTURE_LAND_MARKED_LAND     578
+#define  TEXTURE_LAND_MARKED_GOLD     579
+/******************************************************************************/
 DLLIMPORT unsigned char *_DK_block_mem;
-#define block_mem _DK_block_mem
-DLLIMPORT unsigned char *_DK_block_ptrs[592];//[TEXTURE_BLOCKS_COUNT]
-#define block_ptrs _DK_block_ptrs
+DLLIMPORT unsigned char *_DK_block_ptrs[TEXTURE_BLOCKS_COUNT];
 DLLIMPORT long _DK_anim_counter;
 #define anim_counter _DK_anim_counter
 /******************************************************************************/
+
+extern unsigned char block_mem[TEXTURE_FILES_COUNT * TEXTURE_BLOCKS_STAT_COUNT * 32 * 32];
+extern unsigned char *block_ptrs[TEXTURE_FILES_COUNT * TEXTURE_BLOCKS_COUNT];
+extern unsigned char slab_ext_data[85 * 85];
 extern long block_dimension;
 /******************************************************************************/
 void setup_texture_block_mem(void);
