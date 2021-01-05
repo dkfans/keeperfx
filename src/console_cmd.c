c94fb304f17c6242953b086e9af9fcf0941bc471
SimLV <github@iamsim.ru>
2021-01-05 12:14:11 +0300

    testing commands

--- src/console_cmd.c
+++ src/console_cmd.c
@@ -31,6 +31,7 @@
 #include "net_sync.h"
 #include "player_computer.h"
 #include "slab_data.h"
+#include "creature_instances.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -46,7 +47,10 @@ struct GuiBoxOption cmd_comp_checks_data[COMPUTER_CHECKS_COUNT + 1] = { 0 };
 static char cmd_comp_checks_label[COMPUTER_CHECKS_COUNT][COMMAND_WORD_LEN + 8];
 
 struct GuiBoxOption cmd_comp_events_data[COMPUTER_EVENTS_COUNT + 1] = { 0 };
-  static char cmd_comp_events_label[COMPUTER_EVENTS_COUNT][COMMAND_WORD_LEN + 8];
+
+static TbBool script_set_pool(PlayerNumber player_idx, const char *creature, const char *num);
+
+ static char cmd_comp_events_label[COMPUTER_EVENTS_COUNT][COMMAND_WORD_LEN + 8];
 
 static long cmd_comp_procs_click(struct GuiBox *gbox, struct GuiBoxOption *goptn, unsigned char btn, long *args)
 {
@@ -218,9 +222,11 @@ static void cmd_comp_checks(PlayerNumber plyr_idx)
 
 static char *cmd_strtok(char *tail)
 {
+    if (tail == NULL)
+        return NULL;
     char* next = strchr(tail,' ');
     if (next == NULL)
-        return next;
+        return NULL;
     next[0] = '\0';
     next++; // it was space
     while (*next == ' ')
@@ -228,11 +234,53 @@ static char *cmd_strtok(char *tail)
     return next;
 }
 
+static TbBool cmd_magic_instance(const char* creature_str, const char*  slot_str, const char* insance_str)
+{
+    if (creature_str == NULL || slot_str == NULL || insance_str == NULL)
+        return false;
+    int creature = get_id(creature_desc, creature_str);
+    if (creature == -1)
+    {
+        message_add(10, "Invalid creature");
+        return false;
+    }
+    int slot = atoi(slot_str);
+    if (slot < 0 || slot > 9)
+    {
+        message_add(10, "Invalid slot");
+        return false;
+    }
+    int instance = get_id(instance_desc, insance_str);
+    if (instance == -1)
+    {
+        message_add(10, "Invalid instance");
+        return false;
+    }
+    struct CreatureStats* crstat = creature_stats_get(creature);
+    crstat->learned_instance_id[slot] = instance;
+    for (long i = 0; i < THINGS_COUNT; i++)
+    {
+        struct Thing* thing = thing_get(i);
+        if ((thing->alloc_flags & TAlF_Exists) != 0)
+        {
+            if (thing->class_id == TCls_Creature)
+            {
+                creature_increase_available_instances(thing);
+            }
+        }
+    }
+
+    return true;
+}
+
 TbBool cmd_exec(PlayerNumber plyr_idx, char *msg)
 {
     SYNCDBG(2,"Command %d: %s",(int)plyr_idx, msg);
     const char * parstr = msg + 1;
     const char * pr2str = cmd_strtok(msg + 1);
+    const char * pr3str = cmd_strtok((char*) pr2str);
+    const char * pr4str = cmd_strtok((char*) pr3str);
+
     if (strcmp(parstr, "stats") == 0)
     {
       message_add_fmt(plyr_idx, "Now time is %d, last loop time was %d",LbTimerClock(),last_loop_time);
@@ -370,6 +418,9 @@ TbBool cmd_exec(PlayerNumber plyr_idx, char *msg)
             } else
                 message_add_fmt(plyr_idx, "computer assistant is %d", atoi(pr2str));
             return true;
+        } else if (strcmp(parstr, "magic.instance") == 0)
+        {
+            return cmd_magic_instance(pr2str, pr3str, pr4str);
         } else if (strcmp(parstr, "give.trap") == 0)
         {
             if (!is_my_player_number(plyr_idx))
@@ -412,12 +463,36 @@ TbBool cmd_exec(PlayerNumber plyr_idx, char *msg)
                     thing->health>>=1;
                 }
             }
-            return true;
+        } else if (strcmp(parstr, "map.pool") == 0)
+        {
+            return script_set_pool(plyr_idx, pr2str, pr3str);
         }
+        return true;
     }
     return false;
 }
 
+static TbBool script_set_pool(PlayerNumber plyr_idx, const char *creature, const char *str_num)
+{
+    if (creature == NULL)
+        return false;
+    long kind = get_id(creature_desc, creature);
+    if (kind == -1)
+    {
+        if (0 == strcasecmp(creature, "EMPTY"))
+        {
+            clear_creature_pool();
+            return true;
+        }
+        message_add_fmt(10, "Invalid creature");
+        return false;
+    }
+    int num = atoi(str_num);
+    if (num < 0)
+        return false;
+    game.pool.crtr_kind[kind] = num;
+    return true;
+}
 
 #ifdef __cplusplus
 }
