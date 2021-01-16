371cf2e9b9c98ce10552d42401f8cca7768812a2
AdamPlenty <58278560+AdamPlenty@users.noreply.github.com>
2021-01-16 22:32:53 +0000

    Added thing creating and slab placement commands.
    You can place any terrain, and create any thing, or specifically objects or creatures.

--- src/console_cmd.h
+++ src/console_cmd.h
@@ -27,6 +27,8 @@ extern "C" {
 #endif
 
 TbBool cmd_exec(PlayerNumber plyr_idx, char *msg);
+long get_creature_model_for_command(char *msg);
+PlayerNumber get_player_number_for_command(char *msg);
 
 #ifdef __cplusplus
 }
