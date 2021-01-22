0caae47e301460dee6c8fbc66ba3e1812a91a2ac
AdamPlenty <58278560+AdamPlenty@users.noreply.github.com>
2021-01-22 10:41:30 +0000

    Added console commands for thing creating and slab placement (#937)
    New console commands:
    !create.thing [class#] [model#]
    !create.object [object name]
    !create.creature [creature] [level] [amount]* [owner]*
    !create.gold [amount]
    !place.slab [name]
    
    Co-authored-by: Loobinex <Loobinex@users.noreply.github.com>

--- src/console_cmd.h
+++ src/console_cmd.h
@@ -27,6 +27,11 @@ extern "C" {
 #endif
 
 TbBool cmd_exec(PlayerNumber plyr_idx, char *msg);
+long get_creature_model_for_command(char *msg);
+PlayerNumber get_player_number_for_command(char *msg);
+TbBool parameter_is_number(char* parstr);
+char get_trap_number_for_command(char* msg);
+char get_door_number_for_command(char* msg);
 
 #ifdef __cplusplus
 }
