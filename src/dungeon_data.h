dkfans/keeperfx

commit a00fc04ec6d3a46d1356fe569703993bc75e4032

Rewrote DK_bounce_thing_off_wall_at and DK_slide_thing_against_wall_at (#951)

src/thing_physics.c
@@ -26,6 +26,7 @@
 #include "thing_stats.h"
 #include "thing_creature.h"
 #include "thing_list.h"
+#include "thing_navigate.h"
 #include "creature_control.h"
 #include "config_creature.h"
 #include "config_terrain.h"
@@ -39,8 +40,6 @@
 extern "C" {
 #endif
 /******************************************************************************/
-DLLIMPORT void _DK_slide_thing_against_wall_at(struct Thing *thing, struct Coord3d *pos, long a3);
-DLLIMPORT void _DK_bounce_thing_off_wall_at(struct Thing *thing, struct Coord3d *pos, long a3);
 DLLIMPORT long _DK_get_thing_height_at_with_radius(const struct Thing *thing, const struct Coord3d *pos, unsigned long a3);
 /******************************************************************************/
 
@@ -69,12 +68,194 @@ TbBool thing_above_flight_altitude(const struct Thing* thing)
 
 void slide_thing_against_wall_at(struct Thing *thing, struct Coord3d *pos, long a3)
 {
-    _DK_slide_thing_against_wall_at(thing, pos, a3); return;
+   // _DK_slide_thing_against_wall_at(thing, pos, a3); return;
+  unsigned short x_thing;
+  unsigned short sizexy;
+  unsigned short x_pos;
+  unsigned short y_thing;
+  unsigned short y_pos;
+  switch ( blocked_flags )
+  {
+    case SlbBloF_WalledX:
+      x_thing = thing->mappos.x.val;
+      sizexy = (unsigned short)actual_sizexy_to_nav_sizexy_table[thing->clipbox_size_xy] >> 1;
+      x_pos = pos->x.val;
+      if ( x_pos != x_thing )
+      {
+        if ( x_pos > x_thing )
+        {
+          pos->x.val = ((x_pos + sizexy) & 0xFF00) - sizexy - 1;
+          return;
+        }
+        x_thing = (((x_pos - sizexy) & 0xFF00) + sizexy + 256);
+      }
+      pos->x.val = x_thing;
+      break;
+    case SlbBloF_WalledY:
+      y_thing = thing->mappos.y.val;
+      sizexy = (unsigned short)actual_sizexy_to_nav_sizexy_table[thing->clipbox_size_xy] >> 1;
+      y_pos = pos->y.val;
+      if ( y_thing != y_pos )
+      {
+        if ( y_thing < y_pos )
+        {
+          pos->y.val = ((y_pos + sizexy) & 0xFF00) - sizexy - 1;
+          return;
+        }
+        y_thing = ((y_pos - sizexy & 0xFF00) + sizexy + 256);
+      }
+      pos->y.val = y_thing;
+      break;
+    case SlbBloF_WalledX|SlbBloF_WalledY:
+      x_thing = thing->mappos.x.val;
+      sizexy = (unsigned short)actual_sizexy_to_nav_sizexy_table[thing->clipbox_size_xy] >> 1;
+      x_pos = pos->x.val;
+      if ( x_pos != x_thing )
+      {
+        if ( x_pos <= x_thing )
+          x_thing = ((x_pos - sizexy & 0xFF00) + sizexy + 256);
+        else
+          x_thing = (((sizexy + x_pos) & 0xFF00) - sizexy - 1);
+      }
+      y_thing = thing->mappos.y.val;
+      y_pos = pos->y.val;
+      if ( y_pos != y_thing )
+      {
+        if ( y_pos <= y_thing )
+          y_thing = ((y_pos - sizexy & 0xFF00) + sizexy + 256);
+        else
+          y_thing = (((sizexy + y_pos) & 0xFF00) - sizexy - 1);
+      }
+      pos->x.val = x_thing;
+      pos->y.val = y_thing;
+      break;
+    case SlbBloF_WalledZ:
+      pos->z.val = get_slide_z_coord(thing, pos);
+      break;
+    case SlbBloF_WalledZ|SlbBloF_WalledX:
+      x_thing = thing->mappos.x.val;
+      sizexy = (unsigned short)actual_sizexy_to_nav_sizexy_table[thing->clipbox_size_xy] >> 1;
+      x_pos = pos->x.val;
+      if ( x_pos != x_thing )
+      {
+        if ( x_pos <= x_thing )
+          x_thing = (((x_pos - sizexy) & 0xFF00) + sizexy + 256);
+        else
+          x_thing = (((sizexy + x_pos) & 0xFF00) - sizexy - 1);
+      }
+      pos->x.val = x_thing;
+      pos->z.val = get_slide_z_coord(thing, pos);
+      break;
+    case SlbBloF_WalledZ|SlbBloF_WalledY:
+      y_thing = thing->mappos.y.val;
+      sizexy = (unsigned short)actual_sizexy_to_nav_sizexy_table[thing->clipbox_size_xy] >> 1;
+      y_pos = pos->y.val;
+      if ( y_thing != y_pos )
+      {
+        if ( y_thing >= y_pos )
+          y_thing = ((y_pos - sizexy & 0xFF00) + sizexy + 256);
+        else
+          y_thing = (((y_pos + sizexy) & 0xFF00) - sizexy - 1);
+      }
+      pos->y.val = y_thing;
+      pos->z.val = get_slide_z_coord(thing, pos);
+      break;
+    case SlbBloF_WalledX|SlbBloF_WalledY|SlbBloF_WalledZ:
+      x_thing = thing->mappos.x.val;
+      sizexy = (unsigned short)actual_sizexy_to_nav_sizexy_table[thing->clipbox_size_xy] >> 1;
+      x_pos = pos->x.val;
+      if ( x_pos != x_thing )
+      {
+        if ( x_pos <= x_thing )
+          x_thing = (((x_pos - sizexy) & 0xFF00) + sizexy + 256);
+        else
+          x_thing = (((sizexy + x_pos) & 0xFF00) - sizexy - 1);
+      }
+      y_pos = pos->y.val;
+      y_thing = thing->mappos.y.val;
+      if ( y_pos != y_thing )
+      {
+        if ( y_pos <= y_thing )
+          y_thing = (((y_pos - sizexy) & 0xFF00) + sizexy + 256);
+        else
+          y_thing = (((sizexy + y_pos) & 0xFF00) - sizexy - 1);
+      }
+      pos->x.val = x_thing;
+      pos->y.val = y_thing;
+      pos->z.val = get_slide_z_coord(thing, pos);
+      break;
+    default:
+      return;
+  }
 }
 
-void bounce_thing_off_wall_at(struct Thing *thing, struct Coord3d *pos, long a3)
+void bounce_thing_off_wall_at(struct Thing *thing, struct Coord3d *pos, long blocked_flags)
 {
-    _DK_bounce_thing_off_wall_at(thing, pos, a3); return;
+   // _DK_bounce_thing_off_wall_at(thing, pos, a3); return;
+  short x = (short)thing->veloc_base.x.val;
+  short y = (short)thing->veloc_base.y.val;
+  short z = (short)thing->veloc_base.z.val;
+  int i;
+  switch ( blocked_flags )
+  {
+    case SlbBloF_WalledX:
+      pos->x.val = thing->mappos.x.val;
+      thing->veloc_base.x.val = -(short)(x * thing->field_22 / 128);
+      i = 256 - thing->field_23;
+      thing->veloc_base.y.val = i * (short)thing->veloc_base.y.val / 256;
+      thing->veloc_base.z.val = i * (short)thing->veloc_base.z.val / 256;
+      break;
+    case SlbBloF_WalledY:
+      pos->y.val = thing->mappos.y.val;
+      thing->veloc_base.y.val = -(short)(y * thing->field_22 / 128);
+      i = 256 - thing->field_23;
+      thing->veloc_base.x.val = i * (short)thing->veloc_base.x.val / 256;
+      thing->veloc_base.z.val = i * (short)thing->veloc_base.z.val / 256;
+      break;
+    case SlbBloF_WalledX|SlbBloF_WalledY:
+      pos->x.val = thing->mappos.x.val;
+      pos->y.val = thing->mappos.y.val;
+      i = thing->field_22;
+      thing->veloc_base.x.val = -(short)(i * x / 128);
+      thing->veloc_base.y.val = -(short)(i * y / 128);
+      break;
+    case SlbBloF_WalledZ:
+      pos->z.val = thing->mappos.z.val;
+      thing->veloc_base.z.val = -(short)(z * thing->field_22 / 128);
+      i = 256 - thing->field_23;
+      thing->veloc_base.x.val = i * (short)thing->veloc_base.x.val / 256;
+      thing->veloc_base.y.val = i * (short)thing->veloc_base.y.val / 256;
+      break;
+    case SlbBloF_WalledZ|SlbBloF_WalledX:
+      pos->z.val = thing->mappos.z.val;
+      pos->x.val = thing->mappos.x.val;
+      i = thing->field_22;
+      thing->veloc_base.x.val = -(short)(i * x / 128);
+      thing->veloc_base.z.val = -(short)(i * z / 128);
+      break;
+    case SlbBloF_WalledZ|SlbBloF_WalledY:
+      pos->y.val = thing->mappos.y.val;
+      pos->z.val = thing->mappos.z.val;
+      i = thing->field_22;
+      thing->veloc_base.y.val = -(short)(i * y / 128);
+      int n = i * y;
+      int j = thing->field_23;
+      int k = (short)thing->veloc_base.x.val;
+      thing->veloc_base.z.val = -(short)(n / 128);
+      thing->veloc_base.x.val = k * (256 - j) / 256;
+      break;
+    case SlbBloF_WalledX|SlbBloF_WalledY|SlbBloF_WalledZ:
+      pos->x.val = thing->mappos.x.val;
+      pos->y.val = thing->mappos.y.val;
+      pos->z.val = thing->mappos.z.val;
+      i = thing->field_22;
+      thing->veloc_base.x.val = -(short)(i * x / 128);
+      thing->veloc_base.y.val = -(short)(i * y / 128);
+      thing->veloc_base.z.val = -(short)(i * z / 128);
+      break;
+    default:
+      return;
+  }
 }
 
 void remove_relevant_forces_from_thing_after_slide(struct Thing *thing, struct Coord3d *pos, long a3)
@@ -667,6 +848,28 @@ TbBool thing_is_exempt_from_z_axis_clipping(const struct Thing *thing)
         return (!shot_is_boulder(thing));
     }
     return false;
+}
+
+unsigned short get_slide_z_coord(const struct Thing *thing, const struct Coord3d *pos)
+// function at 451700. Original name unknown.
+{
+  unsigned short clipbox_size = thing->clipbox_size_yz;
+  long height = get_ceiling_height_above_thing_at(thing, pos);
+  short z_thing = (short)thing->mappos.z.val;
+  short z_pos = (short)pos->z.val;
+  if ( (height - 1) <= (z_pos + clipbox_size) )
+  {
+    return (height - clipbox_size) - 1;
+  }
+  if ( z_pos == z_thing )
+  {
+    return pos->z.val;
+  }
+  if ( z_pos < z_thing )
+  {
+    return (z_pos & 0xFF00) + 256;
+  }
+  return ((((z_pos + clipbox_size) & 0xFFFFFF00) - clipbox_size) - 1);
 }
 /******************************************************************************/
 #ifdef __cplusplus

src/ariadne.h
@@ -226,6 +226,7 @@ DLLIMPORT struct Path _DK_bak_path;
 #pragma pack()
 /******************************************************************************/
 extern unsigned char const actual_sizexy_to_nav_block_sizexy_table[];
+extern unsigned long const actual_sizexy_to_nav_sizexy_table[];
 extern struct Path fwd_path;
 extern struct Path bak_path;
 /******************************************************************************/

src/thing_effects.c
@@ -736,7 +736,7 @@ void process_spells_affected_by_effect_elements(struct Thing *thing)
 void move_effect_blocked(struct Thing *thing, struct Coord3d *prev_pos, struct Coord3d *next_pos)
 {
     struct EffectElementStats* eestat = get_effect_element_model_stats(thing->model);
-    unsigned long blocked_flags = get_thing_blocked_flags_at(thing, next_pos);
+    long blocked_flags = get_thing_blocked_flags_at(thing, next_pos);
     slide_thing_against_wall_at(thing, next_pos, blocked_flags);
     if ( ((blocked_flags & SlbBloF_WalledZ) != 0) && eestat->field_15 && eestat->field_22 )
     {

src/thing_physics.h
@@ -59,8 +59,8 @@ TbBool things_collide_while_first_moves_to(const struct Thing *firstng, const st
 TbBool cross_x_boundary_first(const struct Coord3d *pos1, const struct Coord3d *pos2);
 TbBool cross_y_boundary_first(const struct Coord3d *pos1, const struct Coord3d *pos2);
 
-void slide_thing_against_wall_at(struct Thing *thing, struct Coord3d *pos, long a3);
-void bounce_thing_off_wall_at(struct Thing *thing, struct Coord3d *pos, long a3);
+void slide_thing_against_wall_at(struct Thing *thing, struct Coord3d *pos, long blocked_flags);
+void bounce_thing_off_wall_at(struct Thing *thing, struct Coord3d *pos, long blocked_flags);
 TbBool get_thing_next_position(struct Coord3d *pos, const struct Thing *thing);
 void remove_relevant_forces_from_thing_after_slide(struct Thing *thing, struct Coord3d *pos, long a3);
 void apply_transitive_velocity_to_thing(struct Thing *thing, struct ComponentVector *veloc);
@@ -78,6 +78,7 @@ long get_ceiling_height_above_thing_at(const struct Thing *thing, const struct C
 void get_floor_and_ceiling_height_under_thing_at(const struct Thing *thing,
     const struct Coord3d *pos, MapCoord *floor_height_cor, MapCoord *ceiling_height_cor);
 TbBool thing_is_exempt_from_z_axis_clipping(const struct Thing *thing);
+unsigned short get_slide_z_coord(const struct Thing *thing, const struct Coord3d *pos);
 /******************************************************************************/
 #ifdef __cplusplus
 }

src/thing_shots.c
@@ -217,7 +217,7 @@ TbBool give_gold_to_creature_or_drop_on_map_when_digging(struct Thing *creatng, 
     return true;
 }
 
-void process_dig_shot_hit_wall(struct Thing *thing, unsigned long blocked_flags)
+void process_dig_shot_hit_wall(struct Thing *thing, long blocked_flags)
 {
     MapSubtlCoord stl_x;
     MapSubtlCoord stl_y;
@@ -364,7 +364,7 @@ TbBool shot_hit_wall_at(struct Thing *shotng, struct Coord3d *pos)
     struct Thing* efftng = INVALID_THING;
     TbBool shot_explodes = 0;
     struct ShotConfigStats* shotst = get_shot_model_stats(shotng->model);
-    unsigned long blocked_flags = get_thing_blocked_flags_at(shotng, pos);
+    long blocked_flags = get_thing_blocked_flags_at(shotng, pos);
     if (shotst->model_flags & ShMF_Digging)
     {
         process_dig_shot_hit_wall(shotng, blocked_flags);

commit 0db3d66cbdead017eb069f75ec3985ae31b066c6

One click interface conflict fix (#983)
* Rudimentary GUI Layer support
* Fixes #836 Scrolling message and camera
* Fixes building bridges being unable to be done without rotating camera in ctrl mode
* Fixed build error introduced in a00fc04e

Co-authored-by: Loobinex <Loobinex@users.noreply.github.com>

src/dungeon_data.h
@@ -307,6 +307,7 @@ struct DungeonAdd
     unsigned char         creature_entrance_level;
     unsigned long         evil_creatures_converted;
     unsigned long         good_creatures_converted;
+    TbBool                painter_build_mode; // this is not ideal, but I do not know how else to track what I need
 };
 /******************************************************************************/
 extern struct Dungeon bad_dungeon;

src/front_input.c
@@ -77,6 +77,9 @@ unsigned short const zoom_key_room_order[] =
 
 KEEPERSPEECH_EVENT last_speech_event;
 
+// define the current GUI layer as the default
+struct GuiLayer gui_layer = {GuiLayer_Default};
+
 /******************************************************************************/
 void get_dungeon_control_nonaction_inputs(void);
 void get_creature_control_nonaction_inputs(void);
@@ -87,6 +90,76 @@ short get_bookmark_inputs(void);
 }
 #endif
 /******************************************************************************/
+long get_current_gui_layer()
+{
+    return gui_layer.current_gui_layer;
+}
+
+TbBool set_current_gui_layer(long layer_id)
+{
+    gui_layer.current_gui_layer = layer_id;
+    return true;
+}
+
+TbBool check_current_gui_layer(long layer_id)
+{
+    // Check the current gui layer against the one passed as a parameter
+    // Also checks if the passed layer_id is the parent of the current gui layer
+    
+    // This is just a basic example, GuiLayer objects should be created with parent properties etc etc
+    
+    if (gui_layer.current_gui_layer == layer_id)
+    {
+        return true;
+    }
+    else
+    {
+        // check children
+        switch (layer_id)
+        {
+            case GuiLayer_OneClick:
+                switch (gui_layer.current_gui_layer)
+                {
+                    case GuiLayer_OneClickBridgeBuild:
+                        return true;
+                        break;
+                    default:
+                        return false;
+                }
+                break;
+            default:
+                return false;
+        }
+    }
+}
+
+void update_gui_layer()
+{
+    // Determine the current/correct GUI Layer to use at this moment
+
+    struct PlayerInfo* player = get_my_player();
+    struct DungeonAdd *dungeonadd = get_dungeonadd(player->id_number);
+    if ( ((player->work_state == PSt_Sell) || (player->work_state == PSt_BuildRoom))  &&
+         (is_game_key_pressed(Gkey_BestRoomSpace, NULL, true) || is_game_key_pressed(Gkey_SquareRoomSpace, NULL, true)) )
+    {
+        if (dungeonadd->painter_build_mode)
+        {
+            // Is the user in "one-click bridge building" mode
+            set_current_gui_layer(GuiLayer_OneClickBridgeBuild);
+        }
+        else
+        {
+            // Is the user in "one-click" mode (i.e. they are in the build/sell player state, and are pressing the square/automagic button)
+            set_current_gui_layer(GuiLayer_OneClick);
+        }
+    }
+    else
+    {
+        // For now this is equivilent to "old input behaviour"
+        set_current_gui_layer(GuiLayer_Default);
+    }
+}
+
 short game_is_busy_doing_gui_string_input(void)
 {
   return (input_button != NULL);
@@ -107,6 +180,21 @@ int is_game_key_pressed(long key_id, long *val, TbBool ignore_mods)
   if (val != NULL)
   {
     *val = settings.kbkeys[key_id].code;
+  }
+  if (get_current_gui_layer() == GuiLayer_OneClickBridgeBuild)
+  {
+    if ( (key_id == Gkey_RotateMod) && (
+         (settings.kbkeys[Gkey_RotateMod].code == settings.kbkeys[Gkey_BestRoomSpace].code) ||
+         (settings.kbkeys[Gkey_RotateMod].code == settings.kbkeys[Gkey_SquareRoomSpace].code) ) )
+    {
+      return 0;
+    }
+    if ( (key_id == Gkey_SpeedMod) && (
+         (settings.kbkeys[Gkey_SpeedMod].code == settings.kbkeys[Gkey_BestRoomSpace].code) ||
+         (settings.kbkeys[Gkey_SpeedMod].code == settings.kbkeys[Gkey_SquareRoomSpace].code) ) )
+    {
+      return 0;
+    }
   }
   if ((key_id == Gkey_RotateMod) || (key_id == Gkey_SpeedMod) || (key_id == Gkey_CrtrContrlMod) || (key_id == Gkey_CrtrQueryMod))
   {
@@ -1572,7 +1660,7 @@ void get_isometric_view_nonaction_inputs(void)
     if (speed_pressed != 0)
       pckt->field_10 |= PCAdV_SpeedupPressed;
     TbBool no_mods = false;
-    if ((rotate_pressed != 0) || (speed_pressed != 0))
+    if ((rotate_pressed != 0) || (speed_pressed != 0) || (check_current_gui_layer(GuiLayer_OneClick)))
       no_mods = true;
 
     get_isometric_or_front_view_mouse_inputs(pckt,rotate_pressed,speed_pressed);
@@ -1648,7 +1736,7 @@ void get_front_view_nonaction_inputs(void)
     int rotate_pressed = is_game_key_pressed(Gkey_RotateMod, NULL, true);
     int speed_pressed = is_game_key_pressed(Gkey_SpeedMod, NULL, true);
     TbBool no_mods = false;
-    if ((rotate_pressed != 0) || (speed_pressed != 0))
+    if ((rotate_pressed != 0) || (speed_pressed != 0) || (check_current_gui_layer(GuiLayer_OneClick)))
       no_mods = true;
 
     if ((player->allocflags & PlaF_Unknown10) != 0)
@@ -2189,6 +2277,7 @@ void input(void)
 
     update_mouse();
     update_key_modifiers();
+    update_gui_layer();
 
     if (KeeperSpeechPopEvent(&last_speech_event)) {
       last_speech_event.type = KS_UNUSED;

src/front_input.h
@@ -77,6 +77,20 @@ enum TbButtonFrontendFlags {
     LbBFeF_NoTooltip    = 0x8000,
 };
 
+// Rudimentary GUI Layer support
+// Add layers to this enum to distinguish between input layers
+// This allows conflicting use of the same input to be resolved sensibly
+// e.g. `GuiLayer_OneClick` is supposed to signify that the user is in "one-click mode"
+enum GuiLayers {
+    GuiLayer_Default  = 0,
+    GuiLayer_OneClick,
+    GuiLayer_OneClickBridgeBuild,
+};
+
+struct GuiLayer {
+    long current_gui_layer;
+};
+
 struct GuiMenu;
 struct GuiButton;
 
@@ -95,6 +109,8 @@ short game_is_busy_doing_gui_string_input(void);
 short get_gui_inputs(short gameplay_on);
 extern unsigned short const zoom_key_room_order[];
 TbBool check_if_mouse_is_over_button(const struct GuiButton *gbtn);
+long get_current_gui_layer();
+TbBool check_current_gui_layer(long layer_id);
 
 /******************************************************************************/
 #ifdef __cplusplus

src/frontend.cpp
@@ -793,9 +793,12 @@ void maintain_scroll_up(struct GuiButton *gbtn)
     //_DK_maintain_scroll_up(gbtn);
     scrollwnd = (struct TextScrollWindow *)gbtn->content;
     gbtn->flags ^= (gbtn->flags ^ LbBtnF_Enabled * (scrollwnd->start_y < 0)) & LbBtnF_Enabled;
-    if (wheel_scrolled_up & lbKeyOn[KC_LCONTROL])
+    if (!check_current_gui_layer(GuiLayer_OneClick))
     {
-        scrollwnd->action = 1;
+        if (wheel_scrolled_up & lbKeyOn[KC_LCONTROL])
+        {
+            scrollwnd->action = 1;
+        }
     }
 }
 
@@ -806,9 +809,12 @@ void maintain_scroll_down(struct GuiButton *gbtn)
     scrollwnd = (struct TextScrollWindow *)gbtn->content;
     gbtn->flags ^= (gbtn->flags ^ LbBtnF_Enabled
         * (scrollwnd->window_height - scrollwnd->text_height + 2 < scrollwnd->start_y)) & LbBtnF_Enabled;
-    if (wheel_scrolled_down & lbKeyOn[KC_LCONTROL])
+    if (!check_current_gui_layer(GuiLayer_OneClick))
     {
-        scrollwnd->action = 2;
+        if (wheel_scrolled_down & lbKeyOn[KC_LCONTROL])
+        {
+            scrollwnd->action = 2;
+        }
     }
 }
 

src/roomspace.c
@@ -292,11 +292,14 @@ void get_dungeon_build_user_roomspace(PlayerNumber plyr_idx, RoomKind rkind, Map
     MapSlabCoord slb_x = subtile_slab_fast(stl_x);
     MapSlabCoord slb_y = subtile_slab_fast(stl_y);
     int width = 1, height = 1; // 1x1 slabs
+    struct DungeonAdd *dungeonadd = get_dungeonadd(player->id_number);
+    dungeonadd->painter_build_mode = 0;
     if (rkind == RoK_BRIDGE)
     {
         reset_dungeon_build_room_ui_variables();
         if (drag_check) // Enable "paint mode" if Ctrl or Shift are held
         {
+            dungeonadd->painter_build_mode = 1; // Enable GuiLayer_OneClickBridgeBuild layer
             (*mode) = drag_placement_mode;
         }
     }

src/thing_physics.c
@@ -66,7 +66,7 @@ TbBool thing_above_flight_altitude(const struct Thing* thing)
     return (thing->mappos.z.val > floor_height + 19 * NORMAL_FLYING_ALTITUDE / 17);
 }
 
-void slide_thing_against_wall_at(struct Thing *thing, struct Coord3d *pos, long a3)
+void slide_thing_against_wall_at(struct Thing *thing, struct Coord3d *pos, long blocked_flags)
 {
    // _DK_slide_thing_against_wall_at(thing, pos, a3); return;
   unsigned short x_thing;