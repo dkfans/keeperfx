222b72e5966e0dad2e10a87c0348bdb99bddac5d
Loobinex <Loobinex@users.noreply.github.com>
2021-04-05 18:03:41 +0200

    Added sounds for claiming in possession
    Claiming path or rooms have different sound.

--- src/creature_instances.c
+++ src/creature_instances.c
@@ -595,12 +595,19 @@ long instf_destroy(struct Thing *creatng, long *param)
     struct SlabMap* slb = get_slabmap_block(slb_x, slb_y);
     struct Room* room = room_get(slb->room_index);
     long prev_owner = slabmap_owner(slb);
+    struct PlayerInfo* player;
+    player = get_my_player();
 
     if ( !room_is_invalid(room) && (prev_owner != creatng->owner) )
     {
         if (room->health > 1)
         {
             room->health--;
+
+            if ((player->view_type == PVT_CreatureContrl) || (player->view_type == PVT_CreaturePasngr))
+            {
+                thing_play_sample(creatng, 5 + UNSYNC_RANDOM(1), 200, 0, 3, 0, 2, FULL_LOUDNESS);
+            }
             return 0;
         }
         clear_dig_on_room_slabs(room, creatng->owner);
@@ -621,11 +628,19 @@ long instf_destroy(struct Thing *creatng, long *param)
     if (slb->health > 1)
     {
         slb->health--;
+        if ((player->view_type == PVT_CreatureContrl) || (player->view_type == PVT_CreaturePasngr))
+        {
+            thing_play_sample(creatng, 66 + UNSYNC_RANDOM(2), 120 + UNSYNC_RANDOM(30), 0, 3, 0, 2, FULL_LOUDNESS);
+        }
         return 0;
     }
     if (prev_owner != game.neutral_player_num) {
         struct Dungeon* prev_dungeon = get_dungeon(prev_owner);
         prev_dungeon->lvstats.territory_lost++;
+    }
+    if ((player->view_type == PVT_CreatureContrl) || (player->view_type == PVT_CreaturePasngr))
+    {
+        thing_play_sample(creatng, 66 + UNSYNC_RANDOM(2), 120 + UNSYNC_RANDOM(30), 0, 3, 0, 2, FULL_LOUDNESS);
     }
     decrease_dungeon_area(prev_owner, 1);
     neutralise_enemy_block(creatng->mappos.x.stl.num, creatng->mappos.y.stl.num, creatng->owner);
@@ -859,7 +874,6 @@ long instf_reinforce(struct Thing *creatng, long *param)
         if (!S3DEmitterIsPlayingSample(creatng->snd_emitter_id, 63, 0))
         {
             struct PlayerInfo* player;
-            //_DK_reset_gui_based_on_player_mode();
             player = get_my_player();
             if ((player->view_type == PVT_CreatureContrl) || (player->view_type == PVT_CreaturePasngr))
             {
