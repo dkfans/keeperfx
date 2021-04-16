dkfans/keeperfx

commit 00284a54e97e1dd489d137c7d44d61930046bd13

RMB + SHIFT fixes
when holding RMB + SHIFT:
tag/untag mode is set when the player first clicks RMB (it uses the starting slab for the dragged boundbox)

whilst holding RMB + SHIFT:
can now untag when starting from a slab that is undiggable highslab - if the first diggable slab the player hovers over after clicking on a undiggable highslab is tagged, then the cursor switches to untag mode

src/roomspace.c
@@ -517,7 +517,7 @@ void get_dungeon_highlight_user_roomspace(PlayerNumber plyr_idx, MapSubtlCoord s
     if ((pckt->control_flags & PCtr_LBtnHeld) == PCtr_LBtnHeld) // highlight "paint mode" enabled
     {
         dungeonadd->one_click_lock_cursor = 1;
-        untag_mode = render_roomspace.untag_mode;
+        untag_mode = render_roomspace.untag_mode; // get tag/untag mode from the slab that was clicked (before the user started holding mouse button)
     }
     else // user is hovering the mouse cursor
     {
@@ -531,6 +531,7 @@ void get_dungeon_highlight_user_roomspace(PlayerNumber plyr_idx, MapSubtlCoord s
         if ((pckt->control_flags & PCtr_HeldAnyButton) != 0) 
         {
             dungeonadd->one_click_lock_cursor = 1; // Allow click and drag over low slabs (if clicked on high slab)
+            untag_mode = render_roomspace.untag_mode; // get tag/untag mode from the slab that was clicked (before the user started holding mouse button)
             one_click_mode_exclusive = true; // Block camera zoom/rotate if Ctrl is held with LMB/RMB
             drag_start_x = render_roomspace.drag_start_x; // if we are dragging, get the starting coords from the slab the player clicked on
             drag_start_y = render_roomspace.drag_start_y;
@@ -573,7 +574,7 @@ void get_dungeon_highlight_user_roomspace(PlayerNumber plyr_idx, MapSubtlCoord s
     current_roomspace.untag_mode = untag_mode;
     current_roomspace.one_click_mode_exclusive = one_click_mode_exclusive;
     current_roomspace = check_roomspace_for_diggable_slabs(current_roomspace, plyr_idx);
-    if (dungeonadd->one_click_lock_cursor == 0 && !untag_mode && current_roomspace.slab_count == 0 && ((current_roomspace.width > 1) || (current_roomspace.height > 1)))
+    if (((dungeonadd->one_click_lock_cursor == 0) || current_roomspace.drag_mode) && !untag_mode && current_roomspace.slab_count == 0 && ((current_roomspace.width > 1) || (current_roomspace.height > 1)))
     {
         // if highlight roomspace is empty (and we aren't in paint mode, and we were trying to tag slabs)
         // then check for slabs to untag instead, and if some are found, give that option to the player

commit a588052602444bc772c81ffc6e72326a01fdcacd

bugfix
Boundbox colour was wrong in LMB+SHIFT mode, now fixed.

Was caused by 00284a54

src/roomspace.c
@@ -574,7 +574,8 @@ void get_dungeon_highlight_user_roomspace(PlayerNumber plyr_idx, MapSubtlCoord s
     current_roomspace.untag_mode = untag_mode;
     current_roomspace.one_click_mode_exclusive = one_click_mode_exclusive;
     current_roomspace = check_roomspace_for_diggable_slabs(current_roomspace, plyr_idx);
-    if (((dungeonadd->one_click_lock_cursor == 0) || current_roomspace.drag_mode) && !untag_mode && current_roomspace.slab_count == 0 && ((current_roomspace.width > 1) || (current_roomspace.height > 1)))
+    if (((dungeonadd->one_click_lock_cursor == 0) || (current_roomspace.drag_mode && ((pckt->control_flags & PCtr_RBtnHeld) == PCtr_RBtnHeld) && ((pckt->control_flags & PCtr_LBtnHeld) == 0))) 
+        && !untag_mode && current_roomspace.slab_count == 0 && ((current_roomspace.width > 1) || (current_roomspace.height > 1)))
     {
         // if highlight roomspace is empty (and we aren't in paint mode, and we were trying to tag slabs)
         // then check for slabs to untag instead, and if some are found, give that option to the player