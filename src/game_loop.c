1da372c030b3409cf6bcc8e498d3e13c7c8233d3
Loobinex <Loobinex@users.noreply.github.com>
2021-07-14 14:31:15 +0200

    Added game loop source file

---
+++ src/game_loop.c
@@ -0,0 +1,25 @@
+/******************************************************************************/
+// Free implementation of Bullfrog's Dungeon Keeper strategy game.
+/******************************************************************************/
+/** @file game_loop.c
+ *     Module which contains functions from the main game loop.
+ * @author   Loobinex
+ * @date     14 Jul 2021
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/******************************************************************************/
+/******************************************************************************/
+#ifdef __cplusplus
+}
+#endif
+/******************************************************************************/
+/******************************************************************************/
