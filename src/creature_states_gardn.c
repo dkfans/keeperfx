de93fd76e99627df6b8a38b4d5032f9686d07652
Loobinex <Loobinex@users.noreply.github.com>
2021-07-15 22:52:29 +0200

    restored food related field

--- src/creature_states_gardn.c
+++ src/creature_states_gardn.c
@@ -185,8 +185,8 @@ void person_search_for_food_again(struct Thing *creatng, struct Room *room)
         cctrl->stateblock_flags |= CCSpl_ChickenRel;
     } else
     {
-        near_food_tng->byte_15 = 255;
-        near_food_tng->hit_type = 127;
+        near_food_tng->food.byte_15 = 255;
+        near_food_tng->food.byte_16 = 127;
     }
     creatng->continue_state = CrSt_CreatureEatingAtGarden;
     cctrl = creature_control_get_from_thing(creatng);
