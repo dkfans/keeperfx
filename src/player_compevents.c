6b02faf18ac8c9244aff5ee2ff70e16df4e1edcc
Loobinex <Loobinex@users.noreply.github.com>
2021-10-13 23:52:35 +0200

    Nested it all based on room availability

--- src/player_compevents.c
+++ src/player_compevents.c
@@ -493,19 +493,22 @@ long computer_event_rebuild_room(struct Computer2* comp, struct ComputerEvent* c
 {
     SYNCDBG(18, "Starting");
     long cproc_idx = 0;
-    for (int i = 0; i < COMPUTER_PROCESSES_COUNT + 1; i++)
+    if (computer_get_room_kind_free_capacity(comp, event->target) == 0)
     {
-        struct ComputerProcess* cproc = &comp->processes[i];
-        if ((cproc->flags & ComProc_Unkn0002) != 0)
-            break;
-        struct Dungeon* dungeon = comp->dungeon;
-        if ((cproc->func_check == &computer_check_any_room) && (cproc->confval_4 == event->target) && (computer_get_room_kind_free_capacity(comp, event->target) == 0))
+        for (int i = 0; i < COMPUTER_PROCESSES_COUNT + 1; i++)
         {
-            SYNCDBG(8, "Resetting process for player %d to build room",(int)dungeon->owner);
-            cproc->flags &= ~ComProc_Unkn0008;
-            cproc->flags &= ~ComProc_Unkn0001;
-            cproc->last_run_turn = 0;
-            cproc_idx = 1;
+            struct ComputerProcess* cproc = &comp->processes[i];
+            if ((cproc->flags & ComProc_Unkn0002) != 0)
+                break;
+            struct Dungeon* dungeon = comp->dungeon;
+            if ((cproc->func_check == &computer_check_any_room) && (cproc->confval_4 == event->target))
+            {
+                SYNCDBG(8, "Resetting process for player %d to build room",(int)dungeon->owner);
+                cproc->flags &= ~ComProc_Unkn0008;
+                cproc->flags &= ~ComProc_Unkn0001;
+                cproc->last_run_turn = 0;
+                cproc_idx = 1;
+            }
         }
     }
     return cproc_idx;
