dkfans/keeperfx

commit af18704544e9cb960db57399c461efc09200f4bb

Moving books reworked (#1279)
- When selling libraries you took over, you no longer keep books you already have a copy of
- Fixed a recent crash in selling libraries with books
- Books placed on level start are now positioned high above the floor (as books you get through other means).
- Made sure to stop books from getting lost when there's free capacity
- Made sure that you can research to get books back you lose through limited capacity.

Co-authored-by: AdamPlenty <58278560+AdamPlenty@users.noreply.github.com>

src/thing_objects.c
@@ -1792,13 +1792,16 @@ TngUpdateRet move_object(struct Thing *thing)
             long blocked_flags = get_thing_blocked_flags_at(thing, &pos);
             if (blocked_flags & SlbBloF_WalledZ)
             {
-                struct Dungeon* dungeon = get_dungeon(thing->owner);
-                if (dungeon->sight_casted_thing_idx != thing->index)
+                if (thing->owner != game.neutral_player_num)
                 {
-                    if (!find_free_position_on_slab(thing, &pos))
+                    struct Dungeon* dungeon = get_dungeon(thing->owner);
+                    if (dungeon->sight_casted_thing_idx != thing->index)
                     {
-                        SYNCDBG(7, "Found no free position next to (%ld,%ld) due to blocked flag %d. Move to valid position.",pos.x.val,pos.y.val, blocked_flags);
-                        move_creature_to_nearest_valid_position(thing);
+                        if (!find_free_position_on_slab(thing, &pos))
+                        {
+                            SYNCDBG(7, "Found no free position next to (%ld,%ld) due to blocked flag %d. Move to valid position.", pos.x.val, pos.y.val, blocked_flags);
+                            move_creature_to_nearest_valid_position(thing);
+                        }
                     }
                 }
             }

src/creature_states.c
@@ -2390,6 +2390,29 @@ TbBool find_random_valid_position_for_thing_in_room_avoiding_object(struct Thing
     // Get the selected index
     while (i != 0)
     {
+        struct DungeonAdd *dungeonadd = get_dungeonadd(room->owner);
+        if (dungeonadd->roomspace.is_active)
+        {
+            MapSlabCoord slb_x = slb_num_decode_x(i);
+            MapSlabCoord slb_y = slb_num_decode_y(i);
+            for (long j = 0; j < SMALL_AROUND_LENGTH; j++)
+            {
+                MapSlabCoord aslb_x = slb_x + small_around[j].delta_x;
+                MapSlabCoord aslb_y = slb_y + small_around[j].delta_y;
+                struct Room* nroom = slab_room_get(aslb_x, aslb_y);
+                if (!room_is_invalid(nroom))
+                {
+                    if ( (nroom->kind == room->kind) && (nroom->owner == room->owner) )
+                    {
+                        if (nroom->index != room->index)
+                        {
+                            ERRORLOG("Tried to find free position in %s %d but ended up looking in %s %d instead", room_code_name(room->kind),room->index,room_code_name(nroom->kind), nroom->index);
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
         // Per room tile code
         if (n >= selected)
         {
@@ -2399,20 +2422,27 @@ TbBool find_random_valid_position_for_thing_in_room_avoiding_object(struct Thing
         i = get_next_slab_number_in_room(i);
         n++;
     }
-    if (i == 0) {
+    if (i == 0) 
+    {
+        if (n < room->slabs_count)
+        {
+            WARNLOG("Number of slabs in %s (%d) is smaller than count (%d)",room_code_name(room->kind), n, room->slabs_count);
+        }
         n = 0;
         i = room->slabs_list;
-        WARNLOG("Amount of slabs in %s is smaller than count",room_code_name(room->kind));
     }
     // Sweep rooms starting on that index
     unsigned long k = 0;
+    long nround;
+    MapSubtlCoord stl_x;
+    MapSubtlCoord stl_y;
     while (i != 0)
     {
-        MapSubtlCoord stl_x = slab_subtile(slb_num_decode_x(i), 0);
-        MapSubtlCoord stl_y = slab_subtile(slb_num_decode_y(i), 0);
+        stl_x = slab_subtile(slb_num_decode_x(i), 0);
+        stl_y = slab_subtile(slb_num_decode_y(i), 0);
         // Per room tile code
         MapSubtlCoord start_stl = CREATURE_RANDOM(thing, AROUND_TILES_COUNT);
-        for (long nround = 0; nround < AROUND_TILES_COUNT; nround++)
+        for (nround = 0; nround < AROUND_TILES_COUNT; nround++)
         {
             MapSubtlCoord x = start_stl % 3 + stl_x;
             MapSubtlCoord y = start_stl / 3 + stl_y;
@@ -2446,7 +2476,42 @@ TbBool find_random_valid_position_for_thing_in_room_avoiding_object(struct Thing
             break;
         }
     }
-    ERRORLOG("Could not find valid RANDOM point in %s for %s",room_code_name(room->kind),thing_model_name(thing));
+    if (room->used_capacity <= room->total_capacity)
+    {
+        SYNCLOG("Could not find valid random point in %s %d for %s. Attempting thorough check.",room_code_name(room->kind),room->index,thing_model_name(thing));
+        k = 0;
+        for (i = room->slabs_list; (i != 0); i = get_next_slab_number_in_room(i))
+        {
+            stl_x = slab_subtile_center(slb_num_decode_x(i));
+            stl_y = slab_subtile_center(slb_num_decode_y(i));
+            for (nround = 0; nround < (AROUND_TILES_COUNT - 1); nround++)
+            {
+                MapSubtlCoord astl_x = stl_x + around[nround].delta_x;
+                MapSubtlCoord astl_y = stl_y + around[nround].delta_y;
+                if (get_floor_filled_subtiles_at(astl_x, astl_y) == 1)
+                {
+                    struct Thing* objtng = find_base_thing_on_mapwho(TCls_Object, 0, astl_x, astl_y);
+                    if (thing_is_invalid(objtng))
+                    {
+                        pos->x.val = subtile_coord_center(astl_x);
+                        pos->y.val = subtile_coord_center(astl_y);
+                        pos->z.val = get_thing_height_at_with_radius(thing, pos, nav_sizexy);
+                        if (!thing_in_wall_at_with_radius(thing, pos, nav_sizexy)) {
+                            SYNCLOG("Thorough check succeeded where random check failed.");
+                            return true;
+                        }
+                    }
+                }
+            }
+            k++;
+            if (k > room->slabs_count)
+            {
+                ERRORLOG("Infinite loop detected when sweeping room slabs list");
+                break;
+            }
+        }
+    }
+    SYNCLOG("Could not find any valid point in %s %d for %s",room_code_name(room->kind),room->index,thing_model_name(thing));
     return false;
 }
 

src/room_data.h
@@ -264,6 +264,8 @@ void destroy_dungeon_heart_room(PlayerNumber plyr_idx, const struct Thing *heart
 
 void count_gold_hoardes_in_room(struct Room *room);
 
+TbBool find_random_valid_position_for_thing_in_room_avoiding_object_excluding_room_slab(struct Thing *thing, struct Room *room, struct Coord3d *pos, long slbnum);
+
 /* MOVE TO room_list.c/h */
 struct Room *find_nearest_room_for_thing_with_spare_item_capacity(struct Thing *thing, PlayerNumber plyr_idx, RoomKind rkind, unsigned char nav_flags);
 struct Room *find_random_room_for_thing(struct Thing *thing, PlayerNumber owner, RoomKind rkind, unsigned char nav_flags);

src/room_util.c
@@ -406,25 +406,35 @@ short check_and_asimilate_thing_by_room(struct Thing *thing)
     if (thing_is_spellbook(thing))
     {
         room = get_room_thing_is_on(thing);
-        if (room_is_invalid(room) || !room_role_matches(room->kind, RoRoF_PowersStorage) || !player_exists(get_player(room->owner)))
+        if (room->owner != game.neutral_player_num)
         {
-            // No room - oh well, leave it as free spell
-            if (((gameadd.classic_bugs_flags & ClscBug_ClaimRoomAllThings) != 0) && !room_is_invalid(room)) {
-                // Preserve classic bug - object is claimed with the room
-                thing->owner = room->owner;
-            } else {
-                // Make correct owner so that Imps can pick it up
-                thing->owner = game.neutral_player_num;
+            if (room_is_invalid(room) || !room_role_matches(room->kind, RoRoF_PowersStorage) || (!player_exists(get_player(room->owner)) && (game.play_gameturn >= 10)))
+            {
+                // No room - oh well, leave it as free spell
+                if (((gameadd.classic_bugs_flags & ClscBug_ClaimRoomAllThings) != 0) && !room_is_invalid(room)) {
+                    // Preserve classic bug - object is claimed with the room
+                    thing->owner = room->owner;
+                }
+                else {
+                    // Make correct owner so that Imps can pick it up
+                    thing->owner = game.neutral_player_num;
+                    return false;
+                }
+                return false;
             }
-            return true;
-        }
-        if (!add_power_to_player(book_thing_to_power_kind(thing), room->owner))
-        {
-            thing->owner = game.neutral_player_num;
-            return true;
+            if (!add_power_to_player(book_thing_to_power_kind(thing), room->owner))
+            {
+                thing->owner = game.neutral_player_num;
+                return false;
+            }
+            else
+            {
+                thing->owner = room->owner;
+                return true;
+            }
         }
         thing->owner = room->owner;
-        return true;
+        return false;
     }
     if (thing_is_workshop_crate(thing))
     {

src/thing_physics.h
@@ -79,6 +79,7 @@ void get_floor_and_ceiling_height_under_thing_at(const struct Thing *thing,
     const struct Coord3d *pos, MapCoord *floor_height_cor, MapCoord *ceiling_height_cor);
 TbBool thing_is_exempt_from_z_axis_clipping(const struct Thing *thing);
 unsigned short get_slide_z_coord(const struct Thing *thing, const struct Coord3d *pos);
+TbBool position_over_floor_level(const struct Thing* thing, const struct Coord3d* pos);
 /******************************************************************************/
 #ifdef __cplusplus
 }

commit b34ae99a765b0d1469923e8b215c4987df5c27c6

Fixed a recent issue of stuff not dropping into water well
Introduced in #1279 (af1870454)

src/thing_objects.c
@@ -1792,16 +1792,13 @@ TngUpdateRet move_object(struct Thing *thing)
             long blocked_flags = get_thing_blocked_flags_at(thing, &pos);
             if (blocked_flags & SlbBloF_WalledZ)
             {
-                if (thing->owner != game.neutral_player_num)
+                struct Dungeon* dungeon = get_dungeon(thing->owner);
+                if (dungeon->sight_casted_thing_idx != thing->index)
                 {
-                    struct Dungeon* dungeon = get_dungeon(thing->owner);
-                    if (dungeon->sight_casted_thing_idx != thing->index)
+                    if (!find_free_position_on_slab(thing, &pos))
                     {
-                        if (!find_free_position_on_slab(thing, &pos))
-                        {
-                            SYNCDBG(7, "Found no free position next to (%ld,%ld) due to blocked flag %d. Move to valid position.", pos.x.val, pos.y.val, blocked_flags);
-                            move_creature_to_nearest_valid_position(thing);
-                        }
+                        SYNCDBG(7, "Found no free position next to (%ld,%ld) due to blocked flag %d. Move to valid position.", pos.x.val, pos.y.val, blocked_flags);
+                        move_creature_to_nearest_valid_position(thing);
                     }
                 }
             }