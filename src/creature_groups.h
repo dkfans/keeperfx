91d3bd736d3fd272c0dc1b023f6077c73e857e2e
Pieter Vandecandelaere <pieter00@gmail.com>
2022-02-21 12:59:39 +0100

    Refactored lvl_script (#1260)
    Huge change in the code, but should have 0 differences for the player.

--- src/creature_groups.h
+++ src/creature_groups.h
@@ -21,6 +21,7 @@
 
 #include "globals.h"
 #include "bflib_basics.h"
+#include "map_locations.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -58,6 +59,23 @@ struct MemberPos { // sizeof=3
     unsigned char flags;
 };
 
+struct PartyMember { // sizeof = 13
+  unsigned char flags;
+  unsigned char field_65;
+  unsigned char crtr_kind;
+  unsigned char objectv;
+  long countdown;
+  unsigned char crtr_level;
+  unsigned short carried_gold;
+  unsigned short field_6F;
+};
+
+struct Party { // sizeof = 208
+  char prtname[100];
+  struct PartyMember members[GROUP_MEMBERS_COUNT];
+  unsigned long members_num;
+};
+
 #pragma pack()
 /******************************************************************************/
 /******************************************************************************/
@@ -84,6 +102,9 @@ TbBool add_member_to_party(int party_id, long crtr_model, long crtr_level, long 
 TbBool delete_member_from_party(int party_id, long crtr_model, long crtr_level);
 long process_obey_leader(struct Thing *thing);
 void leader_find_positions_for_followers(struct Thing *leadtng);
+
+struct Thing *script_process_new_party(struct Party *party, PlayerNumber plyr_idx, TbMapLocation location, long copies_num);
+void script_process_new_tunneller_party(PlayerNumber plyr_idx, long prty_id, TbMapLocation location, TbMapLocation heading, unsigned char crtr_level, unsigned long carried_gold);
 /******************************************************************************/
 #ifdef __cplusplus
 }
---
+++ src/lvl_script_commands.h
@@ -0,0 +1,34 @@
+/******************************************************************************/
+// Free implementation of Bullfrog's Dungeon Keeper strategy game.
+/******************************************************************************/
+/** @file lvl_script_commands.h
+ *     Header file for lvl_script_commands.c.
+ * @par Purpose:
+ *     should only be used by files under lvl_script_*
+ * @par Comment:
+ *     Just a header file - #defines, typedefs, function prototypes etc.
+ * @author   KeeperFX Team
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+#ifndef DK_LVLSCRIPTCOMMANDS_H
+#define DK_LVLSCRIPTCOMMANDS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "lvl_script_lib.h"
+#include "lvl_script_parser.h"
+
+void script_add_command(const struct CommandDesc *cmd_desc, const struct ScriptLine *scline);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
