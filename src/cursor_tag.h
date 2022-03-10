70a5611c309ea8d20aa0d4b1e6327141d5a17efb
AdamPlenty <58278560+AdamPlenty@users.noreply.github.com>
2022-03-10 19:31:17 +0000

    Moved tag_cursor_blocks_place_trap to cursor_tag.c

--- src/cursor_tag.h
+++ src/cursor_tag.h
@@ -37,6 +37,7 @@ void tag_cursor_blocks_place_terrain(PlayerNumber plyr_idx, MapSubtlCoord stl_x,
 TbBool tag_cursor_blocks_place_thing(PlayerNumber plyr_idx, MapSubtlCoord stl_x, MapSubtlCoord stl_y);
 TbBool tag_cursor_blocks_order_creature(PlayerNumber plyr_idx, MapSubtlCoord stl_x, MapSubtlCoord stl_y, struct Thing* creatng);
 TbBool tag_cursor_blocks_steal_slab(PlayerNumber plyr_idx, MapSubtlCoord stl_x, MapSubtlCoord stl_y);
+TbBool tag_cursor_blocks_place_trap(PlayerNumber plyr_idx, MapSubtlCoord stl_x, MapSubtlCoord stl_y, TbBool full_slab);
 /******************************************************************************/
 #ifdef __cplusplus
 }
