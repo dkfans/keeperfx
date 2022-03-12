a771442b6f5bb3d6e18417231d3b09e4f1b415ba
Loobinex <Loobinex@users.noreply.github.com>
2022-03-13 00:40:34 +0100

    Undid change to get neutral dungeon
    Now need to fix log warnings/errors on functions looking for the dungeons of the active neutrals

--- src/dungeon_data.c
+++ src/dungeon_data.c
@@ -57,19 +57,12 @@ struct Dungeon *get_players_dungeon_f(const struct PlayerInfo *player,const char
 
 struct Dungeon *get_dungeon_f(PlayerNumber plyr_num,const char *func_name)
 {
-    if ((plyr_num >= 0) || (plyr_num < DUNGEONS_COUNT))
+    if ((plyr_num < 0) || (plyr_num >= DUNGEONS_COUNT))
     {
-        return &(game.dungeon[(int)plyr_num]);
+        ERRORLOG("%s: Tried to get non-existing dungeon %ld!", func_name, (long)plyr_num);
+        return INVALID_DUNGEON;
     }
-    if (plyr_num == NEUTRAL_PLAYER) // Suppress error for never existing but valid neutral 'player'
-    {
-        SYNCDBG(3, "%s: Tried to get neutral dungeon!",func_name);
-    }
-    else
-    {
-        ERRORLOG("%s: Tried to get non-existing dungeon %ld!",func_name,(long)plyr_num);
-    }
-    return INVALID_DUNGEON;
+    return &(game.dungeon[(int)plyr_num]);
 }
 
 struct DungeonAdd *get_dungeonadd_f(PlayerNumber plyr_num,const char *func_name)
