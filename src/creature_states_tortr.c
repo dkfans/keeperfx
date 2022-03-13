f96ad699a0e0ddfb1c0c1b0ac79036f8201d3547
Loobinex <Loobinex@users.noreply.github.com>
2022-03-13 23:16:01 +0100

    neutrals will never be converted

--- src/creature_states_tortr.c
+++ src/creature_states_tortr.c
@@ -488,14 +488,14 @@ CrCheckRet process_torture_function(struct Thing *creatng)
         set_start_state(creatng);
         return CrCkRet_Continue;
     }
-    if (room->owner == game.neutral_player_num)
+    if (room->owner == game.neutral_player_num || creatng->owner == game.neutral_player_num)
     {
         return CrCkRet_Available;
     }
     struct CreatureStats* crstat = creature_stats_get_from_thing(creatng);
     struct CreatureControl* cctrl = creature_control_get_from_thing(creatng);
     anger_apply_anger_to_creature(creatng, crstat->annoy_in_torture, AngR_Other, 1);
-    if (((long)game.play_gameturn >= cctrl->turns_at_job + game.turns_per_torture_health_loss) && (creatng->owner != game.neutral_player_num))
+    if ((long)game.play_gameturn >= cctrl->turns_at_job + game.turns_per_torture_health_loss)
     {
         i = compute_creature_max_health(game.torture_health_loss,cctrl->explevel);
         remove_health_from_thing_and_display_health(creatng, i);
