76542175d4f500923bbaa562ae93f373c18e7f19
Pieter Vandecandelaere <pieter00@gmail.com>
2022-03-28 21:21:54 +0200

    changed command name to MOVE_PLAYER_CAMERA_TO_LOCATION

--- src/lvl_script_commands.c
+++ src/lvl_script_commands.c
@@ -2254,7 +2254,7 @@ const struct CommandDesc command_desc[] = {
   {"HEART_LOST_QUICK_OBJECTIVE",        "NAl     ", Cmd_HEART_LOST_QUICK_OBJECTIVE, &heart_lost_quick_objective_check, &heart_lost_quick_objective_process},
   {"HEART_LOST_OBJECTIVE",              "Nl      ", Cmd_HEART_LOST_OBJECTIVE, &heart_lost_objective_check, &heart_lost_objective_process},
   {"SET_DOOR",                          "ANN     ", Cmd_SET_DOOR, &set_door_check, &set_door_process},
-  {"MOVE_PLAYER_CAMERA_TO",             "LP      ", Cmd_MOVE_PLAYER_CAMERA_TO, &player_zoom_to_check, &player_zoom_to_process},
+  {"MOVE_PLAYER_CAMERA_TO_LOCATION",    "LP      ", Cmd_MOVE_PLAYER_CAMERA_TO, &player_zoom_to_check, &player_zoom_to_process},
   {NULL,                                "        ", Cmd_NONE, NULL, NULL},
 };
 
