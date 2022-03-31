86d783046f0e031fd388e2861f3a0d4b0a29e93e
Loobinex <Loobinex@users.noreply.github.com>
2022-03-31 20:06:47 +0200

    now without memcopy

--- src/creature_states_combt.c
+++ src/creature_states_combt.c
@@ -534,7 +534,7 @@ void update_battle_events(BattleIndex battle_id)
     MapCoord map_x = -1;
     MapCoord map_y = -1;
     MapCoord map_z = -1;
-    struct DungeonAdd* DungeonAdd;
+    struct DungeonAdd* dungeonadd;
     unsigned long k = 0;
     struct CreatureBattle* battle = creature_battle_get(battle_id);
     int i = battle->first_creatr;
@@ -568,12 +568,10 @@ void update_battle_events(BattleIndex battle_id)
             continue;
         if ((1 << i) & owner_flags) 
         {
-            struct Coord3d pos;
-            pos.x.val = map_x;
-            pos.y.val = map_y;
-            pos.z.val = map_z;
-            DungeonAdd = get_dungeonadd(i);
-            memcpy(&DungeonAdd->last_combat_location, &pos, sizeof(struct Coord3d));
+            dungeonadd = get_dungeonadd(i);
+            dungeonadd->last_combat_location.x.val = map_x;
+            dungeonadd->last_combat_location.y.val = map_y;
+            dungeonadd->last_combat_location.z.val = map_z;
             if ((1 << i) == owner_flags) {
                 event_create_event_or_update_old_event(map_x, map_y, EvKind_FriendlyFight, i, 0);
             } else {
