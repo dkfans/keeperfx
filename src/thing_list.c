e383532d4e7cb68cf02eb24544335123c4f93fc2
Loobinex <Loobinex@users.noreply.github.com>
2022-04-20 17:32:35 +0200

    Flight altitude now adjusted based on slabs nearby

--- src/thing_list.c
+++ src/thing_list.c
@@ -2855,8 +2855,7 @@ TbBool update_thing(struct Thing *thing)
                 }
                 else 
                 {
-                    if (thing_above_flight_altitude(thing) && !is_thing_directly_controlled(thing) && 
-                        !(thing->active_state == CrSt_MoveToPosition || thing->active_state == CrSt_MoveBackwardsToPosition))
+                    if (thing_above_flight_altitude(thing) && !is_thing_directly_controlled(thing))
                     {
                         thing->veloc_push_add.z.val -= thing->fall_acceleration;
                         thing->state_flags |= TF1_PushAdd;
