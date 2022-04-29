f8a55eb3445c22ee91891cf2aab46aa461648723
AdamPlenty <58278560+AdamPlenty@users.noreply.github.com>
2022-04-29 20:30:43 +0100

    Update creature_instances.c

--- src/creature_instances.c
+++ src/creature_instances.c
@@ -839,7 +839,7 @@ long instf_first_person_do_imp_task(struct Thing *creatng, long *param)
                         {
                             clear_messages_from_player(id);
                         }
-                        targeted_message_add(id, plyr_idx, 50, "%d/%d", room->health, compute_room_max_health(room->slabs_count, room->efficiency));
+                        targeted_message_add(id, player->id_number, 50, "%d/%d", room->health, compute_room_max_health(room->slabs_count, room->efficiency));
                     }
                     else
                     {
