c5b89a95d349b4662eb982cb825bc916796a699f
Pieter Vandecandelaere <pieter00@gmail.com>
2022-06-28 20:34:12 +0200

    fix name

--- src/game_lghtshdw.h
+++ src/game_lghtshdw.h
@@ -61,7 +61,7 @@ struct LightsShadows { // sizeof = 164886
     char light_enabled;
     char lighting_tables_initialised;
     unsigned long light_rand_seed;
-    int _1DD41_idx;
+    int lighting_tables_idx;
     unsigned short subtile_lightness[256*256];
 };
 
