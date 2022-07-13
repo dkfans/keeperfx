6f90b1f48e389e04866aaa77d2e594ca6b1bb395
Loobinex <Loobinex@users.noreply.github.com>
2022-07-13 23:26:09 +0200

    Fixed script modified traps/doors from being disfunctional (#1623)
    Traps/Doors modified with SET_DOOR_CONFIGURATION and SET_TRAP_CONFIGURATION would no longer work as planned.

--- src/lvl_script_commands.c
+++ src/lvl_script_commands.c
@@ -807,6 +807,8 @@ static void set_trap_configuration_check(const struct ScriptLine* scline)
     ALLOCATE_SCRIPT_VALUE(scline->command, 0);
 
     const char *trapname = scline->tp[0];
+    const char *valuestring = scline->tp[2];
+    long newvalue;
     short trap_id = get_id(trap_desc, trapname);
     if (trap_id == -1)
     {
@@ -842,18 +844,25 @@ static void set_trap_configuration_check(const struct ScriptLine* scline)
             return;
         }
     }
-    else if (
-        (trapvar != 4) && // PointerSprites
-        (trapvar != 12) // Model
-        )
+    else if ((trapvar != 4) && (trapvar != 12))  // PointerSprites && Model
     {
-        if ((scline->np[2] > 0xFFFF) || (scline->np[2] < 0))
+        if (parameter_is_number(valuestring))
         {
-            SCRPTERRLOG("Value out of range: %d", scline->np[2]);
+            newvalue = atoi(valuestring);
+            if ((newvalue > SHORT_MAX) || (newvalue < 0))
+            {
+                SCRPTERRLOG("Value out of range: %d", newvalue);
+                DEALLOCATE_SCRIPT_VALUE
+                return;
+            }
+            value->shorts[2] = newvalue;
+        }
+        else 
+        {
+            SCRPTERRLOG("Trap property %s needs a number value, '%s' is invalid.", scline->tp[1], scline->tp[2]);
             DEALLOCATE_SCRIPT_VALUE
             return;
         }
-        value->shorts[2] = (short)scline->np[2];
     }
     else
     {
@@ -865,7 +874,7 @@ static void set_trap_configuration_check(const struct ScriptLine* scline)
             return;
         }
     }
-    SCRIPTDBG(7, "Setting trap %s property %d to %d", trapname, trapvar, value->shorts[2]);
+    SCRIPTDBG(7, "Setting trap %s property %s to %d", trapname, scline->tp[1], value->shorts[2]);
     PROCESS_SCRIPT_VALUE(scline->command);
 }
 
@@ -1018,6 +1027,9 @@ static void set_door_configuration_check(const struct ScriptLine* scline)
 
     const char *doorname = scline->tp[0];
     short door_id = get_id(door_desc, doorname);
+    const char* valuestring = scline->tp[2];
+    long newvalue;
+
     if (door_id == -1)
     {
         SCRPTERRLOG("Unknown door, '%s'", doorname);
@@ -1088,15 +1100,25 @@ static void set_door_configuration_check(const struct ScriptLine* scline)
             return;
         }
     }
-    else if (doorvar != 9) // PointerSprites
+    else if (doorvar != 9) // Not PointerSprites
     {
-        if ((scline->np[2] > 0xFFFF) || (scline->np[2] < 0))
+        if (parameter_is_number(valuestring))
         {
-            SCRPTERRLOG("Value out of range: %d", scline->np[2]);
+            newvalue = atoi(valuestring);
+            if ((newvalue > SHORT_MAX) || (newvalue < 0))
+            {
+                SCRPTERRLOG("Value out of range: %d", newvalue);
+                DEALLOCATE_SCRIPT_VALUE
+                return;
+            }
+            value->shorts[2] = newvalue;
+        }
+        else
+        {
+            SCRPTERRLOG("Door property %s needs a number value, '%s' is invalid.", scline->tp[1], scline->tp[2]);
             DEALLOCATE_SCRIPT_VALUE
             return;
         }
-        value->shorts[2] = (short)scline->np[2];
     }
     else
     {
@@ -1108,7 +1130,7 @@ static void set_door_configuration_check(const struct ScriptLine* scline)
             return;
         }
     }
-    SCRIPTDBG(7, "Setting door %s property %s to %d", doorname, doorvar, value->shorts[2]);
+    SCRIPTDBG(7, "Setting door %s property %s to %d", doorname, scline->tp[1], value->shorts[2]);
     PROCESS_SCRIPT_VALUE(scline->command);
 }
 
