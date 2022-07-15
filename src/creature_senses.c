dkfans/keeperfx

commit 5b4c039ad487b5c36ee2bd40be3a37e3c42ddd74

Fixed line of sight 3d returning false where it should not (#1628)
Fixes #1626 and possibly more errors

src/creature_senses.c
@@ -202,7 +202,7 @@ TbBool line_of_sight_3d_ignoring_specific_door(const struct Coord3d *frpos,
     nextpos.x.val = prevpos.x.val + increase_x;
     nextpos.y.val = prevpos.y.val + increase_y;
     nextpos.z.val = prevpos.z.val + increase_z;
-    while (distance > 0)
+    while (distance > COORD_PER_STL)
     {
         if (point_in_map_is_solid_ignoring_door(&nextpos, doortng)) {
             return false;
@@ -809,7 +809,7 @@ TbBool line_of_sight_3d(const struct Coord3d *frpos, const struct Coord3d *topos
     nextpos.x.val = prevpos.x.val + increase_x;
     nextpos.y.val = prevpos.y.val + increase_y;
     nextpos.z.val = prevpos.z.val + increase_z;
-    while (distance > 0)
+    while (distance > COORD_PER_STL)
     {
         if (point_in_map_is_solid(&nextpos)) {
             SYNCDBG(7, "Player cannot see through (%d,%d) due to linear path solid flags (downcount %d)",

commit e691074cb5e4f6da22d2bad157a22fd0459f7a88

Fixed flying units being able to see and shoot through doors (#1636)
- Bug was introduced in #1628
- Also made other 3d sight functions consistent

src/creature_senses.c
@@ -202,7 +202,7 @@ TbBool line_of_sight_3d_ignoring_specific_door(const struct Coord3d *frpos,
     nextpos.x.val = prevpos.x.val + increase_x;
     nextpos.y.val = prevpos.y.val + increase_y;
     nextpos.z.val = prevpos.z.val + increase_z;
-    while (distance > COORD_PER_STL)
+    while (distance > 1)
     {
         if (point_in_map_is_solid_ignoring_door(&nextpos, doortng)) {
             return false;
@@ -381,7 +381,7 @@ TbBool jonty_line_of_sight_3d_including_lava_check_ignoring_specific_door(const 
     nextpos.x.val = prevpos.x.val + increase_x;
     nextpos.y.val = prevpos.y.val + increase_y;
     nextpos.z.val = prevpos.z.val + increase_z;
-    while (distance > 0)
+    while (distance > 1)
     {
         if (get_point_in_map_solid_flags_ignoring_door(&nextpos, doortng) & 0x01) {
             return false;
@@ -561,7 +561,7 @@ TbBool jonty_line_of_sight_3d_including_lava_check_ignoring_own_door(const struc
     nextpos.y.val = prevpos.y.val + increase_y;
     nextpos.z.val = prevpos.z.val + increase_z;
 
-    while (distance > 0)
+    while (distance > 1)
     {
         if (get_point_in_map_solid_flags_ignoring_own_door(&nextpos, plyr_idx) & 0x01) {
             SYNCDBG(17, "Player %d cannot see through (%d,%d) due to linear path solid flags (downcount %d)",
@@ -809,7 +809,7 @@ TbBool line_of_sight_3d(const struct Coord3d *frpos, const struct Coord3d *topos
     nextpos.x.val = prevpos.x.val + increase_x;
     nextpos.y.val = prevpos.y.val + increase_y;
     nextpos.z.val = prevpos.z.val + increase_z;
-    while (distance > COORD_PER_STL)
+    while (distance > 1)
     {
         if (point_in_map_is_solid(&nextpos)) {
             SYNCDBG(7, "Player cannot see through (%d,%d) due to linear path solid flags (downcount %d)",
@@ -916,7 +916,7 @@ TbBool nowibble_line_of_sight_3d(const struct Coord3d *frpos, const struct Coord
     nextpos.x.val = prevpos.x.val + increase_x;
     nextpos.y.val = prevpos.y.val + increase_y;
     nextpos.z.val = prevpos.z.val + increase_z;
-    while (distance > 0)
+    while (distance > 1)
     {
         if (point_in_map_is_solid(&nextpos)) {
             SYNCDBG(7, "Player cannot see through (%d,%d) due to linear path solid flags (downcount %d)",