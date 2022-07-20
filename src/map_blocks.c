4f3a0c104049cc7cb65a96bec6d70be1e59ee117
Pieter Vandecandelaere <pieter00@gmail.com>
2022-07-20 21:07:01 +0200

    Fixed deleting torches (#1666)
    Bugfix to recently rewritten function.

--- src/map_blocks.c
+++ src/map_blocks.c
@@ -536,12 +536,12 @@ long delete_unwanted_things_from_liquid_slab(MapSlabCoord slb_x, MapSlabCoord sl
 
 static void delete_attached_things_on_slab(long slb_x, long slb_y)
 {
-    MapSubtlCoord stl_x = slab_subtile(slb_x,0);
-    MapSubtlCoord stl_y = slab_subtile(slb_y,0);
+    MapSubtlCoord stl_x = slab_subtile(slb_x,-1);
+    MapSubtlCoord stl_y = slab_subtile(slb_y,-1);
 
-    for (MapSubtlCoord y = stl_y; y < stl_y+STL_PER_SLB; y++)
+    for (MapSubtlCoord y = stl_y; y < stl_y+STL_PER_SLB+2; y++)
     {
-        for (MapSubtlCoord x = stl_x; x < stl_x+STL_PER_SLB; x++)
+        for (MapSubtlCoord x = stl_x; x < stl_x+STL_PER_SLB+2; x++)
         {
             struct Map *mapblk = get_map_block_at(x,y);
             if (mapblk == INVALID_MAP_BLOCK)
@@ -555,7 +555,7 @@ static void delete_attached_things_on_slab(long slb_x, long slb_y)
                 do
                 {
                     next_thing = thing_get(thing->next_on_mapblk);
-                    if (thing->parent_idx == (85 * slb_y + slb_x))
+                    if (thing->parent_idx == get_slab_number(slb_x,slb_y))
                     {
                         char class_id = thing->class_id;
                         if (class_id == TCls_Object || class_id == TCls_EffectGen)
