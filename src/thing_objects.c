8655179a4b9a2c4ad96970f4af4bfdb358dd6164
xtremeqg <xtremeq_g@hotmail.com>
2022-08-01 03:41:19 +0300

    re-rewrote object_update_power_sight

--- src/thing_objects.c
+++ src/thing_objects.c
@@ -1756,185 +1756,125 @@ DLLIMPORT int _DK_lbSinTable[2048];
 
 TngUpdateRet object_update_power_sight(struct Thing *objtng)
 {
-   int owner; // ecx
-  struct Dungeon *dungeon; // ebx
-  int sight_casted_splevel; // eax
-  unsigned int max_time_active; // ecx
-  int close_time; // ebp
-  int sight_casted_gameturn; // edx
-  int time_active; // eax
-  int v8; // eax
-  int time_active_2; // edi
-  int v10; // eax
-  int v11; // edi
-  int v12; // ecx
-  int strength; // kr08_4
-  int v14; // ebp
-  int shift_x; // eax
-  int shift_y; // ecx
-  int result; // eax
-  int v20; // edi
-  int splevel; // eax
-  int close_time_2; // ecx
-  int v23; // ebx
-  int v24; // ecx
-  int v25; // edi
-  int v26; // eax
-  int v27; // ebx
-  int *v28; // ebp
-  int v29; // eax
-  __int16 v30; // ax
-  struct Coord3d pos; // [esp+10h] [ebp-10h] BYREF
-  int v32; // [esp+18h] [ebp-8h]
-  int max_active_div_16; // [esp+1Ch] [ebp-4h]
+    int result; // eax
 
-  owner = objtng->owner;
-  objtng->health = 2;
-  dungeon = get_dungeon(owner);
-  
-  const struct MagicStats *pwrdynst = get_power_dynamic_stats(PwrK_SIGHT);
-  static const enum ThingEffectElements twinkle_eff_elements[] = {TngEffElm_RedTwinkle, TngEffElm_BlueTwinkle, TngEffElm_GreenTwinkle, TngEffElm_YellowTwinkle};
+    objtng->health = 2;
 
-  if ( !S3DEmitterIsPlayingSample((unsigned __int8)objtng->snd_emitter_id, 51, 0) )
-    thing_play_sample(objtng, 51, NORMAL_PITCH, -1, 3u, 1u, 3, FULL_LOUDNESS);
-  sight_casted_splevel = (unsigned __int8)dungeon->sight_casted_splevel;
-  max_time_active = pwrdynst->strength[sight_casted_splevel];
-  if ( game.play_gameturn - objtng->creation_turn >= max_time_active
-    && game.play_gameturn - dungeon->sight_casted_gameturn < max_time_active )
-  {
-    close_time = power_sight_close_instance_time[sight_casted_splevel];
-    max_active_div_16 = (int)max_time_active / 4 / 4;
-    sight_casted_gameturn = dungeon->sight_casted_gameturn;
-    v32 = max_active_div_16 / close_time;
-    time_active = game.play_gameturn - sight_casted_gameturn;
-    if ( game.play_gameturn - sight_casted_gameturn >= 0 )
+    static const long effkind[] = {43, 86, 87, 88};
+    struct Dungeon * dungeon = get_dungeon(objtng->owner);
+
+    if ( !S3DEmitterIsPlayingSample(objtng->snd_emitter_id, 51, 0) ) {
+        thing_play_sample(objtng, 51, 0x64u, -1, 3u, 1u, 3, 256);
+    }
+
+    int sight_casted_splevel = dungeon->sight_casted_splevel;
+
+    const struct MagicStats *pwrdynst = get_power_dynamic_stats(PwrK_SIGHT);
+    unsigned int max_time_active = pwrdynst->strength[sight_casted_splevel];
+
+    if ( game.play_gameturn - objtng->creation_turn >= max_time_active
+        && game.play_gameturn - dungeon->sight_casted_gameturn < max_time_active )
     {
-      if ( max_active_div_16 < time_active )
-        time_active = max_active_div_16;
+        int time_active = game.play_gameturn - dungeon->sight_casted_gameturn;
+        if ( game.play_gameturn - dungeon->sight_casted_gameturn >= 0 )
+        {
+            if ( max_time_active / 16 < time_active )
+                time_active = max_time_active / 16;
+        }
+        else
+        {
+            time_active = 0;
+        }
+        const int v32 = (max_time_active / 16) / power_sight_close_instance_time[sight_casted_splevel];
+        dungeon->sight_casted_gameturn = game.play_gameturn - max_time_active + time_active / v32 - power_sight_close_instance_time[sight_casted_splevel];
+    }
+    if ( max_time_active <= game.play_gameturn - dungeon->sight_casted_gameturn )
+    {
+        if ( power_sight_close_instance_time[dungeon->sight_casted_splevel] <= (game.play_gameturn - dungeon->sight_casted_gameturn) - max_time_active )
+        {
+            if ( (dungeon->computer_enabled & 4) != 0 )
+            {
+                dungeon->sight_casted_gameturn = game.play_gameturn;
+                struct Coord3d pos;
+                pos.x.val = (dungeon->sight_casted_stl_x << 8) + 128;
+                pos.z.val = 1408;
+                pos.y.val = (dungeon->sight_casted_stl_y << 8) + 128;
+                memset(dungeon->soe_explored_flags, 0, sizeof(dungeon->soe_explored_flags));
+                move_thing_in_map(objtng, &pos);
+                result = 1;
+                dungeon->computer_enabled &= ~4u;
+            }
+            else
+            {
+                dungeon->sight_casted_thing_idx = 0;
+                memset(dungeon->soe_explored_flags, 0, sizeof(dungeon->soe_explored_flags));
+                delete_thing_structure(objtng, 0);
+                return 0;
+            }
+        }
+        else
+        {
+            int v25 = 4;
+            const int v26 = 8 * (pwrdynst->strength[dungeon->sight_casted_splevel] / 4) / (4 * power_sight_close_instance_time[dungeon->sight_casted_splevel]);
+            int v27 = 4 * (power_sight_close_instance_time[dungeon->sight_casted_splevel] - ((game.play_gameturn - dungeon->sight_casted_gameturn) - max_time_active)) * v26;
+            const int v32 = v26;
+            do
+            {
+                int * v28 = lbSinTable;
+                do
+                {
+                    struct Coord3d pos;
+                    pos.x.val = objtng->mappos.x.val + ((unsigned int)(v27 * (*v28)) >> 16);
+                    v28 += 64;
+                    pos.y.val = objtng->mappos.y.val + (-((v27 * v28[448]) >> 8) >> 8);
+                    pos.z.val = 1408;
+                    create_effect_element(&pos, effkind[objtng->owner], objtng->owner);
+                }
+                while ( v28 < &lbCosTable[1536] );
+                v27 -= v32;
+                --v25;
+            }
+            while ( v25 );
+            return 1;
+        }
     }
     else
     {
-      time_active = 0;
-    }
-    v8 = game.play_gameturn - max_time_active + time_active / v32 - close_time;
-    dungeon->sight_casted_gameturn = game.play_gameturn - max_time_active;
-    dungeon->sight_casted_gameturn = v8;
-  }
-  time_active_2 = game.play_gameturn - dungeon->sight_casted_gameturn;
-  v10 = pwrdynst->strength[sight_casted_splevel];
-  if ( v10 <= time_active_2 )
-  {
-    v20 = time_active_2 - v10;
-    if ( power_sight_close_instance_time[(unsigned __int8)dungeon->sight_casted_splevel] <= v20 )
-    {
-      if ( (dungeon->computer_enabled & 4) != 0 )
-      {
-        dungeon->sight_casted_gameturn = game.play_gameturn;
-        pos.x.val = subtile_coord_center(dungeon->sight_casted_stl_x);
-        pos.z.val = 1408;
-        pos.y.val = subtile_coord_center(dungeon->sight_casted_stl_y);
-        memset(dungeon->soe_explored_flags, 0, sizeof(dungeon->soe_explored_flags));
-        move_thing_in_map(objtng, &pos);
-        result = 1;
-        dungeon->computer_enabled &= ~4u;
-      }
-      else
-      {
-        dungeon->sight_casted_thing_idx = 0;
-        memset(dungeon->soe_explored_flags, 0, sizeof(dungeon->soe_explored_flags));
-        delete_thing_structure(objtng, 0);
-        return 0;
-      }
-    }
-    else
-    {
-      pos.z.val = 1408;
-      splevel = (unsigned __int8)dungeon->sight_casted_splevel;
-      close_time_2 = power_sight_close_instance_time[splevel];
-      v23 = 4 * close_time_2;
-      v24 = close_time_2 - v20;
-      v25 = 4;
-      v26 = 8 * (pwrdynst->strength[splevel] / 4) / v23;
-      v27 = 4 * v24 * v26;
-      v32 = v26;
-
-      int angle = 0;
-
-      do
-      {
+        int v32 = 4;
+        int v11 = 4 * (game.play_gameturn - dungeon->sight_casted_gameturn);
         do
         {
-          angle += 64;
-          pos.x.val = move_coord_with_angle_x(objtng->mappos.x.val,v27,angle);
-          pos.y.val = move_coord_with_angle_y(objtng->mappos.y.val,v27,angle);
-          create_effect_element(&pos, twinkle_eff_elements[objtng->owner], objtng->owner);
+            int v12 = v11;
+            if ( v11 >= 0 )
+            {
+                if ( max_time_active / 4 < v11 )
+                    v12 = max_time_active / 4;
+            }
+            else
+            {
+                v12 = 0;
+            }
+            const int v14 = (v11 & 0x1F) << 8;
+            const int pos_x = (unsigned __int16)objtng->mappos.x.val + (__int16)((v12 * *(int *)((char *)lbSinTable + v14)) >> 13);
+            const int pos_y = (-(char)((v12 * *(int *)((char *)lbCosTable + v14)) >> 5) >> 8) + (unsigned __int16)objtng->mappos.y.val;
+            if ( pos_x >= 0 && pos_x < 65280 && pos_y >= 0 && pos_y < 65280 )
+            {
+                struct Coord3d pos;
+                pos.z.val = 1408;
+                pos.x.val = pos_x;
+                pos.y.val = pos_y;
+                create_effect_element(&pos, effkind[objtng->owner], objtng->owner);
+                const int shift_x = pos.x.stl.pos - objtng->mappos.x.stl.pos + 13;
+                const int shift_y = pos.y.stl.pos - objtng->mappos.y.stl.pos + 13;
+                dungeon->soe_explored_flags[shift_y][shift_x] = pos.x.val < 0xFF00u && pos.y.val < 0xFF00u;
+            }
+            ++v11;
+            --v32;
         }
-        while ( angle < 2 * LbFPMath_PI );
-        v27 -= v32;
-        --v25;
-      }
-      while ( v25 );
-      return 1;
-/*
-      do
-      {
-        v28 = lbSinTable;
-        do
-        {
-          v29 = *v28;
-          v28 += 64;
-          pos.x.val = objtng->mappos.x.val + ((unsigned int)(v27 * v29) >> 16);
-          pos.y.val = objtng->mappos.y.val + (-((v27 * v28[448]) >> 8) >> 8);
-          create_effect_element(&pos, twinkle_eff_elements[objtng->owner], objtng->owner);
-        }
-        while ( v28 < &lbCosTable[1536] );
-        v27 -= v32;
-        --v25;
-      }
-      while ( v25 );
-      return 1;
-    */
+        while ( v32 );
+        return 1;
     }
-  }
-  else
-  {
-    v32 = 4;
-    v11 = 4 * time_active_2;
-    do
-    {
-      v12 = v11;
-      if ( v11 >= 0 )
-      {
-        strength = pwrdynst->strength[(unsigned __int8)dungeon->sight_casted_splevel];
-        if ( strength / 4 < v11 )
-          v12 = strength / 4;
-      }
-      else
-      {
-        v12 = 0;
-      }
-      v14 = (v11 & 0x1F) << 8;
-      MapCoord pos_x = (unsigned __int16)objtng->mappos.x.val + (__int16)((v12 * *(int *)((char *)lbSinTable + v14)) >> 13);
-      MapCoord pos_y = (-(char)((v12 * *(int *)((char *)lbCosTable + v14)) >> 5) >> 8) + (unsigned __int16)objtng->mappos.y.val;
-        //MapCoord pos_x = move_coord_with_angle_x(objtng->mappos.x.val,v12 * 8,v14);
-        //MapCoord pos_y = move_coord_with_angle_y(objtng->mappos.y.val,v12 * 8,v14);
-      if ( pos_x >= 0 && pos_x < 65280 && pos_y >= 0 && pos_y < 65280 )
-      {
-        pos.z.val = 1408;
-        pos.x.val = pos_x;
-        pos.y.val = pos_y;
-        create_effect_element(&pos, twinkle_eff_elements[objtng->owner], objtng->owner);
-        shift_x = pos.x.stl.pos - objtng->mappos.x.stl.pos + 13;
-        shift_y = pos.y.stl.pos - objtng->mappos.y.stl.pos + 13;
-        dungeon->soe_explored_flags[shift_y][shift_x] = pos.x.val < 0xFF00u && pos.y.val < 0xFF00u;
-      }
-      ++v11;
-      --v32;
-    }
-    while ( v32 );
-    return 1;
-  }
-  return result;
+    return result;
 }
 
 #define NUM_ANGLES 16
