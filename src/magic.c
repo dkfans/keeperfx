8c9c2811a3298f6b3d8ec44b1c8b9f1e90b093c1
Loobinex <Loobinex@users.noreply.github.com>
2022-08-20 21:01:16 +0200

    use the slap sounds from the config when units get slapped
    instead of the hurt sound

--- src/magic.c
+++ src/magic.c
@@ -537,7 +537,7 @@ void slap_creature(struct PlayerInfo *player, struct Thing *thing)
     }
     cctrl->field_B1 = 6;
     cctrl->field_27F = 18;
-    play_creature_sound(thing, CrSnd_Hurt, 3, 0);
+    play_creature_sound(thing, CrSnd_Slap, 3, 0);
 }
 
 TbBool can_cast_power_at_xy(PlayerNumber plyr_idx, PowerKind pwkind,
