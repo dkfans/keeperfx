a085e85a9e09cfa00fa001dd2ff8f8dd8430b7df
xtremeqg <xtremeq_g@hotmail.com>
2022-08-20 16:40:59 +0300

    Added some comments

--- src/frontmenu_options_data.cpp
+++ src/frontmenu_options_data.cpp
@@ -38,6 +38,9 @@ extern "C" {
 #endif
 /******************************************************************************/
 struct GuiButtonInit frontend_define_keys_buttons[] = {
+  // gbtype           id_num       gbifield_3                rclick_event ptover_event       btype_value   pos_x        height                                  sprite_idx        parent_menu  gbifield_31
+  //                                  gbifield_5                                                 scr_pos_x      pos_y        draw_call                             tooltip_stridx   content                   maintain_call
+  //                                     click_event                                                  scr_pos_y     width
   {LbBtnT_NormalBtn,  BID_DEFAULT, 0, 0, NULL,               NULL,        NULL,               0, 999,  30, 999,  30,371, 46, frontend_draw_large_menu_button,   0, GUIStr_Empty,  0,      {92},            0, NULL },
   {LbBtnT_NormalBtn,  BID_DEFAULT, 0, 0, NULL,               NULL,        NULL,               0,  82, 117,  82, 117,450,246, frontend_draw_scroll_box,          0, GUIStr_Empty,  0,      {94},            0, NULL },
   {LbBtnT_HoldableBtn,BID_DEFAULT, 0, 0, frontend_define_key_up,NULL, frontend_over_button,   0, 532, 116, 532, 116, 26, 14, frontend_draw_slider_button,       0, GUIStr_Empty,  0,      {17},            0, frontend_define_key_up_maintain },
@@ -58,6 +61,9 @@ struct GuiButtonInit frontend_define_keys_buttons[] = {
 };
 
 struct GuiButtonInit frontend_option_buttons[] = {
+  // gbtype           id_num       gbifield_3                rclick_event ptover_event       btype_value   pos_x        height                                  sprite_idx        parent_menu  gbifield_31
+  //                                  gbifield_5                                                 scr_pos_x      pos_y        draw_call                             tooltip_stridx   content                   maintain_call
+  //                                     click_event                                                  scr_pos_y     width
   {LbBtnT_NormalBtn,  BID_DEFAULT, 0, 0, NULL,               NULL,        NULL,               0, 999,  30, 999,  30,371, 46, frontend_draw_large_menu_button,   0, GUIStr_Empty,  0,      {96},            0, NULL },
   {LbBtnT_NormalBtn,  BID_DEFAULT, 0, 0, NULL,               NULL,        NULL,               0,  95, 107,  95, 107,220, 26, frontend_draw_scroll_box_tab,      0, GUIStr_Empty,  0,      {28},            0, NULL },
   {LbBtnT_NormalBtn,  BID_DEFAULT, 0, 0, NULL,               NULL,        NULL,               0,  95, 133,  95, 133,450, 88, frontend_draw_scroll_box,          0, GUIStr_Empty,  0,      {89},            0, NULL },
