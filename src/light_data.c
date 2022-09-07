b63b3cbdb7d41e18ae7f8f1b5f8aba91ad035975
AdamPlenty <58278560+AdamPlenty@users.noreply.github.com>
2022-09-07 19:54:05 +0100

    Hand light on high ground should always work again
    Fixes #1823

--- src/light_data.c
+++ src/light_data.c
@@ -1543,7 +1543,8 @@ static char light_render_light(struct Light* lgt)
     lightadd->interp_mappos.y.val = interpolate(lightadd->interp_mappos.y.val, lightadd->previous_mappos.y.val, lgt->mappos.y.val);
   }
   lightadd->last_turn_drawn = game.play_gameturn;
-  lgt->mappos = lightadd->interp_mappos;
+  lgt->mappos.x.val = lightadd->interp_mappos.x.val;
+  lgt->mappos.y.val = lightadd->interp_mappos.y.val;
   // Stop flicker by rounding off position
   TbBool is_dynamic = lgt->flags & LgtF_Dynamic;
   if (is_dynamic)
