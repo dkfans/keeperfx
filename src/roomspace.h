d65818029e56285be7eb6d51415ff71b068d480c
AdamPlenty <58278560+AdamPlenty@users.noreply.github.com>
2022-09-16 23:07:07 +0100

    Moved and refined override code

--- src/roomspace.h
+++ src/roomspace.h
@@ -147,6 +147,8 @@ void reset_dungeon_build_room_ui_variables(PlayerNumber plyr_idx);
 void update_slab_grid(struct RoomSpace* roomspace, unsigned char mode, TbBool sell);
 
 TbBool roomspace_can_build_room_at_slab(PlayerNumber plyr_idx, RoomKind rkind, MapSlabCoord slb_x, MapSlabCoord slb_y);
+void update_bridge_build_direction(PlayerNumber plyr_idx);
+void detect_roomspace_direction(PlayerNumber plyr_idx);
 
 /******************************************************************************/
 #include "roomspace_detection.h"
