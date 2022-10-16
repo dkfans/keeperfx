e3c09f596b4f03c2a82cbae84a7de1d29b55c374
Loobinex <Loobinex@users.noreply.github.com>
2022-10-16 15:47:01 +0200

    Stop resetting heal_requirement to 0 for imps (#1928)

--- src/config_creature.c
+++ src/config_creature.c
@@ -343,9 +343,9 @@ void check_and_auto_fix_stats(void)
     for (long model = 0; model < gameadd.crtr_conf.model_count; model++)
     {
         struct CreatureStats* crstat = creature_stats_get(model);
-        if ( (crstat->lair_size <= 0) && (crstat->heal_requirement != 0) )
+        if ( (crstat->lair_size <= 0) && (crstat->toking_recovery <= 0) && (crstat->heal_requirement != 0) )
         {
-            ERRORLOG("Creature model %d No LairSize But Heal Requirment - Fixing", (int)model);
+            ERRORLOG("Creature model %d has no LairSize and no TokingRecovery but has HealRequirment - Fixing", (int)model);
             crstat->heal_requirement = 0;
         }
         if (crstat->heal_requirement > crstat->heal_threshold)
