aeb2ca075cc2d4f2a8e4f66e735f802ac1aa61ff
Sim <68837817+SimLV@users.noreply.github.com>
2022-10-30 20:11:14 +0300

    Added console command !thing.show_id <level> (#1956)
    It shows id of creatures (default) or all thing classes (when level is set to > 1)

--- src/engine_render.c
+++ src/engine_render.c
@@ -166,6 +166,8 @@ DLLIMPORT char _DK_splittypes[64];
 #endif
 /******************************************************************************/
 static void do_map_who(short tnglist_idx);
+static void (*render_sprite_debug_fn) (struct Thing*, long scrpos_x, long scrpos_y) = NULL;
+static int render_sprite_debug_level = 0;
 /******************************************************************************/
 
 void calculate_hud_scale(struct Camera *cam) {
@@ -4877,6 +4879,99 @@ unsigned short choose_health_sprite(struct Thing* thing)
     }
 }
 
+void fill_status_sprite_indexes(struct Thing *thing, struct CreatureControl *cctrl, short *health_spridx,
+                                short *state_spridx, short *anger_spridx)
+{
+    (*health_spridx) = choose_health_sprite(thing);
+    if (is_my_player_number(thing->owner))
+    {
+        lbDisplay.DrawFlags |= Lb_SPRITE_TRANSPAR4;
+        if (game.play_gameturn - cctrl->thought_bubble_last_turn_drawn == 1)
+        {
+            if (cctrl->thought_bubble_display_timer < 40) {
+                cctrl->thought_bubble_display_timer++;
+            }
+        } else {
+            if (game.play_gameturn - cctrl->thought_bubble_last_turn_drawn > 1) {
+                cctrl->thought_bubble_display_timer = 0;
+            }
+        }
+        cctrl->thought_bubble_last_turn_drawn = game.play_gameturn;
+        if (cctrl->thought_bubble_display_timer == 40)
+        {
+            struct StateInfo *stati;
+            stati = get_creature_state_with_task_completion(thing);
+            if (!stati->field_23)
+            {
+                if ((cctrl->spell_flags & CSAfF_MadKilling) != 0)
+                {
+                    stati = &states[CrSt_MadKillingPsycho];
+                }
+                else if (anger_is_creature_livid(thing))
+                {
+                    stati = &states[CrSt_CreatureLeavingDungeon];
+                }
+                else if (creature_is_called_to_arms(thing))
+                {
+                    stati = &states[CrSt_ArriveAtCallToArms];
+                }
+                else if (creature_is_at_alarm(thing))
+                {
+                    stati = &states[CrSt_ArriveAtAlarm];
+                }
+                else if (anger_is_creature_angry(thing))
+                {
+                    stati = &states[CrSt_PersonSulkAtLair];
+                }
+                else if (hunger_is_creature_hungry(thing))
+                {
+                    stati = &states[CrSt_CreatureArrivedAtGarden];
+                }
+                else if (creature_requires_healing(thing))
+                {
+                    stati = &states[CrSt_CreatureSleep];
+                }
+                else if (cctrl->paydays_owed)
+                {
+                    stati = &states[CrSt_CreatureWantsSalary];
+                }
+                else
+                {
+                    stati = get_creature_state_with_task_completion(thing);
+                }
+                if ((*(short *)&stati->field_26 == 1) || (thing_pointed_at == thing))
+                {
+                    (*state_spridx) = stati->sprite_idx;
+                }
+                switch (anger_get_creature_anger_type(thing))
+                {
+                case AngR_NotPaid:
+                    if ((cctrl->paydays_owed <= 0) && (cctrl->paydays_advanced >= 0))
+                    {
+                        (*anger_spridx) = GBS_creature_states_angry;
+                    }
+                    else
+                    {
+                        (*anger_spridx) = GBS_creature_states_getgold;
+                    }
+                    break;
+                case AngR_Hungry:
+                    (*anger_spridx) = GBS_creature_states_hungry;
+                    break;
+                case AngR_NoLair:
+                    (*anger_spridx) = GBS_creature_states_sleep;
+                    break;
+                case AngR_Other:
+                    (*anger_spridx) = GBS_creature_states_angry;
+                    break;
+                default:
+                    break;
+                }
+            }
+        }
+    }
+}
+
 void draw_status_sprites(long scrpos_x, long scrpos_y, struct Thing *thing)
 {
     struct PlayerInfo *player = get_my_player();
@@ -4932,95 +5027,7 @@ void draw_status_sprites(long scrpos_x, long scrpos_y, struct Thing *thing)
     exp = min(cctrl->explevel,9);
     if (cam->view_mode != PVM_ParchmentView)
     {
-        health_spridx = choose_health_sprite(thing);
-        if (is_my_player_number(thing->owner))
-        {
-            lbDisplay.DrawFlags |= Lb_SPRITE_TRANSPAR4;
-            cctrl = creature_control_get_from_thing(thing);
-            if (game.play_gameturn - cctrl->thought_bubble_last_turn_drawn == 1)
-            {
-                if (cctrl->thought_bubble_display_timer < 40) {
-                    cctrl->thought_bubble_display_timer++;
-                }
-            } else {
-                if (game.play_gameturn - cctrl->thought_bubble_last_turn_drawn > 1) {
-                    cctrl->thought_bubble_display_timer = 0;
-                }
-            }
-            cctrl->thought_bubble_last_turn_drawn = game.play_gameturn;
-            if (cctrl->thought_bubble_display_timer == 40)
-            {
-                struct StateInfo *stati;
-                stati = get_creature_state_with_task_completion(thing);
-                if (!stati->field_23)
-                {
-                    if ((cctrl->spell_flags & CSAfF_MadKilling) != 0)
-                    {
-                        stati = &states[CrSt_MadKillingPsycho];
-                    }
-                    else if (anger_is_creature_livid(thing))
-                    {
-                        stati = &states[CrSt_CreatureLeavingDungeon];
-                    }
-                    else if (creature_is_called_to_arms(thing))
-                    {
-                        stati = &states[CrSt_ArriveAtCallToArms];
-                    }
-                    else if (creature_is_at_alarm(thing))
-                    {
-                        stati = &states[CrSt_ArriveAtAlarm];
-                    }
-                    else if (anger_is_creature_angry(thing))
-                    {
-                        stati = &states[CrSt_PersonSulkAtLair];
-                    }
-                    else if (hunger_is_creature_hungry(thing))
-                    {
-                        stati = &states[CrSt_CreatureArrivedAtGarden];
-                    }
-                    else if (creature_requires_healing(thing))
-                    {
-                        stati = &states[CrSt_CreatureSleep];
-                    }
-                    else if (cctrl->paydays_owed)
-                    {
-                        stati = &states[CrSt_CreatureWantsSalary];
-                    }
-                    else
-                    {
-                        stati = get_creature_state_with_task_completion(thing);
-                    }
-                    if ((*(short *)&stati->field_26 == 1) || (thing_pointed_at == thing))
-                    {
-                        state_spridx = stati->sprite_idx;
-                    }
-                    switch (anger_get_creature_anger_type(thing))
-                    {
-                    case AngR_NotPaid:
-                        if ((cctrl->paydays_owed <= 0) && (cctrl->paydays_advanced >= 0))
-                        {
-                            anger_spridx = GBS_creature_states_angry;
-                        }
-                        else
-                        {
-                            anger_spridx = GBS_creature_states_getgold;
-                        }
-                        break;
-                    case AngR_Hungry:
-                        anger_spridx = GBS_creature_states_hungry;
-                        break;
-                    case AngR_NoLair:
-                        anger_spridx = GBS_creature_states_sleep;
-                        break;
-                    case AngR_Other:
-                        anger_spridx = GBS_creature_states_angry;
-                        break;
-                    default:
-                        break;
-                    }
-                }
-            }
-        }
+        fill_status_sprite_indexes(thing, cctrl, &health_spridx, &state_spridx, &anger_spridx);
     }
 
     int h_add;
@@ -7897,6 +7904,10 @@ void draw_jonty_mapwho(struct BucketKindJontySprite *jspr)
         thing_being_displayed_is_creature = 0;
         thing_being_displayed = NULL;
     }
+    if (render_sprite_debug_fn)
+    {
+        render_sprite_debug_fn(thing, jspr->scr_x, jspr->scr_y);
+    }
 
     if (
         ((thing->anim_sprite >= CREATURE_FRAMELIST_LENGTH) && (thing->anim_sprite < KEEPERSPRITE_ADD_OFFSET))
@@ -8936,5 +8947,50 @@ void draw_frontview_engine(struct Camera *cam)
     LbScreenLoadGraphicsWindow(&grwnd);
     cam->zoom = zoom_mem;//TODO [zoom] remove when all cam->zoom will be changed to camera_zoom
     SYNCDBG(9,"Finished");
+}
+
+static void render_sprite_debug_id(struct Thing* thing, long scr_x, long scr_y)
+{
+    if (render_sprite_debug_level < 2)
+    {
+        if (thing->class_id != TCls_Creature)
+            return;
+    }
+    ushort flg_mem = lbDisplay.DrawFlags;
+    lbDisplay.DrawFlags = Lb_TEXT_ONE_COLOR;
+    struct TbSprite *spr = &button_sprite[GBS_fontchars_number_dig0];
+    long w = scale_ui_value(spr->SWidth);
+    long h = scale_ui_value(spr->SHeight);
+
+    long val, value = thing->index;
+
+    // Count digits to be displayed
+    int ndigits=0;
+    for (val = value; val > 0; val /= 10)
+        ndigits++;
+    // Show the digits
+    scr_y -= h;
+    long pos_x = w * (ndigits - 1) / 2 + scr_x;
+    for (val = value; val > 0; val /= 10)
+    {
+        spr = &button_sprite[(val%10) + GBS_fontchars_number_dig0];
+        LbSpriteDrawScaled(pos_x, scr_y - h, spr, w, h);
+
+        pos_x -= w;
+    }
+    lbDisplay.DrawFlags = flg_mem;
+}
+
+void render_set_sprite_debug(int level)
+{
+    render_sprite_debug_level = level;
+    switch (level)
+    {
+        case 0:
+            render_sprite_debug_fn = NULL;
+            break;
+        default:
+            render_sprite_debug_fn = &render_sprite_debug_id;
+    }
 }
 /******************************************************************************/
