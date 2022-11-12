ed7219aa0dbb154f6bd3f05e5edb71d6c36967a5
Pieter Vandecandelaere <pieter00@gmail.com>
2022-11-12 16:01:12 +0100

    split map_ceiling to own file

---
+++ src/map_ceiling.c
@@ -0,0 +1,38 @@
+/******************************************************************************/
+// Free implementation of Bullfrog's Dungeon Keeper strategy game.
+/******************************************************************************/
+/** @file map_ceiling.c
+ *     Map ceiling support functions.
+ * @par Purpose:
+ *     Functions to calculate the ceiling.
+ * @par Comment:
+ *     None.
+ * @author   keeperFx Team
+ * @date     12 Nov 2022
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+
+#include "pre_inc.h"
+#include "globals.h"
+#include "map_columns.h"
+#include "globals.h"
+#include "game_legacy.h"
+#include "post_inc.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+DLLIMPORT long _DK_ceiling_init(unsigned long a1, unsigned long a2);
+
+
+
+/******************************************************************************/
+#ifdef __cplusplus
+}
+#endif
