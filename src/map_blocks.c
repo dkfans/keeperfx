a430e69e39948563b10274a9c98c49d0357eb7cd
Pieter Vandecandelaere <pieter00@gmail.com>
2022-11-12 14:23:27 +0100

    use rc as base instead

--- src/map_blocks.c
+++ src/map_blocks.c
@@ -2469,239 +2469,288 @@ int find_column_height_including_lintels(struct Column *col)
   return i + 1;
 }
 
-TbBool sub_448DB0(int a1)
-{
-  return (game.map[a1 + 257].flags & SlbAtFlg_Blocking) != 0
-      || (game.columns_data[game.map[a1 + 257].data & 0x7FF].bitfields & 0xE) != 0;
-}
-
-int __cdecl sub_448D70(int a1)
-{
-  unsigned __int8 bitfields; // cl
-
-  bitfields = game.columns_data[game.map[a1 + 257].data & 0x7FF].bitfields;
-  if ( (bitfields & 0xE) != 0 )
-    return find_column_height_including_lintels(&game.columns_data[game.map[a1 + 257].data & 0x7FF]);
-  else
-    return bitfields >> 4;
-}
-
-static int dword_555DA8;
-
 DLLIMPORT extern unsigned char *_DK_scratch;
 #define scratch _DK_scratch
 
+signed char *ceiling_cache; // weak
+
+int __fastcall ceiling_block_is_solid_including_corners_return_height(int a1, int a2, int a3)
+{
+  struct Column *col; // eax
+  unsigned __int8 v6; // dl
+  int v8; // edx
+  unsigned __int8 v10; // ch
+  int v11; // edx
+  unsigned __int8 v13; // cl
+  int v14; // edx
+  unsigned __int8 v16; // bh
+  int v17; // edx
+  unsigned __int8 bitfields; // bh
+
+  if ( (game.map[a1].flags & 0x10) == 0 && (game.columns.lookup[game.map[a1].data & 0x7FF]->bitfields & 0xE) == 0 )
+  {
+    if ( a2 <= 0 )
+    {
+      if ( a3 > 0 )
+      {
+        v17 = a1 - 256;
+        if ( (game.map[v17].flags & 0x10) != 0 || (game.columns.lookup[game.map[v17].data & 0x7FF]->bitfields & 0xE) != 0 )
+        {
+          col = game.columns.lookup[game.map[a1 - 256].data & 0x7FF];
+          bitfields = col->bitfields;
+          if ( (bitfields & 0xE) != 0 )
+            return find_column_height_including_lintels(col);
+          return bitfields >> 4;
+        }
+      }
+    }
+    else
+    {
+      v8 = a1 - 1;
+      if ( (game.map[v8].flags & 0x10) != 0 || (game.columns.lookup[game.map[v8].data & 0x7FF]->bitfields & 0xE) != 0 )
+      {
+        col = game.columns.lookup[game.map[a1 - 1].data & 0x7FF];
+        v10 = col->bitfields;
+        if ( (v10 & 0xE) != 0 )
+          return find_column_height_including_lintels(col);
+        return v10 >> 4;
+      }
+      if ( a3 > 0 )
+      {
+        v11 = a1 - 256;
+        if ( (game.map[v11].flags & 0x10) != 0 || (game.columns.lookup[game.map[v11].data & 0x7FF]->bitfields & 0xE) != 0 )
+        {
+          col = game.columns.lookup[game.map[a1 - 256].data & 0x7FF];
+          v13 = col->bitfields;
+          if ( (v13 & 0xE) != 0 )
+            return find_column_height_including_lintels(col);
+          return v13 >> 4;
+        }
+        v14 = a1 - 257;
+        if ( (game.map[v14].flags & 0x10) != 0 || (game.columns.lookup[game.map[v14].data & 0x7FF]->bitfields & 0xE) != 0 )
+        {
+          col = game.columns.lookup[game.map[a1 - 257].data & 0x7FF];
+          v16 = col->bitfields;
+          if ( (v16 & 0xE) != 0 )
+            return find_column_height_including_lintels(col);
+          return v16 >> 4;
+        }
+      }
+    }
+    return -1;
+  }
+  col = game.columns.lookup[game.map[a1].data & 0x7FF];
+  v6 = col->bitfields;
+  if ( (v6 & 0xE) != 0 )
+    return find_column_height_including_lintels(col);
+  return v6 >> 4;
+}
+
+int ceiling_calculate_height_from_nearest_walls(int result, int a2)
+{
+  int v2; // edx
+
+  v2 = game.ceiling_step * a2;
+  if ( result >= game.ceiling_height_max )
+  {
+    if ( result > game.ceiling_height_max )
+    {
+      result -= v2;
+      if ( result <= game.ceiling_height_min )
+        return game.ceiling_height_min;
+    }
+  }
+  else
+  {
+    result += v2;
+    if ( result >= game.ceiling_height_max )
+      return game.ceiling_height_max;
+  }
+  return result;
+}
+
+
 long ceiling_partially_recompute_heights(long sx, long sy, long ex, long ey)
 {
-  int v4; // edx
-  int v5; // eax
-  int v6; // eax
-  int v7; // ecx
-  int v8; // eax
-  int v9; // eax
-  int v10; // ebp
-  int v11; // ebx
-  int v12; // esi
-  int *p_data; // edi
-  unsigned __int8 bitfields; // cl
-  char v15; // al
-  unsigned __int8 v16; // cl
-  int v17; // ecx
-  int v18; // ecx
-  int v19; // ebx
-  struct MapOffset *v20; // edx
-  int v21; // ebp
-  int v22; // esi
-  int v23; // edi
-  int v24; // ecx
-  TbBool v25; // zf
-  signed int v27; // ecx
-  signed int v28; // eax
-  int v29; // edi
-  int v30; // [esp+10h] [ebp-18h]
-  int *v31; // [esp+10h] [ebp-18h]
-  int v32; // [esp+14h] [ebp-14h]
-  __int32 v33; // [esp+14h] [ebp-14h]
-  int v34; // [esp+18h] [ebp-10h]
-  int v35; // [esp+1Ch] [ebp-Ch]
-  int v36; // [esp+20h] [ebp-8h]
-  __int32 v37; // [esp+20h] [ebp-8h]
-  int v38; // [esp+24h] [ebp-4h]
-  __int32 xa; // [esp+2Ch] [ebp+4h]
-  __int32 ya; // [esp+30h] [ebp+8h]
-  int wa; // [esp+34h] [ebp+Ch]
-  int ha; // [esp+38h] [ebp+10h]
+  int v5; // edx
+  int v6; // edi
+  int v7; // eax
+  int v8; // edx
+  int v9; // edx
+  int v10; // edx
+  int v11; // eax
+  int v12; // ebx
+  int v13; // eax
+  int v14; // eax
+  int v15; // ebp
+  int v16; // edx
+  int v17; // edi
+  int v18; // esi
+  int i; // ecx
+  signed __int8 is_solid_including_corners_return_height; // al
+  unsigned long *p_data; // ebp
+  int v22; // eax
+  int v23; // edx
+  struct MapOffset *v24; // eax
+  int v25; // ecx
+  int v26; // ebx
+  int v27; // esi
+  int v28; // ecx
+  int v29; // ebx
+  int v30; // eax
+  int v31; // eax
+  int v32; // edi
+  char v33; // bl
+  int v34; // ebx
+  int v35; // edx
+  unsigned int v37; // [esp+4h] [ebp-54h]
+  int v38; // [esp+8h] [ebp-50h] BYREF
+  int v39; // [esp+Ch] [ebp-4Ch]
+  int v40; // [esp+10h] [ebp-48h]
+  int v41; // [esp+14h] [ebp-44h]
+  int v42; // [esp+18h] [ebp-40h]
+  int v43; // [esp+1Ch] [ebp-3Ch]
+  int v44; // [esp+20h] [ebp-38h]
+  int v45; // [esp+24h] [ebp-34h]
+  int v46; // [esp+28h] [ebp-30h]
+  int v47; // [esp+2Ch] [ebp-2Ch]
+  int *v48; // [esp+30h] [ebp-28h]
+  int v49; // [esp+34h] [ebp-24h]
+  unsigned int v50; // [esp+38h] [ebp-20h]
+  int v51; // [esp+3Ch] [ebp-1Ch]
+  int v52; // [esp+40h] [ebp-18h]
+  int v53; // [esp+44h] [ebp-14h]
+  int v54; // [esp+48h] [ebp-10h]
 
-  v4 = game.ceiling_dist;
-  if ( game.ceiling_dist >= 4 )
-    v4 = 4;
-  xa = (sx - v4) & ((sx - v4 <= 0) - 1);
-  v5 = v4 + ex;
-  ya = (sy - v4) & ((sy - v4 <= 0) - 1);
-  if ( v4 + ex >= 256 )
-    v5 = 256;
-  wa = v5;
-  v6 = v4 + ey;
-  if ( v4 + ey >= 256 )
-    v6 = 256;
-  ha = v6;
-  dword_555DA8 = (int)scratch;
-  v32 = (xa - game.ceiling_dist) & ((xa - game.ceiling_dist <= 0) - 1);
-  v7 = (ya - game.ceiling_dist) & ((ya - game.ceiling_dist <= 0) - 1);
-  v8 = game.ceiling_dist + wa;
-  if ( game.ceiling_dist + wa >= 256 )
-    v8 = 256;
-  v35 = v8;
-  v9 = game.ceiling_dist + ha;
-  if ( game.ceiling_dist + ha >= 256 )
-    v9 = 256;
-  v10 = (ya - game.ceiling_dist) & ((ya - game.ceiling_dist <= 0) - 1);
-  v36 = v9;
-  if ( v9 > v7 )
+  v5 = game.ceiling_dist;
+  if ( game.ceiling_dist > 4 )
+    v5 = 4;
+  v6 = sx - v5;
+  v7 = v5;
+  v8 = v6;
+  if ( v6 <= 0 )
+    v8 = 0;
+  v45 = v8;
+  v9 = sy - v7;
+  if ( sy - v7 <= 0 )
+    v9 = 0;
+  v40 = v9;
+  v10 = ex + v7;
+  if ( ex + v7 >= 256 )
+    v10 = 256;
+  v11 = ey + v7;
+  v47 = v10;
+  if ( v11 >= 256 )
+    v11 = 256;
+  v39 = v11;
+  v12 = v11;
+  ceiling_cache = scratch;
+  v13 = v45 - game.ceiling_dist;
+  if ( v45 - game.ceiling_dist <= 0 )
+    v13 = 0;
+  v44 = v13;
+  v14 = v40 - game.ceiling_dist;
+  if ( v40 - game.ceiling_dist <= 0 )
+    v14 = 0;
+  v15 = game.ceiling_dist + v47;
+  if ( game.ceiling_dist + v47 >= 256 )
+    v15 = 256;
+  v16 = v12 + game.ceiling_dist;
+  if ( v12 + game.ceiling_dist >= 256 )
+    v16 = 256;
+  v41 = v16;
+  v17 = v14;
+  if ( v14 < v16 )
   {
-    v30 = (v7 << 8) + v32;
-    while ( 1 )
-    {
-      v11 = v32;
-      v12 = v30;
-      if ( v32 < v35 )
-        break;
-LABEL_33:
-      ++v10;
-      v30 += 256;
-      if ( v36 <= v10 )
-        goto LABEL_34;
-    }
-    p_data = &game.map[v30 + 257].data;
-    while ( 1 )
-    {
-      if ( sub_448DB0(v12) )
-      {
-        bitfields = game.columns_data[*p_data & 0x7FF].bitfields;
-        if ( (bitfields & 0xE) != 0 )
-          v15 = find_column_height_including_lintels(&game.columns_data[*p_data & 0x7FF]);
-        else
-          v15 = bitfields >> 4;
-        goto LABEL_32;
-      }
-      if ( v11 <= 0 )
-        break;
-      if ( sub_448DB0(v12 - 1) )
-      {
-        v16 = game.columns_data[*(int *)((char *)p_data - 5) & 0x7FF].bitfields;
-        if ( (v16 & 0xE) != 0 )
-          v15 = find_column_height_including_lintels(&game.columns_data[*(int *)((char *)p_data - 5) & 0x7FF]);
-        else
-          v15 = v16 >> 4;
-        goto LABEL_32;
-      }
-      if ( v10 <= 0 )
-      {
-LABEL_31:
-        v15 = -1;
-        goto LABEL_32;
-      }
-      if ( sub_448DB0(v12 - 256) )
-        goto LABEL_26;
-      if ( !sub_448DB0(v12 - 257) )
-        goto LABEL_31;
-      v15 = sub_448D70(v12 - 257);
-LABEL_32:
-      ++v11;
-      p_data = (int *)((char *)p_data + 5);
-      ++v12;
-      *(char *)(dword_555DA8 + v12 - 1) = v15;
-      if ( v11 >= v35 )
-        goto LABEL_33;
-    }
-    if ( v10 <= 0 || !sub_448DB0(v12 - 256) )
-      goto LABEL_31;
-LABEL_26:
-    v15 = sub_448D70(v12 - 256);
-    goto LABEL_32;
-  }
-LABEL_34:
-  if ( ha > ya )
-  {
-    v38 = (ya << 8) + xa;
-    v33 = ha - ya;
+    v43 = v14 << 8;
     do
     {
-      v34 = v38;
-      if ( wa > xa )
+      v18 = v44;
+      for ( i = v44 + v43; v18 < v15; ceiling_cache[i - 1] = is_solid_including_corners_return_height )
+        is_solid_including_corners_return_height = ceiling_block_is_solid_including_corners_return_height(
+                                                     i++,
+                                                     v18++,
+                                                     v17);
+      ++v17;
+      v43 += 256;
+    }
+    while ( v17 < v41 );
+  }
+  if ( v40 < v39 )
+  {
+    v46 = v40 << 8;
+    v42 = v39 << 8;
+    do
+    {
+      v49 = v45;
+      v51 = v46 + v45;
+      if ( v47 > v45 )
       {
-        v31 = &game.map[v38 + 257].data;
-        v37 = wa - xa;
+        v50 = 5 * v51;
         do
         {
-          v17 = *(char *)(dword_555DA8 + v34);
-          if ( v17 <= -1 )
+          p_data = &game.map[v50 / 5].data;
+          v22 = ceiling_cache[v51];
+          v38 = v22;
+          if ( v22 <= -1 )
           {
-            v18 = ((v34 >> 31) ^ (unsigned __int8)abs(v34)) - (v34 >> 31);
-            v19 = 0;
-            v20 = spiral_step;
-            if ( game.ceiling_search_dist <= 0 )
-            {
-LABEL_46:
-              v17 = game.ceiling_height_max;
-            }
-            else
+            v52 = v51;
+            v48 = &v38;
+            v54 = v51 % 256;
+            v53 = v51 / 256;
+            v23 = 0;
+            v24 = spiral_step;
+            if ( game.ceiling_search_dist > 0 )
             {
               while ( 1 )
               {
-                v21 = v20->h + v18;
-                v22 = v34 / 256 + v20->v;
-                if ( v21 >= 0 && v21 < 256 && v22 >= 0 && v22 < 256 )
+                v25 = v54 + v24->h;
+                v26 = v53 + v24->v;
+                if ( v25 >= 0 && v25 < 256 && v26 >= 0 && v26 < 256 )
                 {
-                  v23 = *(char *)(dword_555DA8 + v34 + v20->both);
-                  if ( v23 > -1 )
+                  v27 = ceiling_cache[v52 + (*(long *)v24 >> 16)];
+                  if ( v27 > -1 )
                     break;
                 }
-                ++v19;
-                ++v20;
-                if ( v19 >= game.ceiling_search_dist )
-                  goto LABEL_46;
+                ++v23;
+                ++v24;
+                if ( v23 >= game.ceiling_search_dist )
+                  goto LABEL_43;
               }
-              v27 = abs(-v20->h);
-              v28 = abs(-v20->v);
-              if ( v27 <= v28 )
-                v27 = v28;
-              if ( v23 >= game.ceiling_height_max )
-              {
-                if ( v23 <= game.ceiling_height_max )
-                {
-                  v17 = *(char *)(dword_555DA8 + v34 + v20->both);
-                }
-                else
-                {
-                  v29 = v23 - game.ceiling_step * v27;
-                  if ( v29 <= game.ceiling_height_min )
-                    v29 = game.ceiling_height_min;
-                  v17 = v29;
-                }
-              }
+              *v48 = v27;
+              v28 = v54 - v25;
+              v29 = v53 - v26;
+              if ( (int)abs(v28) <= (int)abs(v29) )
+                v30 = v29;
               else
-              {
-                v17 = v23 + game.ceiling_step * v27;
-                if ( v17 >= game.ceiling_height_max )
-                  v17 = game.ceiling_height_max;
-              }
+                v30 = v28;
+              v37 = abs(v30);
+              v31 = 1;
             }
+            else
+            {
+LABEL_43:
+              v31 = 0;
+            }
+            if ( v31 )
+              v22 = ceiling_calculate_height_from_nearest_walls(v38, v37);
+            else
+              v22 = game.ceiling_height_max;
           }
-          v24 = *v31 ^ (*v31 ^ (v17 << 24)) & 0xF000000;
-          ++v34;
-          v25 = v37 == 1;
-          v31 = (int *)((char *)v31 + 5);
-          --v37;
-          *(int *)((char *)v31 - 5) = v24;
+          v32 = v47;
+          v33 = *((char *)p_data + 3) & 0xF0;
+          v50 += 5;
+          ++v51;
+          *((char *)p_data + 3) = v33;
+          v35 = ((v22 & 0xF) << 24) | *p_data;
+          v34 = ++v49;
+          *p_data = v35;
         }
-        while ( !v25 );
+        while ( v34 < v32 );
       }
-      v25 = v33 == 1;
-      v38 += 256;
-      --v33;
+      v46 += 256;
     }
-    while ( !v25 );
+    while ( v46 < v42 );
   }
   return 1;
 
@@ -2837,7 +2886,7 @@ void fill_in_reinforced_corners(PlayerNumber plyr_idx, MapSlabCoord slb_x, MapSl
           MapSlabCoord y3 = y2 + small_around[m].delta_y;
           struct SlabMap *slb4 = get_slabmap_block(x3, y3);
           struct SlabAttr* slbattr4 = get_slab_attrs(slb4);
-          if ( (slbattr4->category == SlbAtCtg_FriableDirt) )
+          if (slbattr4->category == SlbAtCtg_FriableDirt)
           {
             unsigned char pretty_type = choose_pretty_type(plyr_idx, x3, y3);
             place_slab_type_on_map(pretty_type, slab_subtile(x3, 0), slab_subtile(y3, 0), plyr_idx, 1);
