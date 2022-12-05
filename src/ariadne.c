772b919a8c1f0ef188204d2b2acd715af64f6bc2
Pieter Vandecandelaere <pieter00@gmail.com>
2022-12-05 20:25:58 +0100

    cleanup

--- src/ariadne.c
+++ src/ariadne.c
@@ -2979,179 +2979,6 @@ AriadneReturn ariadne_update_state_on_line(struct Thing *thing, struct Ariadne *
     return AridRet_OK;
 }
 
-#if defined(__GNUC__)
-  typedef          long long ll;
-  typedef unsigned long long ull;
-  #define __int64 long long
-  #define __int32 int
-  #define __int16 short
-  #define __int8  char
-  #define MAKELL(num) num ## LL
-  #define FMT_64 "ll"
-#elif defined(_MSC_VER)
-  typedef          __int64 ll;
-  typedef unsigned __int64 ull;
-  #define MAKELL(num) num ## i64
-  #define FMT_64 "I64"
-#elif defined (__BORLANDC__)
-  typedef          __int64 ll;
-  typedef unsigned __int64 ull;
-  #define MAKELL(num) num ## i64
-  #define FMT_64 "L"
-#else
-  #error "unknown compiler"
-#endif
-typedef unsigned int uint;
-typedef unsigned char uchar;
-typedef unsigned short ushort;
-typedef unsigned long ulong;
-
-typedef          char   int8;
-typedef   signed char   sint8;
-typedef unsigned char   uint8;
-typedef          short  int16;
-typedef   signed short  sint16;
-typedef unsigned short  uint16;
-typedef          int    int32;
-typedef   signed int    sint32;
-typedef unsigned int    uint32;
-typedef ll              int64;
-typedef ll              sint64;
-typedef ull             uint64;
-
-// Partially defined types. They are used when the decompiler does not know
-// anything about the type except its size.
-#define _BYTE  uint8
-#define _WORD  uint16
-#define _DWORD uint32
-#define _QWORD uint64
-#if !defined(_MSC_VER)
-#define _LONGLONG __int128
-#endif
-
-// Non-standard boolean types. They are used when the decompiler cannot use
-// the standard "bool" type because of the size mistmatch but the possible
-// values are only 0 and 1. See also 'BOOL' type below.
-typedef int8 _BOOL1;
-typedef int16 _BOOL2;
-typedef int32 _BOOL4;
-typedef int64 _BOOL8;
-
-#ifndef _WINDOWS_
-typedef int8 BYTE;
-typedef int16 WORD;
-typedef int32 DWORD;
-typedef int32 LONG;
-typedef int BOOL;       // uppercase BOOL is usually 4 bytes
-#endif
-typedef int64 QWORD;
-#ifndef __cplusplus
-typedef int bool;       // we want to use bool in our C programs
-#endif
-
-#define __pure  // pure function:
-                // when given the same arguments, always returns the same value
-                // has no side effects
-
-// Non-returning function
-#if defined(__GNUC__)
-#define __noreturn  __attribute__((noreturn))
-#else
-#define __noreturn  __declspec(noreturn)
-#endif
-
-
-#ifndef NULL
-#define NULL 0
-#endif
-
-// Some convenience macros to make partial accesses nicer
-#define LAST_IND(x,part_type)    (sizeof(x)/sizeof(part_type) - 1)
-#if defined(__BYTE_ORDER) && __BYTE_ORDER == __BIG_ENDIAN
-#  define LOW_IND(x,part_type)   LAST_IND(x,part_type)
-#  define HIGH_IND(x,part_type)  0
-#else
-#  define HIGH_IND(x,part_type)  LAST_IND(x,part_type)
-#  define LOW_IND(x,part_type)   0
-#endif
-// first unsigned macros:
-#define BYTEn(x, n)   (*((_BYTE*)&(x)+n))
-#define WORDn(x, n)   (*((_WORD*)&(x)+n))
-#define DWORDn(x, n)  (*((_DWORD*)&(x)+n))
-
-#define LOBYTE(x)  BYTEn(x,LOW_IND(x,_BYTE))
-#define LOWORD(x)  WORDn(x,LOW_IND(x,_WORD))
-#define LODWORD(x) DWORDn(x,LOW_IND(x,_DWORD))
-#define HIBYTE(x)  BYTEn(x,HIGH_IND(x,_BYTE))
-#define HIWORD(x)  WORDn(x,HIGH_IND(x,_WORD))
-#define HIDWORD(x) DWORDn(x,HIGH_IND(x,_DWORD))
-#define BYTE1(x)   BYTEn(x,  1)         // byte 1 (counting from 0)
-#define BYTE2(x)   BYTEn(x,  2)
-#define BYTE3(x)   BYTEn(x,  3)
-#define BYTE4(x)   BYTEn(x,  4)
-#define BYTE5(x)   BYTEn(x,  5)
-#define BYTE6(x)   BYTEn(x,  6)
-#define BYTE7(x)   BYTEn(x,  7)
-#define BYTE8(x)   BYTEn(x,  8)
-#define BYTE9(x)   BYTEn(x,  9)
-#define BYTE10(x)  BYTEn(x, 10)
-#define BYTE11(x)  BYTEn(x, 11)
-#define BYTE12(x)  BYTEn(x, 12)
-#define BYTE13(x)  BYTEn(x, 13)
-#define BYTE14(x)  BYTEn(x, 14)
-#define BYTE15(x)  BYTEn(x, 15)
-#define WORD1(x)   WORDn(x,  1)
-#define WORD2(x)   WORDn(x,  2)         // third word of the object, unsigned
-#define WORD3(x)   WORDn(x,  3)
-#define WORD4(x)   WORDn(x,  4)
-#define WORD5(x)   WORDn(x,  5)
-#define WORD6(x)   WORDn(x,  6)
-#define WORD7(x)   WORDn(x,  7)
-
-// now signed macros (the same but with sign extension)
-#define SBYTEn(x, n)   (*((int8*)&(x)+n))
-#define SWORDn(x, n)   (*((int16*)&(x)+n))
-#define SDWORDn(x, n)  (*((int32*)&(x)+n))
-
-#define SLOBYTE(x)  SBYTEn(x,LOW_IND(x,int8))
-#define SLOWORD(x)  SWORDn(x,LOW_IND(x,int16))
-#define SLODWORD(x) SDWORDn(x,LOW_IND(x,int32))
-#define SHIBYTE(x)  SBYTEn(x,HIGH_IND(x,int8))
-#define SHIWORD(x)  SWORDn(x,HIGH_IND(x,int16))
-#define SHIDWORD(x) SDWORDn(x,HIGH_IND(x,int32))
-#define SBYTE1(x)   SBYTEn(x,  1)
-#define SBYTE2(x)   SBYTEn(x,  2)
-#define SBYTE3(x)   SBYTEn(x,  3)
-#define SBYTE4(x)   SBYTEn(x,  4)
-#define SBYTE5(x)   SBYTEn(x,  5)
-#define SBYTE6(x)   SBYTEn(x,  6)
-#define SBYTE7(x)   SBYTEn(x,  7)
-#define SBYTE8(x)   SBYTEn(x,  8)
-#define SBYTE9(x)   SBYTEn(x,  9)
-#define SBYTE10(x)  SBYTEn(x, 10)
-#define SBYTE11(x)  SBYTEn(x, 11)
-#define SBYTE12(x)  SBYTEn(x, 12)
-#define SBYTE13(x)  SBYTEn(x, 13)
-#define SBYTE14(x)  SBYTEn(x, 14)
-#define SBYTE15(x)  SBYTEn(x, 15)
-#define SWORD1(x)   SWORDn(x,  1)
-#define SWORD2(x)   SWORDn(x,  2)
-#define SWORD3(x)   SWORDn(x,  3)
-#define SWORD4(x)   SWORDn(x,  4)
-#define SWORD5(x)   SWORDn(x,  5)
-#define SWORD6(x)   SWORDn(x,  6)
-#define SWORD7(x)   SWORDn(x,  7)
-
-// Generate a pair of operands. S stands for 'signed'
-#define __SPAIR16__(high, low)  (((int16)  (high) <<  8) | (uint8) (low))
-#define __SPAIR32__(high, low)  (((int32)  (high) << 16) | (uint16)(low))
-#define __SPAIR64__(high, low)  (((int64)  (high) << 32) | (uint32)(low))
-#define __SPAIR128__(high, low) (((int128) (high) << 64) | (uint64)(low))
-#define __PAIR16__(high, low)   (((uint16) (high) <<  8) | (uint8) (low))
-#define __PAIR32__(high, low)   (((uint32) (high) << 16) | (uint16)(low))
-#define __PAIR64__(high, low)   (((uint64) (high) << 32) | (uint32)(low))
-#define __PAIR128__(high, low)  (((uint128)(high) << 64) | (uint64)(low))
-
 static TbBool ariadne_check_forward_for_wallhug_gap_new(struct Thing *thing, struct Ariadne *arid, struct Coord3d *pos, long hug_angle)
 {
     struct Coord3d nav_boundry_pos;
@@ -3159,155 +2986,82 @@ static TbBool ariadne_check_forward_for_wallhug_gap_new(struct Thing *thing, str
     struct Coord3d potentional_next_pos_2d;
     struct Coord3d original_mappos;
 
-    long nav_radius = thing_nav_sizexy(thing) /2;
-/*
-    switch ( hug_angle )
+    long nav_radius = thing_nav_sizexy(thing) / 2;
+
+    switch (hug_angle)
     {
-        case ANGLE_NORTH:
-            if ( ((pos->y.val - nav_radius) / COORD_PER_STL) < ((thing->mappos.y.val - nav_radius) / COORD_PER_STL) )
-            {
-                nav_boundry_pos.x.val = pos->x.val;
-                nav_boundry_pos.y.stl.num = (thing->mappos.y.val - nav_radius) / COORD_PER_STL;
-                nav_boundry_pos.y.stl.pos = 0;
-                nav_boundry_pos.y.val += nav_radius;
-            }
-            break;
-        case ANGLE_SOUTH:
-            if ( ((nav_radius + pos->y.val) / COORD_PER_STL) > ((nav_radius + thing->mappos.y.val) / COORD_PER_STL) )
-            {
-                nav_boundry_pos.x.val = pos->x.val;
-                nav_boundry_pos.y.stl.num = (thing->mappos.y.val + nav_radius) / COORD_PER_STL;
-                nav_boundry_pos.y.stl.pos = -1;
-                nav_boundry_pos.y.val -= nav_radius;
-            }
-            break;
-        case ANGLE_WEST:
-            if ( ((pos->x.val - nav_radius) / COORD_PER_STL) < ((thing->mappos.x.val - nav_radius) / COORD_PER_STL) )
-            {
-                nav_boundry_pos.y.val = pos->y.val;
-                nav_boundry_pos.x.stl.num = (thing->mappos.x.val + nav_radius) / COORD_PER_STL;
-                nav_boundry_pos.x.stl.pos = 0;
-                nav_boundry_pos.x.val += nav_radius;
-            }
-            break;
-        case ANGLE_EAST:
-            if ( ((nav_radius + pos->x.val) / COORD_PER_STL) > ((nav_radius + thing->mappos.x.val) / COORD_PER_STL) )
-            {
-                nav_boundry_pos.y.val = pos->y.val;
-                nav_boundry_pos.x.stl.num = (thing->mappos.y.val - nav_radius) / COORD_PER_STL;
-                nav_boundry_pos.x.stl.pos = -1;
-                nav_boundry_pos.x.val -= nav_radius;
-            }
-            break;
-        default: //not a 90 degree angle
-            return false;
-    }
-
-*/
-
-
-
-
-
-
-    TbBool is_90_degree_angle = 0;
-    struct Coord3d *some_pos1; // ebp
-    if ( hug_angle )
-    {
-        switch ( hug_angle )
+    case ANGLE_NORTH:
+        if ((int)(((unsigned short)pos->y.val - nav_radius) & 0xFFFFFF00) < (int)(((unsigned short)thing->mappos.y.val - nav_radius) & 0xFFFFFF00))
         {
-        case 1024:
-            some_pos1 = pos;
-            if ( (int)((nav_radius + (unsigned __int16)pos->y.val) & 0xFFFFFF00) > (int)((nav_radius
-                                                                                        + (unsigned __int16)thing->mappos.y.val) & 0xFFFFFF00) )
-            {
             nav_boundry_pos.x.val = pos->x.val;
-            nav_boundry_pos.y.stl.num = (unsigned __int16)(nav_radius + thing->mappos.y.val) >> 8;
-            is_90_degree_angle = 1;
+            nav_boundry_pos.y.stl.num = (unsigned short)(thing->mappos.y.val - nav_radius) >> 8;
+            nav_boundry_pos.y.stl.pos = 0;
+            nav_boundry_pos.y.val += nav_radius;
+        }
+        break;
+    case 1024:
+        if ((int)((nav_radius + (unsigned short)pos->y.val) & 0xFFFFFF00) > (int)((nav_radius + (unsigned short)thing->mappos.y.val) & 0xFFFFFF00))
+        {
+            nav_boundry_pos.x.val = pos->x.val;
+            nav_boundry_pos.y.stl.num = (unsigned short)(nav_radius + thing->mappos.y.val) >> 8;
             nav_boundry_pos.y.stl.pos = -1;
             nav_boundry_pos.y.val -= nav_radius;
-            }
-            break;
-        case 1536:
-            some_pos1 = pos;
-            if ( (int)(((unsigned __int16)pos->x.val - nav_radius) & 0xFFFFFF00) < (int)(((unsigned __int16)thing->mappos.x.val
-                                                                                        - nav_radius) & 0xFFFFFF00) )
-            {
+        }
+        break;
+    case 1536:
+        if ((int)(((unsigned short)pos->x.val - nav_radius) & 0xFFFFFF00) < (int)(((unsigned short)thing->mappos.x.val - nav_radius) & 0xFFFFFF00))
+        {
             nav_boundry_pos.y.val = pos->y.val;
-            nav_boundry_pos.x.stl.num = (unsigned __int16)(thing->mappos.x.val - nav_radius) >> 8;
-            is_90_degree_angle = 1;
+            nav_boundry_pos.x.stl.num = (unsigned short)(thing->mappos.x.val - nav_radius) >> 8;
             nav_boundry_pos.x.stl.pos = 0;
             nav_boundry_pos.x.val += nav_radius;
-            }
-            break;
-        case 512:
-            some_pos1 = pos;
-            if ( (int)((nav_radius + (unsigned __int16)pos->x.val) & 0xFFFFFF00) > (int)((nav_radius
-                                                                                        + (unsigned __int16)thing->mappos.x.val) & 0xFFFFFF00) )
-            {
+        }
+        break;
+    case 512:
+        if ((int)((nav_radius + (unsigned short)pos->x.val) & 0xFFFFFF00) > (int)((nav_radius + (unsigned short)thing->mappos.x.val) & 0xFFFFFF00))
+        {
             nav_boundry_pos.y.val = pos->y.val;
-            nav_boundry_pos.x.stl.num = (unsigned __int16)(nav_radius + thing->mappos.x.val) >> 8;
-            is_90_degree_angle = 1;
+            nav_boundry_pos.x.stl.num = (unsigned short)(nav_radius + thing->mappos.x.val) >> 8;
             nav_boundry_pos.x.stl.pos = -1;
             nav_boundry_pos.x.val -= nav_radius;
-            }
-            break;
-        default:
-            some_pos1 = pos;
-            break;
         }
-    }
-    else
-    {
-        some_pos1 = pos;
-        if ( (int)(((unsigned __int16)pos->y.val - nav_radius) & 0xFFFFFF00) < (int)(((unsigned __int16)thing->mappos.y.val
-                                                                                    - nav_radius) & 0xFFFFFF00) )
-        {
-        nav_boundry_pos.x.val = pos->x.val;
-        nav_boundry_pos.y.stl.num = (unsigned __int16)(thing->mappos.y.val - nav_radius) >> 8;
-        nav_boundry_pos.y.stl.pos = 0;
-        is_90_degree_angle = 1;
-        nav_boundry_pos.y.val += nav_radius;
-        }
-    }
-    if ( !is_90_degree_angle )
+        break;
+    default:
         return 0;
-
-
-
+    }
 
     char angle_offset;
 
-    if ( arid->field_20 == 1 )
+    if (arid->field_20 == 1)
         angle_offset = -1;
-    else if ( arid->field_20 == 2 )
+    else if (arid->field_20 == 2)
         angle_offset = 1;
-    else 
+    else
         return 0;
 
-    long quadrant = (LbFPMath_PI/2) * ((angle_to_quadrant(hug_angle) + angle_offset) & 3);
+    long quadrant = (LbFPMath_PI / 2) * ((angle_to_quadrant(hug_angle) + angle_offset) & 3);
 
-    potentional_next_pos_2d.x.val = move_coord_with_angle_x(thing->mappos.x.val,arid->move_speed,quadrant);
-    potentional_next_pos_2d.y.val = move_coord_with_angle_y(thing->mappos.y.val,arid->move_speed,quadrant);
+    potentional_next_pos_2d.x.val = move_coord_with_angle_x(thing->mappos.x.val, arid->move_speed, quadrant);
+    potentional_next_pos_2d.y.val = move_coord_with_angle_y(thing->mappos.y.val, arid->move_speed, quadrant);
 
     potentional_next_pos_3d.x.val = potentional_next_pos_2d.x.val;
     potentional_next_pos_3d.y.val = potentional_next_pos_2d.y.val;
 
     potentional_next_pos_3d.z.val = get_floor_height_under_thing_at(thing, &thing->mappos);
-    
+
     thing->mappos.z.val = potentional_next_pos_3d.z.val;
     TbBool cant_move_to_pos_directly = creature_cannot_move_directly_to(thing, &potentional_next_pos_3d);
     thing->mappos.z.val = arid->move_speed;
 
-    if ( cant_move_to_pos_directly )
+    if (cant_move_to_pos_directly)
     {
         original_mappos = thing->mappos;
         thing->mappos = nav_boundry_pos;
 
         thing->mappos.z.val = get_thing_height_at(thing, &thing->mappos);
-        
-        potentional_next_pos_2d.x.val = move_coord_with_angle_x(thing->mappos.x.val,arid->move_speed,quadrant);
-        potentional_next_pos_2d.y.val = move_coord_with_angle_y(thing->mappos.y.val,arid->move_speed,quadrant);
+
+        potentional_next_pos_2d.x.val = move_coord_with_angle_x(thing->mappos.x.val, arid->move_speed, quadrant);
+        potentional_next_pos_2d.y.val = move_coord_with_angle_y(thing->mappos.y.val, arid->move_speed, quadrant);
 
         potentional_next_pos_3d = potentional_next_pos_2d;
         potentional_next_pos_3d.z.val = get_floor_height_under_thing_at(thing, &thing->mappos);
@@ -3316,7 +3070,7 @@ static TbBool ariadne_check_forward_for_wallhug_gap_new(struct Thing *thing, str
 
         thing->mappos = original_mappos;
 
-        if ( cant_move_to_pos_directly )
+        if (cant_move_to_pos_directly)
         {
             return false;
         }
@@ -3324,9 +3078,6 @@ static TbBool ariadne_check_forward_for_wallhug_gap_new(struct Thing *thing, str
         return true;
     }
     return false;
-
-
-
 }
 
 DLLIMPORT TbBool _DK_ariadne_check_forward_for_wallhug_gap(struct Thing *thing, struct Ariadne *arid, struct Coord3d *pos, long hug_angle);
