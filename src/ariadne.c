99e085e59fe0d2e53230807657cddf5f46f20e6c
Pieter Vandecandelaere <pieter00@gmail.com>
2022-12-18 11:43:37 +0100

    fix

--- src/ariadne.c
+++ src/ariadne.c
@@ -2987,6 +2987,7 @@ static TbBool ariadne_check_forward_for_wallhug_gap_new(struct Thing *thing, str
 
     long nav_radius = thing_nav_sizexy(thing) / 2;
 
+    TbBool isOk = false;
     switch (hug_angle)
     {
     case ANGLE_NORTH:
@@ -2996,6 +2997,7 @@ static TbBool ariadne_check_forward_for_wallhug_gap_new(struct Thing *thing, str
             nav_boundry_pos.y.stl.num = (thing->mappos.y.val - nav_radius) >> 8;
             nav_boundry_pos.y.stl.pos = 0;
             nav_boundry_pos.y.val += nav_radius;
+            isOk = true;
         }
         break;
     case ANGLE_SOUTH:
@@ -3005,6 +3007,7 @@ static TbBool ariadne_check_forward_for_wallhug_gap_new(struct Thing *thing, str
             nav_boundry_pos.y.stl.num = (nav_radius + thing->mappos.y.val) >> 8;
             nav_boundry_pos.y.stl.pos = COORD_PER_STL - 1;
             nav_boundry_pos.y.val -= nav_radius;
+            isOk = true;
         }
         break;
     case ANGLE_WEST:
@@ -3014,6 +3017,7 @@ static TbBool ariadne_check_forward_for_wallhug_gap_new(struct Thing *thing, str
             nav_boundry_pos.x.stl.num = (thing->mappos.x.val - nav_radius) >> 8;
             nav_boundry_pos.x.stl.pos = 0;
             nav_boundry_pos.x.val += nav_radius;
+            isOk = true;
         }
         break;
     case ANGLE_EAST:
@@ -3023,11 +3027,14 @@ static TbBool ariadne_check_forward_for_wallhug_gap_new(struct Thing *thing, str
             nav_boundry_pos.x.stl.num = (nav_radius + thing->mappos.x.val) >> 8;
             nav_boundry_pos.x.stl.pos = COORD_PER_STL - 1;
             nav_boundry_pos.x.val -= nav_radius;
+            isOk = true;
         }
         break;
     default:
         return 0;
     }
+    if (!isOk)
+        return 0;
 
     char angle_offset;
 
