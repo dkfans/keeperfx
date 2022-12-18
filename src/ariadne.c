66d65fd2dbb7f097c928be06f0e096fb29b64d00
Pieter Vandecandelaere <pieter00@gmail.com>
2022-12-18 12:44:00 +0100

    Revert "remove testcode"
    This reverts commit cbf6f43910e1e39e5b8eeb3c1597989afd32beb8.

--- src/ariadne.c
+++ src/ariadne.c
@@ -2979,7 +2979,7 @@ AriadneReturn ariadne_update_state_on_line(struct Thing *thing, struct Ariadne *
     return AridRet_OK;
 }
 
-static TbBool ariadne_check_forward_for_wallhug_gap(struct Thing *thing, struct Ariadne *arid, struct Coord3d *pos, long hug_angle)
+static TbBool ariadne_check_forward_for_wallhug_gap_new(struct Thing *thing, struct Ariadne *arid, struct Coord3d *pos, long hug_angle)
 {
     struct Coord3d nav_boundry_pos;
     struct Coord3d potentional_next_pos_3d;
@@ -3078,6 +3078,44 @@ static TbBool ariadne_check_forward_for_wallhug_gap(struct Thing *thing, struct 
         return true;
     }
     return false;
+}
+
+
+extern TbBool creature_cannot_move_directly_to_logging;
+
+DLLIMPORT TbBool _DK_ariadne_check_forward_for_wallhug_gap(struct Thing *thing, struct Ariadne *arid, struct Coord3d *pos, long hug_angle);
+static TbBool ariadne_check_forward_for_wallhug_gap(struct Thing *thing, struct Ariadne *arid, struct Coord3d *pos, long hug_angle)
+{
+    struct Coord3d thingstartpos;
+    struct Coord3d oldpos;
+
+
+    memcpy(&thingstartpos,&thing->mappos,sizeof(struct Coord3d));
+    memcpy(&oldpos,pos,sizeof(struct Coord3d));
+
+creature_cannot_move_directly_to_logging = true;
+JUSTLOG("----old");
+    TbBool old = _DK_ariadne_check_forward_for_wallhug_gap(thing, arid, &oldpos, hug_angle);
+
+
+    memcpy(&thing->mappos,&thingstartpos,sizeof(struct Coord3d));
+    JUSTLOG("----new");
+
+    TbBool new = ariadne_check_forward_for_wallhug_gap_new(thing, arid, pos, hug_angle);
+
+creature_cannot_move_directly_to_logging = false;
+    if (old == new && memcmp(&oldpos,pos,4/*sizeof(struct Coord3d)*/) == 0)
+    {
+        if(old != 0)
+            ERRORLOG("----Ok %d",(int)old);
+    }
+    else
+    {    
+        JUSTLOG("----oldpos %d.%d %d.%d %d.%d",oldpos.x.stl.num,oldpos.x.stl.pos,oldpos.y.stl.num,oldpos.y.stl.pos,oldpos.z.stl.num,oldpos.z.stl.pos);
+        JUSTLOG("----newpos %d.%d %d.%d %d.%d",pos->x.stl.num,pos->x.stl.pos,pos->y.stl.num,pos->y.stl.pos,pos->z.stl.num,pos->z.stl.pos);
+        ERRORLOG("----Nope %d, %d",(int)old,(int)new);
+    }
+    return new;
 }
 
 TbBool ariadne_creature_on_circular_hug(const struct Thing *thing, const struct Ariadne *arid)
