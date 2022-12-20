6596b95e059875d3115495173b4597bce67099d5
Pieter Vandecandelaere <pieter00@gmail.com>
2022-12-20 19:35:13 +0100

    remove useless ida stuff

--- src/ariadne_wallhug.c
+++ src/ariadne_wallhug.c
@@ -327,28 +327,7 @@ static short hug_round_new(struct Thing *creatng, struct Coord3d *pos1, struct C
 
 
 
-#ifndef HEXRAYS_DEFS_H
-#define HEXRAYS_DEFS_H
 
-#if defined(__GNUC__)
-  typedef          long long ll;
-  typedef unsigned long long ull;
-  #define __int64 long long
-  #define MAKELL(num) num ## LL
-  #define FMT_64 "ll"
-#elif defined(_MSC_VER)
-  typedef          __int64 ll;
-  typedef unsigned __int64 ull;
-  #define MAKELL(num) num ## i64
-  #define FMT_64 "I64"
-#elif defined (__BORLANDC__)
-  typedef          __int64 ll;
-  typedef unsigned __int64 ull;
-  #define MAKELL(num) num ## i64
-  #define FMT_64 "L"
-#else
-  #error "unknown compiler"
-#endif
 typedef unsigned int uint;
 typedef unsigned char uchar;
 typedef unsigned short ushort;
@@ -363,9 +342,6 @@ typedef unsigned short  uint16;
 typedef          int    int32;
 typedef   signed int    sint32;
 typedef unsigned int    uint32;
-typedef ll              int64;
-typedef ll              sint64;
-typedef ull             uint64;
 
 // Partially defined types. They are used when the decompiler does not know
 // anything about the type except its size.
@@ -373,88 +349,24 @@ typedef ull             uint64;
 #define _WORD  uint16
 #define _DWORD uint32
 #define _QWORD uint64
-#if !defined(_MSC_VER)
-#define _LONGLONG __int128
-#endif
-
-// Non-standard boolean types. They are used when the decompiler cannot use
-// the standard "bool" type because of the size mistmatch but the possible
-// values are only 0 and 1. See also 'BOOL' type below.
-typedef int8 _BOOL1;
-typedef int16 _BOOL2;
-typedef int32 _BOOL4;
-typedef int64 _BOOL8;
-
-#ifndef _WINDOWS_
-typedef int8 BYTE;
-typedef int16 WORD;
-typedef int32 DWORD;
-typedef int32 LONG;
-typedef int BOOL;       // uppercase BOOL is usually 4 bytes
-#endif
-typedef int64 QWORD;
-#ifndef __cplusplus
-typedef int bool;       // we want to use bool in our C programs
-#endif
-
-#define __pure  // pure function:
-                // when given the same arguments, always returns the same value
-                // has no side effects
-
-// Non-returning function
-#if defined(__GNUC__)
-#define __noreturn  __attribute__((noreturn))
-#else
-#define __noreturn  __declspec(noreturn)
-#endif
 
 
-#ifndef NULL
-#define NULL 0
-#endif
+
 
 // Some convenience macros to make partial accesses nicer
 #define LAST_IND(x,part_type)    (sizeof(x)/sizeof(part_type) - 1)
-#if defined(__BYTE_ORDER) && __BYTE_ORDER == __BIG_ENDIAN
-#  define LOW_IND(x,part_type)   LAST_IND(x,part_type)
-#  define HIGH_IND(x,part_type)  0
-#else
+
 #  define HIGH_IND(x,part_type)  LAST_IND(x,part_type)
 #  define LOW_IND(x,part_type)   0
-#endif
 // first unsigned macros:
 #define BYTEn(x, n)   (*((_BYTE*)&(x)+n))
 #define WORDn(x, n)   (*((_WORD*)&(x)+n))
 #define DWORDn(x, n)  (*((_DWORD*)&(x)+n))
 
-#define LOBYTE(x)  BYTEn(x,LOW_IND(x,_BYTE))
 #define LOWORD(x)  WORDn(x,LOW_IND(x,_WORD))
-#define LODWORD(x) DWORDn(x,LOW_IND(x,_DWORD))
-#define HIBYTE(x)  BYTEn(x,HIGH_IND(x,_BYTE))
-#define HIWORD(x)  WORDn(x,HIGH_IND(x,_WORD))
 #define HIDWORD(x) DWORDn(x,HIGH_IND(x,_DWORD))
-#define BYTE1(x)   BYTEn(x,  1)         // byte 1 (counting from 0)
-#define BYTE2(x)   BYTEn(x,  2)
-#define BYTE3(x)   BYTEn(x,  3)
 #define BYTE4(x)   BYTEn(x,  4)
-#define BYTE5(x)   BYTEn(x,  5)
-#define BYTE6(x)   BYTEn(x,  6)
-#define BYTE7(x)   BYTEn(x,  7)
-#define BYTE8(x)   BYTEn(x,  8)
-#define BYTE9(x)   BYTEn(x,  9)
-#define BYTE10(x)  BYTEn(x, 10)
-#define BYTE11(x)  BYTEn(x, 11)
-#define BYTE12(x)  BYTEn(x, 12)
-#define BYTE13(x)  BYTEn(x, 13)
-#define BYTE14(x)  BYTEn(x, 14)
-#define BYTE15(x)  BYTEn(x, 15)
-#define WORD1(x)   WORDn(x,  1)
-#define WORD2(x)   WORDn(x,  2)         // third word of the object, unsigned
-#define WORD3(x)   WORDn(x,  3)
-#define WORD4(x)   WORDn(x,  4)
-#define WORD5(x)   WORDn(x,  5)
-#define WORD6(x)   WORDn(x,  6)
-#define WORD7(x)   WORDn(x,  7)
+#define WORD2(x)   WORDn(x,  2)
 
 // now signed macros (the same but with sign extension)
 #define SBYTEn(x, n)   (*((int8*)&(x)+n))
@@ -482,20 +394,11 @@ typedef int bool;       // we want to use bool in our C programs
 #define SBYTE13(x)  SBYTEn(x, 13)
 #define SBYTE14(x)  SBYTEn(x, 14)
 #define SBYTE15(x)  SBYTEn(x, 15)
-#define SWORD1(x)   SWORDn(x,  1)
-#define SWORD2(x)   SWORDn(x,  2)
-#define SWORD3(x)   SWORDn(x,  3)
-#define SWORD4(x)   SWORDn(x,  4)
-#define SWORD5(x)   SWORDn(x,  5)
-#define SWORD6(x)   SWORDn(x,  6)
-#define SWORD7(x)   SWORDn(x,  7)
 
 
 
 
 
-
-#endif // HEXRAYS_DEFS_H
 
 
 
@@ -710,7 +613,7 @@ static int hug_round_sub_RC2(struct Thing *creatng, MapSubtlCoord *pos1_stl_x, M
     {
         return -1;
     }
-    __int64 v18 = ((unsigned short)LbArcTanAngle(pos2_stl_x - *pos1_stl_x, pos2_stl_y - *pos1_stl_y) % 2048 + 256) & 0x7FF;
+    __int64 v18 = (LbArcTanAngle(pos2_stl_x - *pos1_stl_x, pos2_stl_y - *pos1_stl_y) % 2048 + 256) & 0x7FF;
     int v53 = (int)(v18 - ((HIDWORD(v18) << 9) + (HIDWORD(v18) << 9))) >> 9;
     int v20 = max(abs(*pos1_stl_x - pos2_stl_x), abs(*pos1_stl_y - pos2_stl_y));
     if ((int)abs(v20) <= *v22 && hug_can_move_on(
