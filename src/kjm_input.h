a3937fd2be70f6347ce18cc76f6d0f7b870c638a
Pieter Vandecandelaere <pieter00@gmail.com>
2022-12-27 19:34:35 +0100

    revert kjm_input

--- src/kjm_input.h
+++ src/kjm_input.h
@@ -36,36 +36,66 @@ struct KeyToStringInit { // sizeof = 5
 };
 
 /******************************************************************************/
-extern unsigned long key_modifiers;
-extern int defining_a_key;
-extern long defining_a_key_id;
-extern long left_button_held_x;
-extern long left_button_held_y;
-extern long left_button_double_clicked_y;
-extern long left_button_double_clicked_x;
-extern long right_button_double_clicked_y;
-extern long right_button_double_clicked_x;
-extern char right_button_clicked;
-extern char left_button_clicked;
-extern long right_button_released_x;
-extern long right_button_released_y;
-extern char right_button_double_clicked;
-extern long left_button_released_y;
-extern long left_button_released_x;
-extern char left_button_double_clicked;
-extern char right_button_released;
-extern char right_button_held;
-extern long right_button_click_space_count;
-extern long right_button_held_y;
-extern long left_button_clicked_y;
-extern long left_button_clicked_x;
-extern long left_button_click_space_count;
-extern long right_button_held_x;
-extern char left_button_released;
-extern long right_button_clicked_y;
-extern long right_button_clicked_x;
-extern char left_button_held;
-extern long key_to_string[256];
+DLLIMPORT extern unsigned long _DK_key_modifiers;
+#define key_modifiers _DK_key_modifiers
+DLLIMPORT extern int _DK_defining_a_key;
+#define defining_a_key _DK_defining_a_key
+DLLIMPORT extern long _DK_defining_a_key_id;
+#define defining_a_key_id _DK_defining_a_key_id
+DLLIMPORT extern long _DK_left_button_held_x;
+#define left_button_held_x _DK_left_button_held_x
+DLLIMPORT extern long _DK_left_button_held_y;
+#define left_button_held_y _DK_left_button_held_y
+DLLIMPORT extern long _DK_left_button_double_clicked_y;
+#define left_button_double_clicked_y _DK_left_button_double_clicked_y
+DLLIMPORT extern long _DK_left_button_double_clicked_x;
+#define left_button_double_clicked_x _DK_left_button_double_clicked_x
+DLLIMPORT extern long _DK_right_button_double_clicked_y;
+#define right_button_double_clicked_y _DK_right_button_double_clicked_y
+DLLIMPORT extern long _DK_right_button_double_clicked_x;
+#define right_button_double_clicked_x _DK_right_button_double_clicked_x
+DLLIMPORT extern char _DK_right_button_clicked;
+#define right_button_clicked _DK_right_button_clicked
+DLLIMPORT extern char _DK_left_button_clicked;
+#define left_button_clicked _DK_left_button_clicked
+DLLIMPORT extern long _DK_right_button_released_x;
+#define right_button_released_x _DK_right_button_released_x
+DLLIMPORT extern long _DK_right_button_released_y;
+#define right_button_released_y _DK_right_button_released_y
+DLLIMPORT extern char _DK_right_button_double_clicked;
+#define right_button_double_clicked _DK_right_button_double_clicked
+DLLIMPORT extern long _DK_left_button_released_y;
+#define left_button_released_y _DK_left_button_released_y
+DLLIMPORT extern long _DK_left_button_released_x;
+#define left_button_released_x _DK_left_button_released_x
+DLLIMPORT extern char _DK_left_button_double_clicked;
+#define left_button_double_clicked _DK_left_button_double_clicked
+DLLIMPORT extern char _DK_right_button_released;
+#define right_button_released _DK_right_button_released
+DLLIMPORT extern char _DK_right_button_held;
+#define right_button_held _DK_right_button_held
+DLLIMPORT extern long _DK_right_button_click_space_count;
+#define right_button_click_space_count _DK_right_button_click_space_count
+DLLIMPORT extern long _DK_right_button_held_y;
+#define right_button_held_y _DK_right_button_held_y
+DLLIMPORT extern long _DK_left_button_clicked_y;
+#define left_button_clicked_y _DK_left_button_clicked_y
+DLLIMPORT extern long _DK_left_button_clicked_x;
+#define left_button_clicked_x _DK_left_button_clicked_x
+DLLIMPORT extern long _DK_left_button_click_space_count;
+#define left_button_click_space_count _DK_left_button_click_space_count
+DLLIMPORT extern long _DK_right_button_held_x;
+#define right_button_held_x _DK_right_button_held_x
+DLLIMPORT extern char _DK_left_button_released;
+#define left_button_released _DK_left_button_released
+DLLIMPORT extern long _DK_right_button_clicked_y;
+#define right_button_clicked_y _DK_right_button_clicked_y
+DLLIMPORT extern long _DK_right_button_clicked_x;
+#define right_button_clicked_x _DK_right_button_clicked_x
+DLLIMPORT extern char _DK_left_button_held;
+#define left_button_held _DK_left_button_held
+DLLIMPORT long _DK_key_to_string[256];
+#define key_to_string _DK_key_to_string
 
 #pragma pack()
 /******************************************************************************/
