41b0a56efe81956956051b44f249000a453285fb
Loobinex <Loobinex@users.noreply.github.com>
2023-01-09 01:11:55 +0100

    Revert "Revert 0d5b7c367"
    This reverts commit 0550147758d23af89ffe3610b7494af968cebacf.

--- src/gui_topmsg.c
+++ src/gui_topmsg.c
@@ -34,7 +34,7 @@ extern "C" {
 #endif
 /******************************************************************************/
 char onscreen_msg_text[255]="";
-int onscreen_msg_turns = 0;
+float render_onscreen_msg_time;
 
 struct ErrorStatistics erstat[] = {
     {0, 0, "Out of thing slots"},
@@ -70,13 +70,13 @@ TbBool show_onscreen_msg_va(int nturns, const char *fmt_str, va_list arg)
 {
     vsprintf(onscreen_msg_text, fmt_str, arg);
     SYNCMSG("Onscreen message: %s",onscreen_msg_text);
-    onscreen_msg_turns = nturns;
+    render_onscreen_msg_time = (float)nturns;
     return true;
 }
 
 TbBool is_onscreen_msg_visible(void)
 {
-    return (onscreen_msg_turns > 0);
+    return (render_onscreen_msg_time > 0.0);
 }
 
 TbBool show_onscreen_msg(int nturns, const char *fmt_str, ...)
@@ -126,11 +126,11 @@ TbBool draw_onscreen_direct_messages(void)
         tx_units_per_px = scale_ui_value_lofi(16);
     }
     // Display in-game message for debug purposes
-    if ((onscreen_msg_turns > 0) || erstat_check())
+    if ((render_onscreen_msg_time > 0.0) || erstat_check())
     {
         if ( LbScreenIsLocked() )
       LbTextDrawResized(scale_value_by_horizontal_resolution(160), 0, tx_units_per_px, onscreen_msg_text);
-        onscreen_msg_turns--;
+        render_onscreen_msg_time -= gameadd.delta_time;
     }
     unsigned int msg_pos = scale_value_by_vertical_resolution(200);
     if ((game.system_flags & GSF_NetGameNoSync) != 0)
