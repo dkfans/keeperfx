cd628aaee1760e886285eaa4063e4d437e3e6785
Pieter Vandecandelaere <pieter00@gmail.com>
2023-01-14 14:48:51 +0100

    slightly better makefile

--- luau.mk
+++ luau.mk
@@ -1,16 +1,18 @@
 CC = $(PREFIX)gcc
 AR = $(PREFIX)ar
-INCS = -Ideps/luau/Compiler/include -Ideps/luau/Compiler/include
-CFLAGS = -g -Og -std=gnu11 -fmessage-length=0 $(WARNFLAGS) -Werror=implicit $(INCS)
+INCS = -Ideps/luau/Compiler/include -Ideps/luau/VM/include -Ideps/luau/Common/include
+CFLAGS = -g -Og -fmessage-length=0 $(WARNFLAGS) -Werror=implicit $(INCS)
 ARFLAGS = rcs
 
-LUAU_FILES = $(wildcard *.cpp)
-OBJS = $(LUAU_FILES:%.c=obj/luau/VM/src/%.o %.c=obj/luau/Compiler/src/%.o)
 
-obj/luau/Compiler/src/%.o: deps/luau/Compiler/src/%.c
+LUAU_VM_FILES = lapi.cpp laux.cpp lbaselib.cpp lbitlib.cpp lbuiltins.cpp lcorolib.cpp ldblib.cpp ldebug.cpp ldo.cpp lfunc.cpp lgc.cpp lgcdebug.cpp linit.cpp lmathlib.cpp lmem.cpp lnumprint.cpp lobject.cpp loslib.cpp lperf.cpp lstate.cpp lstring.cpp lstrlib.cpp ltable.cpp ltablib.cpp ltm.cpp ludata.cpp lutf8lib.cpp lvmexecute.cpp lvmload.cpp lvmutils.cpp
+LUAU_COMPILER_FILES = BuiltinFolding.cpp Builtins.cpp BytecodeBuilder.cpp Compiler.cpp ConstantFolding.cpp CostModel.cpp lcode.cpp TableShape.cpp ValueTracking.cpp
+OBJS = $(LUAU_VM_FILES:%.cpp=obj/luau/VM/src/%.o) $(LUAU_COMPILER_FILES:%.cpp=obj/luau/Compiler/src/%.o)
+
+obj/luau/Compiler/src/%.o: deps/luau/Compiler/src/%.cpp
 	$(CC) $(CFLAGS) -c -o $@ $<
 
-obj/luau/VM/src/%.o: deps/luau/VM/src/%.c
+obj/luau/VM/src/%.o: deps/luau/VM/src/%.cpp
 	$(CC) $(CFLAGS) -c -o $@ $<
 
 obj/luau.a: $(OBJS)
