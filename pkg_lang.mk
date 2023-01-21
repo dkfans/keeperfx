0f7cd0d9dee82d1a609e7903935a026882a43c18
Loobinex <Loobinex@users.noreply.github.com>
2023-01-22 00:37:32 +0100

    Revert "Evil Keeper playable in any language (#2069)"
    This reverts commit e72938b53aab7750358394b154620c1e83d2b445.

--- pkg_lang.mk
+++ pkg_lang.mk
@@ -53,6 +53,10 @@ pkg/campgns/cqarctic/text_pol.dat \
 pkg/campgns/dstninja/text_eng.dat \
 pkg/campgns/dstninja/text_chi.dat \
 pkg/campgns/dstninja/text_pol.dat \
+pkg/campgns/evilkeep/text_eng.dat \
+pkg/campgns/evilkeep/text_fre.dat \
+pkg/campgns/evilkeep/text_ger.dat \
+pkg/campgns/evilkeep/text_pol.dat \
 pkg/campgns/grkreign/text_eng.dat \
 pkg/campgns/grkreign/text_pol.dat \
 pkg/campgns/grkreign/text_chi.dat \
