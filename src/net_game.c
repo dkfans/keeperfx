356a56b47ec69fd00a2354b0423d59176451bae6
SimLV <github@iamsim.ru>
2023-01-27 23:46:55 +0300

    Exit on VersionError

--- src/net_game.c
+++ src/net_game.c
@@ -150,7 +150,6 @@ static CoroutineLoopState setup_exchange_player_number(CoroutineLoop *context)
     {
         return CLS_REPEAT; // Repeat
     }
-    NETLOG("CLS_CONTINUE");
     return CLS_CONTINUE; // Skip loop to next function
 }
 
