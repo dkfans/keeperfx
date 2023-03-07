0ab3f4c947edb5d4ee6a62abd0b1c2ebace1fa47
Loobinex <Loobinex@users.noreply.github.com>
2023-03-07 01:01:58 +0100

    removed function from h file

--- src/thing_list.h
+++ src/thing_list.h
@@ -329,7 +329,6 @@ short update_thing_sound(struct Thing *thing);
 struct Thing* find_players_dungeon_heart(PlayerNumber plyridx);
 
 struct Thing *find_random_thing_in_room(ThingClass tngclass, ThingModel tngmodel,struct Room *room);
-struct Thing* find_players_backup_dungeon_heart(PlayerNumber plyridx);
 /******************************************************************************/
 #ifdef __cplusplus
 }
