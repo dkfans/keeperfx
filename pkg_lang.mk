0dbc033395e30f825f9c72997c1a7927ed7b470e
Loobinex <Loobinex@users.noreply.github.com>
2023-03-22 20:04:17 +0100

    Added spanish translations for destiny of the ninja

--- pkg_lang.mk
+++ pkg_lang.mk
@@ -56,6 +56,7 @@ pkg/campgns/cqarctic/text_pol.dat \
 pkg/campgns/dstninja/text_eng.dat \
 pkg/campgns/dstninja/text_chi.dat \
 pkg/campgns/dstninja/text_pol.dat \
+pkg/campgns/dstninja/text_spa.dat \
 pkg/campgns/evilkeep/text_fre.dat \
 pkg/campgns/evilkeep/text_ger.dat \
 pkg/campgns/grkreign/text_eng.dat \
