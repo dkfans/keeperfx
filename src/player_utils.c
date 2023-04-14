ea98eb3042cc1a628e7f8ff888740bf1267d263c
Loobinex <Loobinex@users.noreply.github.com>
2023-04-14 17:07:23 +0200

    now without crash

--- src/player_utils.c
+++ src/player_utils.c
@@ -731,11 +731,11 @@ void init_player(struct PlayerInfo *player, short no_explore)
     player->work_state = PSt_CtrlDungeon;
     player->field_14 = 2;
     player->main_palette = engine_palette;
+    player->minimap_zoom = settings.minimap_zoom;
+    player->isometric_view_zoom_level = settings.isometric_view_zoom_level;
+    player->frontview_zoom_level = settings.frontview_zoom_level;
     if (is_my_player(player))
     {
-        player->minimap_zoom = settings.minimap_zoom;
-        player->isometric_view_zoom_level = settings.isometric_view_zoom_level;
-        player->frontview_zoom_level = settings.frontview_zoom_level;
         set_flag_byte(&game.operation_flags,GOF_ShowPanel,true);
         set_gui_visible(true);
         init_gui();
@@ -754,6 +754,10 @@ void init_player(struct PlayerInfo *player, short no_explore)
         }
         break;
     case GKind_MultiGame:
+        //workaround until settings are synced through multiplayer
+        player->minimap_zoom = 256;
+        player->isometric_view_zoom_level = CAMERA_ZOOM_MAX;
+        player->frontview_zoom_level = FRONTVIEW_CAMERA_ZOOM_MAX;
         if (player->is_active != 1)
         {
           ERRORLOG("Non Keeper in Keeper game");
