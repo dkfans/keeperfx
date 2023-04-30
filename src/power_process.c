7de8c299a58713705dc3e87871e512a194378979
AdamPlenty <58278560+AdamPlenty@users.noreply.github.com>
2023-04-30 21:15:28 +0100

    Damage formula

--- src/power_process.c
+++ src/power_process.c
@@ -723,8 +723,7 @@ void process_timebomb(struct Thing *creatng)
             HitTF_EnemyDeployedDoors|HitTF_AlliedDeployedDoors|HitTF_OwnedDeployedDoors|
             HitTF_AnyFoodObjects|HitTF_AnyGoldPiles;
         struct MagicStats* pwrdynst = get_power_dynamic_stats(PwrK_TIMEBOMB);
-        timebomb_explosion_affecting_area(creatng, &creatng->mappos, subtile_coord(cctrl->timebomb_radius + 1, 0), pwrdynst->strength[cctrl->timebomb_radius] * (cctrl->timebomb_radius + 1), shotst->area_blow * (cctrl->timebomb_radius + 1), hit_targets, shotst->damage_type); //todo configure proper damage
-        kill_creature(creatng, INVALID_THING, -1, CrDed_NoEffects);
+        timebomb_explosion_affecting_area(creatng, &creatng->mappos, subtile_coord(cctrl->timebomb_radius + 1, 0), compute_creature_weight(creatng) * pwrdynst->strength[cctrl->timebomb_radius], shotst->area_blow * (cctrl->timebomb_radius + 1), hit_targets, shotst->damage_type);
     }
 }
 /******************************************************************************/
