dkfans/keeperfx

commit 3efe684cddd9d1b27d97eef6717a9c29d52c8417

More shot properties configurable, reworked hit effects (#2267)
-  Hittype fields have been reworked, added HitCreatureEffect as a new possibility
- Added EXPLODE_FLESH, which used to be hardcoded to Meteor to gib enemies it hits.
- Linked more of the previously hardcoded boulder properties to the BOULDER shot property
- Added ExplosionEffects field, which allows up to 4 effect(elements) attached to the shot when it explodes. Was hardcoded to some shots.
- Added VisualEffect which configures the special effects around the shot model, that were hardcoded before. Added extra options here.
- Made the meteor shot just a bit prettier by igniting later then it did before
- Added a minor visual effect to when a boulder 'hits' a wall
- Made the TNT shot gib creatures
- Fixed a log error about spells with effect 0 from a recent commit
- Fixed word of power not working

src/thing_shots.c
@@ -93,7 +93,8 @@ TbBool detonate_shot(struct Thing *shotng)
     SYNCDBG(8,"Starting for %s index %d owner %d",thing_model_name(shotng),(int)shotng->index,(int)shotng->owner);
     struct Thing* castng = INVALID_THING;
     struct PlayerInfo* myplyr = get_my_player();
-    // Identify the creator if the shot
+    short spell_level;
+    // Identify the creator of the shot
     if (shotng->index != shotng->parent_idx) {
         castng = thing_get(shotng->parent_idx);
         TRACE_THING(castng);
@@ -110,6 +111,18 @@ TbBool detonate_shot(struct Thing *shotng)
         HitTargetFlags hit_targets = hit_type_to_hit_targets(shotst->area_hit_type);
         explosion_affecting_area(castng, &shotng->mappos, dist, damage, shotst->area_blow, hit_targets, shotst->damage_type);
     }
+   
+    create_used_effect_or_element(&shotng->mappos, shotst->explode.effect1_model, shotng->owner);
+    create_used_effect_or_element(&shotng->mappos, shotst->explode.effect2_model, shotng->owner);
+    if (shotst->explode.around_effect1_model != 0)
+    {
+        create_effect_around_thing(shotng, shotst->explode.around_effect1_model);
+    }
+    if (shotst->explode.around_effect2_model > 0)
+    {
+        create_effect_around_thing(shotng, shotst->explode.around_effect2_model);
+    }
+
     //TODO CONFIG shot model dependency, make config option instead
     switch (shotng->model)
     {
@@ -120,30 +133,13 @@ TbBool detonate_shot(struct Thing *shotng)
             PaletteSetPlayerPalette(myplyr, engine_palette);
         }
         break;
-    case ShM_Grenade:
-    case ShM_Lizard:
-        create_effect(&shotng->mappos, TngEff_Explosion7, shotng->owner);
-        create_effect(&shotng->mappos, TngEff_Blood4, shotng->owner);
-        break;
     case ShM_TrapTNT:
-        create_effect(&shotng->mappos, TngEff_Eruption, shotng->owner);
-        short spell_level = shotng->shot.damage;
+        spell_level = shotng->shot.damage;
         if (spell_level > SPELL_MAX_LEVEL)
         {
             spell_level = SPELL_MAX_LEVEL;
         }
         magic_use_power_destroy_walls(shotng->owner, shotng->mappos.x.stl.num, shotng->mappos.y.stl.num, spell_level, PwMod_CastForFree);
-        create_effect_around_thing(shotng, TngEff_Explosion7);
-        break;
-    case ShM_Firebomb:
-        create_effect(&shotng->mappos, TngEff_Explosion7, shotng->owner);
-        break;
-    case ShM_Missile:
-    case ShM_NaviMissile:
-        create_effect(&shotng->mappos, TngEff_ChickenBlood, shotng->owner);
-        break;
-    case ShM_Boulder:
-        create_effect_around_thing(shotng, TngEff_DirtRubble);
         break;
     default:
         break;
@@ -518,25 +514,25 @@ TbBool shot_hit_wall_at(struct Thing *shotng, struct Coord3d *pos)
                 destroy_shot = 1;
             }
         } else
+        {
+            eff_kind = shotst->hit_generic.effect_model;
+            smpl_idx = shotst->hit_generic.sndsample_idx;
+            range = shotst->hit_generic.sndsample_range;
+            if (digging)
             {
-                eff_kind = shotst->hit_generic.effect_model;
-                smpl_idx = shotst->hit_generic.sndsample_idx;
-                range = shotst->hit_generic.sndsample_range;
-                if (digging)
+                slb = get_slabmap_for_subtile(stl_num_decode_x(hit_stl_num), stl_num_decode_y(hit_stl_num));
+                if ((old_health > slb->health) || (slb->kind == SlbT_GEMS))
                 {
-                    slb = get_slabmap_for_subtile(stl_num_decode_x(hit_stl_num), stl_num_decode_y(hit_stl_num));
-                    if ((old_health > slb->health) || (slb->kind == SlbT_GEMS))
-                    {
-                        smpl_idx = shotst->dig.sndsample_idx;
-                        range = shotst->dig.sndsample_range;
-                        eff_kind = shotst->dig.effect_model;
-                    }
-                }
-                efftng = create_shot_hit_effect(&shotng->mappos, shotng->owner, eff_kind, smpl_idx, range);
-                if (!shotst->hit_generic.withstand) {
-                    destroy_shot = 1;
+                    smpl_idx = shotst->dig.sndsample_idx;
+                    range = shotst->dig.sndsample_range;
+                    eff_kind = shotst->dig.effect_model;
                 }
             }
+            efftng = create_shot_hit_effect(&shotng->mappos, shotng->owner, eff_kind, smpl_idx, range);
+            if (!shotst->hit_generic.withstand) {
+                destroy_shot = 1;
+            }
+        }
     }
 
     if ( !destroy_shot )
@@ -902,24 +898,17 @@ long project_damage_of_melee_shot(long shot_dexterity, long shot_damage, const s
 void create_relevant_effect_for_shot_hitting_thing(struct Thing *shotng, struct Thing *target)
 {
     struct Thing* efftng = INVALID_THING;
+    struct ShotConfigStats* shotst = get_shot_model_stats(shotng->model);
     if (target->class_id == TCls_Creature)
     {
+        thing_play_sample(target, shotst->hit_creature.sndsample_idx, NORMAL_PITCH, 0, 3, 0, 2, FULL_LOUDNESS);
+        efftng = create_effect(&shotng->mappos, shotst->hit_creature.effect_model, shotng->owner);
         switch (shotng->model)
         {
-        case ShM_Fireball:
-        case ShM_Firebomb:
-        case ShM_Lightning:
-            efftng = create_effect(&shotng->mappos, TngEff_Explosion1, shotng->owner);
-            break;
         case ShM_PoisonCloud:
-            efftng = create_effect(&shotng->mappos, TngEff_Gas3, shotng->owner);
             if ( !thing_is_invalid(efftng) ) {
                 efftng->shot_effect.hit_type = THit_CrtrsOnly;
             }
-            break;
-        case ShM_NaviMissile:
-        case ShM_Missile:
-            efftng = create_effect(&shotng->mappos, TngEff_Blood3, shotng->owner);
             break;
         case ShM_Arrow:
         case ShM_SwingSword:
@@ -935,15 +924,13 @@ void create_relevant_effect_for_shot_hitting_thing(struct Thing *shotng, struct 
     }
     if (target->class_id == TCls_Trap)
     {
+        //todo introduce trap/object hit
+        thing_play_sample(target, shotst->hit_creature.sndsample_idx, NORMAL_PITCH, 0, 3, 0, 2, FULL_LOUDNESS);
+        efftng = create_effect(&shotng->mappos, shotst->hit_creature.effect_model, shotng->owner);
         switch (shotng->model)
         {
-        case ShM_Fireball:
-        case ShM_Firebomb:
-        case ShM_Lightning:
-            efftng = create_effect(&shotng->mappos, TngEff_Explosion1, shotng->owner);
-            break;
+
         case ShM_PoisonCloud:
-            efftng = create_effect(&shotng->mappos, TngEff_Gas3, shotng->owner);
             if (!thing_is_invalid(efftng)) {
                 efftng->shot_effect.hit_type = THit_CrtrsOnly;
             }
@@ -1023,9 +1010,9 @@ long melee_shot_hit_creature_at(struct Thing *shotng, struct Thing *trgtng, stru
     {
       if (shotst->hit_creature.sndsample_idx > 0)
       {
-          thing_play_sample(trgtng, shotst->hit_creature.sndsample_idx, NORMAL_PITCH, 0, 3, 0, 2, FULL_LOUDNESS);
           play_creature_sound(trgtng, CrSnd_Hurt, 3, 0);
       }
+      create_relevant_effect_for_shot_hitting_thing(shotng, trgtng);
       if (!thing_is_invalid(shooter)) {
           apply_damage_to_thing_and_display_health(trgtng, shotng->shot.damage, shotst->damage_type, shooter->owner);
       } else {
@@ -1044,7 +1031,6 @@ long melee_shot_hit_creature_at(struct Thing *shotng, struct Thing *trgtng, stru
           trgtng->veloc_push_add.y.val += (throw_strength * (long)shotng->velocity.y.val) / 16;
           trgtng->state_flags |= TF1_PushAdd;
       }
-      create_relevant_effect_for_shot_hitting_thing(shotng, trgtng);
       if (trgtng->health >= 0)
       {
           if (trgtng->owner != shotng->owner) {
@@ -1081,9 +1067,8 @@ void set_thing_acceleration_angles(struct Thing *thing, long angle_xy, long angl
 
 TbBool shot_model_makes_flesh_explosion(long shot_model)
 {
-    if ((shot_model == ShM_Firebomb) || (shot_model == ShM_GodLightBall))
-        return true;
-    return false;
+    struct ShotConfigStats* shotst = get_shot_model_stats(shot_model);
+    return (shotst->model_flags & ShMF_Exploding);
 }
 
 long shot_hit_creature_at(struct Thing *shotng, struct Thing *trgtng, struct Coord3d *pos)
@@ -1537,17 +1522,35 @@ TngUpdateRet update_shot(struct Thing *thing)
         {
             affect_nearby_friends_with_alarm(thing);
         }
+        if (shotst->model_flags & ShMF_Boulder)
+        {
+            if (apply_wallhug_force_to_boulder(thing))
+                hit = true;
+        }
+        if (shotst->visual.effect_model != 0)
+        {
+            if ((shotst->visual.shot_health == 0) ||
+                ((shotst->visual.shot_health > 0) && ((shotst->health - thing->health) <= shotst->visual.shot_health)) ||
+                ((shotst->visual.shot_health < 0) && ((thing->health - shotst->health) <= shotst->visual.shot_health)))
+            {
+                for (i = shotst->visual.amount; i > 0; i--)
+                {
+                    pos1.x.val = thing->mappos.x.val - UNSYNC_RANDOM(shotst->visual.random_range) + (shotst->visual.random_range / 2);
+                    pos1.y.val = thing->mappos.y.val - UNSYNC_RANDOM(shotst->visual.random_range) + (shotst->visual.random_range / 2);
+                    pos1.z.val = thing->mappos.z.val - UNSYNC_RANDOM(shotst->visual.random_range) + (shotst->visual.random_range / 2);
+                    if (shotst->visual.effect_model > 0)
+                    {
+                        create_effect(&pos1, shotst->visual.effect_model, thing->owner);
+                    }
+                    if (shotst->visual.effect_model < 0)
+                    {
+                        create_effect_element(&pos1, ~(shotst->visual.effect_model) + 1, thing->owner);
+                    }
+                }
+            }
+        }
         switch (thing->model)
         {
-        case ShM_Firebomb:
-            for (i = 2; i > 0; i--)
-            {
-              pos1.x.val = thing->mappos.x.val - UNSYNC_RANDOM(127) + 63;
-              pos1.y.val = thing->mappos.y.val - UNSYNC_RANDOM(127) + 63;
-              pos1.z.val = thing->mappos.z.val - UNSYNC_RANDOM(127) + 63;
-              create_thing(&pos1, TCls_EffectElem, TngEffElm_Blast1, thing->owner, -1);
-            }
-            break;
         case ShM_Lightning:
         {
             struct PlayerInfo* player;
@@ -1566,22 +1569,10 @@ TngUpdateRet update_shot(struct Thing *thing)
             break;
         }
         case ShM_Wind:
-            for (i = 10; i > 0; i--)
-            {
-              pos1.x.val = thing->mappos.x.val - UNSYNC_RANDOM(1023) + 511;
-              pos1.y.val = thing->mappos.y.val - UNSYNC_RANDOM(1023) + 511;
-              pos1.z.val = thing->mappos.z.val - UNSYNC_RANDOM(1023) + 511;
-              create_thing(&pos1, TCls_EffectElem, TngEffElm_Leaves1, thing->owner, -1);
-            }
             affect_nearby_enemy_creatures_with_wind(thing);
             break;
         case ShM_Grenade:
             thing->move_angle_xy = (thing->move_angle_xy + LbFPMath_PI/9) & LbFPMath_AngleMask;
-            break;
-        case ShM_Boulder:
-        case ShM_SolidBoulder:
-            if (apply_wallhug_force_to_boulder(thing))
-              hit = true;
             break;
         case ShM_GodLightning:
             draw_god_lightning(thing);
@@ -1617,18 +1608,7 @@ TngUpdateRet update_shot(struct Thing *thing)
             update_god_lightning_ball(thing);
             break;
         case ShM_TrapTNT:
-            
             thing->mappos.z.val = 0;
-            if (thing->health < 6)
-            {
-                for (i = 5; i > 0; i--)
-                {
-                    pos1.x.val = thing->mappos.x.val - UNSYNC_RANDOM(127) + 63;
-                    pos1.y.val = thing->mappos.y.val - UNSYNC_RANDOM(127) + 63;
-                    pos1.z.val = thing->mappos.z.val - UNSYNC_RANDOM(127) + 63;
-                    create_thing(&pos1, TCls_EffectElem, TngEffElm_Blast1, thing->owner, -1);
-                }
-            }
             break;
         case ShM_TrapLightning:
             if (((game.play_gameturn - thing->creation_turn) % 16) == 0)
@@ -1642,15 +1622,6 @@ TngUpdateRet update_shot(struct Thing *thing)
                   draw_lightning(&thing->mappos,&target->mappos, 96, TngEffElm_ElectricBall3);
                   apply_damage_to_thing_and_display_health(target, shotst->damage, shotst->damage_type, thing->owner);
               }
-            }
-            break;
-        case ShM_Disease:
-            for (i = 1; i > 0; i--)
-            {
-              pos1.x.val = thing->mappos.x.val - UNSYNC_RANDOM(511) + 255;
-              pos1.y.val = thing->mappos.y.val - UNSYNC_RANDOM(511) + 255;
-              pos1.z.val = thing->mappos.z.val - UNSYNC_RANDOM(511) + 255;
-              create_thing(&pos1, TCls_EffectElem, TngEffElm_DiseaseFly, thing->owner, -1);
             }
             break;
         default:

config/fxdata/creature.cfg
@@ -583,7 +583,7 @@ RangeMin = MIN
 RangeMax = 284
 PrimaryTarget = 3
 Properties = DESTRUCTIVE MELEE_ATTACK
-Function = creature_fire_shot SHOT_WORD_OF_POWER 0
+Function = creature_cast_spell SPELL_WORD_OF_POWER 0
 
 [instance29]
 Name = FART

config/fxdata/magic.cfg
@@ -1,5 +1,5 @@
 ; KeeperFX Magic Configuration file
-; file version 0.60
+; file version 0.70
 
 [common]
 ; Spells are casted by creatures at their position. They can affect the caster or surrounding
@@ -308,19 +308,34 @@ ShotSound = 0
 ShotSoundPriority = 0
 Unshaded = 0
 SoftLanding = 0
-HitCreatureSound = 0
-HitWallSound = 0
-HitWallSoundVariants = 0
+; Effect numbers for two effects on, then two effects around the shot when it detonades.
+; Negative numbers for effect elements. On other fields too.
+ExplosionEffects = 0 0 0 0
+; Effect around the projectile when it is flying.
+VisualEffect = 0
+VisualEffectAmount = 0
+; If the effect spawns only close around the shot position, or in a large area around it.
+VisualEffectSpread = 0
+; Effect runs out when shot health counts down. Negative value on health has it start late instead.
+VisualEffectHealth = 0
+; The effect that the shot creates when it hits [creature/walls/doors/water/lava]
+HitCreatureEffect = 0
+; The sound the effect makes when the shot hits something. Number of variants. '600 3' means 600, 601 or 602.
+HitCreatureSound = 0 0
 HitWallEffect = 0
-HitDoorSound = 0
-HitDoorSoundVariants = 0
+HitWallSound = 0 0
 HitDoorEffect = 0
-HitWaterSound = 0
-HitWaterSoundVariants = 0
+HitDoorSound = 0 0
 HitWaterEffect = 0
-HitLavaSound = 0
-HitLavaSoundVariants = 0
+HitWaterSound = 0 0
 HitLavaEffect = 0
+HitLavaSound = 0 0
+; Works the same as for the other hit effects, but then for digging
+DigHitEffect = 0
+DigHitSound = 0 0
+; If the shot survives impact on hitting these things.
+; Values: CREATURE, WALL, DOOR, LAVA, WATER, DIG
+WithstandHitAgainst =
 ; Shots can get several properties:
 ; ALARMS_UNITS - Draws friendly units to the shot
 ; BOULDER - Will explode without harming units immune to boulder.
@@ -339,10 +354,7 @@ HitLavaEffect = 0
 ; SLAPPABLE - Can be slapped.
 ; STRENGTH_BASED - Damage calculated from creature strength.
 ; WIND_IMMUNE - Shot can not be blown away
-; WITHSTAND_DOOR_HIT - Shot does not get destroyed when it touches a door
-; WITHSTAND_WALL_HIT - Shot does not get destroyed when it hits a wall or solid objects
-; WITHSTAND_LAVA_HIT - Shot does not get destroyed when it touches lava
-; WITHSTAND_WATER_HIT - Shot does not get destroyed when it touches water
+; EXPLODE_FLESH - Creatures killed by the shot are blown to bits
 Properties =
 
 [shot1]
@@ -368,11 +380,12 @@ FiringSound = 46
 ShotSound = 43
 ShotSoundPriority = 1
 Unshaded = 1
-HitCreatureSound = 137
+HitCreatureEffect = 1
+HitCreatureSound = 137 1
 HitWallEffect = 1
 HitDoorEffect = 1
-HitWaterSound = 36
 HitWaterEffect = 19
+HitWaterSound = 36 1
 
 [shot2]
 Name = SHOT_FIREBOMB
@@ -397,12 +410,16 @@ FiringSound = 46
 ShotSound = 43
 Unshaded = 1
 ShotSoundPriority = 1
-HitCreatureSound = 137
+ExplosionEffects = 50 0 0 0
+VisualEffect = 1
+VisualEffectAmount = 2
+VisualEffectSpread = 127
+VisualEffectHealth = -3
+HitCreatureEffect = 1
+HitCreatureSound = 137 1
 HitWallEffect = 4
 HitDoorEffect = 4
-HitWaterSound = 36
-HitWaterEffect = 19
-Properties = CAN_COLLIDE
+Properties = CAN_COLLIDE EXPLODE_FLESH
 
 [shot3]
 Name = SHOT_FREEZE
@@ -426,11 +443,11 @@ FiringSound = 48
 ShotSound = 49
 Unshaded = 1
 ShotSoundPriority = 1
-HitCreatureSound = 50
+HitCreatureSound = 50 1
 HitWallEffect = 22
 HitDoorEffect = 22
-HitWaterSound = 36
 HitWaterEffect = 19
+HitWaterSound = 36 1
 
 [shot4]
 Name = SHOT_LIGHTNING
@@ -455,7 +472,8 @@ Lighting = 10 52 0
 FiringSound = 55
 ShotSound = 0
 ShotSoundPriority = 1
-HitCreatureSound = 137
+HitCreatureEffect = 1
+HitCreatureSound = 137 1
 HitWallEffect = 4
 HitDoorEffect = 4
 Properties = REBOUND_IMMUNE WIND_IMMUNE HIDDEN_PROJECTILE
@@ -484,11 +502,12 @@ FiringSound = 46
 ShotSound = 43
 ShotSoundPriority = 1
 Unshaded = 1
-HitCreatureSound = 137
+HitCreatureEffect = 13
+HitCreatureSound = 137 1
 HitWallEffect = 13
 HitDoorEffect = 13
-HitWaterSound = 36
 HitWaterEffect = 19
+HitWaterSound = 36 1
 
 [shot6]
 Name = SHOT_NAVI_MISSILE
@@ -512,13 +531,15 @@ FiringSound = 0
 ShotSound = 53
 ShotSoundPriority = 1
 Unshaded = 1
-HitCreatureSound = 137
+ExplosionEffects = 7 0 0 0
+HitCreatureEffect = 8
+HitCreatureSound = 137 1
 HitWallEffect = 8
+HitWallSound = 57 1
 HitDoorEffect = 8
-HitDoorSound = 57
-HitWallSound = 57
-HitWaterSound = 36
+HitDoorSound = 57 1
 HitWaterEffect = 19
+HitWaterSound = 36 1
 Properties = NAVIGABLE WIND_IMMUNE
 
 [shot7]
@@ -564,7 +585,12 @@ SoftLanding = 1
 FiringSound = 40
 ShotSound = 0
 ShotSoundPriority = 1
-Properties = NO_HIT WITHSTAND_DOOR_HIT WITHSTAND_WALL_HIT WIND_IMMUNE HIDDEN_PROJECTILE
+ExplosionEffects = 0 0 0 0
+VisualEffect = 12
+VisualEffectAmount = 10
+VisualEffectSpread = 1023
+WithstandHitAgainst = WALL DOOR
+Properties = NO_HIT WIND_IMMUNE HIDDEN_PROJECTILE
 
 [shot9]
 Name = SHOT_MISSILE
@@ -588,13 +614,15 @@ FiringSound = 0
 ShotSound = 53
 ShotSoundPriority = 1
 Unshaded = 1
-HitCreatureSound = 137
+ExplosionEffects = 7 0 0 0
+HitCreatureEffect = 8
+HitCreatureSound = 137 1
 HitWallEffect = 8
+HitWallSound = 57 1
 HitDoorEffect = 8
-HitDoorSound = 57
-HitWallSound = 57
-HitWaterSound = 36
+HitDoorSound = 57 1
 HitWaterEffect = 19
+HitWaterSound = 36 1
 
 [shot10]
 Name = SHOT_SLOW
@@ -618,8 +646,8 @@ FiringSound = 48
 ShotSound = 49
 ShotSoundPriority = 1
 Unshaded = 1
-HitCreatureSound = 50
-HitWaterSound = 36
+HitCreatureSound = 50 1
+HitWaterSound = 36 1
 HitWaterEffect = 19
 Properties = WIND_IMMUNE
 
@@ -648,9 +676,11 @@ SoftLanding = 1
 FiringSound = 0
 ShotSound = 54
 ShotSoundPriority = 1
-HitWaterSound = 36
+ExplosionEffects = 50 9 0 0
 HitWaterEffect = 19
-Properties = NO_HIT CAN_COLLIDE WITHSTAND_DOOR_HIT WITHSTAND_WALL_HIT
+HitWaterSound = 36 1
+WithstandHitAgainst = WALL DOOR
+Properties = NO_HIT CAN_COLLIDE
 
 [shot12]
 Name = SHOT_DRAIN
@@ -697,11 +727,11 @@ TargetHitstopTurns = 6
 FiringSound = 46
 ShotSound = 0
 ShotSoundPriority = 1
-HitCreatureSound = 137
+HitCreatureSound = 137 1
 HitWallEffect = 22
 HitDoorEffect = 22
-HitWaterSound = 36
 HitWaterEffect = 19
+HitWaterSound = 36 1
 
 [shot14]
 Name = SHOT_ARROW
@@ -725,9 +755,9 @@ FiringSound = 44
 ShotSound = 0
 ShotSoundPriority = 1
 Unshaded = 1
-HitCreatureSound = 137
-HitWaterSound = 36
+HitCreatureSound = 137 0
 HitWaterEffect = 19
+HitWaterSound = 36 1
 Properties = STRENGTH_BASED
 
 [shot15]
@@ -753,12 +783,15 @@ SoftLanding = 1
 FiringSound = 0
 ShotSound = 35
 ShotSoundPriority = 1
-HitCreatureSound = 137
-HitWallSound = 1000
-HitWaterSound = 36
+ExplosionEffects = 0 0 26 0
+HitCreatureSound = 137 1
+HitWallEffect = 25
+HitWallSound = 1000 1
 HitWaterEffect = 68
+HitWaterSound = 36 1
 HitLavaEffect = 14
-Properties = SLAPPABLE BOULDER REBOUND_IMMUNE NO_STUN CAN_COLLIDE WITHSTAND_DOOR_HIT WITHSTAND_WALL_HIT NO_AIR_DAMAGE WIND_IMMUNE
+WithstandHitAgainst = WALL DOOR
+Properties = SLAPPABLE BOULDER REBOUND_IMMUNE NO_STUN CAN_COLLIDE NO_AIR_DAMAGE WIND_IMMUNE
 
 [shot16]
 Name = SHOT_GOD_LIGHTNING
@@ -779,8 +812,9 @@ TargetHitstopTurns = 0
 FiringSound = 55
 ShotSound = 0
 ShotSoundPriority = 1
-HitCreatureSound = 137
-Properties = REBOUND_IMMUNE WIND_IMMUNE NO_STUN WITHSTAND_DOOR_HIT WITHSTAND_WALL_HIT HIDDEN_PROJECTILE
+HitCreatureSound = 137 1
+WithstandHitAgainst = WALL DOOR
+Properties = REBOUND_IMMUNE WIND_IMMUNE NO_STUN HIDDEN_PROJECTILE EXPLODE_FLESH
 
 [shot17]
 Name = SHOT_SPIKE
@@ -803,10 +837,11 @@ TargetHitstopTurns = 0
 FiringSound = 0
 ShotSound = 0
 ShotSoundPriority = 1
-HitCreatureSound = 137
-HitWaterSound = 36
+HitCreatureSound = 137 1
 HitWaterEffect = 19
-Properties = REBOUND_IMMUNE WITHSTAND_DOOR_HIT WITHSTAND_WALL_HIT
+HitWaterSound = 36 1
+WithstandHitAgainst = WALL DOOR
+Properties = REBOUND_IMMUNE
 
 [shot18]
 Name = SHOT_LIZARD
@@ -824,7 +859,7 @@ AreaDamage = 4 35 70
 SpellEffect = 0
 Speed = 95
 BaseExperienceGain = 256
-DestroyOnHit = 0;
+DestroyOnHit = 0
 BounceAngle = 0
 BaseExperienceGain = 256
 DestroyOnHit = 1
@@ -836,9 +871,11 @@ SoftLanding = 1
 FiringSound = 0
 ShotSound = 54
 ShotSoundPriority = 1
-HitWaterSound = 36
+ExplosionEffects = 50 9 0 0
 HitWaterEffect = 19
-Properties = NO_HIT WITHSTAND_DOOR_HIT WITHSTAND_WALL_HIT
+HitWaterSound = 36 1
+WithstandHitAgainst = WALL DOOR
+Properties = NO_HIT
 
 [shot19]
 Name = SHOT_ALARM
@@ -859,7 +896,8 @@ TargetHitstopTurns = 0
 FiringSound = 0
 ShotSound = 90
 ShotSoundPriority = 4
-Properties = ALARMS_UNITS WITHSTAND_DOOR_HIT WITHSTAND_WALL_HIT HIDDEN_PROJECTILE
+WithstandHitAgainst = DOOR WALL
+Properties = ALARMS_UNITS HIDDEN_PROJECTILE
 
 [shot20]
 Name = SHOT_SOLID_BOULDER
@@ -884,10 +922,11 @@ SoftLanding = 1
 FiringSound = 0
 ShotSound = 35
 ShotSoundPriority = 1
-HitCreatureSound = 137
-HitWaterSound = 36
+HitCreatureSound = 137 1
 HitWaterEffect = 19
-Properties = SLAPPABLE BOULDER REBOUND_IMMUNE NO_STUN WITHSTAND_DOOR_HIT WITHSTAND_WALL_HIT NO_AIR_DAMAGE WIND_IMMUNE
+HitWaterSound = 36 1
+WithstandHitAgainst = DOOR WALL
+Properties = SLAPPABLE BOULDER REBOUND_IMMUNE NO_STUN NO_AIR_DAMAGE WIND_IMMUNE
 
 [shot21]
 Name = SHOT_SWING_SWORD
@@ -912,15 +951,13 @@ FiringSoundVariants = 6
 ShotSound = 0
 ShotSoundPriority = 1
 Unshaded = 1
-HitCreatureSound = 120
-HitWallSound = 128
-HitWallSoundVariants = 3
+HitCreatureSound = 120 1
 HitWallEffect = 49
-HitDoorSound = 131
-HitDoorSoundVariants = 3
+HitWallSound = 128 3
 HitDoorEffect = 49
-HitWaterSound = 36
+HitDoorSound = 131 3
 HitWaterEffect = 19
+HitWaterSound = 36 1
 Properties = STRENGTH_BASED REBOUND_IMMUNE WIND_IMMUNE HIDDEN_PROJECTILE
 
 [shot22]
@@ -946,15 +983,13 @@ FiringSoundVariants = 6
 ShotSound = 0
 ShotSoundPriority = 1
 Unshaded = 1
-HitCreatureSound = 137
-HitWallSound = 138
-HitWallSoundVariants = 3
+HitCreatureSound = 137 1
 HitWallEffect = 49
-HitDoorSound = 141
-HitDoorSoundVariants = 3
+HitWallSound = 138 3
 HitDoorEffect = 49
-HitWaterSound = 36
+HitDoorSound = 141 3
 HitWaterEffect = 19
+HitWaterSound = 36 1
 Properties = STRENGTH_BASED REBOUND_IMMUNE WIND_IMMUNE HIDDEN_PROJECTILE
 
 [shot23]
@@ -983,17 +1018,14 @@ ShotSound = 0
 ShotSoundPriority = 1
 Unshaded = 1
 SoftLanding = 1
-HitWallSound = 128
-HitWallSoundVariants = 3
 HitWallEffect = 49
-HitDoorSound = 131
-HitDoorSoundVariants = 3
+HitWallSound = 128 3
 HitDoorEffect = 49
-HitWaterSound = 36
+HitDoorSound = 131 3
 HitWaterEffect = 19
-DigSound = 63
-DigSoundVariants = 6
-DigEffect = 25
+HitWaterSound = 36 1
+DigHitEffect = 25
+DigHitSound = 63 6
 Properties = DIGGING NO_HIT WIND_IMMUNE HIDDEN_PROJECTILE
 
 [shot24]
@@ -1017,8 +1049,9 @@ TargetHitstopTurns = 0
 FiringSound = 55
 ShotSound = 0
 ShotSoundPriority = 3
-HitCreatureSound = 137
-Properties = REBOUND_IMMUNE WIND_IMMUNE NO_STUN WITHSTAND_DOOR_HIT WITHSTAND_WALL_HIT
+HitCreatureSound = 137 1
+WithstandHitAgainst = WALL DOOR
+Properties = REBOUND_IMMUNE WIND_IMMUNE NO_STUN
 
 [shot25]
 Name = SHOT_GROUP
@@ -1041,9 +1074,9 @@ TargetHitstopTurns = 0
 FiringSound = 46
 ShotSound = 43
 ShotSoundPriority = 1
-HitCreatureSound = 137
-HitWaterSound = 36
+HitCreatureSound = 137 1
 HitWaterEffect = 19
+HitWaterSound = 36 1
 Properties = REBOUND_IMMUNE WIND_IMMUNE GROUP_UP
 
 [shot26]
@@ -1067,9 +1100,12 @@ TargetHitstopTurns = 6
 FiringSound = 0
 ShotSound = 25
 ShotSoundPriority = 1
-HitCreatureSound = 137
-HitWaterSound = 36
+VisualEffect = -95
+VisualEffectAmount = 1
+VisualEffectSpread = 511
+HitCreatureSound = 137 1
 HitWaterEffect = 19
+HitWaterSound = 36 1
 Properties = NAVIGABLE REBOUND_IMMUNE
 
 [shot27]
@@ -1093,9 +1129,9 @@ TargetHitstopTurns = 6
 FiringSound = 46
 ShotSound = 43
 ShotSoundPriority = 1
-HitCreatureSound = 137
-HitWaterSound = 36
+HitCreatureSound = 137 1
 HitWaterEffect = 19
+HitWaterSound = 36 1
 
 [shot28]
 Name = SHOT_TIME_BOMB
@@ -1118,9 +1154,9 @@ TargetHitstopTurns = 6
 FiringSound = 46
 ShotSound = 43
 ShotSoundPriority = 1
-HitCreatureSound = 137
-HitWaterSound = 36
+HitCreatureSound = 137 1
 HitWaterEffect = 19
+HitWaterSound = 36 1
 
 [shot29]
 Name = SHOT_TRAP_LIGHTNING
@@ -1142,8 +1178,9 @@ Lighting = 10 52 0
 FiringSound = 55
 ShotSound = 157
 ShotSoundPriority = 1
-HitCreatureSound = 137
-Properties = REBOUND_IMMUNE NO_STUN WITHSTAND_DOOR_HIT WITHSTAND_WALL_HIT WIND_IMMUNE HIDDEN_PROJECTILE
+HitCreatureSound = 137 1
+WithstandHitAgainst = WALL DOOR
+Properties = REBOUND_IMMUNE NO_STUN WIND_IMMUNE HIDDEN_PROJECTILE
 
 [shot30]
 Name = SHOT_WORD_OF_POWER
@@ -1202,8 +1239,14 @@ BounceAngle = 0
 FiringSound = 149
 ShotSound = 47
 ShotSoundPriority = 4
-HitCreatureSound = 137
-Properties = REBOUND_IMMUNE NO_HIT NO_STUN WITHSTAND_DOOR_HIT WITHSTAND_WALL_HIT WIND_IMMUNE
+ExplosionEffects = 43 0 50 0
+VisualEffect = -1
+VisualEffectAmount = 5
+VisualEffectSpread = 127
+VisualEffectHealth = 6
+HitCreatureSound = 137 1
+WithstandHitAgainst = WALL DOOR
+Properties = REBOUND_IMMUNE NO_HIT NO_STUN WIND_IMMUNE EXPLODE_FLESH
 
 [power0]
 ; Internal name of the power, used in script and config files

src/config_magic.h
@@ -133,6 +133,7 @@ enum ShotModelFlags {
     ShMF_AlarmsUnits    = 0x0400,
     ShMF_CanCollide     = 0x0800,
     ShMF_Disarming      = 0x1000,
+    ShMF_Exploding      = 0x2000,
 };
 
 enum PowerCanCastFlags {
@@ -228,6 +229,20 @@ struct ShotHitConfig {
     unsigned char withstand; /**< Whether the shot can withstand a hit without getting destroyed; could be converted to flags. */
 };
 
+struct ShotDetonateConfig {
+    short effect1_model;
+    short effect2_model; 
+    short around_effect1_model;
+    short around_effect2_model;
+};
+
+struct ShotVisualConfig {
+    short effect_model;
+    unsigned char amount;
+    short random_range;
+    short shot_health;
+};
+
 /**
  * Configuration parameters for shots.
  */
@@ -254,6 +269,8 @@ struct ShotConfigStats {
     struct ShotHitConfig hit_lava;
     struct ShotHitConfig hit_creature;
     struct ShotHitConfig dig;
+    struct ShotDetonateConfig explode;
+    struct ShotVisualConfig visual;
     short firing_sound;
     short shot_sound;
     short sound_priority;

src/creature_states.c
@@ -3946,7 +3946,10 @@ void create_effect_around_thing(struct Thing *thing, long eff_kind)
             pos.z.val = coord_z_beg;
             while (pos.z.val <= coord_z_end)
             {
-                create_effect(&pos, eff_kind, thing->owner);
+                if (eff_kind != 0)
+                {
+                    create_used_effect_or_element(&pos, eff_kind, thing->owner);
+                }
                 pos.z.val += COORD_PER_STL;
             }
             pos.y.val += COORD_PER_STL;

src/lvl_script_commands.c
@@ -1588,15 +1588,7 @@ static void create_effect_process(struct ScriptContext *context)
     {
         pos.z.val += 128;
     }
-    struct Thing* efftng;
-    if (context->value->chars[0] >= 0)
-    {
-        efftng = create_effect(&pos, context->value->bytes[0], game.neutral_player_num);
-    }
-    else
-    {
-        efftng = create_effect_element(&pos, ~(context->value->bytes[0]) + 1, game.neutral_player_num);
-    }
+    struct Thing* efftng = create_used_effect_or_element(&pos, context->value->bytes[0], game.neutral_player_num);
     if (!thing_is_invalid(efftng))
     {
         if (thing_in_wall_at(efftng, &efftng->mappos))

commit c44a71f04e53b75eb8d62e923678801cfb2ed08b

Fixed flesh explosions, broken in #2267

src/thing_shots.c
@@ -1105,7 +1105,7 @@ void set_thing_acceleration_angles(struct Thing *thing, long angle_xy, long angl
 TbBool shot_model_makes_flesh_explosion(long shot_model)
 {
     struct ShotConfigStats* shotst = get_shot_model_stats(shot_model);
-    return (shotst->model_flags & ShMF_Exploding);
+    return ((shotst->model_flags & ShMF_Exploding) != 0);
 }
 
 long shot_hit_creature_at(struct Thing *shotng, struct Thing *trgtng, struct Coord3d *pos)