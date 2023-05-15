0ac62874f5b038f1255fce3d30352d61f61f6990
Loobinex <Loobinex@users.noreply.github.com>
2023-05-15 19:17:03 +0200

    Revert "strcpy -> strncpy"
    This reverts commit 2fd7b3536c383a2b49f5ef3472026668cde8d659.

--- src/config_creature.c
+++ src/config_creature.c
@@ -1871,7 +1871,7 @@ const char *creature_own_name(const struct Thing *creatng)
             strcat(text,part);
         }
     }
-    strncpy(cctrl->creature_name, text, sizeof(cctrl->creature_name));
+    strcpy(cctrl->creature_name, text);
     return text;
 }
 
