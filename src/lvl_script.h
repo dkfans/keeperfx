9a2b84d813c37da91886435c857ce83df092b114
Watler253 <130906143+Watler253@users.noreply.github.com>
2023-05-15 20:02:02 +0200

    More If Statement
    Double CONDITIONS_COUNT from 255 to 512?

--- src/lvl_script.h
+++ src/lvl_script.h
@@ -33,7 +33,7 @@ extern "C" {
 /******************************************************************************/
 #define PARTY_TRIGGERS_COUNT     256
 #define CREATURE_PARTYS_COUNT    256
-#define CONDITIONS_COUNT         255
+#define CONDITIONS_COUNT         512
 #define TUNNELLER_TRIGGERS_COUNT 256
 #define SCRIPT_VALUES_COUNT      1024
 #define WIN_CONDITIONS_COUNT      4
