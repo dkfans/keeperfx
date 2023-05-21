5f66c8d229e9cabd445ee32922e61dd877a48d45
Watler253 <130906143+Watler253@users.noreply.github.com>
2023-05-21 23:21:02 +0200

    Update lvl_script_commands.c

--- src/lvl_script_commands.c
+++ src/lvl_script_commands.c
@@ -247,7 +247,7 @@ const struct NamedCommand room_config_desc[] = {
   {NULL,                   0},
 };
 
-const struct NamedCommand terrain_room_total_capacity_func_type[] = {
+const struct NamedCommand room_total_capacity[] = {
   {"slabs_all_only",          1},
   {"slabs_all_wth_effcncy",   2},
   {"slabs_div2_wth_effcncy",  3},
@@ -256,7 +256,7 @@ const struct NamedCommand terrain_room_total_capacity_func_type[] = {
   {NULL,                      0},
 };
 
-const struct NamedCommand terrain_room_used_capacity_func_type[] = {
+const struct NamedCommand room_used_capacity[] = {
   {"gold_hoardes_in_room", 1},
   {"books_in_room",        2},
   {"workers_in_room",      3},
