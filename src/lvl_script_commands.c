50d719b8fa73d8ea0cb6eda9bd29f070b7624d7f
Watler253 <130906143+Watler253@users.noreply.github.com>
2023-05-22 13:46:48 +0200

    Update lvl_script_commands.c

--- src/lvl_script_commands.c
+++ src/lvl_script_commands.c
@@ -247,27 +247,6 @@ const struct NamedCommand room_config_desc[] = {
   {NULL,                   0},
 };
 
-const struct NamedCommand room_total_capacity[] = {
-  {"slabs_all_only",          1},
-  {"slabs_all_wth_effcncy",   2},
-  {"slabs_div2_wth_effcncy",  3},
-  {"gold_slabs_wth_effcncy",  4},
-  {"none",                    5},
-  {NULL,                      0},
-};
-
-const struct NamedCommand room_used_capacity[] = {
-  {"gold_hoardes_in_room", 1},
-  {"books_in_room",        2},
-  {"workers_in_room",      3},
-  {"crates_in_room",       4},
-  {"bodies_in_room",       5},
-  {"food_in_room",         6},
-  {"lair_occupants",       7},
-  {"none",                 8},
-  {NULL,                   0},
-};
-
 /**
  * Text names of groups of GUI Buttons.
  */
