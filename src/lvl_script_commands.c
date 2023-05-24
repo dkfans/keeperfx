b4bd3b301c97c1d636c17175cc0d825ab9415ed8
Watler253 <130906143+Watler253@users.noreply.github.com>
2023-05-24 17:24:21 +0200

    Update lvl_script_commands.c

--- src/lvl_script_commands.c
+++ src/lvl_script_commands.c
@@ -225,6 +225,7 @@ const struct NamedCommand trap_config_desc[] = {
   {"Destructible",        32},
   {"Unstable",            33},
   {"Unsellable",          34},
+  {"PlaceOnRoom",         35},
   {NULL,                   0},
 };
 
@@ -1290,6 +1291,9 @@ static void set_trap_configuration_process(struct ScriptContext *context)
             break;
         case 34: // Unsellable
             trapst->unsellable = value;
+            break;
+        case 35: // PlaceOnRoom
+            trapst->placeonroom = value;
             break;
         default:
             WARNMSG("Unsupported Trap configuration, variable %d.", context->value->shorts[1]);
