961d39fc434003b191035b857cbc646cafc1f45c
Watler253 <130906143+Watler253@users.noreply.github.com>
2023-05-25 00:40:16 +0200

    Update thing_traps.c

--- src/thing_traps.c
+++ src/thing_traps.c
@@ -971,10 +971,10 @@ void external_activate_trap_shot_at_angle(struct Thing *thing, long a2, struct T
     }
 }
 
-TbBool trap_can_place_on_room(ThingModel trpkind)
+TbBool trap_on_foundation(ThingModel trpkind)
 {
     struct TrapConfigStats* trapst = &gameadd.trapdoor_conf.trap_cfgstats[trpkind];
-    return trapst->placeonroom;
+    return trapst->foundation;
 }
 
 TbBool can_place_trap_on(PlayerNumber plyr_idx, MapSubtlCoord stl_x, MapSubtlCoord stl_y, ThingModel trpkind)
@@ -995,7 +995,7 @@ TbBool can_place_trap_on(PlayerNumber plyr_idx, MapSubtlCoord stl_x, MapSubtlCoo
     if (slab_kind_is_liquid(slb->kind)) {
         return false;
     }
-    if ((slabmap_owner(slb) == plyr_idx) && ((((slbattr->block_flags & (SlbAtFlg_IsRoom)) != 0) && (trap_can_place_on_room(trpkind))) || (slb->kind == SlbT_CLAIMED) || (slab_is_door(slb_x, slb_y))))
+    if ((slabmap_owner(slb) == plyr_idx) && ((((slbattr->block_flags & (SlbAtFlg_Foundation)) != 0) && (trap_on_foundation(trpkind))) || (slb->kind == SlbT_CLAIMED) || (slab_is_door(slb_x, slb_y))))
     {
         if ((!gameadd.place_traps_on_subtiles))
         {
