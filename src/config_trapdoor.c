4781633042fe81d064106c79ce274d0502781dbd
Watler253 <130906143+Watler253@users.noreply.github.com>
2023-05-26 00:25:38 +0200

    Update config_trapdoor.c
    replace PlaceOnRoom with PlaceOnBridge

--- src/config_trapdoor.c
+++ src/config_trapdoor.c
@@ -100,7 +100,7 @@ const struct NamedCommand trapdoor_trap_commands[] = {
   {"DESTRUCTIBLE",         33},
   {"UNSTABLE",             34},
   {"UNSELLABLE",           35},
-  {"PLACEONROOM",          36},
+  {"PLACEONBRIDGE",        36},
   {NULL,                    0},
 };
 /******************************************************************************/
@@ -264,7 +264,7 @@ TbBool parse_trapdoor_trap_blocks(char *buf, long len, const char *config_textna
           trapst->unstable = 0;
           trapst->unsellable = 0;
           trapst->notify = 0;
-          trapst->placeonroom = 0;
+          trapst->placeonbridge = 0;
 
           gameadd.trap_stats[i].health = 0;
           gameadd.trap_stats[i].sprite_anim_idx = 0;
@@ -920,13 +920,13 @@ TbBool parse_trapdoor_trap_blocks(char *buf, long len, const char *config_textna
                   COMMAND_TEXT(cmd_num), block_buf, config_textname);
           }
           break;
-      case 36: // PLACEONROOM
+      case 36: // PLACEONBRIDGE
           if (get_conf_parameter_single(buf, &pos, len, word_buf, sizeof(word_buf)) > 0)
           {
               k = atoi(word_buf);
               if (k >= 0)
               {
-                  trapst->placeonroom = k;
+                  trapst->placeonbridge = k;
                   n++;
               }
           }
