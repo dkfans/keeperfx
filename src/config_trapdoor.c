04edef766315162aaf7863d8b4ed79492ed9f83b
Watler253 <130906143+Watler253@users.noreply.github.com>
2023-05-30 20:44:21 +0200

    Update config_trapdoor.c

--- src/config_trapdoor.c
+++ src/config_trapdoor.c
@@ -101,6 +101,7 @@ const struct NamedCommand trapdoor_trap_commands[] = {
   {"UNSTABLE",             34},
   {"UNSELLABLE",           35},
   {"PLACEONBRIDGE",        36},
+  {"PLACEONSUBTILE",       37},
   {NULL,                    0},
 };
 /******************************************************************************/
@@ -265,6 +266,7 @@ TbBool parse_trapdoor_trap_blocks(char *buf, long len, const char *config_textna
           trapst->unsellable = 0;
           trapst->notify = 0;
           trapst->placeonbridge = 0;
+          trapst->placeonsubtile = 0;
 
           gameadd.trap_stats[i].health = 0;
           gameadd.trap_stats[i].sprite_anim_idx = 0;
@@ -927,6 +929,22 @@ TbBool parse_trapdoor_trap_blocks(char *buf, long len, const char *config_textna
               if (k >= 0)
               {
                   trapst->placeonbridge = k;
+                  n++;
+              }
+          }
+          if (n < 1)
+          {
+              CONFWRNLOG("Incorrect value of \"%s\" parameter in [%s] block of %s file.",
+                  COMMAND_TEXT(cmd_num), block_buf, config_textname);
+          }
+          break;
+      case 37: // PLACEONSUBTILE
+          if (get_conf_parameter_single(buf, &pos, len, word_buf, sizeof(word_buf)) > 0)
+          {
+              k = atoi(word_buf);
+              if (k >= 0)
+              {
+                  trapst->placeonsubtile = k;
                   n++;
               }
           }
