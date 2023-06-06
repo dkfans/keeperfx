5feecbcca2c17606f496c44e3ce39e7ea11964fc
Pieter Vandecandelaere <pieter00@gmail.com>
2023-06-06 20:33:22 +0200

    fix invalid state

--- src/player_instances.c
+++ src/player_instances.c
@@ -777,6 +777,15 @@ long pinstfe_fade_from_map(struct PlayerInfo *player, long *n)
 
 void set_player_zoom_to_position(struct PlayerInfo *player,struct Coord3d *pos)
 {
+    if(player->instance_num == PI_DirctCtrl ||
+       player->instance_num == PI_PsngrCtrl ||
+       player->instance_num == PI_HeartZoom ||
+       player->instance_num == PI_HeartZoomOut ||
+       player->instance_num == PI_CrCtrlFade ||
+       player->instance_num == PI_MapFadeTo ||
+       player->instance_num == PI_MapFadeFrom)
+        return;
+
     player->zoom_to_pos_x = pos->x.val;
     player->zoom_to_pos_y = pos->y.val;
 
