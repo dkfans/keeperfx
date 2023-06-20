4f5650b16f44f49dd9dfee8eee9d4c3843a422e8
Loobinex <Loobinex@users.noreply.github.com>
2023-06-20 23:21:23 +0200

    Any object can be a custom mystery box (#2448)
    - No longer hardcoded to object 133
    - They all set the same flags, so it is all cosmetic: different box sprite or effect compared to the other boxes

--- src/gui_tooltips.c
+++ src/gui_tooltips.c
@@ -159,7 +159,7 @@ TbBool setup_object_tooltips(struct Coord3d *pos)
   if (!thing_is_invalid(thing))
   {
       update_gui_tooltip_target(thing);
-      if (thing->model == ObjMdl_SpecboxCustom)
+      if (thing_is_custom_special_box(thing))
       {
           // TODO: get it from Map script
           if (gameadd.box_tooltip[thing->custom_box.box_kind][0] == 0)
