2f3f11cec170e0cc0ae88d69737256101275a38c
AdamPlenty <58278560+AdamPlenty@users.noreply.github.com>
2023-07-02 21:16:59 +0100

    Word of Power inflicts damage throughout its life

--- src/thing_effects.c
+++ src/thing_effects.c
@@ -1213,28 +1213,31 @@ TbBool explosion_affecting_thing(struct Thing *tngsrc, struct Thing *tngdst, con
                 }
             } else // Explosions move creatures and other things
             {
-                long move_angle = get_angle_xy_to(pos, &tngdst->mappos);
-                long move_dist = 0;
-                if (blow_strength > 0)
                 {
-                    move_dist = get_radially_decaying_value(blow_strength, max_dist / 4, max_dist * 3 / 4, distance);
-                }
-                else
-                {
-                    move_dist = get_radially_growing_value(blow_strength, max_dist / 4, max_dist * 3 / 4, distance, tngdst->inertia_floor);
-                }
-                if (move_dist != 0)
-                {
-                    tngdst->veloc_push_add.x.val += distance_with_angle_to_coord_x(move_dist, move_angle);
-                    tngdst->veloc_push_add.y.val += distance_with_angle_to_coord_y(move_dist, move_angle);
-                    tngdst->state_flags |= TF1_PushAdd;
-                    affected = true;
+                    long move_angle = get_angle_xy_to(pos, &tngdst->mappos);
+                    long move_dist = 0;
+                    if (blow_strength > 0)
+                    {
+                        move_dist = get_radially_decaying_value(blow_strength, max_dist / 4, max_dist * 3 / 4, distance);
+                    }
+                    else
+                    {
+                        move_dist = get_radially_growing_value(blow_strength, max_dist / 4, max_dist * 3 / 4, distance, tngdst->inertia_floor);
+                    }
+                    if (move_dist != 0)
+                    {
+                        tngdst->veloc_push_add.x.val += distance_with_angle_to_coord_x(move_dist, move_angle);
+                        tngdst->veloc_push_add.y.val += distance_with_angle_to_coord_y(move_dist, move_angle);
+                        tngdst->state_flags |= TF1_PushAdd;
+                        affected = true;
+                    }
                 }
             }
         }
     }
     return affected;
 }
+
 TbBool explosion_affecting_door(struct Thing *tngsrc, struct Thing *tngdst, const struct Coord3d *pos,
     MapCoordDelta max_dist, HitPoints max_damage, long blow_strength, DamageType damage_type, PlayerNumber owner)
 {
@@ -1293,14 +1296,14 @@ long explosion_effect_affecting_map_block(struct Thing *efftng, struct Thing *tn
         // Per thing processing block
         if ((thing->class_id == TCls_Door) && (efftng->shot_effect.hit_type != THit_CrtrsOnlyNotOwn)) //TODO: Find pretty way to say that WoP traps should not destroy doors. And make it configurable through configs.
         {
-            if (explosion_affecting_door(tngsrc, thing, &efftng->mappos, max_dist, max_damage, blow_strength, damage_type, owner))
+            if (explosion_affecting_door(efftng, thing, &efftng->mappos, max_dist, max_damage, blow_strength, damage_type, owner))
             {
                 num_affected++;
             }
         } else
         if (effect_can_affect_thing(efftng, thing))
         {
-            if (explosion_affecting_thing(tngsrc, thing, &efftng->mappos, max_dist, max_damage, blow_strength, damage_type, owner, no_stun))
+            if (explosion_affecting_thing(efftng, thing, &efftng->mappos, max_dist, max_damage, blow_strength, damage_type, owner, no_stun))
             {
                 num_affected++;
             }
@@ -1326,14 +1329,10 @@ long explosion_effect_affecting_map_block(struct Thing *efftng, struct Thing *tn
  */
 void word_of_power_affecting_area(struct Thing *efftng, struct Thing *tngsrc, struct Coord3d *pos)
 {
-    long stl_xmin;
-    long stl_xmax;
-    long stl_ymin;
-    long stl_ymax;
-    // Effect causes area damage only on its birth turn
-    if (efftng->creation_turn != game.play_gameturn) {
-        return;
-    }
+    MapSubtlCoord stl_xmin;
+    MapSubtlCoord stl_xmax;
+    MapSubtlCoord stl_ymin;
+    MapSubtlCoord stl_ymax;
     struct ShotConfigStats* shotst;
     if (thing_is_deployed_trap(tngsrc))
     {
@@ -1383,9 +1382,9 @@ void word_of_power_affecting_area(struct Thing *efftng, struct Thing *tngsrc, st
       stl_ymax = gameadd.map_subtiles_y;
     }
     TbBool no_stun = ((shotst->model_flags & ShMF_NoStun) != 0);
-    for (long stl_y = stl_ymin; stl_y <= stl_ymax; stl_y++)
+    for (MapSubtlCoord stl_y = stl_ymin; stl_y <= stl_ymax; stl_y++)
     {
-        for (long stl_x = stl_xmin; stl_x <= stl_xmax; stl_x++)
+        for (MapSubtlCoord stl_x = stl_xmin; stl_x <= stl_xmax; stl_x++)
         {
             struct Map* mapblk = get_map_block_at(stl_x, stl_y);
             explosion_effect_affecting_map_block(efftng, tngsrc, mapblk, max_dist,
