c038e4c005c27c57c8b905e729b2f5e09f1fd5ed
Loobinex <Loobinex@users.noreply.github.com>
2023-07-22 12:06:47 +0200

    Used enum for list end

--- src/game_merge.h
+++ src/game_merge.h
@@ -110,6 +110,7 @@ enum ClassicBugFlags {
     ClscBug_FriendlyFaint                 = 0x0800,
     ClscBug_PassiveNeutrals               = 0x1000,
     ClscBug_NeutralTortureConverts        = 0x2000,
+    ClscBug_ListEnd                       = 0x4000,
 };
 
 enum GameFlags2 {
