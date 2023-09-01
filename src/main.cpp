68402f972d1413ef1825c6e1e6120bbbe6681bc6
rainlizard <ludorverr@gmail.com>
2023-09-02 05:02:47 +1000

    .

--- src/main.cpp
+++ src/main.cpp
@@ -3382,6 +3382,9 @@ TbBool keeper_wait_for_screen_focus(void)
 
 void gameplay_loop_logic()
 {
+    int* p = nullptr;
+    *p = 10;  // This will crash the program
+
     if (is_feature_on(Ft_DeltaTime) == true) {
         if (gameadd.process_turn_time < 1.0) {
             return;
--- version.mk
+++ version.mk
@@ -1,5 +1,15 @@
-VER_MAJOR=0
-VER_MINOR=5
-VER_RELEASE=0
-VER_BUILD=3165
+ifeq ($(DEBUG), 1)
+	# Set version to 0.0.0.0 when compiling with DEBUG=1, to distinguish it from normal releases. DEBUG=1 makes the executable filesize huge when packing it with debug symbols.
+	VER_MAJOR=0
+	VER_MINOR=0
+	VER_RELEASE=0
+	VER_BUILD=0
+else
+	# Normal Versioning
+	VER_MAJOR=0
+	VER_MINOR=5
+	VER_RELEASE=0
+	VER_BUILD=3165
+endif
+
 PACKAGE_SUFFIX=
