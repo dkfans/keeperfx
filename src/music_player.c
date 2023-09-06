c6ae9036f728b0ed8b68fcbaf45eea41bceab3da
rainlizard <ludorverr@gmail.com>
2023-09-07 07:42:47 +1000

    debug=1 music crash fix

--- src/music_player.c
+++ src/music_player.c
@@ -202,8 +202,7 @@ void music_reinit_after_load()
 
 void free_custom_music()
 {
-    if (exit_keeper == true) {return;}
-    for (int i = (max_track + 1); i <= MUSIC_TRACKS_COUNT; i++)
+    for (int i = (max_track + 1); i < MUSIC_TRACKS_COUNT; i++)
     {
         Mix_Music *music = tracks[i];
         if (music != NULL)
