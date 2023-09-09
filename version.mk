6cc84a0afe596b23129ef02e4018a09f3cb4e80a
rainlizard <ludorverr@gmail.com>
2023-09-09 18:53:11 +1000

    .

--- version.mk
+++ version.mk
@@ -3,8 +3,9 @@ VER_MINOR=5
 VER_RELEASE=0
 VER_BUILD=0
 
+# Set version to 0.0.0.0 when compiling with DEBUG=1, to distinguish it from normal releases. DEBUG=1 makes the executable filesize huge when packing it with debug symbols.
 ifeq ($(DEBUG), 1)
-    VER_MAJOR=0
+	VER_MAJOR=0
 	VER_MINOR=0
 	VER_RELEASE=0
 	VER_BUILD=0
