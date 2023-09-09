17b96ef91e4320a8b9bc28c2d73147a45bcb3089
rainlizard <ludorverr@gmail.com>
2023-09-09 18:35:14 +1000

    test autobuild

--- version.mk
+++ version.mk
@@ -1,15 +1,6 @@
-ifeq ($(DEBUG), 1)
-	# Set version to 0.0.0.0 when compiling with DEBUG=1, to distinguish it from normal releases. DEBUG=1 makes the executable filesize huge when packing it with debug symbols.
-	VER_MAJOR=0
-	VER_MINOR=0
-	VER_RELEASE=0
-	VER_BUILD=0
-else
-	# Normal Versioning
-	VER_MAJOR=0
-	VER_MINOR=5
-	VER_RELEASE=0
-	VER_BUILD=3165
-endif
+VER_MAJOR=0
+VER_MINOR=0
+VER_RELEASE=0
+VER_BUILD=0
 
 PACKAGE_SUFFIX=
