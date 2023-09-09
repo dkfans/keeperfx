a1b8304dfc90652c6b3fb26fdc8d0d79108bf156
rainlizard <ludorverr@gmail.com>
2023-09-09 19:10:31 +1000

    .

--- version.mk
+++ version.mk
@@ -4,7 +4,7 @@ VER_RELEASE=333
 VER_BUILD=444
 
 # Set version to 0.0.0.0 when compiling with DEBUG=1, to distinguish it from normal releases. DEBUG=1 makes the executable filesize huge when packing it with debug symbols.
-ifeq ($(DEBUG), 1)
+ifeq ($(DEBUG), 0)
 	VER_MAJOR=555
 	VER_MINOR=666
 	VER_RELEASE=777
