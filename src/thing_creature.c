b5a71ce54c4f717b32778f1b5afd99ebbf57bd57
Loobinex <Loobinex@users.noreply.github.com>
2023-09-16 02:40:57 +0200

    Added BLOCKS_REBIRTH shot property (#2577)
    Creatures killed with a shot with this property will die instantaneously without rebirth. Includes the area damage it may do.

--- src/thing_creature.c
+++ src/thing_creature.c
@@ -2466,7 +2466,8 @@ void cause_creature_death(struct Thing *thing, CrDeathFlags flags)
         flags |= CrDed_NoEffects;
     }
     if (((flags & CrDed_NoEffects) == 0) && (crstat->rebirth != 0)
-     && (cctrl->lairtng_idx > 0) && (crstat->rebirth-1 <= cctrl->explevel))
+     && (cctrl->lairtng_idx > 0) && (crstat->rebirth-1 <= cctrl->explevel)
+        && ((flags & CrDed_NoRebirth) == 0))
     {
         creature_rebirth_at_lair(thing);
         return;
