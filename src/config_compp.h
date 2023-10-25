08412ddb0e662b0ca4edb1450279382ea6c77aa6
Loobinex <Loobinex@users.noreply.github.com>
2023-10-25 11:37:24 +0200

    changed to GameTurnDelta

--- src/config_compp.h
+++ src/config_compp.h
@@ -39,7 +39,7 @@ struct ComputerProcessTypes {
   long max_room_build_tasks;
   long turn_begin;
   long sim_before_dig;
-  long drop_delay;
+  GameTurnDelta drop_delay;
   struct ComputerProcess *processes[COMPUTER_PROCESSES_COUNT];
   struct ComputerCheck checks[COMPUTER_CHECKS_COUNT];
   struct ComputerEvent events[COMPUTER_EVENTS_COUNT];
