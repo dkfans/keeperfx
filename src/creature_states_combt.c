5a320c7776950337369ea51ecffa3dcae51041c4
AdamPlenty <58278560+AdamPlenty@users.noreply.github.com>
2023-11-22 19:25:20 +0000

    Hold-Audience-teleported units on Guard duty no longer ignore enemies at your Dungeon Heart
    Fixes #2740

--- src/creature_states_combt.c
+++ src/creature_states_combt.c
@@ -1740,8 +1740,11 @@ long ranged_combat_move(struct Thing *thing, struct Thing *enmtng, MapCoordDelta
     }
     if (cctrl->job_assigned == Job_GUARD)
     {
-        if (guard_post_combat_move(thing, nstat)) {
-            return false;
+        if ((cctrl->turns_at_job > 0))
+        {
+            if (guard_post_combat_move(thing, nstat)) {
+                return false;
+            }
         }
     }
     if (!combat_has_line_of_sight(thing, enmtng, enmdist))
@@ -2257,8 +2260,11 @@ long melee_combat_move(struct Thing *thing, struct Thing *enmtng, long enmdist, 
     }
     if (cctrl->job_assigned == Job_GUARD)
     {
-        if (guard_post_combat_move(thing, nstat)) {
-            return false;
+        if ((cctrl->turns_at_job > 0))
+        {
+            if (guard_post_combat_move(thing, nstat)) {
+                return false;
+            }
         }
     }
     if (enmdist < 156)
@@ -2645,8 +2651,11 @@ long waiting_combat_move(struct Thing *figtng, struct Thing *enmtng, long enmdis
     }
     if (figctrl->job_assigned == Job_GUARD)
     {
-        if (guard_post_combat_move(figtng, retreat_crstate)) {
-            return 0;
+        if ((figctrl->turns_at_job > 0))
+        {
+            if (guard_post_combat_move(figtng, retreat_crstate)) {
+                return false;
+            }
         }
     }
     if (enmdist < 768) {
