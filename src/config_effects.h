54634f068006fc418b703aed861ecd5a6155c024
Pieter Vandecandelaere <pieter00@gmail.com>
2023-12-05 22:10:48 +0100

    Update config_effects.h

--- src/config_effects.h
+++ src/config_effects.h
@@ -90,6 +90,7 @@ TbBool load_effects_config(const char *conf_fname,unsigned short flags);
 struct EffectConfigStats *get_effect_model_stats(ThingModel tngmodel);
 struct EffectGeneratorConfigStats *get_effectgenerator_model_stats(ThingModel tngmodel);
 const char *effect_code_name(ThingModel tngmodel);
+const char *effectgenerator_code_name(ThingModel tngmodel);
 /******************************************************************************/
 #ifdef __cplusplus
 }
