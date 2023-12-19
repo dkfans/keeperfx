80bfb7764658ae3de537a4f4443021a335254d7b
Loobinex <Loobinex@users.noreply.github.com>
2023-12-19 23:32:13 +0100

    Fixed set-hand-rule any creature I broke

--- src/lvl_script_commands.c
+++ src/lvl_script_commands.c
@@ -1894,8 +1894,8 @@ static void set_hand_rule_process(struct ScriptContext* context)
     long hand_rule_slot = context->value->shorts[2];
     long hand_rule_type = context->value->shorts[3];
     long param = context->value->shorts[4];
-    long crtr_id_start = crtr_id == (CREATURE_ANY || CREATURE_NOT_A_DIGGER) ? 0 : crtr_id;
-    long crtr_id_end = crtr_id == (CREATURE_ANY || CREATURE_NOT_A_DIGGER) ? CREATURE_TYPES_MAX : crtr_id + 1;
+    long crtr_id_start = (crtr_id == CREATURE_ANY || crtr_id == CREATURE_NOT_A_DIGGER) ? 0 : crtr_id;
+    long crtr_id_end = (crtr_id == CREATURE_ANY || crtr_id == CREATURE_NOT_A_DIGGER) ? CREATURE_TYPES_MAX : crtr_id + 1;
 
     struct Dungeon* dungeon;
     for (int i = context->plr_start; i < context->plr_end; i++)
