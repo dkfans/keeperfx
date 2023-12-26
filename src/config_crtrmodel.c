02401b2631d5befc191bdb78851aeec48924365b
walter253 <130906143+walt253@users.noreply.github.com>
2023-12-26 03:59:05 +0100

    Update config_crtrmodel.c

--- src/config_crtrmodel.c
+++ src/config_crtrmodel.c
@@ -114,6 +114,7 @@ const struct NamedCommand creatmodel_properties_commands[] = {
   {"IMMUNE_TO_DISEASE", 25},
   {"ILLUMINATED",       26},
   {"ALLURING_SCVNGR",   27},
+  {"IMMUNE_TO_FREEZE",  28},
   {NULL,                 0},
   };
 
@@ -738,6 +739,10 @@ TbBool parse_creaturemodel_attributes_blocks(long crtr_model,char *buf,long len,
                 break;
             case 27: // ALLURING_SCVNGR
                 crstat->entrance_force = true;
+                n++;
+                break;
+            case 28: // IMMUNE_TO_FREEZE
+                crconf->model_flags |= CMF_NeverFrozen;
                 n++;
                 break;
             default:
