2c55160e9e63403855923a03a7894cceabf7d26c
walter253 <130906143+walt253@users.noreply.github.com>
2023-12-31 19:17:23 +0100

    Update thing_creature.h

--- src/thing_creature.h
+++ src/thing_creature.h
@@ -93,6 +93,7 @@ struct Thing *create_owned_special_digger(MapCoord x, MapCoord y, PlayerNumber o
 
 TbBool creature_increase_level(struct Thing *thing);
 TbBool creature_change_multiple_levels(struct Thing *thing, int count);
+void decrease_level(struct PlayerInfo *player, int count);
 void set_creature_level(struct Thing *thing, long nlvl);
 void init_creature_level(struct Thing *thing, long nlev);
 long get_creature_speed(const struct Thing *thing);
