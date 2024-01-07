15b8cca586f67344253905320aa48bd8acc8de1f
walter253 <130906143+walt253@users.noreply.github.com>
2024-01-07 07:59:16 +0100

    Update config_rules.h

--- src/config_rules.h
+++ src/config_rules.h
@@ -95,6 +95,9 @@ struct GameRulesConfig {
     TbBool allies_share_drop;
     TbBool allies_share_cta;
     unsigned char max_things_in_hand;
+    TbBool torture_payday;
+    TbBool torture_training_cost;
+    TbBool torture_scavenging_cost;
 };
 
 struct ComputerRulesConfig {
