496f25e1fe59b286fafbd9e9ada1e891792265f2
walter253 <130906143+walt253@users.noreply.github.com>
2024-01-10 01:13:35 +0100

    CostIncreaseOnExp for Training&Scavenging (#2940)
    Also added 3 game rules related to creature costs while one of them is getting scavenged.

--- src/config_rules.c
+++ src/config_rules.c
@@ -79,6 +79,9 @@ const struct NamedCommand rules_game_commands[] = {
   {"ALLIESSHAREDROP",            35},
   {"ALLIESSHARECTA",             36},
   {"MAXTHINGSINHAND",            37},
+  {"TORTUREPAYDAY",              38},
+  {"TORTURETRAININGCOST",        39},
+  {"TORTURESCAVENGINGCOST",      40},
   {NULL,                          0},
   };
 
@@ -757,6 +760,45 @@ TbBool parse_rules_game_blocks(char *buf, long len, const char *config_textname,
             {
               CONFWRNLOG("Incorrect value of \"%s\" parameter in [%s] block of %s file.",
                   COMMAND_TEXT(cmd_num),block_buf,config_textname);
+            }
+            break;
+        case 38: // TORTUREPAYDAY
+            if (get_conf_parameter_single(buf, &pos, len, word_buf, sizeof(word_buf)) > 0)
+            {
+                k = atoi(word_buf);
+                game.conf.rules.game.torture_payday = k;
+                n++;
+            }
+            if (n < 1)
+            {
+                CONFWRNLOG("Incorrect value of \"%s\" parameter in [%s] block of %s file.",
+                    COMMAND_TEXT(cmd_num), block_buf, config_textname);
+            }
+            break;
+        case 39: // TORTURETRAININGCOST
+            if (get_conf_parameter_single(buf, &pos, len, word_buf, sizeof(word_buf)) > 0)
+            {
+                k = atoi(word_buf);
+                game.conf.rules.game.torture_training_cost = k;
+                n++;
+            }
+            if (n < 1)
+            {
+                CONFWRNLOG("Incorrect value of \"%s\" parameter in [%s] block of %s file.",
+                    COMMAND_TEXT(cmd_num), block_buf, config_textname);
+            }
+            break;
+        case 40: // TORTURESCAVENGINGCOST
+            if (get_conf_parameter_single(buf, &pos, len, word_buf, sizeof(word_buf)) > 0)
+            {
+                k = atoi(word_buf);
+                game.conf.rules.game.torture_scavenging_cost = k;
+                n++;
+            }
+            if (n < 1)
+            {
+                CONFWRNLOG("Incorrect value of \"%s\" parameter in [%s] block of %s file.",
+                    COMMAND_TEXT(cmd_num), block_buf, config_textname);
             }
             break;
         case 0: // comment
