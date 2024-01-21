7cc59f1ad98fc7d945cc881c41525f5fc2d49b46
Loobinex <Loobinex@users.noreply.github.com>
2024-01-21 17:38:46 +0100

    actually, gave it the correct name

--- src/player_comptask.c
+++ src/player_comptask.c
@@ -2331,7 +2331,7 @@ static struct Thing *find_creature_for_call_to_arms(struct Computer2 *comp, TbBo
 
         if ( flag_is_set(i->alloc_flags, TAlF_IsInLimbo) )
             continue;
-        if (flag_is_set(i->state_flags, TAlF_IsInMapWho) )
+        if (flag_is_set(i->state_flags, TF1_InCtrldLimbo) )
             continue;
         if ( i->active_state == CrSt_CreatureUnconscious )
             continue;
