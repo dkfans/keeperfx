eaa2f10469a7f7280a0ce06d3c3ec9952baaab09
Loobinex <Loobinex@users.noreply.github.com>
2024-03-26 00:40:16 +0100

    fixed non-idle hero spells

--- src/creature_states_combt.c
+++ src/creature_states_combt.c
@@ -1886,7 +1886,7 @@ CrInstance get_self_spell_casting(const struct Thing *thing)
                         }
                         INSTANCE_RET_IF_AVAIL(thing, i);
                     } else {
-                        if (flag_is_set(inst_inf->activation_flags, InstAF_Roaming))
+                        if (flag_is_set(inst_inf->activation_flags, InstAF_Roaming) || (get_creature_state_type(thing) != CrStTyp_Idle))
                         {
                             if (flag_is_set(inst_inf->activation_flags, InstAF_WhileInjured))
                             {
