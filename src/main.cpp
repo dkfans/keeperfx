90f8e5f3e7d12a7ebb97b3a737d5d66b7d1147dd
Yani <yani@protonmail.com>
2024-03-29 14:06:04 +0100

    also do callbacks outside of a game

--- src/main.cpp
+++ src/main.cpp
@@ -3938,6 +3938,7 @@ void game_loop(void)
       close_packet_file();
       game.packet_load_enable = false;
       game.packet_save_enable = false;
+      steam_api_run_callbacks();
     } // end while
 
     ShutdownMusicPlayer();
