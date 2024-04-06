37960eb764f317d73f2c4770bf7df030f7f6fe5a
Loobinex <Loobinex@users.noreply.github.com>
2024-04-06 19:52:20 +0200

    grow base clipbox size along with it.

--- src/engine_render.c
+++ src/engine_render.c
@@ -4046,8 +4046,9 @@ static void add_draw_status_box(struct Thing *thing, struct EngineCoord *ecor)
     struct EngineCoord coord = *ecor;
     const struct CreatureStats* crstat = creature_stats_get_from_thing(thing);
     struct CreatureControl* cctrl = creature_control_get_from_thing(thing);
-    short offset = (crstat->status_offset + (crstat->status_offset * game.conf.crtr_conf.exp.size_increase_on_exp * cctrl->explevel) / 100);
-    coord.y += thing->clipbox_size_z + offset;
+    short offset = thing->clipbox_size_z + crstat->status_offset;
+    offset += (offset * game.conf.crtr_conf.exp.size_increase_on_exp * cctrl->explevel) / 100;
+    coord.y += offset;
     rotpers(&coord, &camera_matrix);
 
     int z_val = coord.z;
