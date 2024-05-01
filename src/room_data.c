c6aa2fa07f2a6b87ac2d6a4b6cfaba80dbc01873
walter253 <130906143+walt253@users.noreply.github.com>
2024-05-01 19:41:41 +0200

    Update room_data.c

--- src/room_data.c
+++ src/room_data.c
@@ -63,6 +63,7 @@ void count_slabs_all_only(struct Room *room);
 void count_slabs_all_wth_effcncy(struct Room *room);
 void count_slabs_div2_wth_effcncy(struct Room *room);
 void count_gold_slabs_wth_effcncy(struct Room *room);
+void count_gold_slabs_full(struct Room *room);
 
 void count_gold_hoardes_in_room(struct Room *room);
 void count_books_in_room(struct Room *room);
@@ -979,6 +980,11 @@ void count_gold_slabs_wth_effcncy(struct Room *room)
     if (count < 1)
         count = 1;
     room->total_capacity = count;
+}
+
+void count_gold_slabs_full(struct Room *room)
+{
+    room->total_capacity = room->slabs_count * get_wealth_size_types_count();
 }
 
 TbBool recreate_repositioned_crate_in_room_on_subtile(struct Room *room, MapSubtlCoord stl_x, MapSubtlCoord stl_y, struct RoomReposition * rrepos)
