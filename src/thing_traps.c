f372e4cadbe6cb1a3f5337398e26c2ed99f8255e
Loobinex <Loobinex@users.noreply.github.com>
2024-06-02 18:15:17 +0200

    Volley traps can shoot a second time

--- src/thing_traps.c
+++ src/thing_traps.c
@@ -864,6 +864,7 @@ TngUpdateRet update_trap(struct Thing *traptng)
     if (trap_is_active(traptng))
     {
         update_trap_trigger(traptng);
+        traptng->trap.volley_fire = false;
     }
     if (map_pos_is_lava(traptng->mappos.x.stl.num, traptng->mappos.y.stl.num) && !thing_is_dragged_or_pulled(traptng))
     {
