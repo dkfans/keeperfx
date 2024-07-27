dkfans/keeperfx

commit f5facf298ac48e814f61bef564acbeef0678de90

Computer Players no longer flawless in job assignment (#3052)
`MOVE CREATURE TO BEST ROOM` and `MOVE CREATURE TO ROOM` computer processes accept a percentage of creatures that would be assigned to a good room. But it would move that percentage each check, so it quickly had 100% of creatures in a good room. Now it will stick to that percentage by checking how many units are already assigned before moving more creatures.

By default, they are nearly flawless still. Opens up more limited computer players in a future version.

src/player_compchecks.c
@@ -195,17 +195,70 @@ long computer_checks_hates(struct Computer2 *comp, struct ComputerCheck * check)
     }
     return CTaskRet_Unk4;
 }
+// 100 percent_to_reassign = num_to_move is high and creatures are moved around more
+// 0 percent_to_reassign = num_to_move is 0 and all creatures do their default jobs
+int calculate_number_of_creatures_to_move(struct Dungeon *dungeon, int percent_to_reassign)
+{
+    int creatures_doing_primary_or_secondary_job = 0;
+    int creatures_doing_other_jobs = 0;
+
+    for (int i = dungeon->creatr_list_start; i != 0;)
+    {
+        struct Thing* thing = thing_get(i);
+        TRACE_THING(thing);
+        struct CreatureControl* cctrl = creature_control_get_from_thing(thing);
+        if (thing_is_invalid(thing) || creature_control_invalid(cctrl))
+        {
+            ERRORLOG("Jump to invalid creature detected");
+            break;
+        }
+
+        if (!creature_is_being_unconscious(thing) && !thing_is_picked_up(thing) && !creature_is_kept_in_custody_by_enemy(thing))
+        {
+            struct CreatureStats* crstat = creature_stats_get_from_thing(thing);
+            if ((cctrl->job_assigned == crstat->job_primary) || (cctrl->job_assigned == crstat->job_secondary))
+            {
+                creatures_doing_primary_or_secondary_job += 1;
+            } else {
+                creatures_doing_other_jobs += 1;
+            }
+        }
+
+        i = cctrl->players_next_creature_idx;
+    }
+
+    int work_capable_creatures = creatures_doing_primary_or_secondary_job + creatures_doing_other_jobs;
+    if (work_capable_creatures == 0) {
+        return 0;
+    }
+    
+    int percent_doing_other_jobs = (creatures_doing_other_jobs * 100) / work_capable_creatures;
+    int num_to_move = work_capable_creatures * (percent_to_reassign - percent_doing_other_jobs) / 100;
+    if (num_to_move <= 0) {return 0;}
+
+    //JUSTLOG("-----", 0);
+    //JUSTLOG("total creatures = %d", dungeon->num_active_creatrs);
+    //JUSTLOG("work_capable_creatures = %d", work_capable_creatures);
+    //JUSTLOG("cfg percent to reassign = %d percent should do other jobs", percent_to_reassign);
+    //JUSTLOG("creatures_doing_primary_or_secondary_job = %d", creatures_doing_primary_or_secondary_job);
+    //JUSTLOG("creatures_doing_other_jobs = %d", creatures_doing_other_jobs);
+    return num_to_move;
+}
+
 
 long computer_check_move_creatures_to_best_room(struct Computer2 *comp, struct ComputerCheck * check)
 {
     struct Dungeon* dungeon = comp->dungeon;
     SYNCDBG(8,"Starting for player %d",(int)dungeon->owner);
-    int num_to_move = check->param1 * dungeon->num_active_creatrs / 100;
+    
+    int num_to_move = calculate_number_of_creatures_to_move(dungeon, check->param1);
+    
     if (num_to_move <= 0) {
         SYNCDBG(8,"No player %d creatures to move, active %d percentage %d",
             (int)dungeon->owner,(int)dungeon->num_active_creatrs,(int)check->param1);
         return CTaskRet_Unk4;
     }
+
     if (!computer_able_to_use_power(comp, PwrK_HAND, 1, num_to_move)) {
         return CTaskRet_Unk4;
     }
@@ -225,7 +278,7 @@ long computer_check_move_creatures_to_room(struct Computer2 *comp, struct Comput
 {
     struct Dungeon* dungeon = comp->dungeon;
     SYNCDBG(8,"Checking player %d for move to %s", (int)dungeon->owner, room_code_name(check->param2));
-    int num_to_move = check->param1 * dungeon->num_active_creatrs / 100;
+    int num_to_move = calculate_number_of_creatures_to_move(dungeon, check->param1);
     if (num_to_move <= 0) {
         SYNCDBG(8,"No creatures to move, active %d percentage %d", (int)dungeon->num_active_creatrs, (int)check->param1);
         return CTaskRet_Unk4;

commit 78685e0c8117fab52755aea62901fd373257de71

Fixed computers sometimes not assigning creatures to rooms (#3371)
Fixes #3369

Bug was introduced in f5facf2

src/player_compchecks.c
@@ -217,7 +217,7 @@ int calculate_number_of_creatures_to_move(struct Dungeon *dungeon, int percent_t
         if (!creature_is_being_unconscious(thing) && !thing_is_picked_up(thing) && !creature_is_kept_in_custody_by_enemy(thing))
         {
             struct CreatureStats* crstat = creature_stats_get_from_thing(thing);
-            if ((cctrl->job_assigned == crstat->job_primary) || (cctrl->job_assigned == crstat->job_secondary))
+            if ((cctrl->job_assigned == crstat->job_primary) || (cctrl->job_assigned == crstat->job_secondary) || (cctrl->job_assigned == 0))
             {
                 creatures_doing_primary_or_secondary_job += 1;
             } else {