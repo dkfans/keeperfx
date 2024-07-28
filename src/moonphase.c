8cb1692d4f46ee866c71952829d8f08878831742
Yani <yani@protonmail.com>
2024-07-28 17:05:58 +0200

    change some code around

--- src/moonphase.c
+++ src/moonphase.c
@@ -1,36 +1,28 @@
+#include "pre_inc.h"
 #include "moonphase.h"
+#include "post_inc.h"
 
-#ifdef __cplusplus
-extern "C"
+/**
+ * Calculate the moon phase.
+ *
+ * 0.0  = new moon,
+ * 0.25 = first quarter,
+ * 0.5  = full moon,
+ * 0.75 = third quarter
+ *
+ * Returns -1 on failure
+ */
+double moonphase_calculate()
 {
-#endif
+    // Get current time
+    astro_time_t time = Astronomy_CurrentTime();
 
-    /**
-     * Calculate the moon phase.
-     *
-     * 0.0  = new moon,
-     * 0.25 = first quarter,
-     * 0.5  = full moon,
-     * 0.75 = third quarter
-     *
-     * Returns -1 on failure
-     */
-    double moonphase_calculate()
+    // Get phase of the moon
+    astro_angle_result_t phase = Astronomy_MoonPhase(time);
+    if (phase.status != ASTRO_SUCCESS)
     {
-        // Get current time
-        astro_time_t time = Astronomy_CurrentTime();
-
-        // Get phase of the moon
-        astro_angle_result_t phase = Astronomy_MoonPhase(time);
-        if (phase.status != ASTRO_SUCCESS)
-        {
-            WARNLOG("Failed to get moon phase");
-            return -1;
-        }
-
-        return phase.angle / 360;
+        return -1;
     }
 
-#ifdef __cplusplus
+    return phase.angle / 360;
 }
-#endif
