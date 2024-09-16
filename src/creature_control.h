c3f4c261cfad7d21516febf0cef6a1b405717b8f
benlp91 <benedict_lp@hotmail.com>
2024-09-16 16:16:13 +0200

    last version

--- src/creature_control.h
+++ src/creature_control.h
@@ -466,7 +466,7 @@ struct CreatureStats { // These stats are not compatible with original DK - they
     /** Instance identifiers of the instances creature can learn. */
     short learned_instance_id[LEARNED_INSTANCES_COUNT];
     /** Required level to use the instances creature can learn. Scaled 1..CREATURE_MAX_LEVEL. */
-    unsigned char learned_instance_level[LEARNED_INSTANCES_COUNT];
+    unsigned short learned_instance_level[LEARNED_INSTANCES_COUNT];
     unsigned char research_value;
     TbBool humanoid_creature;
     TbBool piss_on_dead;
