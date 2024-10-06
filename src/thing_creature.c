dkfans/keeperfx

commit 9e46b6e6ab79160ed6c170de9d64c3c5d3d40470

Add new Creature spell - Ranged Heal (#3463)
Co-authored-by: Evan (Yifan) Jian <linvail.cs@gmail.com>
Co-authored-by: Loobinex <Loobinex@users.noreply.github.com>

src/thing_creature.c
@@ -373,11 +373,11 @@ TbBool load_swipe_graphic_for_creature(const struct Thing *thing)
     return true;
 }
 
-/** 
+/**
  * Randomise the draw direction of the swipe sprite in the first-person possession view.
- * 
+ *
  * Sets PlayerInfo->swipe_sprite_drawLR to either TRUE or FALSE.
- * 
+ *
  * Draw direction is either: left-to-right (TRUE) or right-to-left (FALSE)
  */
 void randomise_swipe_graphic_direction()
@@ -912,7 +912,7 @@ void first_apply_spell_effect_to_thing(struct Thing *thing, SpellKind spell_idx,
         if (n < 0)
         {
             thing->health = 0;
-        } else 
+        } else
         {
             thing->health = min(n, cctrl->max_health);
         }
@@ -1566,7 +1566,7 @@ short creature_take_wage_from_gold_pile(struct Thing *creatng,struct Thing *gold
  * @param spl_idx Spell index to be casted.
  * @param shot_lvl Spell level to be casted.
  */
-void creature_cast_spell_at_thing(struct Thing *castng, struct Thing *targetng, long spl_idx, long shot_lvl)
+void creature_cast_spell_at_thing(struct Thing *castng, struct Thing *targetng, SpellKind spl_idx, long shot_lvl)
 {
     unsigned char hit_type;
     if ((castng->alloc_flags & TAlF_IsControlled) != 0)
@@ -1776,12 +1776,15 @@ void remove_creature_from_summon_list(struct Dungeon* dungeon, ThingIndex famlrt
     }
 }
 /**
- * Casts a spell by caster creature targeted at given coordinates, most likely using shot to transfer the spell.
+ * @brief Casts a spell by caster creature targeted at given coordinates, most likely using shot to transfer the spell.
+ *
  * @param castng The caster creature.
  * @param spl_idx Spell index to be casted.
  * @param shot_lvl Spell level to be casted.
+ * @param trg_x
+ * @param trg_y
  */
-void creature_cast_spell(struct Thing *castng, long spl_idx, long shot_lvl, long trg_x, long trg_y)
+void creature_cast_spell(struct Thing *castng, SpellKind spl_idx, long shot_lvl, MapSubtlCoord trg_x, MapSubtlCoord trg_y)
 {
     long i;
     const struct SpellConfig* spconf = get_spell_config(spl_idx);
@@ -1796,8 +1799,8 @@ void creature_cast_spell(struct Thing *castng, long spl_idx, long shot_lvl, long
         cctrl->teleport_x = trg_x;
         cctrl->teleport_y = trg_y;
     }
-    // Check if the spell can be fired as a shot
-    if (spconf->shot_model > 0)
+    // Check if the spell can be fired as a shot. It is definitely not if casted on itself.
+    if ((spconf->shot_model > 0) && (cctrl->targtng_idx != castng->index))
     {
         if ((castng->alloc_flags & TAlF_IsControlled) != 0)
           i = THit_CrtrsNObjcts;
@@ -1984,7 +1987,7 @@ void creature_look_for_hidden_doors(struct Thing *creatng)
         struct Thing* doortng = thing_get(i);
         if (thing_is_invalid(doortng))
           break;
-          
+
         if (door_is_hidden_to_player(doortng,creatng->owner))
         {
             MapSubtlCoord z = doortng->mappos.z.stl.num;
@@ -1998,7 +2001,7 @@ void creature_look_for_hidden_doors(struct Thing *creatng)
             }
             else
             // when closed the door itself blocks sight to the doortng so this checks if open, and in sight
-            if(creature_can_see_thing(creatng,doortng)) 
+            if(creature_can_see_thing(creatng,doortng))
             {
                 reveal_secret_door_to_player(doortng,creatng->owner);
             }
@@ -3408,9 +3411,37 @@ short get_creature_eye_height(const struct Thing *creatng)
     return (base_height + (base_height * game.conf.crtr_conf.exp.size_increase_on_exp * cctrl->explevel) / 100);
 }
 
-ThingIndex get_human_controlled_creature_target(struct Thing *thing, long primary_target)
+/**
+ * @brief Get the target of the given instance in the Possession mode.
+ *
+ * @param thing The Thing object of the caster.
+ * @param inst_id The index of the instance be used.
+ * @param packet The caster's owner's control's or action's packet. This is optional.
+ * @return ThingIndex The index of the target thing.
+ */
+ThingIndex get_human_controlled_creature_target(struct Thing *thing, CrInstance inst_id, struct Packet *packet)
 {
     ThingIndex index = 0;
+    struct InstanceInfo *inst_inf = creature_instance_info_get(inst_id);
+
+    if((inst_inf->instance_property_flags & InstPF_SelfBuff) != 0)
+    {
+        if ((inst_inf->instance_property_flags & InstPF_RangedBuff) == 0 ||
+            ((packet != NULL) && (packet->additional_packet_values & PCAdV_CrtrContrlPressed) != 0))
+        {
+            // If it doesn't has RANGED_BUFF or the Possession key (default:left shift) is pressed,
+            // cast on the caster itself.
+            return thing->index;
+        }
+    }
+    if((inst_inf->instance_property_flags & InstPF_RangedBuff) != 0)
+    {
+        if(inst_inf->primary_target != 5 && inst_inf->primary_target != 6)
+        {
+            ERRORLOG("The instance %d has RANGED_BUFF property but has no valid primary target.", inst_id);
+        }
+    }
+
     struct Thing *i;
     long angle_xy_to;
     long angle_difference;
@@ -3453,7 +3484,7 @@ ThingIndex get_human_controlled_creature_target(struct Thing *thing, long primar
                         if (i != thing)
                         {
                             TbBool is_valid_target;
-                            switch (primary_target)
+                            switch (inst_inf->primary_target)
                             {
                                 case 1:
                                     is_valid_target = ((thing_is_creature(i) && !creature_is_being_unconscious(i)) || thing_is_dungeon_heart(i));
@@ -3480,7 +3511,7 @@ ThingIndex get_human_controlled_creature_target(struct Thing *thing, long primar
                                     is_valid_target = true;
                                     break;
                                 default:
-                                    ERRORLOG("Illegal primary target type for shot: %d", (int)primary_target);
+                                    ERRORLOG("Illegal primary target type for shot: %d", (int)inst_inf->primary_target);
                                     is_valid_target = false;
                                     break;
                             }
@@ -3919,7 +3950,7 @@ void remove_first_creature(struct Thing *creatng)
         if (!flag_is_set(cctrl->flgfield_2, TF2_Spectator) && !flag_is_set(cctrl->flgfield_2, TF2_SummonedCreature))
         {
             dungeon->owned_creatures_of_model[creatng->model]--;
-            dungeon->num_active_diggers--;  
+            dungeon->num_active_diggers--;
         }
     } else
     {
@@ -5399,11 +5430,11 @@ void check_for_creature_escape_from_lava(struct Thing *thing)
  */
 ThingModel get_footstep_effect_element(struct Thing* thing)
 {
-    static const unsigned char tileset_footstep_textures[TEXTURE_VARIATIONS_COUNT] = 
+    static const unsigned char tileset_footstep_textures[TEXTURE_VARIATIONS_COUNT] =
     { TngEffElm_None,       TngEffElm_None,         TngEffElm_IceMelt3,     TngEffElm_None,
       TngEffElm_None,       TngEffElm_None,         TngEffElm_None,         TngEffElm_None,
       TngEffElm_StepSand,   TngEffElm_StepGypsum,   TngEffElm_None,         TngEffElm_None,
-      TngEffElm_None,       TngEffElm_None,         TngEffElm_None,         TngEffElm_None 
+      TngEffElm_None,       TngEffElm_None,         TngEffElm_None,         TngEffElm_None
     };
 
     short texture;
@@ -5893,7 +5924,13 @@ TngUpdateRet update_creature(struct Thing *thing)
     {
         return TUFRet_Deleted;
     }
-    process_creature_self_spell_casting(thing);
+
+    if (process_creature_self_spell_casting(thing) == 0)
+    {
+        // If this creature didn't cast anything to itself, try to help others.
+        process_creature_ranged_buff_spell_casting(thing);
+    }
+
     cctrl->moveaccel.x.val = 0;
     cctrl->moveaccel.y.val = 0;
     cctrl->moveaccel.z.val = 0;
@@ -6466,7 +6503,7 @@ void controlled_creature_drop_thing(struct Thing *creatng, struct Thing *droptng
                             
                         }
                     }
-                }    
+                }
             }
         }
     }

config/fxdata/creature.cfg
@@ -58,9 +58,9 @@ TooltipTextID = 201
 SymbolSprites = 0
 ; Graphics animation used to display creature during the instance
 Graphics = STAND
-; Minimum distance the enemy can be to cast the offensive spell.
+; Minimum distance the target can be to cast the spell.
 RangeMin = 0
-; Maximal distance the enemy can be to cast the offensive spell.
+; Maximal distance the target can be to cast the spell.
 RangeMax = 0
 ; Type of thing the aim assist will target first. Full meaning unknown, range 1~8.
 ; 8 targets anything, creatures targetted by all.
@@ -69,10 +69,11 @@ RangeMax = 0
 ; 7 will target traps.
 PrimaryTarget = 0
 ; Instance property flags:
-; RANGED_DEBUFF will be used in  battle, but does not count as an attack
+; RANGED_DEBUFF will be used on enemy creatures, but does not count as an attack
 ; DANGEROUS will be avoided when attacking Doors and Hearts
 ; DESTRUCTIVE is required when attacking Doors and Hearts
-; SELF_BUFF does not do anything
+; SELF_BUFF can be applied to caster.
+; RANGED_BUFF can be applied to another friendly creature.
 ; REPEAT_TRIGGER allows player to hold down the mouse button to cast
 ; QUICK ranged attacks will be used by units going postal
 ; DISARMING allows instance to be used against traps
@@ -80,6 +81,10 @@ PrimaryTarget = 0
 Properties =
 ; Function used as the instance action, and its parameters
 Function = none 0 0
+; Functions used validate the source and the target of the spell.
+ValidateFunc = validate_source_generic validate_target_generic
+; Functions used search targets of the spell.
+SearchTargetsFunc = search_target_generic
 
 [instance1]
 Name = SWING_WEAPON_SWORD
@@ -945,6 +950,27 @@ Graphics = SCREAM
 Properties = SELF_BUFF
 PrimaryTarget = 8
 Function = creature_cast_spell SPELL_SUMMON_CREATURE 0
+
+[instance50]
+Name = RANGED_HEAL
+Time = 10
+ActionTime = 6
+ResetTime = 400
+FPTime = 5
+FPActionTime = 3
+FPResetTime = 250
+FPInstantCast = 0
+ForceVisibility = 10
+TooltipTextID = 1066
+SymbolSprites = 414
+Graphics = ATTACK
+RangeMin = MIN
+RangeMax = 5120
+PrimaryTarget = 6
+Properties = RANGED_BUFF SELF_BUFF
+Function = creature_cast_spell SPELL_HEAL 0
+ValidateFunc = validate_source_ranged_heal validate_target_ranged_heal
+SearchTargetsFunc = search_target_ranged_heal
 
 [job0]
 ; Empty job, indicates no job assigned

config/fxdata/crstates.cfg
@@ -23,7 +23,7 @@ Name = ImpDigsDirt
 Name = ImpMinesGold
 
 [state6]
-Name = Null6
+Name = CreatureCastingPreparation
 
 [state7]
 Name = ImpDropsGold

config/fxdata/effects.toml
@@ -5017,3 +5017,43 @@ LightRadius = 0
 LightIntensity = 0
 LightFlags = 0
 AffectedByWind = 1
+
+[effectElement119]
+Name = "EFFECTELEMENT_HEAL_BEAM"
+DrawClass = 2
+MoveType = 1
+Unanimated = 0
+Lifespan = [0,2]
+AnimationId = 802
+SpriteSize = [122,142]
+RenderFlags = 0
+SpriteSpeed = [192,256]
+AnimateOnFloor = false
+Unshaded = true
+Transparent = 3
+MovementFlags = 0
+SizeChange = 0
+FallAcceleration = 10
+InertiaFloor = 102
+InertiaAir = 0
+SubeffectModel = 0
+SubeffectDelay = 0
+Movable = true
+Impacts = true
+SolidGroundEffmodel = 0
+SolidGroundSoundId = 0
+SolidGroundLoudness = 256
+SolidGroundDestroyOnImpact = false
+WaterEffmodel = 19
+WaterSoundId = 36
+WaterLoudness = 80
+WaterDestroyOnImpact = false
+LavaEffmodel = 0
+LavaSoundId = 0
+LavaLoudness = 256
+LavaDestroyOnImpact = false
+TransformModel = 0
+LightRadius = 0
+LightIntensity = 0
+LightFlags = 0
+AffectedByWind = 1

config/fxdata/magic.cfg
@@ -88,8 +88,8 @@ SymbolSprites = 355 412
 
 [spell7]
 Name = SPELL_HEAL
-CastAtThing = 0
-ShotModel = NOSHOT
+CastAtThing = 1
+ShotModel = SHOT_RANGED_HEAL
 SpellPower = POWER_HEAL_CREATURE
 Duration = 50
 AuraEffect = -45
@@ -1328,6 +1328,28 @@ HitCreatureSound = 137 1
 WithstandHitAgainst = WALL DOOR
 Properties = NO_HIT REBOUND_IMMUNE WIND_IMMUNE EXPLODE_FLESH NO_STUN
 UpdateLogic = 7
+
+[shot33]
+Name = SHOT_RANGED_HEAL
+Animation = 0
+AnimationSize = 0
+AnimationTransparency = 0
+Size_XY = 128
+Size_Z = 128
+Health = 2
+Damage = 0
+DamageType =
+HitType = 11
+Speed = 256
+MaxRange = 5120
+BaseExperienceGain = 0
+DestroyOnHit = 0
+TargetHitstopTurns = 0
+SpellEffect = 7
+Properties = REBOUND_IMMUNE WIND_IMMUNE HIDDEN_PROJECTILE
+FireLogic = 1
+EffectModel = EFFECTELEMENT_HEAL_BEAM
+EffectSpacing = 96
 
 ; Powers types.
 

lang/gtext_chi.po
@@ -1658,7 +1658,7 @@ msgstr "冰雹术: 发射密集的冰雹块。"
 
 #: guitext:245
 msgctxt "Creature spell"
-msgid "Slow: Slows down the target creature, making it attacks and movement delayed."
+msgid "Slow: Slows down the target creature, making its attacks and movement delayed."
 msgstr "缓慢术: 降低目标怪物的速度，使它的行动和攻击变得迟缓。"
 
 #: guitext:246

lang/gtext_cht.po
@@ -2105,7 +2105,7 @@ msgstr "冰雹术: [26.10]"
 #, fuzzy
 msgctxt "Creature spell"
 msgid ""
-"Slow: Slows down the target creature, making it attacks and movement delayed."
+"Slow: Slows down the target creature, making its attacks and movement delayed."
 msgstr "缓慢术: [26.19]"
 
 #: guitext:246

lang/gtext_cze.po
@@ -2240,7 +2240,7 @@ msgstr "Krupobiti:"
 #: guitext:245
 msgctxt "Creature spell"
 msgid ""
-"Slow: Slows down the target creature, making it attacks and movement delayed."
+"Slow: Slows down the target creature, making its attacks and movement delayed."
 msgstr "Zpomaleni:"
 
 #: guitext:246

commit 3433621586ba0cb9ca102e044eb2844bfb6ad46a

Fixed wind spell that broke recently (#3528)
Caused by (#3463)

src/thing_creature.c
@@ -1802,25 +1802,33 @@ void creature_cast_spell(struct Thing *castng, SpellKind spl_idx, long shot_lvl,
         cctrl->teleport_x = trg_x;
         cctrl->teleport_y = trg_y;
     }
-    // Check if the spell can be fired as a shot. It is definitely not if casted on itself.
-    if ((spconf->shot_model > 0) && (cctrl->targtng_idx != castng->index))
-    {
-        if ((castng->alloc_flags & TAlF_IsControlled) != 0)
-            i = THit_CrtrsNObjcts;
-        else
-            i = THit_CrtrsOnlyNotOwn;
 
-        SYNCDBG(8,"The %s(%d) fire shot(%s) without a target with shot level %d, hit type: 0x%X",
-            thing_model_name(castng), castng->index, shot_code_name(spconf->shot_model), shot_lvl, i);
-        thing_fire_shot(castng, INVALID_THING, spconf->shot_model, shot_lvl, i);
-    }
-    // Check if the spell can be self-casted
-    else if (spconf->caster_affected)
+    if (spconf->caster_affected && castng->index == cctrl->targtng_idx)
     {
         if (spconf->caster_affect_sound > 0)
           thing_play_sample(castng, spconf->caster_affect_sound, NORMAL_PITCH, 0, 3, 0, 4, FULL_LOUDNESS);
         apply_spell_effect_to_thing(castng, spl_idx, cctrl->explevel);
     }
+    else if (spconf->shot_model > 0)
+    {
+        // Note that Wind has shot model and its CastAtThing is 0, besides, the target index is itself.
+        if ((castng->alloc_flags & TAlF_IsControlled) != 0)
+            i = THit_CrtrsNObjcts;
+        else
+            i = THit_CrtrsOnlyNotOwn;
+
+        const struct InstanceInfo* inst_inf = creature_instance_info_get(cctrl->instance_id);
+        if (flag_is_set(inst_inf->instance_property_flags, InstPF_RangedBuff))
+        {
+            ERRORLOG("The %s(%d) tried to fire Ranged Buff's shot(%s) without a target!",
+                thing_model_name(castng), castng->index, shot_code_name(spconf->shot_model));
+        }
+        else
+        {
+            thing_fire_shot(castng, INVALID_THING, spconf->shot_model, shot_lvl, i);
+        }
+    }
+
     if (spconf->crtr_summon_model > 0)
     {
         thing_summon_temporary_creature(castng, spconf->crtr_summon_model, spconf->crtr_summon_level, spconf->crtr_summon_amount, spconf->duration, spl_idx);