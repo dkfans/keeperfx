3ec8792a69ea62b902c79b076bb0463bb209ced4
Loobinex <Loobinex@users.noreply.github.com>
2024-10-28 01:16:39 +0100

    keeperfx.exe icons now work in low res again too
    Co-authored-by: xtremeqg <xtremeqg@users.noreply.github.com>

--- prebuilds.mk
+++ prebuilds.mk
@@ -1,7 +1,7 @@
 # Tools and libraries to be used on the host system, not on target
 ifneq (,$(findstring Windows,$(OS)))
   PERESEC_DOWNLOAD=https://github.com/dkfans/peresec/releases/download/1.1.0/peresec-1_1_0_16-devel-win.zip
-  PNGTOICO_DOWNLOAD=https://github.com/dkfans/png2ico/releases/download/2024-10-27/png2ico-win-2024-10-27.zip
+  PNGTOICO_DOWNLOAD=https://github.com/dkfans/png2ico/releases/download/2024-10-28/png2ico-win-2024-10-28.zip
   PNGTORAW_DOWNLOAD=https://github.com/dkfans/pngpal2raw/releases/download/v1.0.2/pngpal2raw-1_0_2_35-devel-win.zip
   PNGTOBSPAL_DOWNLOAD=https://github.com/dkfans/png2bestpal/releases/download/v1.0.3/png2bestpal-1_0_3_21-devel-win.zip
   POTONGDAT_DOWNLOAD=https://github.com/dkfans/po2ngdat/releases/download/v1.0.2.31/po2ngdat-1_0_2_31-devel-win.zip
@@ -12,7 +12,7 @@ else
 UNAME_S := $(shell uname -s)
 ifeq ($(UNAME_S),Linux)
   PERESEC_DOWNLOAD=https://github.com/dkfans/peresec/releases/download/1.1.0/peresec-1_1_0_16-devel-lin.tar.gz
-  PNGTOICO_DOWNLOAD=https://github.com/dkfans/png2ico/releases/download/2024-10-27/png2ico-lin-2024-10-27.tar.gz
+  PNGTOICO_DOWNLOAD=https://github.com/dkfans/png2ico/releases/download/2024-10-28/png2ico-lin-2024-10-28.tar.gz
   PNGTORAW_DOWNLOAD=https://github.com/dkfans/pngpal2raw/releases/download/v1.0.2/pngpal2raw-1_0_2_35-devel-lin.tar.gz
   PNGTOBSPAL_DOWNLOAD=https://github.com/dkfans/png2bestpal/releases/download/v1.0.3/png2bestpal-1_0_3_21-devel-lin.tar.gz
   POTONGDAT_DOWNLOAD=https://github.com/dkfans/po2ngdat/releases/download/v1.0.2.31/po2ngdat-1_0_2_31-devel-lin.tar.gz
