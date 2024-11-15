3c529530994c18b8c46a0a5c6a6aa6a780a286f7
AdamPlenty <58278560+AdamPlenty@users.noreply.github.com>
2024-11-15 20:32:59 +0000

    Revert "Update player_compevents.c"
    This reverts commit 8f40175b117682f17188d13a14cc3fce849200dd.

--- src/player_compevents.c
+++ src/player_compevents.c
@@ -810,9 +810,9 @@ long computer_event_check_imps_in_danger(struct Computer2 *comp, struct Computer
     {
         struct Thing* creatng = thing_get(i);
         struct CreatureControl* cctrl = creature_control_get_from_thing(creatng);
-        if (!thing_is_creature_special_digger(creatng) || creature_control_invalid(cctrl))
+        if (!thing_is_creature(creatng) || creature_control_invalid(cctrl))
         {
-            ERRORLOG("Jump to invalid digger creature detected");
+            ERRORLOG("Jump to invalid creature detected");
             break;
         }
         i = cctrl->players_next_creature_idx;
