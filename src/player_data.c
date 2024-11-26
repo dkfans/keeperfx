dkfans/keeperfx

commit 68ec462187cf28d6260ea88d5fa2659fecd6ae21

Create Tunneller keeper power (#3719)
It summons a tunneller that comes in from the ceiling. Also made it easy for modders to create new summon powers.
Also:
- Reworked keeper powers to no longer need a specific player state
- Cost functions on keeper power configurable (non, digger, dwarf are the current options)
- Power timebomb moved to new slot to make room for create tunneller
---------

Co-authored-by: Loobinex <Loobinex@users.noreply.github.com>
Co-authored-by: walter253 <130906143+walt253@users.noreply.github.com>

src/player_data.c
@@ -317,6 +317,13 @@ void set_player_state(struct PlayerInfo *player, short nwrk_state, long chosen_k
     case PSt_PlaceDoor:
         player->chosen_door_kind = chosen_kind;
         break;
+    case PSt_CastPowerOnSubtile:
+    case PST_CastPowerOnTarget:
+    case PSt_CreateDigger:
+    case PSt_SightOfEvil:
+    case PSt_CallToArms:
+        player->chosen_power_kind = chosen_kind;
+        break;
     }
     return;
   }
@@ -364,6 +371,13 @@ void set_player_state(struct PlayerInfo *player, short nwrk_state, long chosen_k
       break;
   case PSt_PlaceDoor:
       player->chosen_door_kind = chosen_kind;
+      break;
+  case PSt_CastPowerOnSubtile:
+  case PST_CastPowerOnTarget:
+  case PSt_CallToArms:
+  case PSt_SightOfEvil:
+  case PSt_CreateDigger:
+      player->chosen_power_kind = chosen_kind;
       break;
   case PSt_MkGoodCreatr:
       clear_messages_from_player(MsgType_Player, player->cheatselection.chosen_player);

config/fxdata/magic.cfg
@@ -1447,6 +1447,8 @@ SoundPlayed = 0
 Power = 0 0 0 0 0 0 0 0 0 0
 ; Power cost at each overload level.
 Cost = 0 0 0 0 0 0 0 0 0
+; Special formulas for the cost. NONE, DIGGER, DWARF. Default to NONE.
+CostFormula = NONE
 ; How long the power remains in effect in game turns.
 Duration = 0
 ; How long after the power is used the player has to wait to cast again. Only for powers that need a delay.
@@ -1523,6 +1525,8 @@ SoundSamples = 831
 SoundPlayed = 0
 Power =  0   0   0   0   0   0   0   0   0   0
 Cost = 150 150 150 150 150 150 150 150 150
+; Becomes more expensive for every digger and gets cheaper with the temple recipe if enabled.
+CostFormula = DIGGER
 Castability = OWNED_GROUND
 Artifact = SPELLBOOK_IMP
 Properties = 
@@ -1530,7 +1534,7 @@ PlayerState = PLAYER_STATE_CREATEDIGGER
 ParentPower = NOPOWER
 CastExpandFunc = do_not_expand
 UseFunction = magic_use_power_imp
-;only used with magic_use_power_imp, sets the type of unit spawned
+; Only used with 'magic_use_power_imp' and 'magic_use_power_tunneller', this sets the kind of creature spawned.
 CreatureType = IMP
 
 [power3]
@@ -1587,6 +1591,7 @@ Cost =   50  60  70  80  90 100 110  120  130
 Castability = ANYWHERE UNREVEALED
 Artifact = SPELLBOOK_SOE
 Properties = HAS_PROGRESS
+; PLAYER_STATE_SIGHTOFEVIL has your cursor at a high level.
 PlayerState = PLAYER_STATE_SIGHTOFEVIL
 ParentPower = NOPOWER
 CastExpandFunc = sight_of_evil_expand
@@ -1628,7 +1633,7 @@ Duration = 100
 Castability = ALL_GROUND REVEALED_TEMP
 Artifact = SPELLBOOK_CAVI
 Properties = HAS_PROGRESS
-PlayerState = PLAYER_STATE_CAVEIN
+PlayerState = PLAYER_STATE_CAST_SUBTILE
 ParentPower = NOPOWER
 CastExpandFunc = general_expand
 UseFunction = magic_use_power_cave_in
@@ -1648,7 +1653,7 @@ Duration = 16
 Castability = ALL_GROUND OWNED_CRTRS ALLIED_CRTRS BOUND_CRTRS CUSTODY_CRTRS
 Artifact = SPELLBOOK_HEAL
 Properties = 
-PlayerState = PLAYER_STATE_HEAL
+PlayerState = PLAYER_STATE_CAST_ON_TARGET
 ParentPower = NOPOWER
 CastExpandFunc = general_expand
 Spell = SPELL_HEAL
@@ -1688,7 +1693,7 @@ Cooldown = 20
 Castability = ALL_GROUND REVEALED_TEMP NEEDS_DELAY
 Artifact = SPELLBOOK_LIGHTN
 Properties = 
-PlayerState = PLAYER_STATE_LIGHTNING
+PlayerState = PLAYER_STATE_CAST_SUBTILE
 ParentPower = NOPOWER
 CastExpandFunc = general_expand
 UseFunction = magic_use_power_lightning
@@ -1708,7 +1713,7 @@ Cost =  100  200  300  400  500  600  700  800  900
 Castability = ALL_GROUND OWNED_CRTRS ALLIED_CRTRS BOUND_CRTRS CUSTODY_CRTRS
 Artifact = SPELLBOOK_SPDC
 Properties = 
-PlayerState = PLAYER_STATE_SPEEDUP
+PlayerState = PLAYER_STATE_CAST_ON_TARGET
 ParentPower = NOPOWER
 CastExpandFunc = general_expand
 Spell = SPELL_SPEED
@@ -1728,7 +1733,7 @@ Cost =  100  200  300  400  500  600  700  800  900
 Castability = ALL_GROUND OWNED_CRTRS ALLIED_CRTRS BOUND_CRTRS
 Artifact = SPELLBOOK_PROT
 Properties = 
-PlayerState = PLAYER_STATE_ARMOUR
+PlayerState = PLAYER_STATE_CAST_ON_TARGET
 ParentPower = NOPOWER
 CastExpandFunc = general_expand
 Spell = SPELL_ARMOUR
@@ -1748,7 +1753,7 @@ Cost =  100  200  300  400  500  600  700  800  900
 Castability = ALL_GROUND OWNED_CRTRS ALLIED_CRTRS BOUND_CRTRS
 Artifact = SPELLBOOK_CONCL
 Properties = 
-PlayerState = PLAYER_STATE_CONCEAL
+PlayerState = PLAYER_STATE_CAST_ON_TARGET
 ParentPower = NOPOWER
 CastExpandFunc = general_expand
 Spell = SPELL_INVISIBILITY
@@ -1768,7 +1773,7 @@ Cost = 7000 7200 7400 7600 7800 8000 8200 8400 9000
 Castability = OWNED_GROUND ALLIED_GROUND ENEMY_CRTRS CUSTODY_CRTRS
 Artifact = SPELLBOOK_DISEASE
 Properties = 
-PlayerState = PLAYER_STATE_CASTDISEASE
+PlayerState = PLAYER_STATE_CAST_ON_TARGET
 ParentPower = NOPOWER
 CastExpandFunc = general_expand
 Spell = SPELL_DISEASE
@@ -1789,7 +1794,7 @@ Duration = 10
 Castability = OWNED_GROUND ALLIED_GROUND ENEMY_CRTRS CUSTODY_CRTRS
 Artifact = SPELLBOOK_CHKN
 Properties = 
-PlayerState = PLAYER_STATE_TURNCHICKEN
+PlayerState = PLAYER_STATE_CAST_ON_TARGET
 ParentPower = NOPOWER
 CastExpandFunc = general_expand
 Spell = SPELL_CHICKEN
@@ -1809,7 +1814,7 @@ Cost = 10000 12000 14000 16000 18000 20000 22000 24000 26000
 Castability = ALL_TALL CLAIMABLE
 Artifact = SPELLBOOK_DWAL
 Properties = 
-PlayerState = PLAYER_STATE_DESTROYWALLS
+PlayerState = PLAYER_STATE_CAST_SUBTILE
 ParentPower = NOPOWER
 CastExpandFunc = general_expand
 UseFunction = magic_use_power_destroy_walls
@@ -1820,7 +1825,7 @@ NameTextID = 645
 TooltipTextID = 662
 SymbolSprites = 552 550
 PointerSprites = 182
-PanelTabIndex = 18
+PanelTabIndex = 27
 SoundSamples = 0
 SoundPlayed = 0
 Power =  40   60   80  100  120  140  160  180  200  220
@@ -1829,7 +1834,7 @@ Cooldown = 30
 Castability = ALL_GROUND ONLY_DIGGERS OWNED_CRTRS BOUND_CRTRS NEEDS_DELAY
 Artifact = SPELLBOOK_TBMB
 Properties = 
-PlayerState = PLAYER_STATE_TIMEBOMB
+PlayerState = PLAYER_STATE_CAST_ON_TARGET
 ParentPower = NOPOWER
 CastExpandFunc = general_expand
 Spell = SPELL_TIME_BOMB
@@ -1849,7 +1854,7 @@ Cost =  0   0   0   0   0   0   0   0   0
 Castability = ALL_GROUND OWNED_CRTRS OWNED_FOOD
 Artifact = SPELLBOOK_POSS
 Properties = 
-PlayerState = PLAYER_STATE_CTRLDIRECT
+PlayerState = PLAYER_STATE_CAST_ON_TARGET
 ParentPower = NOPOWER
 CastExpandFunc = do_not_expand
 UseFunction = magic_use_power_possess_thing
@@ -1945,7 +1950,7 @@ Cost =  100 200 300 400 500 600 700 800 900
 Castability = ALL_GROUND OWNED_CRTRS ALLIED_CRTRS BOUND_CRTRS
 Artifact = SPELLBOOK_RBND
 Properties = 
-PlayerState = PLAYER_STATE_REBOUND
+PlayerState = PLAYER_STATE_CAST_ON_TARGET
 ParentPower = NOPOWER
 ; To have this power chargeable like other replace 'do_not_expand' with 'general_expand'.
 CastExpandFunc = do_not_expand
@@ -1968,7 +1973,7 @@ Cooldown = 20
 Castability = OWNED_GROUND ALLIED_GROUND UNCLMD_GROUND NEUTRL_GROUND ENEMY_CRTRS CUSTODY_CRTRS NEEDS_DELAY
 Artifact = SPELLBOOK_FRZ
 Properties = 
-PlayerState = PLAYER_STATE_FREEZE
+PlayerState = PLAYER_STATE_CAST_ON_TARGET
 ParentPower = NOPOWER
 ; To have this power chargeable like other replace 'do_not_expand' with 'general_expand'.
 CastExpandFunc = do_not_expand
@@ -1991,7 +1996,7 @@ Cost =  100 200 300 400 500 600 700 800 900
 Castability = ALL_GROUND ENEMY_CRTRS CUSTODY_CRTRS
 Artifact = SPELLBOOK_SLOW
 Properties = 
-PlayerState = PLAYER_STATE_SLOW
+PlayerState = PLAYER_STATE_CAST_ON_TARGET
 ParentPower = NOPOWER
 ; To have this power chargeable like other replace 'do_not_expand' with 'general_expand'.
 CastExpandFunc = do_not_expand
@@ -2014,7 +2019,7 @@ Cost =  100 200 300 400 500 600 700 800 900
 Castability = ALL_GROUND OWNED_CRTRS ALLIED_CRTRS BOUND_CRTRS
 Artifact = SPELLBOOK_FLGT
 Properties = 
-PlayerState = PLAYER_STATE_FLIGHT
+PlayerState = PLAYER_STATE_CAST_ON_TARGET
 ParentPower = NOPOWER
 ; To have this power chargeable like other replace 'do_not_expand' with 'general_expand'.
 CastExpandFunc = do_not_expand
@@ -2035,11 +2040,34 @@ Cost =  100  200  300  400  500  600  700  800  900
 Castability = ALL_GROUND OWNED_CRTRS ALLIED_CRTRS BOUND_CRTRS
 Artifact = SPELLBOOK_VSN
 Properties = 
-PlayerState = PLAYER_STATE_VISION
+PlayerState = PLAYER_STATE_CAST_ON_TARGET
 ParentPower = NOPOWER
 CastExpandFunc = general_expand
 Spell = SPELL_SIGHT
 UseFunction = magic_use_power_apply_spell
+
+[power28]
+Name = POWER_TUNNELLER
+NameTextID = 1078
+TooltipTextID = 1079
+SymbolSprites = 811 809
+PointerSprites = 344
+PanelTabIndex = 18
+SoundSamples = 0
+SoundPlayed = 0
+Cooldown = 35
+Power =  0   0   1   1    2    2    3    3    4    9
+Cost = 700 700 950 950 1150 1150 2100 2100 3000
+; Becomes more expensive after every 7 units.
+CostFormula = DWARF
+Castability = OWNED_GROUND NEEDS_DELAY
+Artifact = SPELLBOOK_TUNLR
+Properties =
+PlayerState = PLAYER_STATE_CREATEDIGGER
+ParentPower = NOPOWER
+CastExpandFunc = general_expand
+UseFunction = magic_use_power_tunneller
+CreatureType = TUNNELLER
 
 ; Specials types.
 

config/fxdata/objects.cfg
@@ -4302,3 +4302,27 @@ Persistence = 1
 Immobile = 1
 Properties = CHOWNED_ON_ROOM_CLAIM
 UpdateFunction = NULL
+
+[object179]
+Name = SPELLBOOK_TUNLR
+Genre = SPELLBOOK
+AnimationID = 777
+AnimationSpeed = 256
+Size_XY = 0
+Size_Z = 0
+MaximumSize = 300
+DestroyOnLava = 1
+DestroyOnLiquid = 0
+Health = 0
+FallAcceleration = 10
+LightUnaffected = 0
+MapIcon = -1
+LightIntensity = 62
+LightRadius = 4
+LightIsDynamic = 0
+RandomStartFrame = 0
+DrawClass = 2
+Persistence = 1
+Immobile = 1
+Properties = CHOWNED_ON_ROOM_CLAIM
+UpdateFunction = NULL

config/fxdata/playerstates.toml
@@ -1,334 +1,212 @@
 [playerstate0]
-Name = "PLAYER_STATE_NONE" 
-#associated power in magic.cfg
-PowerKind = "NOPOWER"
-#how the pointer is determined, possible values
-# NONE, CTRLDUNGEON, BUILDROOM, INVISIBLE, SPELL, QUERY, 
-# PLACETRAP, PLACEDOOR, SELL, PLACETERRAIN, MAKEDIGGER, MAKECREATURE, ORDERCREATURE
+Name = "PLAYER_STATE_NONE"
+# How the pointer is determined, possible values:
+# NONE - CTRLDUNGEON - BUILDROOM - INVISIBLE - SPELL - QUERY - PLACETRAP - PLACEDOOR - SELL - PLACETERRAIN - MAKEDIGGER - MAKECREATURE - ORDERCREATURE.
 PointerGroup = "NONE"
-#own units should freeze while clicking when in this state
+# Own units should freeze while clicking when in this state.
 StopOwnUnits = false
 
 [playerstate1]
 Name = "PLAYER_STATE_CTRLDUNGEON"
-PowerKind = "NOPOWER"
 PointerGroup = "CTRLDUNGEON"
 StopOwnUnits = true
 
 [playerstate2]
 Name = "PLAYER_STATE_BUILDROOM"
-PowerKind = "NOPOWER"
 PointerGroup = "BUILDROOM"
 StopOwnUnits = false
 
 [playerstate3]
 Name = "PLAYER_STATE_MKGOODDIGGER"
-PowerKind = "NOPOWER"
 PointerGroup = "MAKEDIGGER"
 StopOwnUnits = false
 
 [playerstate4]
 Name = "PLAYER_STATE_MKGOODCREATR"
-PowerKind = "NOPOWER"
 PointerGroup = "MAKECREATURE"
 StopOwnUnits = false
 
 [playerstate5]
 Name = "PLAYER_STATE_HOLDINHAND"
-PowerKind = "NOPOWER"
 PointerGroup = "INVISIBLE"
 StopOwnUnits = false
 
 [playerstate6]
 Name = "PLAYER_STATE_CALLTOARMS"
-PowerKind = "POWER_CALL_TO_ARMS"
 PointerGroup = "SPELL"
 StopOwnUnits = false
 
 [playerstate7]
-Name = "PLAYER_STATE_CAVEIN"
-PowerKind = "POWER_CAVE_IN"
+Name = "PLAYER_STATE_CAST_SUBTILE"
 PointerGroup = "SPELL"
 StopOwnUnits = false
 
 [playerstate8]
 Name = "PLAYER_STATE_SIGHTOFEVIL"
-PowerKind = "POWER_SIGHT"
 PointerGroup = "SPELL"
 StopOwnUnits = false
 
 [playerstate9]
 Name = "PLAYER_STATE_SLAP"
-PowerKind = "NOPOWER"
 PointerGroup = "INVISIBLE"
 StopOwnUnits = true
 
 [playerstate10]
 Name = "PLAYER_STATE_CTRLPASSNGR"
-PowerKind = "POWER_POSSESS"
 PointerGroup = "SPELL"
 StopOwnUnits = true
 
 [playerstate11]
 Name = "PLAYER_STATE_CTRLDIRECT"
-PowerKind = "POWER_POSSESS"
 PointerGroup = "SPELL"
 StopOwnUnits = true
 
 [playerstate12]
 Name = "PLAYER_STATE_CREATRQUERY"
-PowerKind = "NOPOWER"
 PointerGroup = "QUERY"
 StopOwnUnits = true
 
 [playerstate13]
 Name = "PLAYER_STATE_ORDERCREATR"
-PowerKind = "NOPOWER"
 PointerGroup = "ORDERCREATURE"
 StopOwnUnits = true
 
 [playerstate14]
 Name = "PLAYER_STATE_MKBADCREATR"
-PowerKind = "NOPOWER"
 PointerGroup = "MAKECREATURE"
 StopOwnUnits = false
 
 [playerstate15]
 Name = "PLAYER_STATE_CREATRINFO"
-PowerKind = "NOPOWER"
 PointerGroup = "QUERY"
 StopOwnUnits = true
 
 [playerstate16]
 Name = "PLAYER_STATE_PLACETRAP"
-PowerKind = "NOPOWER"
 PointerGroup = "PLACETRAP"
 StopOwnUnits = false
 
 [playerstate17]
-Name = "PLAYER_STATE_LIGHTNING"
-PowerKind = "POWER_LIGHTNING"
-PointerGroup = "SPELL"
-StopOwnUnits = false
-
-[playerstate18]
 Name = "PLAYER_STATE_PLACEDOOR"
-PowerKind = "NOPOWER"
 PointerGroup = "PLACEDOOR"
 StopOwnUnits = false
 
+[playerstate18]
+Name = "PLAYER_STATE_CAST_ON_TARGET"
+PointerGroup = "SPELL"
+StopOwnUnits = true
+
 [playerstate19]
-Name = "PLAYER_STATE_SPEEDUP"
-PowerKind = "POWER_SPEED"
-PointerGroup = "SPELL"
-StopOwnUnits = true
-
-[playerstate20]
-Name = "PLAYER_STATE_ARMOUR"
-PowerKind = "POWER_PROTECT"
-PointerGroup = "SPELL"
-StopOwnUnits = true
-
-[playerstate21]
-Name = "PLAYER_STATE_CONCEAL"
-PowerKind = "POWER_CONCEAL"
-PointerGroup = "SPELL"
-StopOwnUnits = true
-
-[playerstate22]
-Name = "PLAYER_STATE_HEAL"
-PowerKind = "POWER_HEAL_CREATURE"
-PointerGroup = "SPELL"
-StopOwnUnits = true
-
-[playerstate23]
 Name = "PLAYER_STATE_SELL"
-PowerKind = "NOPOWER"
 PointerGroup = "SELL"
 StopOwnUnits = false
 
+[playerstate20]
+Name = "PLAYER_STATE_MKGOLDPOT"
+PointerGroup = "NONE"
+StopOwnUnits = false
+
+[playerstate21]
+Name = "PLAYER_STATE_FREEDESTROYWALLS"
+PointerGroup = "SPELL"
+StopOwnUnits = false
+
+[playerstate22]
+Name = "PLAYER_STATE_FREECASTDISEASE"
+PointerGroup = "SPELL"
+StopOwnUnits = false
+
+[playerstate23]
+Name = "PLAYER_STATE_FREETURNCHICKEN"
+PointerGroup = "SPELL"
+StopOwnUnits = false
+
 [playerstate24]
-Name = "PLAYER_STATE_CREATEDIGGER"
-PowerKind = "POWER_IMP"
-PointerGroup = "SPELL"
-StopOwnUnits = false
-
-[playerstate25]
-Name = "PLAYER_STATE_DESTROYWALLS"
-PowerKind = "POWER_DESTROY_WALLS"
-PointerGroup = "SPELL"
-StopOwnUnits = false
-
-[playerstate26]
-Name = "PLAYER_STATE_CASTDISEASE"
-PowerKind = "POWER_DISEASE"
-PointerGroup = "SPELL"
-StopOwnUnits = false
-
-[playerstate27]
-Name = "PLAYER_STATE_TURNCHICKEN"
-PowerKind = "POWER_CHICKEN"
+Name = "PLAYER_STATE_FREECTRLPASSNGR"
 PointerGroup = "SPELL"
 StopOwnUnits = true
 
+[playerstate25]
+Name = "PLAYER_STATE_FREECTRLDIRECT"
+PointerGroup = "SPELL"
+StopOwnUnits = true
+
+[playerstate26]
+Name = "PLAYER_STATE_STEALROOM"
+PointerGroup = "NONE"
+StopOwnUnits = false
+
+[playerstate27]
+Name = "PLAYER_STATE_DESTROYROOM"
+PointerGroup = "NONE"
+StopOwnUnits = false
+
 [playerstate28]
-Name = "PLAYER_STATE_MKGOLDPOT"
-PowerKind = "NOPOWER"
+Name = "PLAYER_STATE_KILLCREATURE"
 PointerGroup = "NONE"
 StopOwnUnits = false
 
 [playerstate29]
-Name = "PLAYER_STATE_TIMEBOMB"
-PowerKind = "POWER_TIME_BOMB"
-PointerGroup = "SPELL"
-StopOwnUnits = true
+Name = "PLAYER_STATE_CONVERTCREATURE"
+PointerGroup = "NONE"
+StopOwnUnits = false
 
 [playerstate30]
-Name = "PLAYER_STATE_FREEDESTROYWALLS"
-PowerKind = "POWER_DESTROY_WALLS"
-PointerGroup = "SPELL"
+Name = "PLAYER_STATE_STEALSLAB"
+PointerGroup = "NONE"
 StopOwnUnits = false
 
 [playerstate31]
-Name = "PLAYER_STATE_FREECASTDISEASE"
-PowerKind = "POWER_DISEASE"
-PointerGroup = "SPELL"
+Name = "PLAYER_STATE_LEVELCREATUREUP"
+PointerGroup = "NONE"
 StopOwnUnits = false
 
 [playerstate32]
-Name = "PLAYER_STATE_FREETURNCHICKEN"
-PowerKind = "POWER_CHICKEN"
-PointerGroup = "SPELL"
+Name = "PLAYER_STATE_LEVELCREATUREDOWN"
+PointerGroup = "NONE"
 StopOwnUnits = false
 
 [playerstate33]
-Name = "PLAYER_STATE_FREECTRLPASSNGR"
-PowerKind = "POWER_POSSESS"
-PointerGroup = "SPELL"
-StopOwnUnits = true
+Name = "PLAYER_STATE_KILLPLAYER"
+PointerGroup = "NONE"
+StopOwnUnits = false
 
 [playerstate34]
-Name = "PLAYER_STATE_FREECTRLDIRECT"
-PowerKind = "POWER_POSSESS"
-PointerGroup = "SPELL"
-StopOwnUnits = true
+Name = "PLAYER_STATE_HEARTHEALTH"
+PointerGroup = "NONE"
+StopOwnUnits = false
 
 [playerstate35]
-Name = "PLAYER_STATE_STEALROOM"
-PowerKind = "NOPOWER"
-PointerGroup = "NONE"
-StopOwnUnits = false
+Name = "PLAYER_STATE_CREATRQUERYALL"
+PointerGroup = "QUERY"
+StopOwnUnits = true
 
 [playerstate36]
-Name = "PLAYER_STATE_DESTROYROOM"
-PowerKind = "NOPOWER"
+Name = "PLAYER_STATE_MAKEHAPPY"
 PointerGroup = "NONE"
-StopOwnUnits = false
+StopOwnUnits = true
 
 [playerstate37]
-Name = "PLAYER_STATE_KILLCREATURE"
-PowerKind = "NOPOWER"
+Name = "PLAYER_STATE_MAKEANGRY"
 PointerGroup = "NONE"
-StopOwnUnits = false
+StopOwnUnits = true
 
 [playerstate38]
-Name = "PLAYER_STATE_CONVERTCREATURE"
-PowerKind = "NOPOWER"
-PointerGroup = "NONE"
+Name = "PLAYER_STATE_PLACETERRAIN"
+PointerGroup = "PLACETERRAIN"
 StopOwnUnits = false
 
 [playerstate39]
-Name = "PLAYER_STATE_STEALSLAB"
-PowerKind = "NOPOWER"
+Name = "PLAYER_STATE_DESTROY_THING"
 PointerGroup = "NONE"
 StopOwnUnits = false
 
 [playerstate40]
-Name = "PLAYER_STATE_LEVELCREATUREUP"
-PowerKind = "NOPOWER"
-PointerGroup = "NONE"
-StopOwnUnits = false
+Name = "PLAYER_STATE_CREATURE_INFO_ALL"
+PointerGroup = "QUERY"
+StopOwnUnits = true
 
 [playerstate41]
-Name = "PLAYER_STATE_LEVELCREATUREDOWN"
-PowerKind = "NOPOWER"
-PointerGroup = "NONE"
-StopOwnUnits = false
-
-[playerstate42]
-Name = "PLAYER_STATE_KILLPLAYER"
-PowerKind = "NOPOWER"
-PointerGroup = "NONE"
-StopOwnUnits = false
-
-[playerstate43]
-Name = "PLAYER_STATE_HEARTHEALTH"
-PowerKind = "NOPOWER"
-PointerGroup = "NONE"
-StopOwnUnits = false
-
-[playerstate44]
-Name = "PLAYER_STATE_CREATRQUERYALL"
-PowerKind = "NOPOWER"
-PointerGroup = "QUERY"
-StopOwnUnits = true
-
-[playerstate45]
-Name = "PLAYER_STATE_MAKEHAPPY"
-PowerKind = "NOPOWER"
-PointerGroup = "NONE"
-StopOwnUnits = true
-
-[playerstate46]
-Name = "PLAYER_STATE_MAKEANGRY"
-PowerKind = "NOPOWER"
-PointerGroup = "NONE"
-StopOwnUnits = true
-
-[playerstate47]
-Name = "PLAYER_STATE_PLACETERRAIN"
-PowerKind = "NOPOWER"
-PointerGroup = "PLACETERRAIN"
-StopOwnUnits = false
-
-[playerstate48]
-Name = "PLAYER_STATE_DESTROY_THING"
-PowerKind = "NOPOWER"
-PointerGroup = "NONE"
-StopOwnUnits = false
-
-[playerstate49]
-Name = "PLAYER_STATE_CREATURE_INFO_ALL"
-PowerKind = "NOPOWER"
-PointerGroup = "QUERY"
-StopOwnUnits = true
-
-[playerstate50]
-Name = "PLAYER_STATE_REBOUND"
-PowerKind = "POWER_REBOUND"
+Name = "PLAYER_STATE_CREATEDIGGER"
 PointerGroup = "SPELL"
-StopOwnUnits = true
-
-[playerstate51]
-Name = "PLAYER_STATE_FREEZE"
-PowerKind = "POWER_FREEZE"
-PointerGroup = "SPELL"
-StopOwnUnits = true
-
-[playerstate52]
-Name = "PLAYER_STATE_SLOW"
-PowerKind = "POWER_SLOW"
-PointerGroup = "SPELL"
-StopOwnUnits = true
-
-[playerstate53]
-Name = "PLAYER_STATE_FLIGHT"
-PowerKind = "POWER_FLIGHT"
-PointerGroup = "SPELL"
-StopOwnUnits = true
-
-[playerstate54]
-Name = "PLAYER_STATE_VISION"
-PowerKind = "POWER_VISION"
-PointerGroup = "SPELL"
-StopOwnUnits = true
+StopOwnUnits = false

config/fxdata/spritecolors.toml
@@ -37,6 +37,8 @@ pointer_sprites = [
     [ 80,  80, 235, 243, 259, 251,   0, 287, 325, 306],#quadptr_power_mkdigger
     [ 38,  38, 271, 272, 274, 273,   0, 296, 334, 315],#tileptr_room_guardpst
     [  9,   9, 275, 276, 278, 277,   0, 297, 335, 316],#tileptr_trap_alarm
+    [344, 344, 352, 360, 368, 376,   0, 384, 400, 392],#quadptr_power_mktunlr
+    [408, 408, 416, 424, 432, 440,   0, 448, 464, 456],#quadptr_power_mkdiggergeneric
 ]
 
 # Stuff in gui1-xx.dat

lang/gtext_chi.po
@@ -3708,8 +3708,8 @@ msgstr "控制怪物: 直接控制一个怪物。左键选择。[18.1]"
 
 #: guitext:648
 msgctxt "Keeper spell description"
-msgid "Create Imp: Create summon a new Imp anywhere in your dungeon. LMB select. [18.2]"
-msgstr "制造小鬼: 召唤一个新的小鬼进入你的地下城。左键选择。[18.2]"
+msgid "Create Imp: Summon a new Imp anywhere in your dungeon. LMB select."
+msgstr "制造小鬼: 召唤一个新的小鬼进入你的地下城。左键选择。"
 
 #: guitext:649
 msgctxt "Keeper spell description"
@@ -5886,7 +5886,17 @@ msgctxt "Keeper spell description"
 msgid "Sight: Allows the target creature to temporarily increase its awareness and notice the invisible."
 msgstr "邪眼术: 使目标怪物暂时提升注意力，能看穿隐身的目标。"
 
-#: guitext:1072 guitext:1073 guitext:1074 guitext:1075
+#: guitext:1072
+msgctxt "Keeper spell name"
+msgid "Recruit Tunneller"
+msgstr "招募挖掘者"
+
+#: guitext:1073
+msgctxt "Keeper spell description"
+msgid "Recruit Tunneller: Summon a new Tunneller anywhere in your dungeon."
+msgstr "招募挖掘者: 在你的地下城任何地方召唤一个新的挖掘者。"
+
+#: guitext:1074 guitext:1075
 msgctxt "Unused"
 msgid "Moo8"
 msgstr ""

lang/gtext_cht.po
@@ -4394,9 +4394,8 @@ msgstr "怪物附体术: 直接控あ一只怪物。按滑鼠左键选择。[18.
 #: guitext:648
 msgctxt "Keeper spell description"
 msgid ""
-"Create Imp: Create summon a new Imp anywhere in your dungeon. LMB select. "
-"[18.2]"
-msgstr "创造小恶魔: 召唤新的小恶魔前来。按滑鼠左键选择。[18.2]"
+"Create Imp: Summon a new Imp anywhere in your dungeon. LMB select."
+msgstr "创造小恶魔: 召唤新的小恶魔前来。按滑鼠左键选择。"
 
 #: guitext:649
 msgctxt "Keeper spell description"
@@ -6745,7 +6744,17 @@ msgctxt "Keeper spell description"
 msgid "Sight: Allows the target creature to temporarily increase its awareness and notice the invisible."
 msgstr ""
 
-#: guitext:1072 guitext:1073 guitext:1074 guitext:1075
+#: guitext:1072
+msgctxt "Keeper spell name"
+msgid "Recruit Tunneller"
+msgstr ""
+
+#: guitext:1073
+msgctxt "Keeper spell description"
+msgid "Recruit Tunneller: Summon a new Tunneller anywhere in your dungeon."
+msgstr ""
+
+#: guitext:1074 guitext:1075
 msgctxt "Unused"
 msgid "Moo8"
 msgstr ""

lang/gtext_cze.po
@@ -4546,10 +4546,9 @@ msgstr ""
 #: guitext:648
 msgctxt "Keeper spell description"
 msgid ""
-"Create Imp: Create summon a new Imp anywhere in your dungeon. LMB select. "
-"[18.2]"
+"Create Imp: Summon a new Imp anywhere in your dungeon. LMB select."
 msgstr ""
-"Vytvorit skreta: Vyvola kdekoliv na tvem uzemi skreta. Zvoli se LTM. [18.2]"
+"Vytvorit skreta: Vyvola kdekoliv na tvem uzemi skreta. Zvoli se LTM."
 
 #: guitext:649
 msgctxt "Keeper spell description"
@@ -6998,7 +6997,17 @@ msgctxt "Keeper spell description"
 msgid "Sight: Allows the target creature to temporarily increase its awareness and notice the invisible."
 msgstr ""
 
-#: guitext:1072 guitext:1073 guitext:1074 guitext:1075
+#: guitext:1072
+msgctxt "Keeper spell name"
+msgid "Recruit Tunneller"
+msgstr ""
+
+#: guitext:1073
+msgctxt "Keeper spell description"
+msgid "Recruit Tunneller: Summon a new Tunneller anywhere in your dungeon."
+msgstr ""
+
+#: guitext:1074 guitext:1075
 msgctxt "Unused"
 msgid "Moo8"
 msgstr ""

commit e6501b1ce750e8ca60ebb05cfbe15523fb11fff2

Fixed not being able to pick up units from battle menu
bug was introduced in #3719

src/player_data.c
@@ -303,6 +303,7 @@ void set_player_ally_locked(PlayerNumber plyr_idx, PlayerNumber ally_idx, TbBool
 void set_player_state(struct PlayerInfo *player, short nwrk_state, long chosen_kind)
 {
   SYNCDBG(6,"Player %d state %s to %s",(int)player->id_number,player_state_code_name(player->work_state),player_state_code_name(nwrk_state));
+  player->chosen_power_kind = 0; //Cleanup for spells. Traps, doors and rooms do not require cleanup.
   // Selecting the same state again - update only 2nd parameter
   if (player->work_state == nwrk_state)
   {