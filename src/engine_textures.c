dkfans/keeperfx

commit 8c65e7099ae1f424ff37d1c53cbb30f2129f2f24

Map specific tmap (texture) files (#3796)
Accepts a `map[mapnumber].tmapa[texturenumber].dat` and a `map[mapnumber].tmapb[texturenumber].dat` file.

src/engine_textures.c
@@ -115,14 +115,18 @@ short update_animating_texture_maps(void)
   return result;
 }
 
-static TbBool load_one_file(unsigned long tmapidx,char letter, void *dst)
+static TbBool load_one_file(unsigned long tmapidx,char letter, void *dst, LevelNumber lvnum, short fgroup)
 {
     SYNCDBG(9,"Starting");
 
-    char* fname = prepare_file_fmtpath(FGrp_CmpgConfig, "tmap%c%03d.dat",letter, tmapidx);
+    char* fname = prepare_file_fmtpath(fgroup, "map%05lu.tmap%c%03d.dat",(unsigned long)lvnum, letter, tmapidx);
     if (!LbFileExists(fname))
     {
-        fname = prepare_file_fmtpath(FGrp_StdData, "tmap%c%03d.dat",letter, tmapidx);
+        fname = prepare_file_fmtpath(FGrp_StdData, "tmap%c%03d.dat", letter, tmapidx);
+    }
+    if (!LbFileExists(fname))
+    {
+        fname = prepare_file_fmtpath(FGrp_CmpgConfig, "tmap%c%03d.dat", letter, tmapidx);
     }
 
     if (!LbFileExists(fname))
@@ -139,25 +143,25 @@ static TbBool load_one_file(unsigned long tmapidx,char letter, void *dst)
     return true;
 }
 
-TbBool load_texture_map_file(unsigned long tmapidx)
+TbBool load_texture_map_file(unsigned long tmapidx, LevelNumber lvnum, short fgroup)
 {
     SYNCDBG(7,"Starting");
     memset(block_mem, 130, sizeof(block_mem));
-    if (!load_one_file(tmapidx,'a', block_mem))
+    if (!load_one_file(tmapidx,'a', block_mem,lvnum,fgroup))
     {
         return false;
     }
     unsigned char *dst = block_mem + (TEXTURE_BLOCKS_STAT_COUNT_A * 32 * 32);    
-    load_one_file(tmapidx,'b', dst);
+    load_one_file(tmapidx,'b', dst, lvnum, fgroup);
     dst += (TEXTURE_BLOCKS_STAT_COUNT_B * 32 * 32);
 
     for (int i = 0; i < TEXTURE_VARIATIONS_COUNT-1; i++)
     
     {
-        load_one_file(i,'a', dst);
+        load_one_file(i,'a', dst, lvnum, fgroup);
         
         dst += (TEXTURE_BLOCKS_STAT_COUNT_A * 32 * 32);
-        load_one_file(i,'b', dst);
+        load_one_file(i,'b', dst, lvnum, fgroup);
         dst += (TEXTURE_BLOCKS_STAT_COUNT_B * 32 * 32);
         
     }

src/engine_textures.h
@@ -54,7 +54,7 @@ extern long block_dimension;
 void setup_texture_block_mem(void);
 short init_animating_texture_maps(void);
 short update_animating_texture_maps(void);
-TbBool load_texture_map_file(unsigned long tmapidx);
+TbBool load_texture_map_file(unsigned long tmapidx, LevelNumber lvnum, short fgroup);
 
 /******************************************************************************/
 #ifdef __cplusplus

src/lvl_filesdk1.c
@@ -1437,7 +1437,7 @@ static TbBool load_level_file(LevelNumber lvnum)
           result = false;
         load_map_wibble_file(lvnum);
         load_and_setup_map_info(lvnum);
-        load_texture_map_file(game.texture_id);
+        load_texture_map_file(game.texture_id, lvnum, fgroup);
         if (new_format)
         {
             load_aptfx_file(lvnum);
@@ -1470,7 +1470,7 @@ static TbBool load_level_file(LevelNumber lvnum)
         load_slab_file();
         init_columns();
         game.texture_id = 0;
-        load_texture_map_file(game.texture_id);
+        load_texture_map_file(game.texture_id, lvnum, fgroup);
         init_top_texture_to_cube_table();
         result = false;
     }

src/main.cpp
@@ -1560,7 +1560,7 @@ void reinit_level_after_load(void)
     update_room_tab_to_config();
     update_powers_tab_to_config();
     update_trap_tab_to_config();
-    load_texture_map_file(game.texture_id);
+    load_texture_map_file(game.texture_id, get_loaded_level_number(), get_level_fgroup(get_loaded_level_number()));
     init_animating_texture_maps();
     init_gui();
     reset_gui_based_on_player_mode();

commit 064114a7832c3c1948a1d840bf40d0b34835316a

Fixed campaign specific textures
It broke in #3796

src/engine_textures.c
@@ -122,11 +122,11 @@ static TbBool load_one_file(unsigned long tmapidx,char letter, void *dst, LevelN
     char* fname = prepare_file_fmtpath(fgroup, "map%05lu.tmap%c%03d.dat",(unsigned long)lvnum, letter, tmapidx);
     if (!LbFileExists(fname))
     {
-        fname = prepare_file_fmtpath(FGrp_StdData, "tmap%c%03d.dat", letter, tmapidx);
+        fname = prepare_file_fmtpath(FGrp_CmpgConfig, "tmap%c%03d.dat", letter, tmapidx);
     }
     if (!LbFileExists(fname))
     {
-        fname = prepare_file_fmtpath(FGrp_CmpgConfig, "tmap%c%03d.dat", letter, tmapidx);
+        fname = prepare_file_fmtpath(FGrp_StdData, "tmap%c%03d.dat", letter, tmapidx);
     }
 
     if (!LbFileExists(fname))