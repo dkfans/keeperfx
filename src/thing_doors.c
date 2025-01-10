d083ee1cc6450cc33b1c12fcd78aa5ce17486f1b
Loobinex <Loobinex@users.noreply.github.com>
2025-01-10 20:58:14 +0100

    Always init, not just after turn 0
    fixed pathfinding issue but crashes on preplaced maps

--- src/thing_doors.c
+++ src/thing_doors.c
@@ -136,10 +136,7 @@ struct Thing *create_door(struct Coord3d *pos, ThingModel tngmodel, unsigned cha
     check_if_enemy_can_see_placement_of_hidden_door(doortng);
     place_animating_slab_type_on_map(doorst->slbkind[orient], 0,  doortng->mappos.x.stl.num, doortng->mappos.y.stl.num, plyr_idx);
     ceiling_partially_recompute_heights(pos->x.stl.num - 1, pos->y.stl.num - 1, pos->x.stl.num + 2, pos->y.stl.num + 2);
-    if (game.play_gameturn > 0) // Update for secret doors, but at map start it would crash
-    {
-        update_navigation_triangulation(pos->x.stl.num - 1, pos->y.stl.num - 1, pos->x.stl.num + 2, pos->y.stl.num + 2);
-    }
+    update_navigation_triangulation(pos->x.stl.num - 1, pos->y.stl.num - 1, pos->x.stl.num + 2, pos->y.stl.num + 2);
     if (game.neutral_player_num != plyr_idx)
     {
         ++game.dungeon[plyr_idx].total_doors;
