1b6c2f2eacc0e257f120f6331677ecbcc004c207
Loobinex <Loobinex@users.noreply.github.com>
2025-02-06 02:22:31 +0100

    dense gold functions like gold

--- src/thing_doors.c
+++ src/thing_doors.c
@@ -386,7 +386,7 @@ TbBool door_can_stand(struct Thing *thing)
         long slb_y = subtile_slab(thing->mappos.y.stl.num) + (int)small_around[i].delta_y;
         struct SlabMap* slb = get_slabmap_block(slb_x, slb_y);
         struct SlabAttr* slbattr = get_slab_attrs(slb);
-        if ((slbattr->category == SlbAtCtg_FortifiedWall) || (slb->kind == SlbT_ROCK) || (slbattr->category == SlbAtCtg_FriableDirt) || (slb->kind == SlbT_GOLD) || (slb->kind == SlbT_GEMS))
+        if ((slbattr->category == SlbAtCtg_FortifiedWall) || (slb->kind == SlbT_ROCK) || (slbattr->category == SlbAtCtg_FriableDirt) || (slb->kind == SlbT_GOLD) || (slb->kind == SlbT_DENSEGOLD) || (slb->kind == SlbT_GEMS))
             wall_flags |= 0x01;
     }
     // The array needs to have 2^4 = 16 values
