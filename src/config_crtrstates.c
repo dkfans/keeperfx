b4294271f9da7b52e89a250099b6f0a3303339bf
PieterVdc <pieter00@gmail.com>
2025-03-24 14:30:06 +0100

    remove count field crstates.cfg

--- src/config_crtrstates.c
+++ src/config_crtrstates.c
@@ -35,168 +35,27 @@
 extern "C" {
 #endif
 /******************************************************************************/
-const struct NamedCommand creatstate_common_commands[] = {
-  {"STATESCOUNT",      1},
-  {NULL,               0},
-  };
-
-const struct NamedCommand creatstate_state_commands[] = {
-  {"NAME",             1},
-  {NULL,               0},
-  };
-
-const char creature_states_file[]="crstates.cfg";
-/******************************************************************************/
 struct NamedCommand creatrstate_desc[CREATURE_STATES_MAX];
 /******************************************************************************/
-TbBool parse_creaturestates_common_blocks(char *buf, long len, const char *config_textname, unsigned short flags)
-{
-    // Block name and parameter word store variables
-    // Initialize block data
-    if ((flags & CnfLd_AcceptPartial) == 0)
-    {
-        //TODO CONFIG We will need to make some cleaning here
-    }
-    // Find the block
-    char block_buf[COMMAND_WORD_LEN];
-    sprintf(block_buf, "common");
-    long pos = 0;
-    int k = find_conf_block(buf, &pos, len, block_buf);
-    if (k < 0)
-    {
-        if ((flags & CnfLd_AcceptPartial) == 0)
-            WARNMSG("Block [%s] not found in %s file.",block_buf,config_textname);
-        return false;
-    }
-#define COMMAND_TEXT(cmd_num) get_conf_parameter_text(creatstate_common_commands,cmd_num)
-    while (pos<len)
-    {
-        // Finding command number in this line
-        int cmd_num = recognize_conf_command(buf, &pos, len, creatstate_common_commands);
-        // Now store the config item in correct place
-        if (cmd_num == ccr_endOfBlock) break; // if next block starts
-        int n = 0;
-        switch (cmd_num)
-        {
-        case 1: // STATESCOUNT
-        {
-            char word_buf[COMMAND_WORD_LEN];
-            if (get_conf_parameter_single(buf, &pos, len, word_buf, sizeof(word_buf)) > 0)
-            {
-              k = atoi(word_buf);
-              if ((k > 0) && (k <= CREATURE_STATES_MAX))
-              {
-                game.conf.crtr_conf.states_count = k;
-                n++;
-              }
-            }
-            if (n < 1)
-            {
-              CONFWRNLOG("Incorrect value of \"%s\" parameter in [%s] block of %s file.",
-                  COMMAND_TEXT(cmd_num),block_buf,config_textname);
-            }
-            break;
-        }
-        case ccr_comment:
-            break;
-        case ccr_endOfFile:
-            break;
-        default:
-            CONFWRNLOG("Unrecognized command (%d) in [%s] block of %s file.",
-                cmd_num,block_buf,config_textname);
-            break;
-        }
-        skip_conf_to_next_line(buf,&pos,len);
-    }
-#undef COMMAND_TEXT
-    if (game.conf.crtr_conf.states_count < 1)
-    {
-        WARNLOG("No creature states defined in [%s] block of %s file.",
-            block_buf,config_textname);
-    }
-    return true;
-}
+const struct NamedField crstates_states_named_fields[] = {
+    //name   //pos   //field                                     //default //min   //max   //NamedCommand
+    {"NAME",   0, field(game.conf.crtr_conf.states[0].code_name), 0,        0,      0, creatrstate_desc,  value_name,      NULL},
+    {NULL},
+};
 
-TbBool parse_creaturestates_state_blocks(char *buf, long len, const char *config_textname, unsigned short flags)
-{
-    int i;
-    // Block name and parameter word store variables
-    // Initialize the array
-    int arr_size;
-    if ((flags & CnfLd_AcceptPartial) == 0)
-    {
-        arr_size = sizeof(game.conf.crtr_conf.states)/sizeof(game.conf.crtr_conf.states[0]);
-        for (i=0; i < arr_size; i++)
-        {
-            memset(game.conf.crtr_conf.states[i].name, 0, COMMAND_WORD_LEN);
-            if (i < game.conf.crtr_conf.states_count)
-            {
-                creatrstate_desc[i].name = game.conf.crtr_conf.states[i].name;
-                creatrstate_desc[i].num = i;
-            } else
-            {
-                creatrstate_desc[i].name = NULL;
-                creatrstate_desc[i].num = 0;
-            }
-        }
-    }
-    // Load the file blocks
-    arr_size = game.conf.crtr_conf.states_count;
-    for (i=0; i < arr_size; i++)
-    {
-        char block_buf[COMMAND_WORD_LEN];
-        sprintf(block_buf, "state%d", i);
-        long pos = 0;
-        int k = find_conf_block(buf, &pos, len, block_buf);
-        if (k < 0)
-        {
-            if ((flags & CnfLd_AcceptPartial) == 0)
-            {
-                WARNMSG("Block [%s] not found in %s file.", block_buf, config_textname);
-                return false;
-            }
-            continue;
-      }
-#define COMMAND_TEXT(cmd_num) get_conf_parameter_text(creatstate_state_commands,cmd_num)
-      while (pos<len)
-      {
-        // Finding command number in this line
-        int cmd_num = recognize_conf_command(buf, &pos, len, creatstate_state_commands);
-        // Now store the config item in correct place
-        if (cmd_num == ccr_endOfBlock) break; // if next block starts
-        if ((flags & CnfLd_ListOnly) != 0) {
-            // In "List only" mode, accept only name command
-            if (cmd_num > 1) {
-                cmd_num = 0;
-            }
-        }
-        int n = 0;
-        switch (cmd_num)
-        {
-        case 1: // NAME
-            if (get_conf_parameter_single(buf,&pos,len,game.conf.crtr_conf.states[i].name,COMMAND_WORD_LEN) <= 0)
-            {
-                CONFWRNLOG("Couldn't read \"%s\" parameter in [%s] block of %s file.",
-                    COMMAND_TEXT(cmd_num),block_buf,config_textname);
-                break;
-            }
-            n++;
-            break;
-        case ccr_comment:
-            break;
-        case ccr_endOfFile:
-            break;
-        default:
-            CONFWRNLOG("Unrecognized command (%d) in [%s] block of %s file.",
-                cmd_num,block_buf,config_textname);
-            break;
-        }
-        skip_conf_to_next_line(buf,&pos,len);
-      }
-#undef COMMAND_TEXT
-    }
-    return true;
-}
+const struct NamedFieldSet crstates_states_named_fields_set = {
+    &game.conf.crtr_conf.states_count,
+    "state",
+    crstates_states_named_fields,
+    creatrstate_desc,
+    CREATURE_STATES_MAX,
+    sizeof(game.conf.crtr_conf.states[0]),
+    game.conf.crtr_conf.states
+};
+
+const char creature_states_file[]="crstates.cfg";
+
+/******************************************************************************/
 
 TbBool load_creaturestates_config_file(const char *textname, const char *fname, unsigned short flags)
 {
@@ -215,22 +74,8 @@ TbBool load_creaturestates_config_file(const char *textname, const char *fname, 
     len = LbFileLoadAt(fname, buf);
     TbBool result = (len > 0);
     // Parse blocks of the config file
-    if (result)
-    {
-        result = parse_creaturestates_common_blocks(buf, len, textname, flags);
-        if ((flags & CnfLd_AcceptPartial) != 0)
-            result = true;
-        if (!result)
-            WARNMSG("Parsing %s file \"%s\" common blocks failed.",textname,fname);
-    }
-    if (result)
-    {
-        result = parse_creaturestates_state_blocks(buf, len, textname, flags);
-        if ((flags & CnfLd_AcceptPartial) != 0)
-            result = true;
-        if (!result)
-          WARNMSG("Parsing %s file \"%s\" state blocks failed.",textname,fname);
-    }
+    parse_named_field_blocks(buf, len, textname, flags, &crstates_states_named_fields_set);
+
     //Freeing and exiting
     free(buf);
     return result;
