0ad8b658e139dbc05345976262bd77728ec10dc1
PieterVdc <pieter00@gmail.com>
2025-03-27 15:18:55 +0100

    Update config_objects.h

--- src/config_objects.h
+++ src/config_objects.h
@@ -125,6 +125,8 @@ extern const char keeper_objects_file[];
 extern struct NamedCommand object_desc[OBJECT_TYPES_MAX];
 extern const struct NamedCommand objects_genres_desc[];
 extern const struct NamedCommand objects_object_commands[];
+
+extern const struct NamedFieldSet objects_named_fields_set;
 /******************************************************************************/
 TbBool load_objects_config(const char *conf_fname,unsigned short flags);
 struct ObjectConfigStats *get_object_model_stats(ThingModel tngmodel);
