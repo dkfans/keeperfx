d5fa0c8b98e1dad83693fdca1246db8707334b5a
Loobinex <Loobinex@users.noreply.github.com>
2025-04-05 02:53:40 +0200

    Flipped the default controls
    seems a small majority prefers this

--- src/config_settings.c
+++ src/config_settings.c
@@ -102,8 +102,8 @@ void setup_default_settings(void)
           {KC_PGUP, KMod_SHIFT},             // Gkey_TiltUp
           {KC_PGDOWN, KMod_SHIFT},           // Gkey_TiltDown
           {KC_INSERT, KMod_SHIFT},           // Gkey_TiltReset
-          {KC_Z, KMod_NONE},                // Gkey_Ascend
-          {KC_X, KMod_NONE},                // Gkey_Descend
+          {KC_X, KMod_NONE},                // Gkey_Ascend
+          {KC_Z, KMod_NONE},                // Gkey_Descend
      },                         // kbkeys
      true,                      // tooltips_on
      0,                         // first_person_move_invert
