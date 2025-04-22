8da7c1836b3d2ee5b27712e5af21f99e92181c4d
PieterVdc <pieter00@gmail.com>
2025-04-22 15:44:11 +0200

    Research_value lower

--- config/fxdata/lua/native.lua
+++ config/fxdata/lua/native.lua
@@ -697,8 +697,8 @@ function Set_creature_property(creature_type,property,enable) end
 ---@param player playerrange player’s name, e.g. PLAYER1. See players section for more information.
 ---@param Research_type "MAGIC"|"ROOM"|"CREATURE" Whether it is a room or spell you are Researching. Use one of the following commands:
 ---@param room_or_spell power_kind|room_type|creature_type The name of the room or spell you want to adjust, e.g. TEMPLE or MAGIC_LIGHTNING. See room names section and spell names section for more information.
----@param Research_value integer The new Research value. This must be a number below 16777216.
-function Research(player,Research_type,room_or_spell,Research_value) end
+---@param research_value integer The new Research value. This must be a number below 16777216.
+function Research(player,Research_type,room_or_spell,research_value) end
 
 ---When this command is first called, the Research list for specified players is cleared.
 ---Using it you may create a Research list from beginning.
@@ -707,8 +707,8 @@ function Research(player,Research_type,room_or_spell,Research_value) end
 ---@param player playerrange player’s name, e.g. PLAYER1. See players section for more information.
 ---@param Research_type "MAGIC"|"ROOM"|"CREATURE" Whether it is a room or spell you are Researching. Use one of the following commands:
 ---@param room_or_spell power_kind|room_type|creature_type The name of the room or spell you want to adjust, e.g. TEMPLE or MAGIC_LIGHTNING. See room names section and spell names section for more information.
----@param Research_value integer The new Research value. This must be a number below 16777216.
-function Research_order(player,Research_type,room_or_spell,Research_value) end
+---@param research_value integer The new Research value. This must be a number below 16777216.
+function Research_order(player,Research_type,room_or_spell,research_value) end
 
 
 ----------------------------------------
