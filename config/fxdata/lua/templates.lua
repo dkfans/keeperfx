3d94d3166a3d7d423e2f0bc69d5cb24ecbf78bb9
PieterVdc <pieter00@gmail.com>
2025-04-23 15:50:45 +0200

    Update templates.lua

--- config/fxdata/lua/templates.lua
+++ config/fxdata/lua/templates.lua
@@ -1,7 +1,7 @@
 --- functions here are templates functions used in cfg files, that can be replaced with a lua function instead
 --- the cfg will take a function name and the function will be called with the parameters
 --- for example, in magic.cfg you'll have [powerXX] blocks in those blocks you have a field UseFunction
---- under here you can fine `magic_power_UseFunction_template` 
+--- under here you can find `magic_power_UseFunction_template` 
 --- this is the template that shows the parameters of a function that can be used for that specific function
 --- 
 --- note: some/most of the functions in the cfgs will be implemented in C, so you can't view their source code
@@ -16,5 +16,5 @@
 ---@param stl_y integer
 ---@param thing Thing
 ---@param is_free boolean
-function magic_power_UseFunction_template(player,pwkind,splevel,stl_x,stl_y,thing,is_free)
+function Magic_power_UseFunction_template(player,pwkind,splevel,stl_x,stl_y,thing,is_free)
 end
