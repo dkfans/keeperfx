96eee3fc5a3478c0c3b071b050a110ab41313662
PieterVdc <pieter00@gmail.com>
2025-04-28 09:51:02 +0200

    Update global.lua

--- config/fxdata/lua/global.lua
+++ config/fxdata/lua/global.lua
@@ -1,5 +1,6 @@
 require "serialisation"
 require "triggers"
+require "util"
 
 --the Game table contains all the game data that needs to be serialized on save, anything not in this table could break save games
 Game = {}
