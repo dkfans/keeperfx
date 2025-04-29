53365fbc7a706077c07adc121227edc70de77ca8
Pieter Vandecandelaere <pieter00@gmail.com>
2025-04-29 18:01:25 +0200

    fix squigles

--- config/fxdata/lua/native.lua
+++ config/fxdata/lua/native.lua
@@ -28,7 +28,7 @@
 ---@alias actionpoint integer
 ---@alias location playersingle|actionpoint|"LAST_EVENT"|"COMBAT"
 ---@alias thing_class "Object"|"Shot"|"EffectElem"|"DeadCreature"|"Creature"|"Effect"|"EffectGen"|"Trap"|"Door"|"AmbientSnd"|"CaveIn"
-
+---@alias effect_or_effelem_type integer|string|effect_type|effect_element_type -- I allow string here because there's to many entries for, the language server to handle 
 
 ---@class Player: creaturefields,roomfields
 ---@field private name string
@@ -728,14 +728,14 @@ function Make_unsafe(player) end
 ---------
 
 ---Create an Effect at a location.
----@param effect effect_type|effect_element_type|integer
+---@param effect effect_or_effelem_type
 ---@param location location
 ---@param height integer The z-position of the effect. However, when using EFFECTELEMENT_PRICE as the 'effect' parameter, this is the gold amount displayed instead.
 ---@return Thing effect the created effect or effect element
 function Create_effect(effect,location,height) local ef return ef end
 
 ---Create an Effect at a location.
----@param effect effect_type|effect_element_type|integer
+---@param effect effect_or_effelem_type
 ---@param stl_x integer
 ---@param stl_y integer
 ---@param height integer The z-position of the effect. However, when using EFFECTELEMENT_PRICE as the 'effect' parameter, this is the gold amount displayed instead.
@@ -748,7 +748,7 @@ function Create_effect_at_pos(effect,stl_x,stl_y,height) local ef return ef end
 ---@param curvature integer 0 to go in a straight line, with bigger values to get a bigger curve. At 64 if you go from the the central top slab to the central bottom slab of a square room, it will go clockwise through the outer right slab. Use negative values to go counter-clockwise.
 ---@param distance integer How far apart the effects forming the line are. Where 24 spawns effects a single slab apart
 ---@param speed integer The delay between effects. The number represents 'number of effects per 4 game turns', set it to '2' to spawn 10 effects per second. Use 0 to spawn all effects at once. Max value is 127.
----@param effect effect_type|effect_element_type|integer The effect to spawn. Can be any effect or effect element that is in game, like the hearts that appear when healing, or the red smoke when claiming a room. Also accepts the names as provided in effects.toml
+---@param effect effect_or_effelem_type The effect to spawn. Can be any effect or effect element that is in game, like the hearts that appear when healing, or the red smoke when claiming a room. Also accepts the names as provided in effects.toml
 function Create_effects_line(origin,destination,curvature,distance, speed, effect) end
 
 ---------
