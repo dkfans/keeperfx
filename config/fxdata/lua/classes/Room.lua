824a4bfde70d51a2b39afe7ecad2dae73da3601c
PieterVdc <pieter00@gmail.com>
2025-05-08 12:35:50 +0200

    .

--- config/fxdata/lua/classes/Room.lua
+++ config/fxdata/lua/classes/Room.lua
@@ -10,5 +10,6 @@
 ---@field used_capacity integer how much of the room's capacity is used
 ---@field max_capacity integer maximum capacity of the room
 ---@field efficiency integer efficiency of the room, 0-255
+---@field centerpos Pos3d center of the room, z will be floor height, for oddly shaped rooms, it'll be the closest point to center within the room
 if not Room then Room = {} end
 
--- config/fxdata/lua/classes/Slab.lua
+++ config/fxdata/lua/classes/Slab.lua
@@ -1,11 +1,10 @@
---some of the Thing functions are implemented in C, those can be found in native.lua file
-
 ---@class Slab
 ---@field slb_x integer
 ---@field slb_y integer
 ---@field revealed boolean
 ---@field owner Player
 ---@field kind slab_type
----@field style texture_pack 
----@field room Room
+---@field style texture_pack
+---@field room Room|nil -- the room this slab belongs to, nil if not in a room
+---@field centerpos Pos3d -- center of the slab, z will be floor height
 local Slab = {}
