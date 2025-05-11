3f941a4a709fd2131c8ec6a4d3a0e21c2cb66503
Loobinex <Loobinex@users.noreply.github.com>
2025-05-11 15:45:28 +0200

    Area drains too

--- src/thing_effects.c
+++ src/thing_effects.c
@@ -1008,6 +1008,10 @@ TbBool explosion_affecting_thing(struct Thing *tngsrc, struct Thing *tngdst, con
                 {
                     HitPoints damage = get_radially_decaying_value(max_damage, max_dist / 4, 3 * max_dist / 4, distance) + 1;
                     SYNCDBG(7,"Causing %d damage to %s at distance %d",(int)damage,thing_model_name(tngdst),(int)distance);
+                    if (shotst->model_flags & ShMF_LifeDrain)
+                    {
+                        give_shooter_drained_health(origtng, damage / 2);
+                    }
                     apply_damage_to_thing_and_display_health(tngdst, damage, owner);
                 }
                 affected = true;
