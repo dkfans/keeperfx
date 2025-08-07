6bf13b3783ee4dd8ac1f78b8ea964459fdfbae18
Pieter Vandecandelaere <pieter00@gmail.com>
2025-08-07 18:15:18 +0200

    Update console_cmd.c

--- src/console_cmd.c
+++ src/console_cmd.c
@@ -1925,7 +1925,6 @@ TbBool cmd_lua(PlayerNumber plyr_idx, char * args)
     if ((game.flags_font & FFlg_AlexCheat) == 0) {
         return false;
     }
-
     execute_lua_code_from_console(args);
     return true;
 }
