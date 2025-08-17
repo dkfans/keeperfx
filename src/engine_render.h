e9660ef10ed9496f0af07798f9f0a39658693164
AdamPlenty <58278560+AdamPlenty@users.noreply.github.com>
2025-08-17 03:35:41 +0100

    Dark green

--- src/engine_render.h
+++ src/engine_render.h
@@ -85,6 +85,7 @@ enum stripey_line_colors {
     SLC_ORANGE,
     SLC_WHITE,
     SLC_GREEN2,
+    SLC_DARKGREEN,
     STRIPEY_LINE_COLOR_COUNT // Must always be the last entry (add new colours above this line)
 };
 
