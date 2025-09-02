93a80dfbb5ec50f76d7c149e55d5c78a1da5dea8
Loobinex <Loobinex@users.noreply.github.com>
2025-09-02 23:22:53 +0200

    don't touch LegacyInitLight

--- src/lvl_filesdk1.c
+++ src/lvl_filesdk1.c
@@ -99,11 +99,15 @@ struct LegacyInitActionPoint { // sizeof = 8
     unsigned short num;
 };
 
-struct LegacyInitLight { 
+struct LegacyInitLight { // sizeof=0x14
     short radius;
     unsigned char intensity;
     unsigned char flags;
+    short field_4_unused;
+    short field_6_unused;
+    short field_8_unused;
     struct LegacyCoord3d mappos;
+    unsigned char field_10_unused;
     unsigned char is_dynamic;
     short attached_slb;
 };
