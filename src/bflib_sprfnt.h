e3857e72b8cfc4e3e5cbb25a3f455a554b2a315a
Loobinex <Loobinex@users.noreply.github.com>
2025-09-06 09:51:14 +0200

    Revert "Proper fix"
    This reverts commit 633e3471e0cb2d0752511483ab51bed4804797ef.

--- src/bflib_sprfnt.h
+++ src/bflib_sprfnt.h
@@ -51,6 +51,7 @@ struct AsianFont {
   unsigned long ndata_scanline;
   unsigned long sdata_shift;
   unsigned long sdata_scanline;
+  unsigned long unusedparam;
   unsigned long narrow_width;
   unsigned long narrow_height;
   unsigned long bits_width;
