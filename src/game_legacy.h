392e50b76e3617e1cdc745e515c040b372b1f470
rainlizard <15337628+rainlizard@users.noreply.github.com>
2025-09-23 20:25:12 +1000

    Multiplayer: Updates to checksum analysis to investigate desyncs (#4264)

--- src/game_legacy.h
+++ src/game_legacy.h
@@ -130,11 +130,14 @@ struct Configs {
 struct LogThingDesyncInfo {
     ThingClass class_id;          // Type of thing (creature, object, etc.)
     ThingModel model;             // Model within the class
+    PlayerNumber owner;           // Owner player of the thing
     TbBigChecksum random_seed;    // Thing's random seed
     MapSubtlCoord pos_x;          // Position X coordinate
     MapSubtlCoord pos_y;          // Position Y coordinate
     MapSubtlCoord pos_z;          // Position Z coordinate
     GameTurn creation_turn;       // Turn when thing was created
+    ThingIndex index;             // Thing's index
+    HitPoints health;             // Thing's health
     TbBigChecksum checksum;       // Thing's computed checksum
 };
 
@@ -147,6 +150,7 @@ struct LogRoomDesyncInfo {
     SlabCodedCoords slabs_count;  // Number of slabs in the room
     long efficiency;              // Room efficiency value
     long used_capacity;           // Current capacity usage
+    RoomIndex index;              // Room's index
     TbBigChecksum checksum;       // Room's computed checksum
 };
 
