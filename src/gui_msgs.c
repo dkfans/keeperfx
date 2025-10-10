dkfans/keeperfx

commit f24248ed0d6c4bf9ba6ff939f5ef82d8e695efeb

Moved 'game turn' code (#4294)
No functional changes.

src/gui_msgs.c
@@ -203,7 +203,7 @@ void message_update(void)
     while (i >= 0)
     {
         struct GuiMessage* gmsg = &game.messages[i];
-        if (gmsg->creation_turn < game.play_gameturn)
+        if (gmsg->expiration_turn <= game.play_gameturn)
         {
             game.active_messages_count--;
             game.messages[game.active_messages_count].text[0] = 0;
@@ -258,7 +258,7 @@ void message_add(char type, PlayerNumber plyr_idx, const char *text)
     }
     snprintf(game.messages[0].text, sizeof(game.messages[0].text), "%s", text);
     game.messages[0].plyr_idx = plyr_idx;
-    game.messages[0].creation_turn = game.play_gameturn + GUI_MESSAGES_DELAY;
+    game.messages[0].expiration_turn = game.play_gameturn + GUI_MESSAGES_DELAY;
     game.messages[0].target_idx = -1;
     game.messages[0].type = type;
     if (game.active_messages_count < GUI_MESSAGES_COUNT) {
@@ -289,7 +289,7 @@ void targeted_message_add(char type, PlayerNumber plyr_idx, PlayerNumber target_
     }
     snprintf(game.messages[0].text, sizeof(game.messages[0].text), "%s", full_msg_text);
     game.messages[0].plyr_idx = plyr_idx;
-    game.messages[0].creation_turn = game.play_gameturn + timeout;
+    game.messages[0].expiration_turn = game.play_gameturn + timeout;
     game.messages[0].target_idx = target_idx;
     game.messages[0].type = type;
     if (game.active_messages_count < GUI_MESSAGES_COUNT) {

src/gui_msgs.h
@@ -46,13 +46,13 @@ enum MessageTypes {
 struct GuiMessage_OLD { // sizeof = 0x45 (69)
     char text[64];
 PlayerNumber plyr_idx;
-unsigned long creation_turn;
+unsigned long expiration_turn;
 };
 
 struct GuiMessage {
     char text[64];
 PlayerNumber plyr_idx;
-unsigned long creation_turn;
+unsigned long expiration_turn;
 PlayerNumber target_idx;
 char type;
 };

src/main.cpp
@@ -2746,6 +2746,7 @@ void update(void)
         things_stats_debug_dump();
         creature_stats_debug_dump();
 #endif
+        game.play_gameturn++;
     }
 
     message_update();

src/sounds.c
@@ -297,7 +297,6 @@ void update_player_sounds(void)
         {
             update_3d_sound_receiver(player);
         }
-        game.play_gameturn++;
     }
     find_nearest_rooms_for_ambient_sound();
     process_3d_sounds();

commit 96906c2bd1e86a51dd3bd3d7a75b6f9d6a36a81f

Fix messages that last for 1 turn (#4296)
fixes bug introduced by #4294

src/gui_msgs.c
@@ -203,7 +203,7 @@ void message_update(void)
     while (i >= 0)
     {
         struct GuiMessage* gmsg = &game.messages[i];
-        if (gmsg->expiration_turn <= game.play_gameturn)
+        if (game.play_gameturn > gmsg->expiration_turn)
         {
             game.active_messages_count--;
             game.messages[game.active_messages_count].text[0] = 0;