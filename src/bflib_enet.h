c3fd4a8aca2b174b7c120af3f12010d925944986
rainlizard <15337628+rainlizard@users.noreply.github.com>
2025-11-06 13:46:34 +1100

    use accurate ping display

--- src/bflib_enet.h
+++ src/bflib_enet.h
@@ -23,7 +23,6 @@ struct NetSP;
 struct NetSP* InitEnetSP();
 unsigned long GetPing(int id);
 unsigned long GetPingVariance(int id);
-unsigned long GetTotalPing(int id);
 unsigned long GetCalculatedPing(int id);
 unsigned int GetPacketLoss(int id);
 unsigned int GetClientDataInTransit();
