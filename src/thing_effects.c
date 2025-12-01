890a8058242410d62b8699c79706810e9ac1c60b
AdamPlenty <58278560+AdamPlenty@users.noreply.github.com>
2025-12-01 03:25:11 +0000

    Use SHOT_TIME_BOMB stats

--- src/thing_effects.c
+++ src/thing_effects.c
@@ -1339,8 +1339,29 @@ long explosion_affecting_map_block(struct Thing *tngsrc, const struct Map *mapbl
         // Per thing processing block
         if (area_effect_can_affect_thing(thing, hit_targets, owner))
         {
-            // Explosions from creatures and effects can affect things too (Time Bomb); so, we are simply passing a null ShotConfigStats instead of throwing an error with non-shots 
-            struct ShotConfigStats* shotst = (thing_is_shot(tngsrc)) ? get_shot_model_stats(tngsrc->model) : &game.conf.magic_conf.shot_cfgstats[0];
+            ThingModel tngmodel;
+            switch (tngsrc->class_id)
+            {
+                case TCls_Shot:
+                {
+                    tngmodel = tngsrc->model;
+                    break;
+                }
+                case TCls_Creature:
+                case TCls_Effect:
+                {
+                    // Time Bomb
+                    tngmodel = ShM_TimeBomb;
+                    break;
+                }
+                default:
+                {
+                    ERRORLOG("Exploding thing %s is not a shot", thing_model_name(tngsrc));
+                    tngmodel = ShM_Null;
+                    break;
+                }
+            } 
+            struct ShotConfigStats* shotst = get_shot_model_stats(tngmodel);
             if (explosion_affecting_thing(tngsrc, thing, pos, max_dist, max_damage, blow_strength, shotst))
                 num_affected++;
         }
