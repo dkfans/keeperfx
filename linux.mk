dkfans/keeperfx

commit 88f7e36f7baed6a3750c2ad5c3f6acb3ae61c96f

Multiplayer: Add UPnP and NAT-PMP port forwarding (#4450)

src/net_portforward.cpp
@@ -1,0 +1,262 @@
+/******************************************************************************/
+// Free implementation of Bullfrog's Dungeon Keeper strategy game.
+/******************************************************************************/
+/**
+ * @file net_portforward.cpp
+ *     Port forwarding support using NAT-PMP and UPnP.
+ * @par Purpose:
+ *     Automatic port forwarding for multiplayer hosting.
+ *     Tries NAT-PMP first (simpler/faster), falls back to UPnP.
+ * @author   KeeperFX Team
+ * @date     01 Jan 2026
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+#include "pre_inc.h"
+#include "net_portforward.h"
+#include "bflib_basics.h"
+
+#define MINIUPNP_STATICLIB
+#include <miniupnpc/miniupnpc.h>
+#include <miniupnpc/upnpcommands.h>
+#include <miniupnpc/upnperrors.h>
+
+#define NATPMP_STATICLIB
+#include <natpmp/natpmp.h>
+
+#include <cstdio>
+#include <ctime>
+#include <thread>
+#include <winsock2.h>
+#include <iphlpapi.h>
+
+#define NATPMP_TIMEOUT_SECONDS 1.0
+#define UPNP_TIMEOUT_MS 3000
+
+#include "post_inc.h"
+
+enum PortForwardMethod {
+    PORT_FORWARD_NONE = 0,
+    PORT_FORWARD_NATPMP,
+    PORT_FORWARD_UPNP
+};
+
+static enum PortForwardMethod active_method = PORT_FORWARD_NONE;
+static uint16_t mapped_port = 0;
+
+static struct UPNPUrls upnp_urls;
+static struct IGDdatas upnp_data;
+static char upnp_lanaddr[64];
+
+static natpmp_t natpmp;
+
+static int is_cgnat_detected() {
+    ULONG buffer_size = 0;
+    if (GetAdaptersInfo(NULL, &buffer_size) != ERROR_BUFFER_OVERFLOW) {
+        return 0;
+    }
+    IP_ADAPTER_INFO *adapter_info = (IP_ADAPTER_INFO *)malloc(buffer_size);
+    if (!adapter_info) {
+        return 0;
+    }
+    if (GetAdaptersInfo(adapter_info, &buffer_size) != NO_ERROR) {
+        free(adapter_info);
+        return 0;
+    }
+    for (IP_ADAPTER_INFO *adapter = adapter_info; adapter; adapter = adapter->Next) {
+        unsigned long local_ip = inet_addr(adapter->IpAddressList.IpAddress.String);
+        if (local_ip != INADDR_NONE && local_ip != 0) {
+            unsigned char first_octet = local_ip & 0xFF;
+            unsigned char second_octet = (local_ip >> 8) & 0xFF;
+            if (first_octet == 100 && second_octet >= 64 && second_octet <= 127) {
+                free(adapter_info);
+                return 1;
+            }
+        }
+        unsigned long gateway_ip = inet_addr(adapter->GatewayList.IpAddress.String);
+        if (gateway_ip != INADDR_NONE && gateway_ip != 0) {
+            unsigned char first_octet = gateway_ip & 0xFF;
+            unsigned char second_octet = (gateway_ip >> 8) & 0xFF;
+            if (first_octet == 100 && second_octet >= 64 && second_octet <= 127) {
+                free(adapter_info);
+                return 1;
+            }
+        }
+    }
+    free(adapter_info);
+    return 0;
+}
+
+static int natpmp_add_port_mapping(uint16_t port) {
+    clock_t start_time = clock();
+    if (initnatpmp(&natpmp, 0, 0) < 0) {
+        LbNetLog("NAT-PMP: Failed to initialize\n");
+        return 0;
+    }
+    if (sendpublicaddressrequest(&natpmp) < 0) {
+        LbNetLog("NAT-PMP: Failed to send public address request\n");
+        closenatpmp(&natpmp);
+        return 0;
+    }
+    natpmpresp_t response;
+    fd_set file_descriptors;
+    struct timeval timeout;
+    int result;
+    do {
+        double elapsed = (double)(clock() - start_time) / CLOCKS_PER_SEC;
+        if (elapsed > NATPMP_TIMEOUT_SECONDS) {
+            LbNetLog("NAT-PMP: Timeout getting public address\n");
+            closenatpmp(&natpmp);
+            return 0;
+        }
+        FD_ZERO(&file_descriptors);
+        FD_SET(natpmp.s, &file_descriptors);
+        getnatpmprequesttimeout(&natpmp, &timeout);
+        select(FD_SETSIZE, &file_descriptors, NULL, NULL, &timeout);
+        result = readnatpmpresponseorretry(&natpmp, &response);
+    } while (result == NATPMP_TRYAGAIN);
+    if (result < 0) {
+        LbNetLog("NAT-PMP: Failed to get public address\n");
+        closenatpmp(&natpmp);
+        return 0;
+    }
+    if (sendnewportmappingrequest(&natpmp, NATPMP_PROTOCOL_UDP, port, port, 0x7FFFFFFF) < 0) {
+        LbNetLog("NAT-PMP: Failed to send port mapping request\n");
+        closenatpmp(&natpmp);
+        return 0;
+    }
+    do {
+        double elapsed = (double)(clock() - start_time) / CLOCKS_PER_SEC;
+        if (elapsed > NATPMP_TIMEOUT_SECONDS) {
+            LbNetLog("NAT-PMP: Timeout on port mapping\n");
+            closenatpmp(&natpmp);
+            return 0;
+        }
+        FD_ZERO(&file_descriptors);
+        FD_SET(natpmp.s, &file_descriptors);
+        getnatpmprequesttimeout(&natpmp, &timeout);
+        select(FD_SETSIZE, &file_descriptors, NULL, NULL, &timeout);
+        result = readnatpmpresponseorretry(&natpmp, &response);
+    } while (result == NATPMP_TRYAGAIN);
+    if (result < 0) {
+        LbNetLog("NAT-PMP: Permanent lease rejected, trying timed lease\n");
+        if (sendnewportmappingrequest(&natpmp, NATPMP_PROTOCOL_UDP, port, port, 3600) < 0) {
+            LbNetLog("NAT-PMP: Failed to send timed port mapping request\n");
+            closenatpmp(&natpmp);
+            return 0;
+        }
+        do {
+            double elapsed = (double)(clock() - start_time) / CLOCKS_PER_SEC;
+            if (elapsed > NATPMP_TIMEOUT_SECONDS) {
+                LbNetLog("NAT-PMP: Timeout on timed lease\n");
+                closenatpmp(&natpmp);
+                return 0;
+            }
+            FD_ZERO(&file_descriptors);
+            FD_SET(natpmp.s, &file_descriptors);
+            getnatpmprequesttimeout(&natpmp, &timeout);
+            select(FD_SETSIZE, &file_descriptors, NULL, NULL, &timeout);
+            result = readnatpmpresponseorretry(&natpmp, &response);
+        } while (result == NATPMP_TRYAGAIN);
+        if (result < 0) {
+            LbNetLog("NAT-PMP: Failed to add port mapping\n");
+            closenatpmp(&natpmp);
+            return 0;
+        }
+    }
+    LbNetLog("NAT-PMP: Mapped port %u\n", port);
+    mapped_port = port;
+    active_method = PORT_FORWARD_NATPMP;
+    closenatpmp(&natpmp);
+    return 1;
+}
+
+static void port_forward_add_mapping_internal(uint16_t port) {
+    if (is_cgnat_detected()) {
+        LbNetLog("CGNAT detected, automatic port forwarding unavailable\n");
+        return;
+    }
+    if (active_method != PORT_FORWARD_NONE) {
+        port_forward_remove_mapping();
+    }
+    if (natpmp_add_port_mapping(port)) {
+        return;
+    }
+    int error = 0;
+    struct UPNPDev *device_list = upnpDiscover(UPNP_TIMEOUT_MS, NULL, NULL, 0, 0, 2, &error);
+    if (!device_list) {
+        LbNetLog("UPnP: No devices found\n");
+        return;
+    }
+    int internet_gateway_device_result = UPNP_GetValidIGD(device_list, &upnp_urls, &upnp_data, upnp_lanaddr, sizeof(upnp_lanaddr), NULL, 0);
+    freeUPNPDevlist(device_list);
+    if (internet_gateway_device_result == 0) {
+        LbNetLog("UPnP: Failed to get valid IGD\n");
+        FreeUPNPUrls(&upnp_urls);
+        return;
+    }
+    char port_string[16];
+    snprintf(port_string, sizeof(port_string), "%u", port);
+    UPNP_DeletePortMapping(upnp_urls.controlURL, upnp_data.first.servicetype, port_string, "UDP", "");
+    int result = UPNP_AddPortMapping(upnp_urls.controlURL, upnp_data.first.servicetype, port_string, port_string, upnp_lanaddr, "KeeperFX", "UDP", "", "0");
+    if (result != UPNPCOMMAND_SUCCESS) {
+        LbNetLog("UPnP: Permanent lease rejected, trying timed lease\n");
+        result = UPNP_AddPortMapping(upnp_urls.controlURL, upnp_data.first.servicetype, port_string, port_string, upnp_lanaddr, "KeeperFX", "UDP", "", "3600");
+        if (result != UPNPCOMMAND_SUCCESS) {
+            LbNetLog("UPnP: Failed to add port mapping\n");
+            FreeUPNPUrls(&upnp_urls);
+            return;
+        }
+    }
+    LbNetLog("UPnP: Mapped port %u\n", port);
+    mapped_port = port;
+    active_method = PORT_FORWARD_UPNP;
+}
+
+int port_forward_add_mapping(uint16_t port) {
+    std::thread(port_forward_add_mapping_internal, port).detach();
+    return 1;
+}
+
+void port_forward_remove_mapping(void) {
+    if (active_method == PORT_FORWARD_NONE || mapped_port == 0) {
+        return;
+    }
+    if (active_method == PORT_FORWARD_NATPMP) {
+        if (initnatpmp(&natpmp, 0, 0) < 0) {
+            LbNetLog("NAT-PMP: Failed to initialize for removal\n");
+            mapped_port = 0;
+            active_method = PORT_FORWARD_NONE;
+            return;
+        }
+        sendnewportmappingrequest(&natpmp, NATPMP_PROTOCOL_UDP, mapped_port, 0, 0);
+        natpmpresp_t response;
+        fd_set file_descriptors;
+        struct timeval timeout;
+        int result;
+        clock_t start_time = clock();
+        do {
+            double elapsed = (double)(clock() - start_time) / CLOCKS_PER_SEC;
+            if (elapsed > NATPMP_TIMEOUT_SECONDS) {
+                break;
+            }
+            FD_ZERO(&file_descriptors);
+            FD_SET(natpmp.s, &file_descriptors);
+            getnatpmprequesttimeout(&natpmp, &timeout);
+            select(FD_SETSIZE, &file_descriptors, NULL, NULL, &timeout);
+            result = readnatpmpresponseorretry(&natpmp, &response);
+        } while (result == NATPMP_TRYAGAIN);
+        closenatpmp(&natpmp);
+    } else if (active_method == PORT_FORWARD_UPNP) {
+        char port_string[16];
+        snprintf(port_string, sizeof(port_string), "%u", mapped_port);
+        UPNP_DeletePortMapping(upnp_urls.controlURL, upnp_data.first.servicetype, port_string, "UDP", NULL);
+        FreeUPNPUrls(&upnp_urls);
+    }
+    mapped_port = 0;
+    active_method = PORT_FORWARD_NONE;
+}

.gitignore
@@ -34,6 +34,8 @@ res/keeperfx_icon.ico
 /deps/ffmpeg
 /deps/openal
 /deps/luajit
+/deps/miniupnpc
+/deps/libnatpmp
 /deps/*.tar.gz
 /cppcheck.cache
 /src/ver_defs.h

Makefile
@@ -104,6 +104,7 @@ obj/bflib_crash.o \
 obj/bflib_datetm.o \
 obj/bflib_dernc.o \
 obj/bflib_enet.o \
+obj/net_portforward.o \
 obj/bflib_fileio.o \
 obj/bflib_filelst.o \
 obj/bflib_fmvids.o \
@@ -366,6 +367,8 @@ LINKLIB = -mwindows \
 	-L"deps/openal" -lOpenAL32 \
 	-L"deps/astronomy" -lastronomy \
 	-L"deps/enet" -lenet \
+	-L"deps/miniupnpc" -lminiupnpc \
+	-L"deps/libnatpmp" -lnatpmp -liphlpapi \
 	-L"deps/spng" -lspng \
 	-L"deps/centijson" -ljson \
 	-L"deps/zlib" -lminizip -lz \
@@ -382,7 +385,9 @@ INCS = \
 	-I"deps/astronomy/include" \
 	-I"deps/ffmpeg" \
 	-I"deps/openal/include" \
-	-I"deps/luajit/include"
+	-I"deps/luajit/include" \
+	-I"deps/miniupnpc/include" \
+	-I"deps/libnatpmp/include"
 CXXINCS =  $(INCS)
 
 STDOBJS   = $(subst obj/,obj/std/,$(OBJS))
@@ -631,10 +636,10 @@ libexterns: libexterns.mk
 
 clean-libexterns: libexterns.mk
 	-$(MAKE) -f libexterns.mk clean-libexterns
-	-$(RM) -rf deps/enet deps/zlib deps/spng deps/astronomy deps/centijson deps/luajit
+	-$(RM) -rf deps/enet deps/zlib deps/spng deps/astronomy deps/centijson deps/luajit deps/miniupnpc deps/libnatpmp
 	-$(RM) libexterns
 
-deps/enet deps/zlib deps/spng deps/astronomy deps/centijson deps/ffmpeg deps/openal deps/luajit:
+deps/enet deps/zlib deps/spng deps/astronomy deps/centijson deps/ffmpeg deps/openal deps/luajit deps/miniupnpc deps/libnatpmp:
 	$(MKDIR) $@
 
 src/api.c: deps/centijson/include/json.h
@@ -647,6 +652,7 @@ src/bflib_fmvids.cpp: deps/ffmpeg/libavformat/avformat.h
 src/bflib_sndlib.cpp: deps/openal/include/AL/al.h
 src/net_resync.cpp: deps/zlib/include/zlib.h
 src/console_cmd.c: deps/luajit/include/lua.h
+src/net_portforward.cpp: deps/miniupnpc/include/miniupnpc/miniupnpc.h deps/libnatpmp/include/natpmp/natpmp.h
 
 deps/enet-mingw32.tar.gz:
 	curl -Lso $@ "https://github.com/dkfans/kfx-deps/releases/download/initial/enet-mingw32.tar.gz"
@@ -697,6 +703,18 @@ deps/luajit/lib/libluajit.a: | deps/luajit/include/lua.h
 
 deps/luajit/include/lua.h: deps/luajit-mingw32.tar.gz | deps/luajit
 	tar xzmf $< -C deps/luajit
+
+deps/miniupnpc-mingw32.tar.gz:
+	curl -Lso $@ "https://github.com/dkfans/kfx-deps/releases/download/20260102/miniupnpc-mingw32.tar.gz"
+
+deps/miniupnpc/include/miniupnpc/miniupnpc.h: deps/miniupnpc-mingw32.tar.gz | deps/miniupnpc
+	tar xzmf $< -C deps/miniupnpc
+
+deps/libnatpmp-mingw32.tar.gz:
+	curl -Lso $@ "https://github.com/dkfans/kfx-deps/releases/download/20260102/libnatpmp-mingw32.tar.gz"
+
+deps/libnatpmp/include/natpmp/natpmp.h: deps/libnatpmp-mingw32.tar.gz | deps/libnatpmp
+	tar xzmf $< -C deps/libnatpmp
 
 cppcheck: | src/ver_defs.h
 cppcheck: | deps/zlib/include/zlib.h

src/bflib_enet.cpp
@@ -16,6 +16,7 @@
 #include "bflib_enet.h"
 #include "bflib_network.h"
 #include "bflib_math.h"
+#include "net_portforward.h"
 #include "game_legacy.h"
 #include "player_data.h"
 
@@ -80,6 +81,7 @@ namespace
 
     void bf_enet_exit()
     {
+        port_forward_remove_mapping();
         host_destroy();
         g_drop_callback = nullptr;
         enet_deinitialize();
@@ -94,18 +96,19 @@ namespace
      */
     TbError bf_enet_host(const char *session, void *options)
     {
-        ENetAddress addr = {.host = 0,
+        ENetAddress address = {.host = 0,
                             .port = DEFAULT_PORT };
         if (!*session)
             return Lb_FAIL;
         int port = atoi(session);
         if (port > 0)
-            addr.port = port;
-        host = enet_host_create(&addr, 4, NUM_CHANNELS, 0, 0);
+            address.port = port;
+        host = enet_host_create(&address, 4, NUM_CHANNELS, 0, 0);
         if (!host) {
             return Lb_FAIL;
         }
         enet_host_compress_with_range_coder(host);
+        port_forward_add_mapping(address.port);
         return Lb_OK;
     }
 

src/bflib_network.cpp
@@ -193,6 +193,7 @@ TbError LbNetwork_Stop(void) {
         netstate.sp->exit();
     }
     memset(&netstate, 0, sizeof(netstate));
+    netstate.my_id = INVALID_USER_ID;
     return Lb_OK;
 }
 

src/bflib_network_exchange.cpp
@@ -301,11 +301,11 @@ void LbNetwork_WaitForMissingPackets(void* server_buf, size_t client_frame_size)
 }
 
 TbError LbNetwork_Exchange(enum NetMessageType msg_type, void *send_buf, void *server_buf, size_t client_frame_size) {
-    netstate.sp->update(OnNewUser);
     if (netstate.my_id < 0 || netstate.my_id >= netstate.max_players) {
-        ERRORLOG("Critical error: Invalid my_id %i in LbNetwork_Exchange", netstate.my_id);
-        abort();
+        ERRORLOG("Invalid my_id %i in LbNetwork_Exchange (disconnected?)", netstate.my_id);
+        return Lb_FAIL;
     }
+    netstate.sp->update(OnNewUser);
     memcpy(((char*)server_buf) + netstate.my_id * client_frame_size, send_buf, client_frame_size);
     SendFrameToPeers(netstate.my_id, send_buf, client_frame_size, netstate.seq_nbr, msg_type);
     NetUserId id;

src/net_portforward.h
@@ -1,0 +1,35 @@
+/******************************************************************************/
+// Free implementation of Bullfrog's Dungeon Keeper strategy game.
+/******************************************************************************/
+/**
+ * @file net_portforward.h
+ *     Port forwarding support using NAT-PMP and UPnP.
+ * @par Purpose:
+ *     Automatic port forwarding for multiplayer hosting.
+ *     Tries NAT-PMP first (simpler/faster), falls back to UPnP.
+ * @author   KeeperFX Team
+ * @date     01 Jan 2026
+ * @par  Copying and copyrights:
+ *     This program is free software; you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation; either version 2 of the License, or
+ *     (at your option) any later version.
+ */
+/******************************************************************************/
+#ifndef NET_PORTFORWARD_H
+#define NET_PORTFORWARD_H
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int port_forward_add_mapping(uint16_t port);
+void port_forward_remove_mapping(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

commit 8cb6376c331d235bd92c5c7375add293df57262e

Linux: fix lights (#4466)
- fixes #4398
and also the compilation on linux that broke by #4450

linux.mk
@@ -29,6 +29,7 @@ src/bflib_cpu.c \
 src/bflib_datetm.cpp \
 src/bflib_dernc.c \
 src/bflib_enet.cpp \
+src/net_portforward.cpp \
 src/bflib_fileio.c \
 src/bflib_filelst.c \
 src/bflib_fmvids.cpp \
@@ -294,7 +295,9 @@ KFX_LDFLAGS += \
 	$(shell pkg-config --libs-only-l luajit) \
 	$(shell pkg-config --libs-only-l spng) \
 	$(shell pkg-config --libs-only-l minizip) \
-	$(shell pkg-config --libs-only-l zlib)
+	$(shell pkg-config --libs-only-l zlib) \
+	-lminiupnpc \
+	-lnatpmp
 
 TOML_SOURCES = \
 	deps/centitoml/toml_api.c

src/bflib_math.c
@@ -608,11 +608,11 @@ long LbCosL(long x)
  * @param y
  * @return
  */
-long LbArcTanAngle(long x,long y)
+int32_t LbArcTanAngle(int32_t x,int32_t y)
 {
-    long ux;
-    long uy;
-    unsigned long index;
+    int32_t ux;
+    int32_t uy;
+    uint32_t index;
     if ((x == 0) && (y == 0))
         return 0;
     if (x < 0)
@@ -625,10 +625,10 @@ long LbArcTanAngle(long x,long y)
             // This way we won't exceed factors array bounds (which is 256 elements).
             if (ux < uy) {
                 index = (ux << 8)/uy;
-                return DEGREES_360   - (long)lbArcTanFactors[index];
+                return DEGREES_360   - (int32_t)lbArcTanFactors[index];
             } else {
                 index = (uy << 8)/ux;
-                return ANGLE_WEST + (long)lbArcTanFactors[index];
+                return ANGLE_WEST + (int32_t)lbArcTanFactors[index];
             }
         } else
         {
@@ -636,10 +636,10 @@ long LbArcTanAngle(long x,long y)
             // Make sure we'll have smaller value * 256 / greater value.
             if (ux < uy) {
                 index = (ux << 8)/uy;
-                return   DEGREES_180   + (long)lbArcTanFactors[index];
+                return   DEGREES_180   + (int32_t)lbArcTanFactors[index];
             } else {
                 index = (uy << 8)/ux;
-                return ANGLE_WEST - (long)lbArcTanFactors[index];
+                return ANGLE_WEST - (int32_t)lbArcTanFactors[index];
             }
         }
     } else
@@ -651,10 +651,10 @@ long LbArcTanAngle(long x,long y)
             // Make sure we'll have smaller value * 256 / greater value.
             if (ux < uy) {
                 index = (ux << 8)/uy;
-                return                 (long)lbArcTanFactors[index];
+                return                 (int32_t)lbArcTanFactors[index];
             } else {
                 index = (uy << 8)/ux;
-                return ANGLE_EAST - (long)lbArcTanFactors[index];
+                return ANGLE_EAST - (int32_t)lbArcTanFactors[index];
             }
         } else
         {
@@ -662,10 +662,10 @@ long LbArcTanAngle(long x,long y)
             // Make sure we'll have smaller value * 256 / greater value.
             if (ux < uy) {
                 index = (ux << 8)/uy;
-                return DEGREES_180   - (long)lbArcTanFactors[index];
+                return DEGREES_180   - (int32_t)lbArcTanFactors[index];
             } else {
                 index = (uy << 8)/ux;
-                return ANGLE_EAST + (long)lbArcTanFactors[index];
+                return ANGLE_EAST + (int32_t)lbArcTanFactors[index];
             }
         }
     }

src/bflib_math.h
@@ -70,7 +70,7 @@ struct Proportion { // sizeof = 8
 long LbSinL(long x);
 long LbCosL(long x);
 long LbSqrL(long x);
-long LbArcTanAngle(long x,long n);
+int32_t LbArcTanAngle(int32_t x,int32_t y);
 long LbMathOperation(unsigned char opkind, long first_operand, long second_operand);
 unsigned long LbRandomSeries(unsigned long range, uint32_t *seed, const char *func_name, unsigned long place);
 TbBool LbNumberSignsSame(long num_a, long num_b);

src/net_portforward.cpp
@@ -26,13 +26,19 @@
 #include <miniupnpc/upnperrors.h>
 
 #define NATPMP_STATICLIB
+#ifdef __WIN32__
 #include <natpmp/natpmp.h>
+#else
+#include <natpmp.h>
+#endif
 
 #include <cstdio>
 #include <ctime>
 #include <thread>
+#ifdef __WIN32__
 #include <winsock2.h>
 #include <iphlpapi.h>
+#endif
 
 #define NATPMP_TIMEOUT_SECONDS 1.0
 #define UPNP_TIMEOUT_MS 3000
@@ -54,6 +60,7 @@ static char upnp_lanaddr[64];
 
 static natpmp_t natpmp;
 
+#ifdef __WIN32__
 static int is_cgnat_detected() {
     ULONG buffer_size = 0;
     if (GetAdaptersInfo(NULL, &buffer_size) != ERROR_BUFFER_OVERFLOW) {
@@ -90,6 +97,11 @@ static int is_cgnat_detected() {
     free(adapter_info);
     return 0;
 }
+#else
+static int is_cgnat_detected() {
+    return 0;
+}
+#endif
 
 static int natpmp_add_port_mapping(uint16_t port) {
     clock_t start_time = clock();
@@ -192,7 +204,11 @@ static void port_forward_add_mapping_internal(uint16_t port) {
         LbNetLog("UPnP: No devices found\n");
         return;
     }
+#ifdef __WIN32__
     int internet_gateway_device_result = UPNP_GetValidIGD(device_list, &upnp_urls, &upnp_data, upnp_lanaddr, sizeof(upnp_lanaddr), NULL, 0);
+#else
+    int internet_gateway_device_result = UPNP_GetValidIGD(device_list, &upnp_urls, &upnp_data, upnp_lanaddr, sizeof(upnp_lanaddr));
+#endif
     freeUPNPDevlist(device_list);
     if (internet_gateway_device_result == 0) {
         LbNetLog("UPnP: Failed to get valid IGD\n");