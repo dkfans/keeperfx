dbc508c4dee3c290a0d7706ad1da3aec79473c8b
Loobinex <Loobinex@users.noreply.github.com>
2026-02-06 01:04:27 +0100

    Some long gones

--- src/thing_objects.h
+++ src/thing_objects.h
@@ -209,9 +209,9 @@ int get_wealth_size_types_count(void);
 struct Thing *create_gold_hoard_object(const struct Coord3d *pos, PlayerNumber plyr_idx, GoldAmount value);
 struct Thing *find_gold_hoard_at(MapSubtlCoord stl_x, MapSubtlCoord stl_y);
 struct Thing *create_gold_hoarde(struct Room *room, const struct Coord3d *pos, GoldAmount value);
-long add_gold_to_hoarde(struct Thing *thing, struct Room *room, GoldAmount amount);
-long remove_gold_from_hoarde(struct Thing *thing, struct Room *room, GoldAmount amount);
-long gold_being_dropped_at_treasury(struct Thing* thing, struct Room* room);
+GoldAmount add_gold_to_hoarde(struct Thing *thing, struct Room *room, GoldAmount amount);
+GoldAmount remove_gold_from_hoarde(struct Thing *thing, struct Room *room, GoldAmount amount);
+GoldAmount gold_being_dropped_at_treasury(struct Thing* thing, struct Room* room);
 
 struct Thing *drop_gold_pile(long value, struct Coord3d *pos);
 struct Thing *create_gold_pot_at(long pos_x, long pos_y, PlayerNumber plyr_idx);
