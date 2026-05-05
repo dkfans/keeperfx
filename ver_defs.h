a95cd3b2b515a575c08a375784d93093689bce2d
Elias <axidal@gmail.com>
2026-05-05 06:54:07 +0200

    remove autogenerated file

--- ver_defs.h
+++
@@ -1,7 +0,0 @@
-#define VER_MAJOR       1
-#define VER_MINOR       2
-#define VER_RELEASE     0
-#define VER_BUILD       0
-#define VER_STRING      "1.2.0.0 "
-#define PACKAGE_SUFFIX  ""
-#define GIT_REVISION    "de7f52669"
