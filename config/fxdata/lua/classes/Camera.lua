d31d561a64ab5161496e9351529638b3a92e4389
PieterVdc <pieter00@gmail.com>
2026-05-12 15:30:54 +0200

    .

---
+++ config/fxdata/lua/classes/Camera.lua
@@ -0,0 +1,10 @@
+---@class Camera
+---@field pos Pos3d
+---@field yaw integer angle 0-2048
+---@field pitch integer angle 0-2048
+---@field roll integer angle 0-2048
+---@field horizontal_fov integer default 94
+---@field zoom integer 
+---@field view_mode integer
+local Camera = {}
+
