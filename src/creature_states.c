2104f94146abc322ba9e985c6f6b9f57926a7812
Loobinex <Loobinex@users.noreply.github.com>
2026-05-16 18:15:46 +0200

    Deep work is work in code too

--- src/creature_states.c
+++ src/creature_states.c
@@ -544,7 +544,8 @@ const CreatureStateCheck move_check_func_list[] = {
  * - 2: Fighting.
  */
 long const state_type_to_gui_state[STATE_TYPES_COUNT] = {
-    0, 1, 0, 0, 0, 2, 0, 0, 1, 0, 0, 2, 2, 1, 1,
+    CrGUIJob_Wandering, CrGUIJob_Working, CrGUIJob_Wandering, CrGUIJob_Wandering, CrGUIJob_Wandering, CrGUIJob_Fighting, CrGUIJob_Wandering, CrGUIJob_Wandering,
+    CrGUIJob_Working, CrGUIJob_Wandering, CrGUIJob_Wandering, CrGUIJob_Fighting, CrGUIJob_Fighting,CrGUIJob_Working, CrGUIJob_Working, CrGUIJob_Working
 };
 
 /******************************************************************************/
