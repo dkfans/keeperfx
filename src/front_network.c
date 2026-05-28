11ad32db132315d7b005b31daca31e5a0c67ec07
rainlizard <15337628+rainlizard@users.noreply.github.com>
2026-05-28 12:01:34 +1000

    alliance fixes

--- src/front_network.c
+++ src/front_network.c
@@ -476,6 +476,12 @@ static void process_frontend_packets(void)
   for (i = 0; i < MAX_NET_USERS; i++) {
     nspckt = &net_screen_packet[i];
     if ((nspckt->networkstatus_flags & NetStat_PlayerConnected) != 0) {
+      if (frontend_alliances == -1) {
+        frontend_alliances = nspckt->frontend_alliances;
+      }
+      if (frontend_alliances == -1) {
+        frontend_alliances = 0;
+      }
         switch (screen_packet_action(nspckt)) {
         case NetAct_HostStartLevel:
             if (version_mismatch_found) {
@@ -504,11 +510,6 @@ static void process_frontend_packets(void)
         default:
             break;
         }
-      if (frontend_alliances == -1) {
-        if (nspckt->frontend_alliances != -1) {
-          frontend_alliances = nspckt->frontend_alliances;
-        }
-      }
       if (fe_computer_players == 2) {
         int32_t k = (nspckt->networkstatus_flags & NetStat_ComputerPlayersMask) >> NetStat_ComputerPlayersShift;
         if (k != 2) {
@@ -518,14 +519,10 @@ static void process_frontend_packets(void)
     }
     screen_packet_set_action(nspckt, NetAct_None);
   }
-  if (frontend_alliances == -1) {
-    frontend_alliances = 0;
-  }
   for (i = 0; i < MAX_NET_USERS; i++) {
     if (!network_player_active(i)) {
-      if (frontend_is_player_allied(my_player_number, i)) {
-        frontend_set_alliance(my_player_number, i);
-      }
+      const int32_t alliances_to_clear = alliance_grid[i][0] | alliance_grid[i][1] | alliance_grid[i][2] | alliance_grid[i][3];
+      frontend_alliances = frontend_alliances & ~alliances_to_clear;
     }
   }
 }
