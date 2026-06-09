052651bf11a06651add79df41dc556d83b9a1545
rainlizard <15337628+rainlizard@users.noreply.github.com>
2026-06-09 21:30:44 +1000

    display flag fix

--- src/engine_render.c
+++ src/engine_render.c
@@ -5306,9 +5306,8 @@ void draw_status_sprites(long scrpos_x, long scrpos_y, struct Thing *thing)
 
     struct CreatureControl *cctrl;
     cctrl = creature_control_get_from_thing(thing);
-    if (cctrl->force_health_flower_hidden == true)
-        return;
-    if (flag_is_set(get_creature_model_flags(thing), CMF_NoHealthFlower)) {
+    if ((cctrl->force_health_flower_hidden == true) || flag_is_set(get_creature_model_flags(thing), CMF_NoHealthFlower)) {
+        lbDisplay.DrawFlags = flg_mem;
         return;
     }
     if (flag_is_set(game.mode_flags,MFlg_NoHeroHealthFlower))
