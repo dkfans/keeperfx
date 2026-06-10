80f6a643852be708153a98c9167a62a831d717f7
J.W. Jagersma <jwjagersma@gmail.com>
2026-06-10 19:24:22 +0200

    Add config option ROTATE_AROUND_MOUSE

--- src/front_input.c
+++ src/front_input.c
@@ -100,6 +100,7 @@ long old_mx;
 long old_my;
 
 enum ZoomToMouseOptions zoom_to_mouse_option = ZoomToMouse_Always;
+enum RotateAroundMouseOptions rotate_around_mouse_option = RotateAroundMouse_Always;
 
 const struct GamekeySettings game_key_settings[GAME_KEYS_COUNT] = {
     {"MoveUp",                GUIStr_CtrlUp,                  KC_W, KMod_NONE,               CBtn_LS_UP,               BMV_Visible,        },       // Gkey_MoveUp
@@ -2255,9 +2256,17 @@ static void get_isometric_view_nonaction_inputs(void)
         if (rotate_pressed)
         {
             if (is_game_key_pressed(Gkey_MoveLeft, false, no_mods) || is_key_pressed(KC_LEFT, KMod_DONTCARE))
+            {
+                if (rotate_around_mouse_option == RotateAroundMouse_OnlyCtrl)
+                    set_packet_control(packet, PCtr_ViewRotatePos);
                 set_packet_control(packet, PCtr_ViewRotateCW);
+            }
             if (is_game_key_pressed(Gkey_MoveRight, false, no_mods) || is_key_pressed(KC_RIGHT, KMod_DONTCARE))
+            {
+                if (rotate_around_mouse_option == RotateAroundMouse_OnlyCtrl)
+                    set_packet_control(packet, PCtr_ViewRotatePos);
                 set_packet_control(packet, PCtr_ViewRotateCCW);
+            }
             if (is_game_key_pressed(Gkey_MoveUp, false, no_mods) || is_key_pressed(KC_UP, KMod_DONTCARE))
                 set_packet_control(packet, PCtr_ViewZoomIn);
             if (is_game_key_pressed(Gkey_MoveDown, false, no_mods) || is_key_pressed(KC_DOWN, KMod_DONTCARE))
@@ -2265,9 +2274,17 @@ static void get_isometric_view_nonaction_inputs(void)
         } else
         {
             if (is_game_key_pressed(Gkey_RotateCW, false, false))
+            {
+                if (rotate_around_mouse_option == RotateAroundMouse_NotCtrl)
+                    set_packet_control(packet, PCtr_ViewRotatePos);
                 set_packet_control(packet, PCtr_ViewRotateCW);
+            }
             if (is_game_key_pressed(Gkey_RotateCCW, false, false))
+            {
+                if (rotate_around_mouse_option == RotateAroundMouse_NotCtrl)
+                    set_packet_control(packet, PCtr_ViewRotatePos);
                 set_packet_control(packet, PCtr_ViewRotateCCW);
+            }
             if (is_game_key_pressed(Gkey_ZoomIn, false, false))
                 set_packet_control(packet, PCtr_ViewZoomIn);
             if (is_game_key_pressed(Gkey_ZoomOut, false, false))
@@ -2512,6 +2529,8 @@ static void get_dungeon_control_nonaction_inputs(void)
   get_options_menu_inputs();
   if (zoom_to_mouse_option == ZoomToMouse_Always)
       set_packet_control(pckt, PCtr_ViewZoomPos);
+  if (rotate_around_mouse_option == RotateAroundMouse_Always)
+      set_packet_control(pckt, PCtr_ViewRotatePos);
   if ((player->allocflags & PlaF_NewMPMessage) == 0)
   {
       switch (player->view_mode)
