86d620e17f52a7430c3c26558ecb55e062a01578
AdamPlenty <58278560+AdamPlenty@users.noreply.github.com>
2026-06-16 21:33:27 +0100

    Update thing_objects.c

--- src/thing_objects.c
+++ src/thing_objects.c
@@ -465,7 +465,7 @@ TbBool object_is_gold_pile(const struct Thing *thing)
     if (thing->class_id != TCls_Object)
         return false;
     struct ObjectConfigStats* objst = get_object_model_stats(thing->model);
-	return (objst->genre == OCtg_Valuable);
+    return (objst->genre == OCtg_Valuable);
 }
 
 TbBool object_is_gold_laying_on_ground(const struct Thing *thing)
