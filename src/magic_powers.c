9e0cb9a48d2fb66d08420f8abd4206eacfb81a6d
Loobinex <Loobinex@users.noreply.github.com>
2026-07-10 20:49:04 +0200

    Lua: OnPickup and OnSlap triggers (#4805)

--- src/magic_powers.c
+++ src/magic_powers.c
@@ -1207,9 +1207,11 @@ static TbResult magic_use_power_hand(PowerKind power_kind, PlayerNumber plyr_idx
 {
     if (power_hand_is_full(get_player(plyr_idx)))
         return Lb_FAIL;
-    else
-    if (place_thing_in_power_hand(thing, plyr_idx))
+    else if (place_thing_in_power_hand(thing, plyr_idx))
+    {
+        lua_on_pick_up(thing, plyr_idx);
         return Lb_SUCCESS;
+    }
     else
         return Lb_FAIL;
 }
@@ -1799,6 +1801,7 @@ static TbResult magic_use_power_slap_thing(PowerKind power_kind, PlayerNumber pl
 {
     struct PlayerInfo *player;
     struct Dungeon *dungeon;
+    lua_on_slap(thing, plyr_idx);
     if (!thing_exists(thing)) {
         return Lb_FAIL;
     }
