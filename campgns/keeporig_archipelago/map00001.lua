bb8b9c467cfc6e0e6b4f6d3a26407efd549e57c3
Spatulade <100633508+Spatulade@users.noreply.github.com>
2026-08-06 03:20:42 +0100

    little bit of lua progress
    Can place and name specials.

--- campgns/keeporig_archipelago/map00001.lua
+++ campgns/keeporig_archipelago/map00001.lua
@@ -10,6 +10,7 @@
 function OnGameStart()
 	Setup()
 	SetupTriggers()
+
 end
 
 function OnGameLoad()
@@ -25,9 +26,27 @@ function OnItemReceived(itemid)
       RoomAvailableById("ALL_PLAYERS", roomid, 2, true)
 end
 
+--example list, I assume we could have a way to link these from the full list.
+local BoxTooltips = {
+      [101] = "Cooldave's BOULDERBADGE",
+      [102] = "PinkGuy's Hookshot",
+      [103] = "xxSkullBoixx's BFG9000",
+}
+
 --here we setup things 
 function Setup()
+      --is there a clever way to just check the APs on map from 101+ and spawn specbox n on AP n?
+      --also want it to check if the check has gone through yet or not
 
+
+      --I assume this isn't good enough. We want it to check if the box has ever been activated/the check has been sent out.
+      --RegisterOnConditionEvent(function() AddObjectToLevel("SPECBOX_CUSTOM",101,101,"PLAYER_NEUTRAL",0) end, function() return (PLAYER0.BOX101_ACTIVATED == 0) end)
+      --RegisterOnConditionEvent(function() AddObjectToLevel("TEMPLE_STATUE",101,101,"PLAYER_NEUTRAL",0) end, function() return (PLAYER0.BOX101_ACTIVATED >= 1) end)
+      --RegisterOnConditionEvent(function() QuickMessage("Sent ") end, function() return (PLAYER0.BOX101_ACTIVATED >= 1) end)
+      for id, tooltip in pairs(BoxTooltips) do
+            AddObjectToLevel("SPECBOX_CUSTOM",id,id,"PLAYER_NEUTRAL",0)
+            SetBoxTooltip(id, tooltip)
+      end
 end
 
 function SetupTriggers()
@@ -35,8 +54,9 @@ function SetupTriggers()
 
     local activated_box = eventData.SpecialBoxId
     print(activated_box)
-
     SendLocation(activated_box)
  end)
+
+
 
 end
