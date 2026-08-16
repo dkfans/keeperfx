06960183c01d8195442b683f385784fa69f2f220
Spatulade <100633508+Spatulade@users.noreply.github.com>
2026-08-16 17:35:16 +0100

    added mockup hub level

---
+++ campgns/keeporig_archipelago/map01000.lua
@@ -0,0 +1,23 @@
+-- ********************************************
+--
+--        Unnamed 2026.8.16 map
+--        by --insert author--
+--
+-- ********************************************
+
+
+--will get called when the game starts
+function OnGameStart()
+	Setup()
+	SetupTriggers()
+end
+
+--here we setup things 
+function Setup()
+
+end
+
+--here we setup the triggers, these can be found in fxdata/lua/triggers/Events.lua
+function SetupTriggers()
+
+end
