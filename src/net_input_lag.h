dc435182b04054172c51560fdc0437e99c898a3d
rainlizard <15337628+rainlizard@users.noreply.github.com>
2026-08-24 00:41:52 +1000

    Multiplayer: more responsive dynamic input lag (#5150)

--- src/net_input_lag.h
+++ src/net_input_lag.h
@@ -5,6 +5,11 @@
 #include "bflib_basics.h"
 
 #define MAXIMUM_INPUT_LAG_TURNS 12
+#define INPUT_LAG_INCREASE_MISSES 3
+#define INPUT_LAG_INCREASE_TURNS 4
+#define INPUT_LAG_INCREASE_COOLDOWN_TURNS 600
+#define INPUT_LAG_DECREASE_SAMPLE_TURNS 600
+#define INPUT_LAG_DECREASE_MISS_PERCENT 1
 
 #ifdef __cplusplus
 extern "C" {
@@ -16,8 +21,8 @@ TbBool input_lag_skips_processing(void);
 unsigned short calculate_skip_input(void);
 void input_lag_update(struct Packet *packet);
 void input_lag_reset(void);
-void input_lag_reset_intervals(void);
-void input_lag_get_stats(int32_t *packet_misses, TbClockMSec *increase_countdown, TbClockMSec *decrease_countdown);
+void input_lag_reset_request(int32_t input_lag_turns);
+void input_lag_get_stats(int32_t *increase_missed_turns, int32_t *increase_sampled_turns, int32_t *decrease_missed_turns, int32_t *decrease_sampled_turns);
 void input_lag_note_packet_wait(void);
 void input_lag_observe_host_packet(const struct Packet *packet);
 TbBool input_lag_needs_lookahead(void);
