53e27cfbceafb37b229abd835c6e7506ea2cfaf2
Loobinex <Loobinex@users.noreply.github.com>
2026-08-24 20:39:47 +0200

    Remove scaling from custom icons
    Best to do it how it is, then people can use a correct sprite. Tiny looks bad too.

--- src/gui_msgs.c
+++ src/gui_msgs.c
@@ -72,7 +72,6 @@ void message_draw(void)
             LbTextDrawResized(x+32*units_per_pixel/16, y, tx_units_per_px, game.messages[i].text);
             unsigned long spr_idx = 0;
             PlayerNumber plyr_idx = game.messages[i].plyr_idx;
-            int current_sprite_scale = 0; 
             switch (game.messages[i].type)
             {
                 case MsgType_Player:
@@ -174,8 +173,7 @@ void message_draw(void)
                 case MsgType_Custom:
                 {
                     spr = get_panel_sprite(spr_idx);                    
-                    current_sprite_scale = (LbTextLineHeight() * units_per_pixel) / spr->SHeight;
-                    LbSpriteDrawResized(x, y, current_sprite_scale, spr);
+                    LbSpriteDrawResized(x, y, ps_units_per_px, spr);
                     break;
                 }
             }
