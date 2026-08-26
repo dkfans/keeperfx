4fe42cd95a570a401d3774e87d208b5ff8e82607
rainlizard <15337628+rainlizard@users.noreply.github.com>
2026-08-27 02:54:57 +1000

    navigation_regions_connected

--- src/ariadne_regions.c
+++ src/ariadne_regions.c
@@ -48,48 +48,9 @@ static long ix_RegionQput;
 static long ix_RegionQget;
 static long count_RegionQ;
 static long RegionQueue[REGION_QUEUE_LEN];
-static uint32_t navigation_components[PLAYERS_COUNT][TRIANLGLES_COUNT];
-static PlayerBitFlags navigation_components_valid;
 /******************************************************************************/
 struct RegionT bad_region;
 /******************************************************************************/
-static void rebuild_navigation_regions(PlayerNumber owner)
-{
-    uint32_t *components = navigation_components[owner];
-    NavColour owner_mask = 1 << (NAVMAP_OWNERSELECT_BIT + owner);
-    NavColour blocked_mask = NAVMAP_FLOORHEIGHT_MASK | owner_mask;
-    memset(components, 0, sizeof(navigation_components[owner]));
-    for (int32_t triangle = 0; triangle < ix_Triangles; triangle++) {
-        if (components[triangle]) {
-            continue;
-        }
-        NavColour triangle_alt = get_triangle_tree_alt(triangle);
-        if ((triangle_alt & blocked_mask) >= NAVMAP_FLOORHEIGHT_MAX) {
-            continue;
-        }
-        components[triangle] = triangle + 1;
-        tree_val[0] = triangle;
-        for (uint32_t head = 0, tail = 1; head < tail; head++) {
-            for (int32_t edge = 0; edge < 3; edge++) {
-                int32_t next = Triangles[tree_val[head]].tags[edge];
-                if (next < 0) {
-                    continue;
-                }
-                if (components[next]) {
-                    continue;
-                }
-                NavColour next_alt = get_triangle_tree_alt(next);
-                if ((next_alt & blocked_mask) >= NAVMAP_FLOORHEIGHT_MAX) {
-                    continue;
-                }
-                components[next] = components[triangle];
-                tree_val[tail++] = next;
-            }
-        }
-    }
-    navigation_components_valid |= to_flag(owner);
-}
-
 TbBool navigation_regions_connected(int32_t first_triangle, int32_t second_triangle, PlayerNumber owner)
 {
     if (!regions_connected(first_triangle, second_triangle)) {
@@ -98,21 +59,33 @@ TbBool navigation_regions_connected(int32_t first_triangle, int32_t second_trian
     if (owner < 0 || owner >= PLAYERS_COUNT) {
         return true;
     }
-    if (!(navigation_components_valid & to_flag(owner))) {
-        rebuild_navigation_regions(owner);
-    }
-    uint32_t *components = navigation_components[owner];
-    uint32_t first_component = components[first_triangle];
-    uint32_t second_component = components[second_triangle];
-    if (!first_component || !second_component) {
+    NavColour blocked_mask = NAVMAP_FLOORHEIGHT_MASK | 1 << (NAVMAP_OWNERSELECT_BIT + owner);
+    if ((get_triangle_tree_alt(first_triangle) & blocked_mask) >= NAVMAP_FLOORHEIGHT_MAX || (get_triangle_tree_alt(second_triangle) & blocked_mask) >= NAVMAP_FLOORHEIGHT_MAX) {
         return true;
     }
-    return first_component == second_component;
-}
-
-void invalidate_navigation_regions(void)
-{
-    navigation_components_valid = 0;
+    if (first_triangle == second_triangle) {
+        return true;
+    }
+    tags_init();
+    store_current_tag(first_triangle);
+    tree_val[0] = first_triangle;
+    for (uint32_t head = 0, tail = 1; head < tail; head++) {
+        for (int32_t edge = 0; edge < 3; edge++) {
+            int32_t next = Triangles[tree_val[head]].tags[edge];
+            if (next < 0 || is_current_tag(next)) {
+                continue;
+            }
+            if ((get_triangle_tree_alt(next) & blocked_mask) >= NAVMAP_FLOORHEIGHT_MAX) {
+                continue;
+            }
+            if (next == second_triangle) {
+                return true;
+            }
+            store_current_tag(next);
+            tree_val[tail++] = next;
+        }
+    }
+    return false;
 }
 
 /**
