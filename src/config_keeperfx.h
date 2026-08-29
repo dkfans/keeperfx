c4e4dd8993490b4b9b92ce03800638b3dc1f1274
NaOH <nstbayless@gmail.com>
2026-08-29 21:09:40 +0000

    fix accidental comma removal

--- src/config_keeperfx.h
+++ src/config_keeperfx.h
@@ -52,7 +52,7 @@ enum TbFeature {
     Ft_DisableCursorCameraPanning   = 0x20000,
     Ft_DeltaTime                    = 0x40000,
     Ft_NoCdMusic                    = 0x80000,
-    Ft_RelativeMouseMode            = 0x100000
+    Ft_RelativeMouseMode            = 0x100000,
 };
 
 enum TbLanguage {
