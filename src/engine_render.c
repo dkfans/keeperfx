3d49bd0103555bc967f4642c7253953d40cd1a04
AdamPlenty <58278560+AdamPlenty@users.noreply.github.com>
2026-09-05 06:33:15 +0100

    Better fix?

--- src/engine_render.c
+++ src/engine_render.c
@@ -8708,12 +8708,12 @@ void create_frontview_map_volume_box(struct Camera *cam, unsigned char stl_width
         coord_x -= box_width;
         break;
     }
-    coord_z -= (16 * (long)stl_width) >> 2;
+    coord_z -= (11 * (long)stl_width) >> 2;
     // Draw 4 horizonal line elements
-    create_line_element(coord_x,             coord_y,                      coord_x + box_width, coord_y,                      coord_z - box_height,             line_color);
-    create_line_element(coord_x,             coord_y + box_height,         coord_x + box_width, coord_y + box_height,         coord_z - box_height,             line_color);
+    create_line_element(coord_x,             coord_y,                      coord_x + box_width, coord_y,                      coord_z,                          line_color);
+    create_line_element(coord_x,             coord_y + box_height,         coord_x + box_width, coord_y + box_height,         coord_z - box_height - stl_width, line_color);
     create_line_element(coord_x,             coord_y + depth,              coord_x + box_width, coord_y + depth,              coord_z - box_height,             line_color);
-    create_line_element(coord_x,             coord_y + box_height + depth, coord_x + box_width, coord_y + box_height + depth, coord_z - box_height,             line_color);
+    create_line_element(coord_x,             coord_y + box_height + depth, coord_x + box_width, coord_y + box_height + depth, coord_z - box_height - stl_width, line_color);
     // Now the lines at left and right
     create_line_element(coord_x,             coord_y,                      coord_x,             coord_y + box_height,         coord_z - box_height,             line_color);
     create_line_element(coord_x + box_width, coord_y,                      coord_x + box_width, coord_y + box_height,         coord_z - box_height,             line_color);
@@ -8802,7 +8802,7 @@ void create_fancy_frontview_map_volume_box(struct RoomSpace roomspace, struct Ca
         }
         break;
     }
-    coord_z -= (16 * (long)stl_width) >> 2;
+    coord_z -= (11 * (long)stl_width) >> 2;
     for (int roomspace_y = 0; roomspace_y < room_slab_height; roomspace_y += 1)
     {
         int y_start = (box_height * roomspace_y       / room_slab_height) + ((((box_height * roomspace_y)       % room_slab_height) >= room_slab_height) ? 1 : 0);
@@ -8891,12 +8891,12 @@ void create_fancy_frontview_map_volume_box(struct RoomSpace roomspace, struct Ca
                     }
                     if (top_edge)
                     {
-                        create_line_element(    coord_x + x_start, coord_y + y_start,         coord_x + x_end,   coord_y + y_start,         bckt_idx,             line_color);
+                        create_line_element(    coord_x + x_start, coord_y + y_start,         coord_x + x_end,   coord_y + y_start,         bckt_idx - stl_width, line_color);
                         create_line_element(    coord_x + x_start, coord_y + y_start + depth, coord_x + x_end,   coord_y + y_start + depth, bckt_idx,             line_color);
                     }
                     if (bottom_edge)
                     {
-                        create_line_element(    coord_x + x_start, coord_y + y_end,           coord_x + x_end,   coord_y + y_end,           bckt_idx,             line_color);
+                        create_line_element(    coord_x + x_start, coord_y + y_end,           coord_x + x_end,   coord_y + y_end,           bckt_idx - stl_width, line_color);
                         create_line_element(    coord_x + x_start, coord_y + y_end + depth,   coord_x + x_end,   coord_y + y_end + depth,   bckt_idx,             line_color);
                     }
                     line_color = map_volume_box.color; // switch back to default color (red/green) for the inner line
