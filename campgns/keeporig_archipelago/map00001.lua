7872887cfd409ed17d74cde9d339256efbb18109
gibb3h <gibb3h@gmail.com>
2026-09-09 21:47:03 +0100

    expose player.victory_state so we can check when level is won

--- campgns/keeporig_archipelago/map00001.lua
+++ campgns/keeporig_archipelago/map00001.lua
@@ -19,8 +19,3 @@ function OnGameLoad()
       QuickMessage("Game loaded.", "ARCHIPELAGO_ICON")
       CommandsMain.MainSetup()
 end
-
--- Need some sort of OnGameWin command so we can send location 10000 + Map.map_number on map win.
---IF(PLAYER0,FLAG0 == 7) you win, try to send the check 10000+level here.
---Replace the flag condition with a general "level is won" one when we can.
-RegisterOnConditionEvent(function() SendLocation(10000+Map.map_number) end, function() return (PLAYER0.FLAG0 == 7) end)
