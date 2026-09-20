67fd4990b69a541d50ce4f7683ea5773d70fc129
Shinthoras0815 <Shinthoras0815@gmail.com>
2026-09-20 17:31:12 +0200

    lua more room fields

--- config/fxdata/lua/classes/Room.lua
+++ config/fxdata/lua/classes/Room.lua
@@ -5,11 +5,13 @@
 ---@field type room_type
 ---@field owner Player the player that owns the room
 ---@field slabs Slab[] list of slabs in the room
+---@field slabs_count integer Number of slabs associated with the room
 ---@field workers Creature[] list of creatures currently doing a job in the room
 ---@field health integer current health of the room
 ---@field max_health integer maximum health of the room
 ---@field used_capacity integer how much of the room's capacity is used
 ---@field max_capacity integer maximum capacity of the room
+---@field capacity_used_for_storage integer storage in use without workers, in gold rooms the gold amount. Don't set on entrance, lair or hatchery since they reuse the same memory slot for other room data
 ---@field efficiency integer efficiency of the room, 0-255
 ---@field centerpos Pos3d center of the room, z will be floor height, for oddly shaped rooms, it'll be the closest point to center within the room
 if not Room then Room = {} end
